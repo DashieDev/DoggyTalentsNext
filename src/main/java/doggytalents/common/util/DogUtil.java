800b5eddbbd56a2f7d81a73eb0a355f70f98fa1e
DashieDev <lamtinh.asian@gmail.com>
2022-11-04 01:33:35 +0700

    add note to DogUtil distanceFromPointToLineOfUnitVector2DSqr

--- src/main/java/doggytalents/common/util/DogUtil.java
+++ src/main/java/doggytalents/common/util/DogUtil.java
@@ -225,11 +225,14 @@ public class DogUtil {
      * If Angle(u, v) < 90 this function will return the squared distance between A and d
      * else this function will return -1.
      * 
+     * <p>NOTE : Every parameter here accept Vec3, but all have to have exactly one component being zeroed
+     * and it should be the same component across, like all Zs zero or else the function wont 
+     * work</p>
      * 
      * @author DashieDev
      * @param A 
      * @param B
-     * @param v
+     * @param v have to be a unit vector
      * @return the distance squared, -1 if Angle(u, v) > 90 
      */
     public static double distanceFromPointToLineOfUnitVector2DSqr(Vec3 A, Vec3 B, Vec3 v) {
