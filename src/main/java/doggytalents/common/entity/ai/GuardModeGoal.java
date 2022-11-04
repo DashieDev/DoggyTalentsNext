4d50262b03cda5324c143e7e3433463766b9de44
DashieDev <lamtinh.asian@gmail.com>
2022-11-04 14:28:47 +0700

    Add Guard Minor Mode (Reserved)

--- src/main/java/doggytalents/common/entity/ai/GuardModeGoal.java
+++ src/main/java/doggytalents/common/entity/ai/GuardModeGoal.java
@@ -1,10 +1,18 @@
 package doggytalents.common.entity.ai;
 
+import java.util.EnumSet;
+import java.util.List;
+
+import doggytalents.ChopinLogger;
 import doggytalents.api.feature.EnumMode;
 import doggytalents.common.entity.Dog;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.phys.AABB;
 
 public class GuardModeGoal extends NearestAttackableTargetGoal<Monster> {
 
@@ -18,23 +26,16 @@ public class GuardModeGoal extends NearestAttackableTargetGoal<Monster> {
 
     @Override
     public boolean canUse() {
-        LivingEntity owner = this.dog.getOwner();
-        if (owner == null) {
+        this.owner = this.dog.getOwner();
+        
+        if (this.owner == null) return false;
+
+        if (!this.dog.isMode(EnumMode.GUARD))
             return false;
-        }
 
-        if (!this.dog.isMode(EnumMode.GUARD)) {
-            return false;
-        }
+        if (!super.canUse()) return false;
 
-        this.owner = owner;
-
-        if (super.canUse()) {
-            this.owner = owner;
-            return true;
-        }
-
-        return false;
+        return true;
     }
 
     @Override
@@ -45,5 +46,74 @@ public class GuardModeGoal extends NearestAttackableTargetGoal<Monster> {
     @Override
     protected void findTarget() {
        this.target = this.dog.level.getNearestEntity(this.targetType, this.targetConditions, this.dog, this.owner.getX(), this.owner.getEyeY(), this.owner.getZ(), this.getTargetSearchArea(this.getFollowDistance()));
+    }
+
+    public static class Minor extends Goal {
+
+        private final Dog dog;
+        private LivingEntity owner;
+        private Monster nearest_danger;
+        private int tickUntilRescanDanger = 0;
+
+        private final int SEARCH_RADIUS = 4;
+
+        public Minor(Dog dog) {
+            this.dog = dog;
+            this.setFlags(EnumSet.of(Goal.Flag.LOOK));
+        }
+
+        @Override
+        public boolean canUse() {
+
+            this.owner = this.dog.getOwner();
+            
+            if (this.owner == null) return false;
+
+            if (!this.dog.isMode(EnumMode.GUARD_MINOR))
+                return false;
+
+            return true;
+        }
+
+        @Override
+        public void tick() { 
+            if (this.tickUntilRescanDanger <= 0) {
+                this.findDanger();
+                
+                if (this.nearest_danger != null) {
+                    dog.playSound(
+                        SoundEvents.WOLF_GROWL, 
+                        dog.getSoundVolume(), 
+                        (dog.getRandom().nextFloat() - dog.getRandom().nextFloat()) * 0.2F + 1.0F
+
+                    );
+                    ChopinLogger.lwn(this.dog, "Hey! danger!");
+                    this.tickUntilRescanDanger = 60 + this.dog.getRandom().nextInt(40);
+                    ChopinLogger.l(""+this.tickUntilRescanDanger);
+                } else {
+                    this.tickUntilRescanDanger = 10;
+                }
+                
+            } else {
+                --this.tickUntilRescanDanger;
+            }
+        }
+
+        protected void findDanger() {
+            this.nearest_danger = this.dog.level
+                .getNearestEntity(
+                    Monster.class,
+                    TargetingConditions.forCombat()
+                        .selector(e -> {
+                            return this.dog.hasLineOfSight(e);
+                        }),
+                    this.dog,
+                    this.owner.getX(),
+                    this.owner.getEyeY(),
+                    this.owner.getZ(),
+                    new AABB(this.owner.blockPosition()).inflate(SEARCH_RADIUS, 4, SEARCH_RADIUS)
+                );
+        }
+
     }
 }
