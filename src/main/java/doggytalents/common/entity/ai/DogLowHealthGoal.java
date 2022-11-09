b65e6704b33b0ff4c7e0c1c150c8e8b9a41dde55
DashieDev <lamtinh.asian@gmail.com>
2022-11-09 09:56:36 +0700

    float

--- src/main/java/doggytalents/common/entity/ai/DogLowHealthGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogLowHealthGoal.java
@@ -37,7 +37,7 @@ public class DogLowHealthGoal extends Goal {
         this.dog = dogIn;
         this.world = dogIn.level;
         this.followSpeed = speedIn;
-        this.stopDist = 1.5;
+        this.stopDist = 1.5f;
         this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
     }
 
