a70bc733b1e0bc4e8d894c8d20c53751bbf121d8
DashieDev <lamtinh.asian@gmail.com>
2022-11-11 14:41:06 +0700

    Make stop logic make more sense
    Idea got from Sophisticated Wolves
    
    Make Dog always check on if owner is mining.
    
    Desc change

--- src/main/java/doggytalents/common/entity/ai/DogRandomStrollGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogRandomStrollGoal.java
@@ -6,6 +6,8 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ShovelItem;
 
@@ -18,6 +20,17 @@ public class DogRandomStrollGoal extends WaterAvoidingRandomStrollGoal {
         super(dog, speedModifier);
         this.dog = dog;
     }
+
+    @Override
+    public boolean canUse() {
+
+        if (this.ownerMayBeMining()) {
+            this.tickCountStopMiningCautious = this.dog.tickCount + 600; // keep checking for 30 seconds
+        }
+
+
+        return super.canUse();
+    }
     
     //  TODO : Make this more user friendly, maybe make this into a talent or mode
     //
@@ -25,9 +38,8 @@ public class DogRandomStrollGoal extends WaterAvoidingRandomStrollGoal {
     //Because this reason here.., and it is also kinda annoying to see dogs going in 
     //front of owner when mining.
     //
-    //  If Owner is Holding a Pickaxe or a Shovel, go into Mining Cautious Mode, 
-    //when Owner no longer holding the item, continue the mode for a certain amount of 
-    //time.
+    //  If Owner Swing with a DiggerItem in hand, go into Mining Cautious Mode, 
+    //retains the mode for 30 seconds when the owner stops swining. 
     //
     //  In the mode, upon moving towards a random block (even when out of reach)
     //the dog will actively checks the path if 
@@ -35,10 +47,7 @@ public class DogRandomStrollGoal extends WaterAvoidingRandomStrollGoal {
     public void tick() {
         super.tick();
         
-        if (this.ownerMayBeMining()) {
-            this.tickCountStopMiningCautious = this.dog.tickCount + 600; // keep checking for 30 seconds
-        }
-
+        
         if (this.dog.tickCount < this.tickCountStopMiningCautious) {
             if (this.pathObstructOwnerMining()) {
                 this.stop();
@@ -49,13 +58,15 @@ public class DogRandomStrollGoal extends WaterAvoidingRandomStrollGoal {
     //TODO Mining genius : the dog will follow owner while putting torch down 
     //Closely and also run with him
     //And lead any other dogs which is close too .... 
+    //TODO CHANGE : make the logic make more sense and efficent
+    //Check if owner is swinging with a digger item in hand.
     private boolean ownerMayBeMining() {
         var owner = this.dog.getOwner();
         if (owner == null) return false;
-
         return
-            owner.getMainHandItem().getItem() instanceof PickaxeItem
-            || owner.getMainHandItem().getItem() instanceof ShovelItem;
+            owner.swinging 
+            && owner.getMainHandItem().getItem() instanceof DiggerItem;
+            
     }
 
     /**
