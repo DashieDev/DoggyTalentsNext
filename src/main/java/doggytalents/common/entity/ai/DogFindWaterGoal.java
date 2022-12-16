007a3fac516936152942ce91903d7491fb1ec355
DashieDev <lamtinh.asian@gmail.com>
2022-12-16 18:58:11 +0700

    Improve DogFindWaterGoal
    Instead of searching via interating normally like
     nested loops, the search now done thorugh
     inflating "shells" which means that dog have a higher chance of finding
     the nearest water block.

--- src/main/java/doggytalents/common/entity/ai/DogFindWaterGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogFindWaterGoal.java
@@ -2,7 +2,9 @@ package doggytalents.common.entity.ai;
 
 import java.util.EnumSet;
 
+import doggytalents.ChopinLogger;
 import doggytalents.common.entity.Dog;
+import doggytalents.common.util.DogUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.ai.goal.Goal;
@@ -30,9 +32,11 @@ public class DogFindWaterGoal extends Goal {
 
         if (!dog.isOnFire()) return false;
 
+        this.waterPos = null; //This is why sometimes there used to seems to be WaterPos Saving .......
         if (--this.tickUntilSearch <= 0) {
             this.tickUntilSearch = 5;
             this.waterPos = this.searchForWaterPos();
+            ChopinLogger.l("in water pos" + this.waterPos);
         }
         if (this.waterPos == null) return false;
 
@@ -55,12 +59,22 @@ public class DogFindWaterGoal extends Goal {
 
     @Override
     public void tick() {
+
+        var dog_bp = dog.blockPosition();
+        var n = dog.getNavigation();
+
         if (this.isWaterPos(waterPos)) {
+            if (n.isDone() && dog_bp.distSqr(waterPos) <= 1 ) {
+                dog.getMoveControl().setWantedPosition(this.waterPos.getX() + 0.5, this.waterPos.getY(), this.waterPos.getZ() + 0.5, 1.0);
+            }
+
             if (--this.tickUntilPathRecalc <= 0) {
                 this.tickUntilPathRecalc = 10;
-                this.dog.getNavigation().moveTo(
-                    waterPos.getX(), waterPos.getY(), waterPos.getZ(),
-                    1.5
+                DogUtil.moveToIfReachOrElse(
+                    dog, waterPos, 1.5, dog.getMaxFallDistance(), 
+                    dog1 -> {
+                        this.waterPos = null;
+                    }    
                 );
             }
         } else {
@@ -69,15 +83,77 @@ public class DogFindWaterGoal extends Goal {
     }
 
     private BlockPos searchForWaterPos() {
-        BlockPos b0 = this.dog.blockPosition();
+        var b0 = this.dog.blockPosition();
+        var b0m = b0.mutable();
 
-        for (BlockPos x : BlockPos.betweenClosed(
-            b0.offset(-this.SEARCH_RANGE, -4, -this.SEARCH_RANGE), 
-            b0.offset(this.SEARCH_RANGE, 4, this.SEARCH_RANGE))
-        ) {
-            if (this.isWaterPos(x)) {
-                return x;
+        // for (BlockPos x : BlockPos.betweenClosed(
+        //     b0m.offset(-this.SEARCH_RANGE, -4, -this.SEARCH_RANGE), 
+        //     b0m.offset(this.SEARCH_RANGE, 4, this.SEARCH_RANGE))
+        // ) {
+        //     if (this.isWaterPos(x)) {
+        //         return x; // If dog can path to it.
+        //     }
+        // }
+        
+        int inflate = 1;
+        while (inflate <= SEARCH_RANGE) {
+            final int minX = b0.getX() - inflate;
+            final int maxX = b0.getX() + inflate;
+            final int minZ = b0.getZ() - inflate;
+            final int maxZ = b0.getZ() + inflate;
+
+            b0m.setX(minX);
+            b0m.setZ(minZ);
+            //ChopinLogger.l("blockpos" + b0);
+
+            for (int i = minX; i <= maxX; ++i) {
+                b0m.setX(i);
+                //ChopinLogger.l("" + b0m);
+                for (int j = -4; j <= 4; ++j) {
+                    b0m.setY(b0.getY() + j);
+                    if (this.isWaterPos(b0m)) {
+                        return b0m.immutable();
+                    }
+                }
             }
+
+            //b0m: maxX, minZ
+            for (int i = minZ + 1; i <= maxZ; ++i) {
+                b0m.setZ(i);
+                //ChopinLogger.l("" + b0m);
+                for (int j = -4; j <= 4; ++j) {
+                    b0m.setY(b0.getY() + j);
+                    if (this.isWaterPos(b0m)) {
+                        return b0m.immutable();
+                    }
+                }
+            }
+
+            //b0m: maxX, maxZ
+            for (int i = maxX-1; i >= minX; --i) {
+                b0m.setX(i);
+                //ChopinLogger.l("" + b0m);
+                for (int j = -4; j <= 4; ++j) {
+                    b0m.setY(b0.getY() + j);
+                    if (this.isWaterPos(b0m)) {
+                        return b0m.immutable();
+                    }
+                }
+            }
+
+            //b0m: minX, maxZ
+            for (int i = maxZ - 1; i >= minZ + 1; --i) {
+                b0m.setZ(i);
+                //ChopinLogger.l("" + b0m);
+                for (int j = -4; j <= 4; ++j) {
+                    b0m.setY(b0.getY() + j);
+                    if (this.isWaterPos(b0m)) {
+                        return b0m.immutable();
+                    }
+                }
+            }
+            ++inflate;
+            //ChopinLogger.l("inflate!" + inflate);
         }
 
         return null;
