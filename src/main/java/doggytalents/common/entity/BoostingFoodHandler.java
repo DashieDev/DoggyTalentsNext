521cfbd014c426d763f9cbaed5369855f0745160
DashieDev <lamtinh.asian@gmail.com>
2022-12-26 02:32:16 +0700

    Make dog can eat Golden Apple
    And add effect ChewStick

---
+++ src/main/java/doggytalents/common/entity/BoostingFoodHandler.java
@@ -0,0 +1,57 @@
+package doggytalents.common.entity;
+
+import javax.annotation.Nullable;
+
+import doggytalents.api.inferface.AbstractDog;
+import doggytalents.api.inferface.IDogFoodHandler;
+import doggytalents.common.network.packet.ParticlePackets;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class BoostingFoodHandler implements IDogFoodHandler  {
+
+    @Override
+    public boolean isFood(ItemStack stackIn) {
+        var item = stackIn.getItem();
+        return (item == Items.GOLDEN_APPLE
+            || item == Items.ENCHANTED_GOLDEN_APPLE);
+    }
+
+    @Override
+    public boolean canConsume(AbstractDog dogIn, ItemStack stackIn, @Nullable Entity entityIn) {
+        return this.isFood(stackIn);
+    }
+
+    @Override
+    public InteractionResult consume(AbstractDog dog, ItemStack stack, @Nullable Entity entityIn) {
+        if (!dog.level.isClientSide) {
+            int heal = stack.getItem().getFoodProperties().getNutrition() * 5;
+
+            dog.addHunger(heal);
+            dog.consumeItemFromStack(entityIn, stack);
+
+            for(var pair : stack.getFoodProperties(dog).getEffects()) {
+                if (pair.getFirst() != null && dog.getRandom().nextFloat() < pair.getSecond()) {
+                   dog.addEffect(new MobEffectInstance(pair.getFirst()));
+                }
+             }
+
+            if (dog.level instanceof ServerLevel) {
+                ParticlePackets.DogEatingParticlePacket.sendDogEatingParticlePacketToNearby(dog, stack);
+            }
+            dog.playSound(
+                SoundEvents.GENERIC_EAT, 
+                dog.getSoundVolume(), 
+                (dog.getRandom().nextFloat() - dog.getRandom().nextFloat()) * 0.2F + 1.0F
+            );
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+    
+}
