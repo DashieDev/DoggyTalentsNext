f9fc1ce45586b504660906cbd5f0b374bdf72905
DashieDev <lamtinh.asian@gmail.com>
2022-12-26 02:31:10 +0700

    DogGoAwayFromFire performance
    + Dog doesn't react when lava below the dog,
    + If search safePos fails, the dog will wait at least 15 ticks before
    search again.
    +Limited the search range to 2 if the dog is only avoiding fire.
    + Also use low level pathfind to save 0.1ms.
    
    reorder checks

--- src/main/java/doggytalents/common/entity/ai/DogGoAwayFromFireGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogGoAwayFromFireGoal.java
@@ -2,49 +2,81 @@ package doggytalents.common.entity.ai;
 
 import java.util.EnumSet;
 
+import com.google.common.collect.ImmutableSet;
+
 import doggytalents.common.entity.Dog;
 import doggytalents.common.util.DogUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.level.PathNavigationRegion;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import net.minecraft.world.level.pathfinder.PathFinder;
 import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 public class DogGoAwayFromFireGoal extends Goal {
-    private final int SEARCH_RANGE = 4;
+    private final int LAVA_SEARCH_RANGE = 4;
+    private final int DEFAULT_SEARCH_RANGE = 2;
 
     private Dog dog;
 
     private int tickUntilSearch;
     private int tickUntilPathRecalc;
 
+    //If the safePos search fails, the dog is going to wait at least 0.5s to search again.
+    //This is for performance issues that make the search cost relatively a bit more time.
+    //ex : The pathfinding - eventhough the dog barely do any pathfind, it stills cost 
+    //the dog's time for some reasons, perhaps there is some caching going on.
+    //Maybe a ring search is better for this, or a ring search with spread alg seems ok.
+    private int failedSearchPenalty;
+
+    private PathFinder pathFinder;
+    private PathNavigationRegion region;
+
+
     private BlockPos safePos;
 
     public DogGoAwayFromFireGoal(Dog dog) {
         this.dog = dog;
         this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+
+        var nodeEvaluator = new WalkNodeEvaluator();
+        nodeEvaluator.setCanPassDoors(true);
+        this.pathFinder = new PathFinder(nodeEvaluator, 32);
     }
 
     @Override
     public boolean canUse() {
         if (this.dog.fireImmune()) return false;
 
+        if (this.failedSearchPenalty > 0) {
+            --this.failedSearchPenalty;
+        }
+
         if (!dog.isOnGround()) return false;
 
-        boolean dangerSpot = false;
+        byte dangerSpot = -1;
         if (--tickUntilSearch <= 0) {
             tickUntilSearch = 3;
             dangerSpot = isDogInDangerSpot(this.dog.position());
         }
-        if (!dangerSpot) return false;
+        if (dangerSpot == -1) return false;
 
-        this.safePos = this.searchForSafePos();
+        //This goal has no effect when dog is in lava by the way.
+        if (dog.isInLava()) return false;
 
+        this.safePos = null;
+
+        if (this.failedSearchPenalty <= 0) {
+            this.safePos = this.searchForSafePos(
+                dangerSpot == 0 ? DEFAULT_SEARCH_RANGE : LAVA_SEARCH_RANGE
+            );
+            if (this.safePos == null) this.failedSearchPenalty = 15;
+        }
+            
         if (this.safePos == null) return false;
-
-        
 
         return true;
     }
@@ -86,7 +118,7 @@ public class DogGoAwayFromFireGoal extends Goal {
                     });
             }
         } else {
-            this.safePos = this.searchForSafePos();
+            this.safePos = this.searchForSafePos(LAVA_SEARCH_RANGE);
         }
     }
 
@@ -95,7 +127,15 @@ public class DogGoAwayFromFireGoal extends Goal {
         this.safePos = null;
     }
 
-    private boolean isDogInDangerSpot(Vec3 pos) {
+    /**
+     * 1 means lava
+     * 0 means fire
+     * -1 means safe
+     * 
+     * @param pos
+     * @return
+     */
+    private byte isDogInDangerSpot(Vec3 pos) {
         var half_bbw = 0.5*dog.getBbWidth();
         int minX = Mth.floor(pos.x - half_bbw)-1;
         int minY = Mth.floor(pos.y);
@@ -105,24 +145,36 @@ public class DogGoAwayFromFireGoal extends Goal {
         int maxY = Mth.ceil(pos.y)+1;
         int maxZ = Mth.ceil(pos.z + half_bbw)+1;
 
+        byte ret = -1; //Assume all is safe
+
         for (BlockPos x : BlockPos.betweenClosed(minX, minY, minZ, maxX, maxY, maxZ)) {
-            var isBurning = WalkNodeEvaluator.isBurningBlock(dog.level.getBlockState(x));
+            var state = dog.level.getBlockState(x);
+            var isBurning = WalkNodeEvaluator.isBurningBlock(state);
 
             if (isBurning) {
-                return true;
+                ret = 0; //Definitly not safe
+                if (state.is(Blocks.LAVA)) {
+                    return 1; //if lava then return 1;
+                }
             }
         }
 
-        return false;
+        return ret;
     }
 
-    private BlockPos searchForSafePos() {
+    private BlockPos searchForSafePos(int searchRange) {
         BlockPos b0 = this.dog.blockPosition();
 
+        BlockPos bMin = b0.offset(-searchRange, -2, -searchRange);
+        BlockPos bMax = b0.offset(searchRange, 2, searchRange);
+
+        this.region = new PathNavigationRegion(this.dog.level, bMin, bMax);
+
         for (BlockPos x : BlockPos.betweenClosed(
-            b0.offset(-this.SEARCH_RANGE, -2, -this.SEARCH_RANGE), 
-            b0.offset(this.SEARCH_RANGE, 2, this.SEARCH_RANGE))
+            bMin, 
+            bMax)
         ) {
+            
             if (this.isSafePos(x)) return x;
         }
 
@@ -138,7 +190,8 @@ public class DogGoAwayFromFireGoal extends Goal {
             //Due to the search radius is meant to be small, the path
             //that need to be calc each time is not big
             //, therefore, the pathfinding becomes relatively light.
-            var path = this.dog.getNavigation().createPath(pos, 1);
+            //Used 
+            var path = this.pathFinder.findPath(region, dog, ImmutableSet.of(pos), 4, 1, 1);
             if (path == null) return false;
             boolean flag = 
                 DogUtil.canPathReachTargetBlock(dog, path, pos, 0);
