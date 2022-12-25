7c7bda177e2ca0268033eff98711221d446c8bb1
DashieDev <lamtinh.asian@gmail.com>
2022-12-26 02:32:16 +0700

    Make dog can eat Golden Apple
    And add effect ChewStick

--- src/main/java/doggytalents/common/item/ChewStickItem.java
+++ src/main/java/doggytalents/common/item/ChewStickItem.java
@@ -1,7 +1,11 @@
 package doggytalents.common.item;
 
+import doggytalents.DoggyItems;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.inferface.IDogFoodHandler;
+import doggytalents.common.network.packet.ParticlePackets;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -26,12 +30,22 @@ public class ChewStickItem extends Item implements IDogFoodHandler {
     }
 
     @Override
-    public InteractionResult consume(AbstractDog dogIn, ItemStack stackIn, Entity entityIn) {
-        if (!dogIn.level.isClientSide) {
-            dogIn.addEffect(new MobEffectInstance(MobEffects.GLOWING, 100, 1, false, true));
-            dogIn.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 6, false, true));
-            dogIn.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 100, 2, false, true));
-            dogIn.consumeItemFromStack(entityIn, stackIn);
+    public InteractionResult consume(AbstractDog dog, ItemStack stack, Entity entityIn) {
+        if (!dog.level.isClientSide) {
+            dog.addEffect(new MobEffectInstance(MobEffects.GLOWING, 100, 1, false, true));
+            dog.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 6, false, true));
+            dog.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 100, 2, false, true));
+            dog.consumeItemFromStack(entityIn, stack);
+
+            if (dog.level instanceof ServerLevel) {
+                ParticlePackets.DogEatingParticlePacket.sendDogEatingParticlePacketToNearby(
+                    dog, new ItemStack(DoggyItems.CHEW_STICK.get()));
+            }
+            dog.playSound(
+                SoundEvents.GENERIC_EAT, 
+                dog.getSoundVolume(), 
+                (dog.getRandom().nextFloat() - dog.getRandom().nextFloat()) * 0.2F + 1.0F
+            );
         }
 
         return InteractionResult.SUCCESS;
