548a1674cd497d359cf21affb1d8f3691240c0bd
DashieDev <lamtinh.asian@gmail.com>
2023-01-19 22:18:11 +0700

    Added new TriggerableAction Managing System
    Each dog can have an active action to execute like
    Playing Tag or Greeting Owner that can be triggered
    by method.
    Action is divided into two type according to
    priority, trivial and non trivial, trivial action
    will execute with priority equals to talents
    actions while non trivial one will execute
    with much lower priority.
    If the dog is busy on an action, then you cannot trigger new action unless
    the action is trivial and what the dog is doing is not
    trivial, at that point if the current action can pause, it will
    be stashed. Each dog can have maximum of one action
    being stashed, when another action try to be]
    stashed, it will ended up replacing the old stash action
    and the old action will get removed.
    Action can also be paused  or removed
    via other goals's override.
    First 2 implement :
    + Playing tag: allow dogs to play tag with you when you toss a snowball
    at him. (config)
    + Greet Owner : If owner leave the dog for too long,
    dog will rush to greet him when he is near. (feature and limt
    is configurable)

---
+++ src/main/java/doggytalents/common/entity/ai/triggerable/TriggerableAction.java
@@ -0,0 +1,59 @@
+package doggytalents.common.entity.ai.triggerable;
+
+import doggytalents.common.entity.Dog;
+
+public abstract class TriggerableAction {
+    
+    protected Dog dog;
+    private ActionState state = ActionState.PENDING;
+    private final boolean isTrivial;
+    private final boolean canPause;
+
+    public TriggerableAction(Dog dog, boolean trivial, boolean canPause) {
+        this.dog = dog;
+        this.isTrivial = trivial;
+        this.canPause = canPause;
+    }
+    
+    public abstract void onStart();
+
+    public abstract void tick();
+
+    public abstract void onStop();
+
+    //Only a non trivial action can override a trivial action.
+    public final boolean isTrivial() {
+        return this.isTrivial;
+    }
+
+    public boolean canPreventSit() {
+        return false;
+    }
+
+    public boolean canOverrideSit() {
+        return false;
+    }
+
+    public boolean shouldPersistAfterSit() {
+        return true;
+    }
+
+    public final boolean canPause() {
+        return this.canPause;
+    }
+
+    public ActionState getState() {
+        return this.state;
+    }
+
+    public void setState(ActionState state) {
+        this.state = state;
+    }
+
+    public static enum ActionState {
+        PENDING,
+        RUNNING,
+        FINISHED
+    }
+
+}
