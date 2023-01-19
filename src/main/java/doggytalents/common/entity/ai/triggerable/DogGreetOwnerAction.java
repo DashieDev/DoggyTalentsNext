c113fca83455c06203654830a36a676688ee7272
DashieDev <lamtinh.asian@gmail.com>
2023-01-19 22:18:11 +0700

    Added new TriggerableAction Managing System
    Each dog can have an active action to execute like
    Playing Tag or Greeting Owner that can be triggered
    by method.
    Action is divided into two type according to
    priority, trivial and non trivial, trivial action
    will execute with priority equals to talents
    actions while non trivial one will execute
    with much lower priority.
    If the dog is busy on an action, then you cannot trigger new action unless
    the action is trivial and what the dog is doing is not
    trivial, at that point if the current action can pause, it will
    be stashed. Each dog can have maximum of one action
    being stashed, when another action try to be]
    stashed, it will ended up replacing the old stash action
    and the old action will get removed.
    Action can also be paused  or removed
    via other goals's override.
    First 2 implement :
    + Playing tag: allow dogs to play tag with you when you toss a snowball
    at him. (config)
    + Greet Owner : If owner leave the dog for too long,
    dog will rush to greet him when he is near. (feature and limt
    is configurable)

---
+++ src/main/java/doggytalents/common/entity/ai/triggerable/DogGreetOwnerAction.java
@@ -0,0 +1,159 @@
+package doggytalents.common.entity.ai.triggerable;
+
+import javax.annotation.Nonnull;
+
+import doggytalents.common.config.ConfigHandler;
+import doggytalents.common.entity.Dog;
+import doggytalents.common.entity.DogOwnerDistanceManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.LivingEntity;
+
+public class DogGreetOwnerAction extends TriggerableAction {
+
+    private LivingEntity owner;
+
+    private static final int GREET_TICK_PER_LEFT_DAY = 20;
+    private static final int MAX_GREET_TIME = 400;
+    private static final int MAY_CANCEL_GREET_TIME = 100; 
+
+    private static final int START_GREET_DISTANCE_SQR = 4;
+    private static final int GREET_RADIUS = 1;
+    private static final int JUMP_BASE_INTERVAL = 7;
+    private static final int WHINE_BASE_INTERVAL = 50;
+    private static final int HEART_CHANCE_WINDOW = 8;
+    private static final int MSG_CHANCE_WINDOW = 10;
+
+    int greetTime;
+
+    int tickTillPathRecalc;
+
+    BlockPos rbAroundOwner;
+    int tickTillWhine;
+    int tickTillJump;
+
+    boolean tellOwner;
+
+    final int greetStopTime;
+    //24000
+
+    public DogGreetOwnerAction(Dog dog, @Nonnull LivingEntity owner, long ownerLeftInterval) {
+        super(dog, true, true);
+        this.owner = owner;
+        this.greetStopTime = this.calculateGreetTime(ownerLeftInterval);
+    }
+
+    private int calculateGreetTime(long ownerLeftInterval) {
+        int a = Mth.floor((ownerLeftInterval/24000)*GREET_TICK_PER_LEFT_DAY);
+        return Math.min(a, MAX_GREET_TIME);
+    }
+
+    @Override
+    public void onStart() {
+        this.tickTillPathRecalc = 0;
+        this.greetTime = 0;
+        this.tickTillWhine = this.dog.getRandom().nextInt(5)*10;
+        this.tellOwner = true;
+    }
+
+    @Override
+    public void tick() {
+        if (this.greetTime >= this.greetStopTime) {
+            this.setState(ActionState.FINISHED);
+            return;
+        }
+
+        var n = this.dog.getNavigation();
+        this.dog.getLookControl().setLookAt(this.owner, 10.0F, this.dog.getMaxHeadXRot());
+        if (this.dog.distanceToSqr(this.owner) > START_GREET_DISTANCE_SQR) {
+            
+            if (--this.tickTillPathRecalc <= 0) {
+                this.tickTillPathRecalc = 10;
+                n.moveTo(this.owner, this.dog.getUrgentSpeedModifier());
+            }
+            
+
+        } else {
+            if (this.tellOwner) {
+                this.tellOwner = false;
+                int r = this.dog.getRandom().nextInt(MSG_CHANCE_WINDOW);
+                if (r < 5) {
+                    this.owner.sendSystemMessage(Component.translatable("dog.msg.greet_owner." + r, this.dog.getName().getString()));
+                }
+            }
+            this.doGreet();
+        }
+    }
+
+    private void doGreet() {
+        ++greetTime;
+        var n = this.dog.getNavigation();
+        if (n.isDone()) {
+            this.rbAroundOwner = this.getRandomPosAroundOwner(owner);
+            n.moveTo(
+                this.rbAroundOwner.getX(),
+                this.rbAroundOwner.getY(),
+                this.rbAroundOwner.getZ(), 1
+            );
+        }
+        if (--tickTillJump <= 0) {
+            this.tickTillJump = JUMP_BASE_INTERVAL;
+            this.dog.getJumpControl().jump();
+            //ChopinLogger.l("jump");
+        }
+        if (--tickTillWhine <= 0) {
+            tickTillWhine = WHINE_BASE_INTERVAL
+                + dog.getRandom().nextInt(3) *20;
+            this.dog.playSound(SoundEvents.WOLF_WHINE, this.dog.getSoundVolume(), this.dog.getVoicePitch());
+        }
+        if (dog.getRandom().nextInt(HEART_CHANCE_WINDOW) == 0) {
+            if (dog.level instanceof ServerLevel sLevel) {
+                sLevel.sendParticles(
+                    ParticleTypes.HEART, 
+                    dog.getX(), dog.getY(), dog.getZ(), 
+                    1, 
+                    dog.getBbWidth(), 0.8f, dog.getBbWidth(), 
+                    0.1
+                );
+            }
+        } 
+    }
+
+    @Override
+    public void onStop() {
+        int greetOwnerLimit = 
+            ConfigHandler.ServerConfig.getConfig(ConfigHandler.SERVER.DOG_GREET_OWNER_LIMIT);
+        if (greetOwnerLimit < 0) return;
+        DogOwnerDistanceManager.decGreetCountForOwner(owner);
+    }
+
+    @Override
+    public boolean canOverrideSit() {
+        return true;
+    }
+    
+    @Override
+    public boolean canPreventSit() {
+        return this.greetTime < MAY_CANCEL_GREET_TIME;
+    }
+    
+    @Override
+    public boolean shouldPersistAfterSit() {
+        return false;
+    }
+
+    
+    private BlockPos getRandomPosAroundOwner(LivingEntity owner) {
+        var owner_b0 = owner.blockPosition();
+        var r = owner.getRandom();
+        int rX = r.nextIntBetweenInclusive(-GREET_RADIUS, GREET_RADIUS);
+        int rY = r.nextIntBetweenInclusive(-1, 1);
+        int rZ = r.nextIntBetweenInclusive(-GREET_RADIUS, GREET_RADIUS);
+        return owner_b0.offset(rX, rY, rZ);
+    }
+    
+}
