ea47f8708f9fd2b450814050d4c32d0b8aa63a69
DashieDev <lamtinh.asian@gmail.com>
2023-02-14 16:08:55 +0700

    Change Low Health goal
    Make sure search and tp doesn't get called too much.

--- src/main/java/doggytalents/common/entity/ai/DogLowHealthGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogLowHealthGoal.java
@@ -31,6 +31,7 @@ public class DogLowHealthGoal extends Goal {
     private LivingEntity owner;
     private int timeToRecalcPath;
     private int tickTillSearchForTp = 0;
+    private int tickTillInitTeleport = 0;
     private float oldWaterCost;
 
     private boolean whine = false;
@@ -45,6 +46,10 @@ public class DogLowHealthGoal extends Goal {
 
     @Override
     public boolean canUse() {
+        if (this.tickTillInitTeleport > 0) {
+            --this.tickTillInitTeleport;
+        }
+
         LivingEntity owner = this.dog.getOwner();
         if (owner == null) {
             return false;
@@ -71,7 +76,11 @@ public class DogLowHealthGoal extends Goal {
         this.oldWaterCost = this.dog.getPathfindingMalus(BlockPathTypes.WATER);
         this.dog.setPathfindingMalus(BlockPathTypes.WATER, 0.0F);
         this.whine = true;
-        DogUtil.dynamicSearchAndTeleportToOwnwer(dog, owner, 4);
+        if (this.tickTillInitTeleport <= 0) {
+            this.tickTillInitTeleport = 10;
+            //Ensure this function never get called too many times.
+            DogUtil.dynamicSearchAndTeleportToOwnwer(dog, owner, 4);
+        }
     }
 
     @Override
