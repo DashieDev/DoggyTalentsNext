dc6bbdc279f63326bf7fc5bcad631894e14ed4e6
DashieDev <lamtinh.asian@gmail.com>
2023-02-22 01:59:40 +0700

    only cancel when not canPlayerAttack.

--- src/main/java/doggytalents/common/event/EventHandler.java
+++ src/main/java/doggytalents/common/event/EventHandler.java
@@ -150,6 +150,7 @@ public class EventHandler {
             return;
         }
         
-        event.setCanceled(true);
+        if (!dog.canPlayersAttack())
+            event.setCanceled(true);
     }
 }
