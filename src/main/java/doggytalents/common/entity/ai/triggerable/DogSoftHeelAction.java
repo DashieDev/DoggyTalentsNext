ca9b62cfaeb8cb51857ab2d645c8cde8e7578ac1
DashieDev <lamtinh.asian@gmail.com>
2023-02-22 01:39:33 +0700

    Added soft heel to Heel By Name
    Heel but dog will casually move to you like when he is
    following and only tp if too far.

---
+++ src/main/java/doggytalents/common/entity/ai/triggerable/DogSoftHeelAction.java
@@ -0,0 +1,52 @@
+package doggytalents.common.entity.ai.triggerable;
+
+import doggytalents.common.entity.Dog;
+import doggytalents.common.util.DogUtil;
+import net.minecraft.world.entity.LivingEntity;
+
+public class DogSoftHeelAction extends TriggerableAction {
+
+    private LivingEntity owner;
+    private int timeToRecalcPath;
+    private int timeOut;
+
+    public DogSoftHeelAction(Dog dog, LivingEntity owner) {
+        super(dog, true, false);
+        this.owner = owner;
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onStart() {
+        timeOut = 200;
+    }
+
+    @Override
+    public void tick() {
+        double d0 = this.dog.distanceToSqr(owner);
+        if (d0 < 4 || --timeOut <= 0) {
+            this.setState(ActionState.FINISHED);
+            return;
+        }
+        this.dog.getLookControl().setLookAt(this.owner, 10.0F, this.dog.getMaxHeadXRot());
+        if (--this.timeToRecalcPath <= 0) {
+            this.timeToRecalcPath = 10;
+            DogUtil.moveToOwnerOrTeleportIfFarAway(
+                dog, owner, dog.getUrgentSpeedModifier(),
+                256, 
+                false, false, 
+                400,
+                dog.getMaxFallDistance());
+        }
+    }
+
+    @Override
+    public void onStop() {
+    }
+
+    @Override
+    public boolean canOverrideSit() {
+        return true;
+    }
+    
+}
--- src/main/java/doggytalents/common/network/packet/data/HeelByNameData.java
+++ src/main/java/doggytalents/common/network/packet/data/HeelByNameData.java
@@ -3,9 +3,11 @@ package doggytalents.common.network.packet.data;
 public class HeelByNameData extends DogData {
 
     public boolean heelAndSit;
+    public boolean softHeel;
 
-    public HeelByNameData(int entityId, boolean heelAndSit) {
+    public HeelByNameData(int entityId, boolean heelAndSit, boolean softHeel) {
         super(entityId);
         this.heelAndSit = heelAndSit;
+        this.softHeel = softHeel;
     }
 }
