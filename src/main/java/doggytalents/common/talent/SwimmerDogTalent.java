5b8e0acdf465002cacb5311504fd4efa191034c8
DashieDev <lamtinh.asian@gmail.com>
2023-02-24 18:39:47 +0700

    (already in master) SwimmerDog Talent sync
    Already can swim at level 2+

--- src/main/java/doggytalents/common/talent/SwimmerDogTalent.java
+++ src/main/java/doggytalents/common/talent/SwimmerDogTalent.java
@@ -176,7 +176,7 @@ public class SwimmerDogTalent extends TalentInstance {
     //also allow user to ride the dog under level 5!
     @Override
     public InteractionResult canBeRiddenInWater(AbstractDog dogIn) {
-        return this.level() >= 5 ? InteractionResult.SUCCESS : InteractionResult.PASS;
+        return this.level() >= 2 ? InteractionResult.SUCCESS : InteractionResult.PASS;
     }
 
     @Override
