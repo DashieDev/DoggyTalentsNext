f40fb1da09ee6549aa5e7e9cbf16d6aef0d5b480
DashieDev <lamtinh.asian@gmail.com>
2023-02-24 17:28:12 +0700

    Reimplement dog move to bed action
    Move To Bed mechanic is now an triggerable action,
    instead of a goal itself, so targetblock() and the goal
    can be removed now.
    Also added a To Bed Mode on Whistle
    command every dog to return and sit on their bed if near.

---
+++ src/main/java/doggytalents/common/entity/ai/triggerable/DogMoveToBedAction.java
@@ -0,0 +1,77 @@
+package doggytalents.common.entity.ai.triggerable;
+
+import javax.annotation.Nonnull;
+
+import doggytalents.common.block.tileentity.DogBedTileEntity;
+import doggytalents.common.entity.Dog;
+import doggytalents.common.lib.Constants;
+import doggytalents.common.util.WorldUtil;
+import net.minecraft.core.BlockPos;
+
+public class DogMoveToBedAction extends TriggerableAction {
+
+    private final @Nonnull BlockPos targetBedPos;
+    private final boolean claimBed;
+
+    private boolean bedReached = false;
+    private int timeOut;
+
+    public DogMoveToBedAction(Dog dog, @Nonnull BlockPos targetBedPos, boolean claimBed) {
+        super(dog, false, false);
+        this.targetBedPos = targetBedPos;
+        this.claimBed = claimBed;
+    }
+
+    @Override
+    public void onStart() {
+        this.timeOut = 200;
+        this.dog.getNavigation().moveTo(
+            (targetBedPos.getX()) + 0.5D, 
+            targetBedPos.getY() + 1, 
+            (targetBedPos.getZ()) + 0.5D, 1.0D);
+    }
+
+    @Override
+    public void tick() {
+
+        --timeOut;
+
+        if (timeOut <= 0) {
+            this.setState(ActionState.FINISHED);
+            return;
+        }
+
+        this.bedReached =
+            dog.getNavigation().isDone()
+            || targetBedPos.closerToCenterThan(this.dog.position(), 0.5);
+
+        if (bedReached) {
+            if (claimBed) claimBed();
+            this.dog.setOrderedToSit(true);
+            this.setState(ActionState.FINISHED);
+            return;
+        }
+    }
+
+    private void claimBed() {
+        var target = this.targetBedPos;
+        var dogBedTileEntity = WorldUtil.getTileEntity(dog.level, target, DogBedTileEntity.class);
+        if (dogBedTileEntity == null) return;
+        if (dogBedTileEntity.getOwnerUUID() != null) return;
+
+        dogBedTileEntity.setOwner(this.dog);
+        this.dog.setBedPos(this.dog.level.dimension(), target);
+        this.dog.level.broadcastEntityEvent(this.dog, Constants.EntityState.WOLF_HEARTS);
+    }
+
+    @Override
+    public void onStop() {
+        
+    }
+    
+    @Override
+    public boolean canOverrideSit() {
+        return true;
+    }
+
+}
