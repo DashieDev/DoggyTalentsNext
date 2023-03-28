991e8593e977bbb9544ce1950526f1076336041d
DashieDev <lamtinh.asian@gmail.com>
2023-03-29 02:09:48 +0700

    Iwanko model glitch
    mane(upper_body) cube is added twice,
    once in the mane part itself and
    one is in the rock_ring.

--- src/main/java/doggytalents/client/entity/model/dog/IwankoModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/IwankoModel.java
@@ -37,7 +37,7 @@ public class IwankoModel extends DogModel<Dog> {
 
         var upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create().texOffs(21, 0).addBox(-3.0F, -3.0F, -3.0F, 8.0F, 6.0F, 7.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(-1.0F, 14.0F, -3.0F, 1.5707964F, 0.0F, 0.0F));
 
-		upper_body.addOrReplaceChild("rock_ring", CubeListBuilder.create().texOffs(21, 0).addBox(-4.0F, -5.5F, -0.5F, 8.0F, 6.0F, 7.0F, CubeDeformation.NONE)
+		upper_body.addOrReplaceChild("rock_ring", CubeListBuilder.create()
 		.texOffs(2, 34).addBox(-4.0F, -6.0F, -0.5F, 8.0F, 2.0F, 7.0F, new CubeDeformation(0.15F))
 		.texOffs(0, 35).addBox(-1.0F, -6.0F, -2.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(-0.25F))
 		.texOffs(0, 35).addBox(-1.0F, -6.0F, 6.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(-0.25F))
