dca97b79267d1e849cace264d3eedae3b4ea6275
DashieDev <lamtinh.asian@gmail.com>
2023-03-30 12:58:45 +0700

    Fix Luc Model.

--- src/main/java/doggytalents/client/entity/model/dog/LucarioModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/LucarioModel.java
@@ -32,8 +32,11 @@ public class LucarioModel extends DogModel<Dog> {
 
 		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(18, 14).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 9.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 14.0F, 2.0F, 1.5708F, 0.0F, 0.0F));
 
-		PartDefinition upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create().texOffs(21, 0).addBox(-4.0F, -5.5F, -0.5F, 8.0F, 6.0F, 7.0F, new CubeDeformation(0.0F))
-		.texOffs(43, 20).addBox(-1.0F, -2.5F, -2.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(-0.4F)), PartPose.offsetAndRotation(-2.0F, 11.5f, -4.5F, 1.5708F, 0.0F, 0.0F));
+        var upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create().texOffs(21, 0).addBox(-3.0F, -3.0F, -3.0F, 8.0F, 6.0F, 7.0F, CubeDeformation.NONE), PartPose.offsetAndRotation(-1.0F, 14.0F, -3.0F, 1.5707964F, 0.0F, 0.0F));
+
+		upper_body.addOrReplaceChild("spikes", 
+            CubeListBuilder.create().texOffs(43, 20)
+            .addBox(-1.0F, -2.5F, -2.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(-0.4F)), PartPose.offset(1f, 2.5f, -2.5f));
 
 		PartDefinition leg1 = partdefinition.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 18).addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.5F, 16.0F, 7.0F));
 
@@ -47,7 +50,7 @@ public class LucarioModel extends DogModel<Dog> {
 		.texOffs(38, 15).addBox(0.25F, -0.5F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.5F))
 		.texOffs(43, 20).addBox(0.0F, 5.25F, -2.25F, 2.0F, 2.0F, 2.0F, new CubeDeformation(-0.4F)), PartPose.offset(0.5F, 16.0F, -4.0F));
 
-        PartDefinition tail = partdefinition.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.offset(-1.0F, 12.0F, 10.0F));
+        PartDefinition tail = partdefinition.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.offsetAndRotation(-1.0F, 12.0F, 8.0F, 0.62831855F, 0.0F, 0.0F));
 
         PartDefinition real_tail_1 = tail.addOrReplaceChild("real_tail", CubeListBuilder.create().texOffs(9, 18).addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.ZERO);
         
@@ -57,116 +60,5 @@ public class LucarioModel extends DogModel<Dog> {
 
 		return LayerDefinition.create(meshdefinition, 64, 32);
 	}
-
-    @Override
-    public void prepareMobModel(Dog dog, float limbSwing, float limbSwingAmount, float partialTickTime) {
-        this.tail.yRot = dog.getWagAngle(limbSwing, limbSwingAmount, partialTickTime);
-
-        if (dog.isInSittingPose()) {
-            if (dog.isLying()) {
-                this.head.setPos(-1, 19.5F, -7);
-                this.body.setPos(0, 20, 2);
-                this.body.xRot = (float)Math.PI / 2F;
-                this.mane.setPos(-1, 20, -2);
-                this.mane.xRot = this.body.xRot;
-                this.tail.setPos(-1, 18, 8);
-                this.legBackRight.setPos(-4.5F, 23, 7);
-                this.legBackRight.xRot = -(float)Math.PI / 2F;
-                this.legBackLeft.setPos(2.5F, 23, 7);
-                this.legBackLeft.xRot = -(float)Math.PI / 2F;
-                this.legFrontRight.setPos(-4.5F, 23, -4);
-                this.legFrontRight.xRot = -(float)Math.PI / 2F;
-                this.legFrontLeft.setPos(2.5F, 23, -4);
-                this.legFrontLeft.xRot = -(float)Math.PI / 2F;
-
-//                this.body.setRotationPoint(0.0F, 14.0F, 0.0F);
-//                this.body.rotateAngleX = ((float)Math.PI / 2F);
-//                this.mane.setRotationPoint(-1.0F, 19.0F, -3.0F);
-//                this.mane.rotateAngleX = this.body.rotateAngleX;
-//                this.head.setRotationPoint(-1.0F, 17.0F, -7.0F);
-//
-//                this.tail.setRotationPoint(-0.5F, 17.0F, 8.0F); // +4.0D
-//                this.legBackRight.setRotationPoint(-4.5F, 20.0F, 7.0F);
-//                this.legBackLeft.setRotationPoint(2.5F, 20.0F, 7.0F);
-//                this.legFrontRight.setRotationPoint(-3.0F, 22.0F, -3.0F);
-//                this.legFrontLeft.setRotationPoint(1.0F, 22.0F, -3.0F);
-//
-//                this.legBackRight.rotateAngleX = -(float)Math.PI / 2.6F;
-//                this.legBackLeft.rotateAngleX = -(float)Math.PI / 2.6F;
-//
-//                this.legFrontRight.rotateAngleX = -(float)Math.PI / 2;
-//                this.legFrontRight.rotateAngleY = (float)Math.PI / 10;
-//                this.legFrontLeft.rotateAngleX = -(float)Math.PI / 2;
-//                this.legFrontLeft.rotateAngleY = -(float)Math.PI / 10;
-            } else if (dog.isLying() && false) {
-                this.body.setPos(0.0F, 19.0F, 2.0F);
-                this.body.xRot = ((float)Math.PI / 2F);
-                this.mane.setPos(-1.0F, 19.0F, -3.0F);
-                this.mane.xRot = this.body.xRot;
-                this.head.setPos(-1.0F, 17.0F, -7.0F);
-
-                this.tail.setPos(-0.5F, 17.0F, 8.0F); // +4.0D
-                this.legBackRight.setPos(-4.5F, 20.0F, 7.0F);
-                this.legBackLeft.setPos(2.5F, 20.0F, 7.0F);
-                this.legFrontRight.setPos(-3.0F, 22.0F, -3.0F);
-                this.legFrontLeft.setPos(1.0F, 22.0F, -3.0F);
-
-                this.legBackRight.xRot = -(float)Math.PI / 2.6F;
-                this.legBackLeft.xRot = -(float)Math.PI / 2.6F;
-
-                this.legFrontRight.xRot = -(float)Math.PI / 2;
-                this.legFrontRight.yRot = (float)Math.PI / 10;
-                this.legFrontLeft.xRot = -(float)Math.PI / 2;
-                this.legFrontLeft.yRot = -(float)Math.PI / 10;
-            } else {
-                this.head.setPos(-1.0F, 13.5F, -7.0F);
-                this.mane.setPos(0.0F, 19.0F, -1F);
-                this.mane.xRot = ((float)Math.PI * 2F / 5F);
-                this.mane.yRot = 0.0F;
-                this.body.setPos(0.0F, 18.0F, 0.0F);
-                this.body.xRot = ((float)Math.PI / 4F);
-                this.tail.setPos(-0.5F, 21.0F, 6.0F);
-                this.legBackRight.setPos(-2.5F, 22.0F, 2.0F);
-                this.legBackRight.xRot = ((float)Math.PI * 3F / 2F);
-                this.legBackLeft.setPos(0.5F, 22.0F, 2.0F);
-                this.legBackLeft.xRot = ((float)Math.PI * 3F / 2F);
-                this.legFrontRight.xRot = 5.811947F;
-                this.legFrontRight.setPos(-2.49F, 17.0F, -4.0F);
-                this.legFrontLeft.xRot = 5.811947F;
-                this.legFrontLeft.setPos(0.51F, 17.0F, -4.0F);
-
-
-                this.head.setPos(-1.0F, 13.5F, -7.0F);
-                this.legFrontRight.yRot = 0;
-                this.legFrontLeft.yRot = 0;
-            }
-        } else {
-            this.body.setPos(0.0F, 14.0F, 2.0F);
-            this.body.xRot = ((float)Math.PI / 2F);
-            this.mane.setPos(0F, 16.5F, -0.5F);
-            this.mane.xRot = this.body.xRot;
-            this.tail.setPos(-1.0F, 13.5F, 9.2f);
-            this.legBackRight.setPos(-2.5F, 16.0F, 7.0F);
-            this.legBackLeft.setPos(0.5F, 16.0F, 7.0F);
-            this.legFrontRight.setPos(-2.5F, 16.0F, -4.0F);
-            this.legFrontLeft.setPos(0.5F, 16.0F, -4.0F);
-            this.legBackRight.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-            this.legBackLeft.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-            this.legFrontRight.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-            this.legFrontLeft.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-
-            this.head.setPos(-1.0F, 13.5F, -7.0F);
-            this.legFrontRight.yRot = 0.0F;
-            this.legFrontLeft.yRot = 0.0F;
-        }
-
-        this.realHead.zRot = dog.getInterestedAngle(partialTickTime) + dog.getShakeAngle(partialTickTime, 0.0F);
-        this.mane.zRot = dog.getShakeAngle(partialTickTime, -0.08F);
-        this.body.zRot = dog.getShakeAngle(partialTickTime, -0.16F);
-        this.realTail.zRot = dog.getShakeAngle(partialTickTime, -0.2F);
-        this.realTail2.zRot = dog.getShakeAngle(partialTickTime, -0.2F);
-        this.realTail3.zRot = dog.getShakeAngle(partialTickTime, -0.2F);
-
-    }
 
 }
