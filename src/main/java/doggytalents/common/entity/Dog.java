3323bae3b648c9ee4b7d0bd364b2b44787258611
DashieDev <lamtinh.asian@gmail.com>
2023-04-01 15:06:28 +0700

    Only setClientSkin when on.... well... client

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2036,7 +2036,7 @@ public class Dog extends AbstractDog {
             this.refreshDimensions();
         }
 
-        if (CUSTOM_SKIN.equals(key)) {
+        if (this.level.isClientSide && CUSTOM_SKIN.equals(key)) {
             this.setClientSkin(
                 DogTextureManager.INSTANCE
                     .getLocFromHashOrGet(
