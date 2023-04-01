678b3ec8e283e2d350499af9cfc4018bd5043406
DashieDev <lamtinh.asian@gmail.com>
2023-04-01 15:21:40 +0700

    Overhaul Radar mechanic
    Users will be presented with a screen
    to pick dogs to locate, after they selected
    the dog, the locating dog data is then being
    written into the item nbt tag of the stack,
    Right click again to clear.
    
    If user is holding a Radar with the tag,
    there will be a text rendered in the direction of where the dog is telling
    user how far away.

--- src/main/java/doggytalents/client/screen/RadarScreen.java
+++ src/main/java/doggytalents/client/screen/RadarScreen.java
@@ -1,42 +1,294 @@
 package doggytalents.client.screen;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.lang3.tuple.Triple;
+
+import com.google.common.collect.Maps;
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.vertex.PoseStack;
-import doggytalents.common.lib.Resources;
+
+import doggytalents.ChopinLogger;
+import doggytalents.common.entity.Dog;
+import doggytalents.common.network.PacketHandler;
+import doggytalents.common.network.packet.data.ConductingBoneData;
+import doggytalents.common.network.packet.data.RadarData;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.network.PacketDistributor;
 
 public class RadarScreen extends Screen {
 
+    
+    private Rect2i rect;
     private Player player;
+    private final ArrayList<String> dogNameList;
+    private final ArrayList<UUID> dogIdList;
+    private final ArrayList<String> dogNameFilterList;
+    private final ArrayList<UUID> dogIdFilterList;
+    private int hightlightDogName;
+    private boolean showUuid = false;
+    private String value = "";
+    
+    private final int HLC_HEEL_NO_SIT = 0xFF10F9; 
+    private Map<UUID, Integer> dogDistanceMap = Maps.newConcurrentMap();
+    private Map<UUID, BlockPos> dogPosMap = Maps.newConcurrentMap();
+ 
+    private int hightlightTextColor = HLC_HEEL_NO_SIT;
+ 
+    private final int MAX_BUFFER_SIZE = 64;
 
-    protected RadarScreen(Player playerIn) {
-        super(Component.translatable("doggytalents.screen.radar.title"));
-        this.player = playerIn;
+    private int mouseX0;
+    private int mouseY0;
+
+    public RadarScreen(Player player) {
+        super(Component.translatable("doggytalents.screen.conducting_bone"));
+        this.player = player;   
+        this.dogNameList = new ArrayList<String>(4);
+        this.dogIdList = new ArrayList<UUID>(4);
+        this.dogIdFilterList = new ArrayList<UUID>(4);
+        this.dogNameFilterList = new ArrayList<String>(4);
+        this.hightlightDogName = 0;
     }
 
-    public static void open(Player player) {
+    public static void open() { 
         Minecraft mc = Minecraft.getInstance();
-        mc.setScreen(new RadarScreen(player));
+        var screen = new RadarScreen(mc.player);
+        mc.setScreen(screen);
+        screen.requestDogs();
     }
 
     @Override
     public void init() {
         super.init();
+        this.minecraft.keyboardHandler.setSendRepeatsToGui(true);
+        this.rect = new Rect2i(0, 0,500, 500);
+        
+        Button showUuid = new Button(3, 3, 60, 20, Component.translatable("doggytalents.screen.whistler.heel_by_name.show_uuid"), (btn) -> {
+            btn.setMessage(Component.translatable("doggytalents.screen.whistler.heel_by_name."
+                + (this.showUuid? "show" : "hide")
+                +"_uuid"));
+            this.showUuid = !this.showUuid;
+        });
+
+        Button help = new Button(3, 26, 20, 20, Component.literal("?"), b -> {} ) {
+            @Override
+            public void renderToolTip(PoseStack stack, int mouseX, int mouseY) {
+                List<Component> list = new ArrayList<>();
+                list.add(Component.translatable("doggytalents.screen.radar.help_title")
+                    .withStyle(Style.EMPTY.withBold(true)));
+                String str = I18n.get("doggytalents.screen.radar.help");
+                list.addAll(ScreenUtil.splitInto(str, 150, RadarScreen.this.font));
+
+                RadarScreen.this.renderComponentTooltip(stack, list, mouseX, mouseY);
+            }
+        };
+        
+        this.addRenderableWidget(showUuid);
+        this.addRenderableWidget(help);
     }
+
 
     @Override
     public void render(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, Resources.GUI_RADAR);
-        int xSize = 210;
-        int ySize = 210;
-        int x = (this.width - xSize) / 2;
-        int y = (this.height - ySize) / 2;
-        this.blit(stack, x, y, 0, 0, xSize, ySize);
+
+        if (this.mouseX0 != mouseX || this.mouseY0 != mouseY) {
+            this.onMouseMoved(mouseX, mouseY);
+            this.mouseX0 = mouseX;
+            this.mouseY0 = mouseY;
+        }
+
+        int half_width = this.width >> 1;
+        int half_height = this.height >> 1; 
+    
+        Gui.fill(stack, half_width - 100, half_height - 100, half_width + 100, half_height + 100, Integer.MIN_VALUE);
+        Gui.fill(stack, half_width - 100, half_height + 105, half_width + 100, half_height + 117, Integer.MIN_VALUE);
+
+        super.render(stack, mouseX, mouseY, partialTicks);
+        int offset = 0;
+        int textx = half_width - 100 + 2;
+        int texty = half_height - 100 + 2;
+        int textx1 = half_width + 100 - 35;
+        int texty1 = half_height - 100 + 2;
+        if (this.dogNameFilterList.size() <= 0) {
+            this.font.draw(stack, 
+                I18n.get("doggytalents.screen.conducting_bone.no_dog_found"), 
+                textx, texty + offset, 0xf50a0a);
+        }
+
+        for (int i = 0; i < this.dogNameFilterList.size(); ++i) {
+            int color = 0xffffffff;
+            if (i == this.hightlightDogName) color = this.hightlightTextColor;
+            var id = this.dogIdFilterList.get(i);
+            String text = this.dogNameFilterList.get(i) + (
+                this.showUuid ? 
+                " ( " + id + " ) " :
+                ""
+            );
+            this.font.draw(stack, text, textx, texty + offset, color);
+            int dist = this.dogDistanceMap.get(id);
+            String text1 = 
+                this.showUuid ? 
+                "" : (dist > 99_999 ? "far" : "" + dist);
+            this.font.draw(stack, text1, textx1, texty1 + offset, color);
+            offset+=10;
+            if (offset > 190) break;
+        }
+
+        int txtorgx = half_width - 90;
+        int txtorgy = half_height + 107;
+        
+        this.font.draw(stack, this.value + "_", txtorgx, txtorgy,  0xffffffff);
+        
     }
+
+    private int getHoveredIndex(double x, double y, int entry_size) {
+        int mX = this.width/2;
+        int mY = this.height/2;
+        if (Math.abs(x - mX) > 100) return -1;
+        if (Math.abs(y - mY) > 100) return -1;
+        int baseY = mY - 100;
+        int indx = ( Mth.floor(y - baseY) )/10;
+        if (indx >= entry_size) return -1;
+        return indx;
+    }
+
+    private void onMouseMoved(double x, double y) {
+        int newIndx = getHoveredIndex(x, y, this.dogIdFilterList.size());
+        if (newIndx < 0) return;
+        this.hightlightDogName = newIndx;
+    }
+
+    @Override
+    public boolean mouseClicked(double x, double y, int p_94697_) {
+        boolean ret = super.mouseClicked(x, y, p_94697_);
+        int mX = this.width/2;
+        int mY = this.height/2;
+        if (Math.abs(x - mX) > 100) return ret;
+        if (Math.abs(y - mY) > 100) return ret;
+        int indx = getHoveredIndex(x, y, this.dogIdFilterList.size());
+        if (indx >= 0) {
+            var uuid = this.dogIdFilterList.get(indx);
+            var name = this.dogNameFilterList.get(indx);
+            this.startLocateDog(uuid, name, this.dogPosMap.getOrDefault(uuid, BlockPos.ZERO));
+            Minecraft.getInstance().setScreen(null);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        InputConstants.Key mouseKey = InputConstants.getKey(keyCode, scanCode);
+
+        if (keyCode == 264) {
+            this.hightlightDogName = Mth.clamp(this.hightlightDogName +1, 0, this.dogNameFilterList.size()-1);
+        } else if (keyCode == 265) {
+            this.hightlightDogName = Mth.clamp(this.hightlightDogName -1, 0, this.dogNameFilterList.size()-1);
+        } else if (keyCode == 257) {
+            if (this.dogIdFilterList.isEmpty()) return false; 
+            var uuid = this.dogIdFilterList.get(hightlightDogName);
+            var name = this.dogNameFilterList.get(hightlightDogName);
+            this.startLocateDog(uuid, name, this.dogPosMap.getOrDefault(uuid, BlockPos.ZERO));
+            this.minecraft.setScreen(null);
+        } else if (keyCode == 259) {
+            this.popCharInText();
+        }
+        
+        return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    @Override
+    public boolean charTyped(char code, int p_231042_2_) {
+        if (SharedConstants.isAllowedChatCharacter(code)) {
+            this.insertText(Character.toString(code));
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    private void updateFilter() {
+        this.dogNameFilterList.clear();
+        this.dogIdFilterList.clear();
+        this.hightlightDogName =0;
+
+        if (this.value == "") {
+            for (var i : this.dogNameList) {
+                this.dogNameFilterList.add(i);
+            }
+            for (var i : this.dogIdList) {
+                this.dogIdFilterList.add(i);
+            }
+        } else {
+            for (int i = 0; i < this.dogIdList.size(); ++i) {
+                if (this.dogNameList.get(i).length() < this.value.length()) continue; 
+                if (this.dogNameList.get(i).contains(this.value)) {
+                    this.dogIdFilterList.add(this.dogIdList.get(i));
+                    this.dogNameFilterList.add(this.dogNameList.get(i));
+                }
+            }
+        }
+    } 
+
+    private void insertText(String x) {
+        if (this.value.length() < MAX_BUFFER_SIZE) {
+            this.value = this.value + x;
+        }
+        this.updateFilter();
+    }
+
+    private void popCharInText() {
+        if (this.value.length() <= 0) return;
+        this.value = this.value.substring(0, this.value.length()-1);
+        this.updateFilter();
+    }
+    
+    @Override
+    public void removed() {
+        super.removed();
+        this.minecraft.keyboardHandler.setSendRepeatsToGui(false);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
+    }
+
+    private void requestDogs() {
+        PacketHandler.send(PacketDistributor.SERVER.noArg(), new RadarData.RequestDogsData());
+    }
+
+    public void assignResponse(List<Triple<UUID, String, BlockPos>> dogLs) {
+        for (var entry : dogLs) {
+            this.dogNameList.add(entry.getMiddle());
+            this.dogNameFilterList.add(entry.getMiddle());
+            this.dogIdList.add(entry.getLeft());
+            this.dogIdFilterList.add(entry.getLeft());
+            var pos = entry.getRight();
+            int distance = Mth.ceil(Mth.sqrt((float)this.player.blockPosition().distSqr(pos)));
+            this.dogDistanceMap.put(entry.getLeft(), distance);
+            this.dogPosMap.put(entry.getLeft(), pos);
+        }
+    }
+
+    private void startLocateDog(UUID uuid, String name, BlockPos pos) {
+        PacketHandler.send(PacketDistributor.SERVER.noArg(), 
+            new RadarData.StartLocatingData(uuid, name, pos));
+    }
+
 }
---
+++ src/main/java/doggytalents/common/network/packet/data/RadarData.java
@@ -0,0 +1,54 @@
+package doggytalents.common.network.packet.data;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+
+import doggytalents.common.storage.DogLocationData;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ClipContext.Block;
+
+public class RadarData {
+    public static class RequestDogsData {
+    }
+
+    public static class ResponseDogsData {
+        public List<Triple<UUID, String, BlockPos>> entries;
+        public ResponseDogsData(List<Triple<UUID, String, BlockPos>> entries) {
+            this.entries = entries;
+        }
+    }
+
+    public static class StartLocatingData {
+        public UUID uuid;
+        public String name;
+        public BlockPos pos;
+        public StartLocatingData(UUID uuid, String name, BlockPos pos) {
+            this.uuid = uuid;
+            this.name = name;
+            this.pos = pos;
+        }
+    }
+
+    public static class RequestPosUpdateData {
+        public UUID uuid;
+        public BlockPos pos;
+        public RequestPosUpdateData(UUID id, BlockPos pos) {
+            this.uuid = id;
+            this.pos = pos;
+        }
+    }
+
+    public static class ResponsePosUpdateData {
+        public UUID uuid;
+        public BlockPos correctPos;
+        public ResponsePosUpdateData(UUID id, BlockPos pos) {
+            this.uuid = id;
+            this.correctPos = pos;
+        }
+    }
+}
