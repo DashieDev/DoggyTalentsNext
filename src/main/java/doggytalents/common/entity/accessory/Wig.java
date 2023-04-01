434a34826bb771d82fa82e85f4291a6d8519debe
DashieDev <lamtinh.asian@gmail.com>
2023-04-01 20:47:23 +0700

    posfix : IAccessosryHasModel is at api
    and refering to the main source file ...
    which is missing from the api...

--- src/main/java/doggytalents/common/entity/accessory/Wig.java
+++ src/main/java/doggytalents/common/entity/accessory/Wig.java
@@ -3,11 +3,11 @@ package doggytalents.common.entity.accessory;
 import java.util.function.Supplier;
 
 import doggytalents.DoggyAccessoryTypes;
-import doggytalents.api.inferface.IAccessoryHasModel;
 import doggytalents.api.registry.Accessory;
 import doggytalents.api.registry.AccessoryInstance;
 import doggytalents.client.entity.render.AccessoryModelManager.Entry;
 import doggytalents.client.entity.render.layer.accessory.modelrenderentry.AccessoryModelRenderEntries;
+import doggytalents.client.entity.render.layer.accessory.modelrenderentry.IAccessoryHasModel;
 import doggytalents.common.item.DyeableAccessoryItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
