4512e7bb0f0e48881f787f6b761a629a36dcb0df
DashieDev <lamtinh.asian@gmail.com>
2023-04-02 09:41:54 +0700

    Forgot to adress case where obstacles is present.

--- src/main/java/doggytalents/common/entity/ai/GuardModeGoal.java
+++ src/main/java/doggytalents/common/entity/ai/GuardModeGoal.java
@@ -128,9 +128,7 @@ public class GuardModeGoal extends NearestAttackableTargetGoal<Mob> {
             if (this.nearestDanger != null)
             this.dog.getLookControl().setLookAt(this.nearestDanger, 10.0F, this.dog.getMaxHeadXRot());
             if (dog.distanceToSqr(owner) > 2 && --this.tickUntilPathRecalc <= 0) {
-                //The dog always stays close to the owner, and tp when a little bit further
-                //So the path is not that long, so interval = 3 is ok
-                this.tickUntilPathRecalc = 3;
+                this.tickUntilPathRecalc = 5;
                 if (!this.dog.isLeashed() && !this.dog.isPassenger()) {
                     //Outside guard radius -> tp 
                     //TODO Swimmer dog won't be able to tp when guard underwater.
@@ -244,9 +242,7 @@ public class GuardModeGoal extends NearestAttackableTargetGoal<Mob> {
         public void tick() { 
             
             if (dog.distanceToSqr(owner) > 2 && --this.tickUntilPathRecalc <= 0) {
-                //The dog always stays close to the owner, and tp when a little bit further
-                //So the path is not that long, so interval = 3 is okw
-                this.tickUntilPathRecalc = 3;
+                this.tickUntilPathRecalc = 5;
                 if (!this.dog.isLeashed() && !this.dog.isPassenger()) {
                     //Outside guard radius -> tp 
                     if (this.dog.distanceToSqr(this.owner) > GUARD_DISTANCE_SQR) {
