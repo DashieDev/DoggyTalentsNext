919f0990f8556a04484e7f85a612294457af001b
DashieDev <lamtinh.asian@gmail.com>
2023-04-08 20:40:16 +0700

    tune defeated whine.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -300,10 +300,10 @@ public class Dog extends AbstractDog {
     @Override
     protected SoundEvent getAmbientSound() {
         if (this.isDefeated()) {
-            if (this.getDogHunger() > 32) {
-                return this.random.nextInt(2) == 0 ? SoundEvents.WOLF_WHINE : SoundEvents.WOLF_PANT;
-            }
-            return SoundEvents.WOLF_WHINE;
+            // if (this.getDogHunger() > 32) {
+            //     return this.random.nextInt(2) == 0 ? SoundEvents.WOLF_WHINE : SoundEvents.WOLF_PANT;
+            // }
+            return this.random.nextInt(2) == 0 ? SoundEvents.WOLF_WHINE : SoundEvents.WOLF_PANT;
         }
         if (this.random.nextInt(3) == 0) {
             return this.isTame() && this.getHealth() < 10.0F ? SoundEvents.WOLF_WHINE : SoundEvents.WOLF_PANT;
