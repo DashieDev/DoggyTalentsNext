c0d9a3b6fec3e11e725dee095ee592663f1449bd
DashieDev <lamtinh.asian@gmail.com>
2023-04-13 01:12:06 +0700

    Improve dogGoBehindAction
    +Only begin the action if can reach
    +Also dog will search in a stair case
    manner, block by block.

--- src/main/java/doggytalents/common/entity/ai/triggerable/DogGoBehindOwnerAction.java
+++ src/main/java/doggytalents/common/entity/ai/triggerable/DogGoBehindOwnerAction.java
@@ -2,11 +2,14 @@ package doggytalents.common.entity.ai.triggerable;
 
 import javax.annotation.Nonnull;
 
+import doggytalents.ChopinLogger;
 import doggytalents.common.entity.Dog;
+import doggytalents.common.util.DogUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
 public class DogGoBehindOwnerAction extends TriggerableAction {
@@ -28,10 +31,11 @@ public class DogGoBehindOwnerAction extends TriggerableAction {
             this.setState(ActionState.FINISHED); return;
         }
         this.dog.getNavigation().stop();
-        this.dog.getNavigation().moveTo(
-            this.targetPos.getX(), this.targetPos.getY(),
-            this.targetPos.getZ(),
-            dog.getUrgentSpeedModifier());
+        DogUtil.moveToIfReachOrElse(dog, targetPos, 
+            dog.getUrgentSpeedModifier(), 1, 1, d -> { this.targetPos = null; });
+        if (this.targetPos == null) {
+            this.setState(ActionState.FINISHED); return;
+        }
         this.holdTimeLeft = 40;
     }
 
@@ -54,19 +58,27 @@ public class DogGoBehindOwnerAction extends TriggerableAction {
     }
 
     private void getBehindOwnerPos() {
+        var owner_pos0 = owner.position();
         float a1 = owner.getYHeadRot();
         float dx1 = Mth.sin(a1*Mth.DEG_TO_RAD);
         float dz1 = -Mth.cos(a1*Mth.DEG_TO_RAD);
-        var offset = new Vec3(3*dx1, 0, 3*dz1);
-        var owner_pos0 = owner.position();
-        var targetPos_precise = owner_pos0.add(offset);
-        this.targetPos = new BlockPos(targetPos_precise);
-        if (!this.dog.level.getBlockState(targetPos).isAir()) {
-            this.targetPos = this.targetPos.above();
-            if (!this.dog.level.getBlockState(targetPos).isAir()) {
-                this.targetPos = null;
+        
+        var vec_back = new Vec3(dx1, 0, dz1);
+        double pX = owner_pos0.x, pY = owner_pos0.y , pZ = owner_pos0.z;
+        BlockPos bpos = null;
+        BlockState state = null;
+        for (int i = 0; i < 3; ++i) {
+            pX += vec_back.x; pZ += vec_back.z;
+            bpos = new BlockPos(pX, pY, pZ);
+            state = dog.level.getBlockState(bpos);
+            if (!state.isAir()) {
+                ++pY;
             }
         }
+        
+        bpos = new BlockPos(pX, pY, pZ);
+        state = dog.level.getBlockState(bpos);
+        this.targetPos = ( state != null && state.isAir() ) ? bpos : null;
     }
     
 }
