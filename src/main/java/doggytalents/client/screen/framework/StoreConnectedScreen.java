c64c39ef2c16560e96bb2f8a65865ddd3f7be0bd
DashieDev <lamtinh.asian@gmail.com>
2023-04-13 14:45:32 +0700

    Move two methods to StoreConnectedScreen

--- src/main/java/doggytalents/client/screen/framework/StoreConnectedScreen.java
+++ src/main/java/doggytalents/client/screen/framework/StoreConnectedScreen.java
@@ -2,8 +2,13 @@ package doggytalents.client.screen.framework;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
+import doggytalents.ChopinLogger;
+import doggytalents.client.screen.DogNewInfoScreen.element.AbstractElement;
 import doggytalents.client.screen.DogNewInfoScreen.store.UIActionTypes;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
@@ -25,6 +30,24 @@ public class StoreConnectedScreen extends Screen {
             new UIAction(UIActionTypes.RESIZE, new Object()),
             width, height
         );
+    }
+
+    
+    @Override
+    public void setFocused(@Nullable GuiEventListener guiEventListener) {
+        if (guiEventListener instanceof AbstractElement) {
+            //TODO Maybe AbstractElement::canFocus() in the future.
+            return;
+        } 
+        ChopinLogger.l("set focus : " + guiEventListener);
+        super.setFocused(guiEventListener);
+    }
+
+    @Override
+    public boolean mouseClicked(double p_94695_, double p_94696_, int p_94697_) {
+        //invalidate focus every click. Prefer focus to be null if the click is outside of any bound.
+        this.setFocused(null);
+        return super.mouseClicked(p_94695_, p_94696_, p_94697_);
     }
 
     public List<Class<? extends AbstractSlice>> getSlices() { return List.of(); }
