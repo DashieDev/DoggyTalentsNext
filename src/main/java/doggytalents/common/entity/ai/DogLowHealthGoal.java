a23053294afe665aad8bf8f33c0df63a690d27a3
DashieDev <lamtinh.asian@gmail.com>
2023-04-16 22:10:31 +0700

    Spam click doesnt make low health dog spam whine

--- src/main/java/doggytalents/common/entity/ai/DogLowHealthGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogLowHealthGoal.java
@@ -32,6 +32,7 @@ public class DogLowHealthGoal extends Goal {
     private int timeToRecalcPath;
     private int tickTillSearchForTp = 0;
     private int tickTillInitTeleport = 0;
+    private int tickTillInitWhine = 0;
     private float oldWaterCost;
 
     private boolean whine = false;
@@ -48,6 +49,10 @@ public class DogLowHealthGoal extends Goal {
     public boolean canUse() {
         if (this.tickTillInitTeleport > 0) {
             --this.tickTillInitTeleport;
+        }
+
+        if (this.tickTillInitWhine > 0) {
+            --this.tickTillInitWhine;
         }
 
         LivingEntity owner = this.dog.getOwner();
@@ -104,6 +109,11 @@ public class DogLowHealthGoal extends Goal {
 
     @Override
     public void tick() {
+
+        if (this.tickTillInitWhine > 0) {
+            --this.tickTillInitWhine;
+        }
+
         if (this.dog.distanceToSqr(this.owner) > stopDist*stopDist) {
             this.dog.getLookControl().setLookAt(this.owner, 10.0F, this.dog.getMaxHeadXRot());
             if (--this.timeToRecalcPath <= 0) {
@@ -117,13 +127,12 @@ public class DogLowHealthGoal extends Goal {
                 if (this.tickTillSearchForTp <= 0) this.tickTillSearchForTp = 10;
             }
         }  else {
-            if (this.whine) {
+            if (this.whine && this.tickTillInitWhine <= 0) {
                 this.whine = false;
                 this.owner.sendSystemMessage(Component.translatable("dog.msg.low_health." + this.dog.getRandom().nextInt(3), this.dog.getName()));
                 this.dog.playSound(SoundEvents.WOLF_WHINE, this.dog.getSoundVolume(), this.dog.getVoicePitch());
             }
             this.dog.getLookControl().setLookAt(this.owner);
-            
         }
     }
 
