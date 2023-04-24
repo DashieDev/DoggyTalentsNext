2eadd2eff19f662b0ff0cab636543d800764a040
DashieDev <lamtinh.asian@gmail.com>
2023-04-25 00:25:27 +0700

    BEASTARS ビースターズ !
    Added "styles" from the popular anime series!
    
    Accessories :
    The series 's school uniform for male and female.
    Skins :
    Legoshi the wolf
    Juno the wolf
    Jack the golden retriever

--- src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/AccessoryModelRenderEntries.java
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/AccessoryModelRenderEntries.java
@@ -8,11 +8,15 @@ public class AccessoryModelRenderEntries {
     public static Entry BOWTIE = new BowtieRenderEntry();
     public static Entry SMARTY_GLASSES = new SmartyGlassesRenderEntry();
     public static Entry WIG = new WigRenderEntry();
+    public static Entry BEASTARS_UNIFORM_MALE = new BeastarsUniformMaleEntry();
+    public static Entry BEASTARS_UNIFORM_FEMALE = new BeastarsUniformFemaleEntry();
 
     public static void registerEntries() {
         AccessoryModelManager.register(BOWTIE);
         AccessoryModelManager.register(SMARTY_GLASSES);
         AccessoryModelManager.register(WIG);
+        AccessoryModelManager.register(BEASTARS_UNIFORM_MALE);
+        AccessoryModelManager.register(BEASTARS_UNIFORM_FEMALE);
     }
 
 }
