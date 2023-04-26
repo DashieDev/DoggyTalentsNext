d8632b18241e475b78620cd7152956c30a0d5c8f
DashieDev <lamtinh.asian@gmail.com>
2023-04-26 14:31:28 +0700

    BEASTARS ビースターズ II !
    Added Egg Sandwhich! Yum !

---
+++ src/main/java/doggytalents/common/item/EggSandwichItem.java
@@ -0,0 +1,23 @@
+package doggytalents.common.item;
+
+import doggytalents.DoggyItemGroups;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+
+public class EggSandwichItem extends Item {
+
+    public EggSandwichItem() {
+        super(
+            (new Properties()).tab(DoggyItemGroups.GENERAL).food(
+                (new FoodProperties.Builder())
+                    .nutrition(6)
+                    .saturationMod(0.6F)
+                    .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 60, 1), 1)
+                    .build()
+            )
+        );
+    }
+    
+}
