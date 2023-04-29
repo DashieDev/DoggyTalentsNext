e59556dcbf7b97394a92da77178db63e0dbd402f
DashieDev <lamtinh.asian@gmail.com>
2023-04-29 12:56:28 +0700

    Fix NavbarButton(s) center bold text

--- src/main/java/doggytalents/client/screen/AmnesiaBoneScreen/widget/NavBarButton.java
+++ src/main/java/doggytalents/client/screen/AmnesiaBoneScreen/widget/NavBarButton.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import doggytalents.client.screen.AmnesiaBoneScreen.store.slice.ActiveTabSlice;
 import doggytalents.client.screen.AmnesiaBoneScreen.store.slice.ActiveTabSlice.Tab;
 import doggytalents.client.screen.framework.Store;
+import doggytalents.client.screen.framework.widget.TextOnlyButton;
 import doggytalents.common.entity.Dog;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.components.AbstractButton;
@@ -12,17 +13,15 @@ import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
-public class NavBarButton extends AbstractButton {
+public class NavBarButton extends TextOnlyButton {
     protected final Tab tab;
-    private Font font;
     private Screen screen;
     private Dog dog;
 
     public NavBarButton(int x, int y, Component text, Tab tab,
             Font font, Screen screen, Dog dog) {
-        super(x, y, font.width(text), font.lineHeight, text);
+        super(x, y, font.width(text), font.lineHeight, text, $ -> {}, font);
         this.tab = tab;
-        this.font = font;
         this.screen = screen;
         this.dog = dog;
     }
@@ -34,20 +33,6 @@ public class NavBarButton extends AbstractButton {
         .dispatchAll(
             ActiveTabSlice.UIActionCreator(dog, tab)
         );
-    }
-
-    public void renderButton(PoseStack stack, int mouseX, int mouseY, float pticks) {
-        // No HightLight
-        if (this.isHoveredOrFocused()) {
-            var s = this.getMessage().copy();
-            s.withStyle(s.getStyle().withUnderlined(true).withBold(true));
-            this.font.draw(stack, s, this.x-4, this.y, 0xffffffff);
-
-        } else {
-            this.font.draw(stack, this.getMessage(), this.x, this.y, 0xffffffff);
-
-        }
-
     }
 
     @Override
