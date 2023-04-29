724fdb00936109986fb7799413460448b1cb41ca
DashieDev <lamtinh.asian@gmail.com>
2023-04-29 13:11:49 +0700

    Only dog with owner can be immortal

--- src/main/java/doggytalents/common/event/IncapacitatedEventHandler.java
+++ src/main/java/doggytalents/common/event/IncapacitatedEventHandler.java
@@ -25,54 +25,54 @@ public class IncapacitatedEventHandler {
         if (!ServerConfig.getConfig(ConfigHandler.SERVER.IMMORTAL_DOGS)) return;
         var e = ev.getEntity();
         //Can't save dog if dog is out of world
-        if (ev.getSource().is(DamageTypeTags.BYPASSES_INVULNERABILITY)) return;
-        if (e instanceof Dog d) { //TODO check based on var
-            ev.setCanceled(true);
-            d.setHealth(1);
-            d.setMode(EnumMode.INCAPACITATED);
-            //in Incapacitated Mode, the dog hunger is rendered by the negative of 
-            //the maxIncapaciatedHunger's complement of the actuall hunger value.
-            //At first the hunger will be zero, when the dog enters Incapacitated.
-            //To exit incapacitated, the dog hunger have to reach maxIncapacitatedHunger.
-            d.setDogHunger(0); //max : 3 days
-            d.setIncapacitatedMutiplier(1);
-            var owner = d.getOwner();
-            if (owner != null) {
-                var msg = ev.getSource().getLocalizedDeathMessage(d).copy();
-                var genderStr = Component.translatable(d.getGender().getUnlocalisedSubject()).getString();
-                var msg005 = ". "
-                    + genderStr.substring(0, 1).toUpperCase()
-                    + genderStr.substring(1)
-                    + " ";
-                var msg01 = Component.translatable(
-                    "dog.mode.incapacitated.msg.partition1",
-                    Component.literal(msg005),
-                    Component.translatable(EnumMode.INCAPACITATED.getUnlocalisedName())
-                    .withStyle(
-                        Style.EMPTY
-                        .withBold(true)
-                        .withColor(0xd60404)
-                    )
-                );
-            
-                msg.append(msg01);
-                owner.sendSystemMessage(msg);
-            }
-            AccessoryInstance hurtLayer;
-            if (ev.getSource().is(DamageTypeTags.IS_FIRE)) {
-                hurtLayer = DoggyAccessories.INCAPACITATED_BURN.get().getDefault();
-            } else if (ev.getSource().is(DamageTypes.MAGIC)) {
-                hurtLayer = DoggyAccessories.INCAPACITATED_POISON.get().getDefault();
-            } else {
-                hurtLayer = DoggyAccessories.INCAPACITATED_BLOOD.get().getDefault();
-            }
-            
-            if (hurtLayer != null) d.addAccessory(hurtLayer);
-            if (d.isPassenger()) d.stopRiding();
-            if (d.isVehicle()) {
-                for (var x : d.getPassengers()) {
-                    x.stopRiding();
-                }
+        if (ev.getSource().isBypassInvul()) return;
+        if (!(e instanceof Dog dog)) return;//TODO check based on var
+        if (dog.getOwnerUUID() == null) return;
+        ev.setCanceled(true);
+        dog.setHealth(1);
+        dog.setMode(EnumMode.INCAPACITATED);
+        //in Incapacitated Mode, the dog hunger is rendered by the negative of 
+        //the maxIncapaciatedHunger's complement of the actuall hunger value.
+        //At first the hunger will be zero, when the dog enters Incapacitated.
+        //To exit incapacitated, the dog hunger have to reach maxIncapacitatedHunger.
+        dog.setDogHunger(0); //max : 3 days
+        dog.setIncapacitatedMutiplier(1);
+        var owner = dog.getOwner();
+        if (owner != null) {
+            var msg = ev.getSource().getLocalizedDeathMessage(dog).copy();
+            var genderStr = Component.translatable(dog.getGender().getUnlocalisedSubject()).getString();
+            var msg005 = ". "
+                + genderStr.substring(0, 1).toUpperCase()
+                + genderStr.substring(1)
+                + " ";
+            var msg01 = Component.translatable(
+                "dog.mode.incapacitated.msg.partition1",
+                Component.literal(msg005),
+                Component.translatable(EnumMode.INCAPACITATED.getUnlocalisedName())
+                .withStyle(
+                    Style.EMPTY
+                    .withBold(true)
+                    .withColor(0xd60404)
+                )
+            );
+        
+            msg.append(msg01);
+            owner.sendSystemMessage(msg);
+        }
+        AccessoryInstance hurtLayer;
+        if (ev.getSource().isFire()) {
+            hurtLayer = DoggyAccessories.INCAPACITATED_BURN.get().getDefault();
+        } else if (ev.getSource() == DamageSource.MAGIC) {
+            hurtLayer = DoggyAccessories.INCAPACITATED_POISON.get().getDefault();
+        } else {
+            hurtLayer = DoggyAccessories.INCAPACITATED_BLOOD.get().getDefault();
+        }
+        
+        if (hurtLayer != null) dog.addAccessory(hurtLayer);
+        if (dog.isPassenger()) dog.stopRiding();
+        if (dog.isVehicle()) {
+            for (var x : dog.getPassengers()) {
+                x.stopRiding();
             }
         }
     }
