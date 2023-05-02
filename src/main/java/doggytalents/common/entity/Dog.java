8b7f9fb04a70dbc8baf577e965a01f82193b3664
DashieDev <lamtinh.asian@gmail.com>
2023-05-02 14:28:03 +0700

    rename shouldSkipAttackFrom to its correct func

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1434,7 +1434,7 @@ public class Dog extends AbstractDog {
         }
 
         for (IDogAlteration alter : this.alterations) {
-            InteractionResult result = alter.hitByEntity(this, entityIn);
+            InteractionResult result = alter.shouldSkipAttackFrom(this, entityIn);
 
             if (result.shouldSwing()) {
                 return true;
