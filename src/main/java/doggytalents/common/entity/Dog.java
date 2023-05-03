8368e853d96d8d9e4f6b8893a9e30c932cab7288
DashieDev <lamtinh.asian@gmail.com>
2023-05-04 00:55:19 +0700

    null check at Dog::doHurtTarget
    not neccessary but it is safe, also the upper part is already being
    null check because those
    make the editor gone crazy ....

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1285,7 +1285,7 @@ public class Dog extends AbstractDog {
         }
 
 
-        if (critModifiers != null) {
+        if (critModifiers != null && attackDamageInst != null) {
             critModifiers.forEach(attackDamageInst::removeModifier);
         }
 
