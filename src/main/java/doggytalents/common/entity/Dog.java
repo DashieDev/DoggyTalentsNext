f5fb355a72143fec48aef0630bda23e54a0a3be1
DashieDev <lamtinh.asian@gmail.com>
2023-05-16 01:54:04 +0700

    remove/adjust debug

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1837,7 +1837,6 @@ public class Dog extends AbstractDog {
         //Never save these entry, these will be loaded by the talents itself.
         compound.remove("HandItems");
         compound.remove("ArmorItems");
-        ChopinLogger.l("added additional data for : " + this);
     }
 
     @Override
