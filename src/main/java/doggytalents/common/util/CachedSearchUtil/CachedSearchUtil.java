2a19e908b91d540be81c461e658c6245f445daf2
DashieDev <lamtinh.asian@gmail.com>
2023-05-15 20:49:19 +0700

    Added batch dynamic teleport functionality
    Teleport a list of dog around a blockpos and only get safe pos once.

--- src/main/java/doggytalents/common/util/CachedSearchUtil/CachedSearchUtil.java
+++ src/main/java/doggytalents/common/util/CachedSearchUtil/CachedSearchUtil.java
@@ -52,6 +52,23 @@ public class CachedSearchUtil {
         }
     }
 
+    public static void populatePoolRaw(Level level, List<Dog> dogs, BlockPos targetPos, int radiusXZ, int radiusY) {
+        var b0 = targetPos;
+        var bMin = b0.offset(-radiusXZ, -radiusY, -radiusXZ);
+        int maxXZ = radiusXZ * 2;
+        int maxY = radiusY * 2;
+        for (int i = 0; i <= maxXZ; ++i) {
+            for (int j = 0; j <= maxY; ++j) {
+                for (int k = 0; k <= maxXZ; ++k) {
+                    var type = WalkNodeEvaluatorDelegate
+                        .getTypeDelegate(level, bMin.offset(i, j, k));
+                    byte val = inferType(dogs, type);
+                    CachedSearchPool.setPoolValue(level, i, j, k, val);
+                }
+            }
+        }
+    }
+
     public static void populateCollideOwner(LivingEntity owner, int radiusXZ, int radiusY) {
         final var DISTANCE_AWAY = 1.5;
 
@@ -109,6 +126,35 @@ public class CachedSearchUtil {
                                         && CachedSearchPool.getPoolValue(dog.level, i1, j1, k1) != ERR
                                     )
                                     CachedSearchPool.setPoolValue(dog.level, i1, j1, k1, COLLIDE);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void populateBlockCollision(Level level, List<Dog> dogs, int radiusXZ, int radiusY) {
+
+        var dog = DogUtil.findBiggestDog(dogs);
+
+        int bbWExt = Mth.ceil((dog.getBbWidth() - 1)*0.5);
+        int bbHExt = Mth.ceil((dog.getBbHeight() - 1)*0.5);
+        if (bbWExt <= 0 && bbHExt <= 0) return;
+        int maxXZ = radiusXZ * 2;
+        int maxY = radiusY * 2;
+        for (int i = 0; i <= maxXZ; ++i) {
+            for (int j = 0; j <= maxY; ++j) {
+                for (int k = 0; k <= maxXZ; ++k) {
+                    if (CachedSearchPool.getPoolValue(level, i, j, k) == BLOCKED) {
+                        for (int i1 = i-bbWExt; i1 <= i+bbWExt; ++i1 ) {
+                            for (int j1 = j-bbHExt; j1 <= j; ++j1) {
+                                for (int k1 = k-bbWExt; k1 <= k+bbWExt; ++k1) {
+                                    if (CachedSearchPool.getPoolValue(level, i1, j1, k1) != BLOCKED
+                                        && CachedSearchPool.getPoolValue(level, i1, j1, k1) != ERR
+                                    )
+                                    CachedSearchPool.setPoolValue(level, i1, j1, k1, COLLIDE);
                                 }
                             }
                         }
@@ -181,6 +227,17 @@ public class CachedSearchUtil {
         populateBlockCollision(dog, radiusXZ, radiusY);
         populateDangerPos(dog.level, radiusXZ, radiusY);
         populateWalkablePos(dog.level, radiusXZ, radiusY);
+    }
+
+    public static void populatePool(Level level, List<Dog> dogs, BlockPos targetPos, int radiusXZ, int radiusY) {
+        resetPool(level, radiusXZ, radiusY);
+        // long startTime1 = System.nanoTime();
+        populatePoolRaw(level, dogs, targetPos, radiusXZ, radiusY);
+        // long stopTime1 = System.nanoTime();
+        //     ChopinLogger.l("populate collision in : " + (stopTime1-startTime1) + " nanoseconds");
+        populateBlockCollision(level, dogs, radiusXZ, radiusY);
+        populateDangerPos(level, radiusXZ, radiusY);
+        populateWalkablePos(level, radiusXZ, radiusY);
     }
 
     public static List<BlockPos> collectSafePos(Level level, BlockPos targetPos, int radiusXZ, int radiusY) {
@@ -271,6 +328,35 @@ public class CachedSearchUtil {
         return safePosList;
     }
 
+    public static List<BlockPos> getAllSafePosUsingPool(Level level, List<Dog> dogs, BlockPos targetPos, int realRadiusXZ, int realRadiusY) {
+        // ChopinLogger.l("Begining search: unit: nanoseconds");
+        // long startTime = System.nanoTime();
+        int poolXZ = realRadiusXZ+1;
+        int poolY = realRadiusY+1;
+        if (poolXZ > CachedSearchPool.MAX_RADIUS_XZ || poolXZ < 0) return null;
+        if (poolY > CachedSearchPool.MAX_RADIUS_Y || poolY < 0) return null;
+        populatePool(level, dogs, targetPos, poolXZ, poolY);
+        
+        var safePosList = collectSafePos(level, targetPos, poolXZ, poolY);
+
+        return safePosList;
+    }
+
+    public static List<BlockPos> getAllSafePosUsingPoolExcludeInfrontOfOwner(Level level, List<Dog> dogs, LivingEntity owner, BlockPos targetPos, int realRadiusXZ, int realRadiusY) {
+        // ChopinLogger.l("Begining search: unit: nanoseconds");
+        // long startTime = System.nanoTime();
+        int poolXZ = realRadiusXZ+1;
+        int poolY = realRadiusY+1;
+        if (poolXZ > CachedSearchPool.MAX_RADIUS_XZ || poolXZ < 0) return null;
+        if (poolY > CachedSearchPool.MAX_RADIUS_Y || poolY < 0) return null;
+        populatePool(level, dogs, targetPos, poolXZ, poolY);
+        populateCollideOwner(owner, poolXZ, poolY);
+        
+        var safePosList = collectSafePos(level, targetPos, poolXZ, poolY);
+
+        return safePosList;
+    }
+
     public static String dumpPool(Level level, int radiusXZ, int radiusY) {
         int maxXZ = radiusXZ * 2;
         int maxY = radiusY * 2;
@@ -309,6 +395,33 @@ public class CachedSearchUtil {
         if (type == BlockPathTypes.OPEN) return OPEN;
         if (type.getDanger() != null) return DAMAGE;
         if (dog.getPathfindingMalus(type) < 0) return BLOCKED;
+        return OPEN;
+    }
+
+    public static byte inferType(List<Dog> dogs, BlockPathTypes type) {
+
+        //If the pos is OK for a dogs via talent, then it must be
+        //OK for all the other dogs to be considered OK here. 
+        boolean all_dog_OK = true;
+        for (var dog : dogs) {
+            boolean is_ok = false;
+            for (var x : dog.getAlterations()) {
+                if (x.isBlockTypeWalkable(dog, type).shouldSwing()) {
+                    is_ok = true;
+                    break;
+                }
+            }
+            if (!is_ok) {
+                all_dog_OK = false; break;
+            }
+        }
+
+        if (all_dog_OK) return OK;
+        if (type == BlockPathTypes.OPEN) return OPEN;
+        if (type.getDanger() != null) return DAMAGE;
+        for (var dog : dogs) {
+            if (dog.getPathfindingMalus(type) < 0) return BLOCKED;
+        }
         return OPEN;
     }
 
