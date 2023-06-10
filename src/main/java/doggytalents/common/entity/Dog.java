202d3fad9bbb1d0fcae43a4487dfb8ced509a65f
DashieDev <lamtinh.asian@gmail.com>
2023-06-10 15:53:09 +0700

    trigger action now return wheter succeed or not.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -618,9 +618,9 @@ public class Dog extends AbstractDog {
         return this.activeAction;
     }
 
-    public void triggerAction(TriggerableAction action) {
+    public boolean triggerAction(TriggerableAction action) {
         if (this.activeAction == action) {
-            return;
+            return false;
         }
         //Trigger Action cancel.
         //If dog have stashed action then will push the action back instead.
@@ -631,18 +631,18 @@ public class Dog extends AbstractDog {
                 this.activeAction = this.stashedAction;
                 this.stashedAction = null;
             }
-            return;
+            return false;
         }
         //Replacement only happens if 
         //old action is Trivial and new action is not.
         if (this.activeAction != null) {
-            if (!this.activeAction.isTrivial()) return;
-            else if (action.isTrivial()) return;
+            if (!this.activeAction.isTrivial()) return false;
+            else if (action.isTrivial()) return false;
         }
         //Only set action dog is not sitting or action can override sit.
         if (this.isOrderedToSit()) {
-            if (this.forceSit()) return;
-            if (!action.canOverrideSit()) return;
+            if (this.forceSit()) return false;
+            if (!action.canOverrideSit()) return false;
         }
         this.setOrderedToSit(false);
         //Check And Stash existing action.
@@ -659,6 +659,7 @@ public class Dog extends AbstractDog {
         }
         //Set.
         this.activeAction = action;
+        return true;
     }
 
     public boolean isBusy() {
