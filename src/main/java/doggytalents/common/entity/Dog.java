652efd414a41d4f387119617e454d15a785f30b3
DashieDev <lamtinh.asian@gmail.com>
2023-06-11 06:32:19 +0700

    remove hunger movement modifier when icap

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1653,6 +1653,9 @@ public class Dog extends AbstractDog {
 
         var owner = this.getOwner();
         if (owner != null) sendIncapacitatedMsg(owner, source);
+    
+        //TODO Maybe need to find somewhere more appropriate to do this.
+        this.removeAttributeModifier(Attributes.MOVEMENT_SPEED, HUNGER_MOVEMENT);
     }
 
     private void sendIncapacitatedMsg(LivingEntity owner, DamageSource source) {
@@ -3075,6 +3078,7 @@ public class Dog extends AbstractDog {
     }
 
     private void hungerHighToLow() {
+        if (!this.isDefeated())
         this.setAttributeModifier(Attributes.MOVEMENT_SPEED, HUNGER_MOVEMENT,
                 (d, u) -> new AttributeModifier(u, "Hunger Slowness", -0.35f, Operation.MULTIPLY_TOTAL) // this
                                                                                                         // operation is
