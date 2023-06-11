60c53db9077a732fda5cab0aea7d09b656bd319a
DashieDev <lamtinh.asian@gmail.com>
2023-06-11 18:56:16 +0700

    Deprecated code migration.

--- src/main/java/doggytalents/common/item/TreatBagItem.java
+++ src/main/java/doggytalents/common/item/TreatBagItem.java
@@ -25,10 +25,10 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.wrapper.EmptyHandler;
@@ -68,7 +68,7 @@ public class TreatBagItem extends Item implements IDogFoodHandler {
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
         super.appendHoverText(stack, worldIn, tooltip, flagIn);
 
-        IItemHandler bagInventory = stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).orElse(EmptyHandler.INSTANCE);
+        IItemHandler bagInventory = stack.getCapability(ForgeCapabilities.ITEM_HANDLER).orElse(EmptyHandler.INSTANCE);
         List<ItemStack> condensedContents = ItemUtil.getContentOverview(bagInventory);
 
         condensedContents.forEach((food) -> {
@@ -79,7 +79,7 @@ public class TreatBagItem extends Item implements IDogFoodHandler {
     @Override
     public ICapabilityProvider initCapabilities(final ItemStack stack, CompoundTag nbt) {
         // https://github.com/MinecraftForge/MinecraftForge/issues/5989
-        if (CapabilityItemHandler.ITEM_HANDLER_CAPABILITY == null) {
+        if (ForgeCapabilities.ITEM_HANDLER == null) {
             return null;
         }
 
@@ -89,7 +89,7 @@ public class TreatBagItem extends Item implements IDogFoodHandler {
             @Override
             @Nonnull
             public <T> LazyOptional<T> getCapability(@Nonnull final Capability<T> cap, final @Nullable Direction side) {
-                if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+                if (cap == ForgeCapabilities.ITEM_HANDLER) {
                     return (LazyOptional<T>) this.itemHandlerInstance;
                 }
                 return LazyOptional.empty();
@@ -115,7 +115,7 @@ public class TreatBagItem extends Item implements IDogFoodHandler {
 
     @Override
     public InteractionResult consume(AbstractDog dogIn, ItemStack stackIn, Entity entityIn) {
-        IItemHandlerModifiable treatBag = (IItemHandlerModifiable) stackIn.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).orElse(EmptyHandler.INSTANCE);
+        IItemHandlerModifiable treatBag = (IItemHandlerModifiable) stackIn.getCapability(ForgeCapabilities.ITEM_HANDLER).orElse(EmptyHandler.INSTANCE);
         return InventoryUtil.feedDogFrom(dogIn, entityIn, treatBag);
     }
 }
