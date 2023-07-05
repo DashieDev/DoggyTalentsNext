0a6d757cbc63c4e9d0af7a98dac0b85d29cda516
DashieDev <lamtinh.asian@gmail.com>
2023-07-05 18:35:25 +0700

    Add author/basedOn/Pack(WIP) information to Skins
    Pack is WIP

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/DogSkinElement.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/DogSkinElement.java
@@ -18,13 +18,17 @@ import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 
 public class DogSkinElement extends AbstractElement {
+
+    private static int LINE_SPACING = 2;
 
     Dog dog;
     List<DogSkin> locList;
     int activeSkinId;
     Font font;
+    boolean showInfo;
 
     public DogSkinElement(AbstractElement parent, Screen screen, Dog dog, List<DogSkin> locList) {
         super(parent, screen);
@@ -39,6 +43,10 @@ public class DogSkinElement extends AbstractElement {
             Store.get(getScreen()).getStateOrDefault(
                 ActiveSkinSlice.class, ActiveSkinSlice.class, 
                 new ActiveSkinSlice()).activeSkinId;
+        this.showInfo = 
+            Store.get(getScreen()).getStateOrDefault(
+                ActiveSkinSlice.class, ActiveSkinSlice.class, 
+                new ActiveSkinSlice()).showInfo;
             
         return this;
     }
@@ -49,12 +57,22 @@ public class DogSkinElement extends AbstractElement {
         if (this.locList == null) return;
         if (this.locList.isEmpty()) return;
 
+        
+
+        if (activeSkinId >= this.locList.size()) return;
+
+        if (this.showInfo)
+            renderShowInfo(stack, mouseX, mouseY, partialTicks);
+        else
+            renderNonShowInfo(stack, mouseX, mouseY, partialTicks);
+         
+    }
+
+    private void renderNonShowInfo(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
         int mX = this.getSizeX()/2;
         int mY = this.getSizeY()/2;
         int e_mX = this.getRealX() + mX;
         int e_mY = this.getRealY() + mY - 10;
-
-        if (activeSkinId >= this.locList.size()) return;
 
         {
             var c1 = Component.literal(locList.get(activeSkinId).getName());
@@ -80,9 +98,77 @@ public class DogSkinElement extends AbstractElement {
             this.renderSkinAndDogModel(prevId, false, stack, 
                 mouseX, mouseY, e_mX - 32 - 25 - 25, e_mY + 32, 50);
         }
+    }
+
+    private void renderShowInfo(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
+        int mX = this.getSizeX()/2;
+        int mY = this.getSizeY()/2;
+        int e_mX = this.getRealX() + mX;
+        int e_mY = this.getRealY() + mY - 10;
+
+        this.renderSkinAndDogModel(activeSkinId, true, stack, 
+            mouseX, mouseY, this.getRealX() + 70, e_mY + 36, 64);
+
+        var manifestSkin = this.locList.get(activeSkinId);
+    
+        if (!manifestSkin.hasExtraInfo()) {
+            renderNoInfo(stack, mouseX, mouseY, partialTicks);
+            return;
+        }
+        
+        //MockData
+        Component basedOn = Component.literal(manifestSkin.getBasedOn());
+        Component authors = Component.literal(manifestSkin.getAuthor());
+        //END
+
+        var split_basedOn = this.font.split(basedOn, 140);
+        var split_authors = this.font.split(authors, 140);
+
+        int lines = 6 + split_authors.size() + split_basedOn.size();
+        int size = Mth.floor(lines * font.lineHeight + (lines - 1) * LINE_SPACING);
+
+        int tX = this.getRealX() + mX - 20;
+        int tY = this.getRealY() + mY - size/2;
+
+        font.draw(stack, "Name: ", tX, tY, 0xffffffff);
+        tY += font.lineHeight + LINE_SPACING;
+        font.draw(stack, manifestSkin.getName(), tX, tY, 0xffffffff);
+        tY += font.lineHeight + LINE_SPACING;
 
 
-         
+        tY += font.lineHeight + LINE_SPACING;
+        
+        font.draw(stack, "Based On: ", tX, tY, 0xffffffff);
+        tY += font.lineHeight + LINE_SPACING;
+        
+        for (var c : split_basedOn) {
+            font.draw(stack, c, tX, tY, 0xffffffff);
+            tY += font.lineHeight + LINE_SPACING;
+        }
+
+        
+        tY += font.lineHeight + LINE_SPACING;
+
+        font.draw(stack, "Author: ", tX, tY, 0xffffffff);
+        tY += font.lineHeight + LINE_SPACING;
+        
+        for (var c : split_authors) {
+            font.draw(stack, c, tX, tY, 0xffffffff);
+            tY += font.lineHeight + LINE_SPACING;
+        }
+        
+    }
+
+    private void renderNoInfo(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
+        int mX = this.getSizeX()/2;
+        int mY = this.getSizeY()/2;
+
+        var c1 = Component.literal("No info to show.");
+
+        int tX = this.getRealX() + mX - 10;
+        int tY = this.getRealY() + mY - font.lineHeight/2;
+
+        font.draw(stack, c1, tX, tY, 0xffffffff);
     }
 
     private void renderSkinAndDogModel(int indx, boolean followMouse, PoseStack stack, int mouseX, 
--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/SkinButtonElement.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/SkinButtonElement.java
@@ -28,8 +28,10 @@ public class SkinButtonElement extends AbstractElement {
     Dog dog;
     Font font;
     List<DogSkin> locList;
+    Button showHideInfoButton;
     Button applyButton;
     int activeSkinId;
+    boolean showInfo;
 
     public SkinButtonElement(AbstractElement parent, Screen screen, Dog dog, List<DogSkin> locList) {
         super(parent, screen);
@@ -46,6 +48,10 @@ public class SkinButtonElement extends AbstractElement {
             .getStateOrDefault(ActiveSkinSlice.class,
             ActiveSkinSlice.class, new ActiveSkinSlice())
             .activeSkinId;
+        showInfo = 
+            Store.get(getScreen()).getStateOrDefault(
+                ActiveSkinSlice.class, ActiveSkinSlice.class, 
+                new ActiveSkinSlice()).showInfo;
         
         
         int mX = this.getSizeX()/2;
@@ -82,6 +88,21 @@ public class SkinButtonElement extends AbstractElement {
             }, this.font);
             nextSkinButton.active = activeSkinId < this.locList.size() - 1;
 
+        showHideInfoButton = applyButton = new Button(
+            this.getRealX() + this.getSizeX() - 30 - 30 - 62,
+            this.getRealY() + mY - 10, 58, 20,
+            Component.literal(!this.showInfo ? "Show Info" : "Hide Info"),
+            b -> {
+                Store.get(getScreen()).dispatch(ActiveSkinSlice.class, 
+                    new UIAction(
+                        !this.showInfo ?
+                        UIActionTypes.Skins.SHOW_INFO : UIActionTypes.Skins.HIDE_INFO,
+                        new ActiveSkinSlice()
+                    ) 
+                );
+            }  
+        );
+
         applyButton = new Button(
             this.getRealX() + this.getSizeX() - 30 - 30,
             this.getRealY() + mY - 10, 40, 20,
@@ -101,6 +122,7 @@ public class SkinButtonElement extends AbstractElement {
         this.addChildren(prevSkinButton);
         this.addChildren(nextSkinButton);
         this.addChildren(applyButton);
+        this.addChildren(showHideInfoButton);
         
         return this;
     }
