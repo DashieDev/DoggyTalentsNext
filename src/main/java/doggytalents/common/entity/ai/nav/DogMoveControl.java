db871ecccf03cac924cd6cb6f63fb28ce97a6fd4
DashieDev <lamtinh.asian@gmail.com>
2023-07-06 00:42:21 +0700

    Imrpove Dog Navigation
    + MoveControl : Dogs now navigate with standard speed if
    target y is lower than -0.75 and slower if lower than -1.75
    
    + PathNavigation : Properly center the target point, Minecraft did some
    weird stuff here which cause the nextNode target center
    to be offset by a fixed direction with the length depends on
    the size of bbWidth. Properly cut corner, by defeault there
    is corner cutting even when there is an questionable node at the
    path after cutting corner.

---
+++ src/main/java/doggytalents/common/entity/ai/nav/DogMoveControl.java
@@ -0,0 +1,119 @@
+package doggytalents.common.entity.ai.nav;
+
+import doggytalents.ChopinLogger;
+import doggytalents.common.entity.Dog;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.control.MoveControl;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class DogMoveControl extends MoveControl {
+
+    private Dog dog;
+
+    public DogMoveControl(Dog dog) {
+        super(dog);
+        this.dog = dog;
+    }
+
+    @Override
+    public void tick() {
+        double dy = 9;
+        if (this.hasWanted()) {
+            dy = Math.abs(this.getWantedY() - this.dog.getY());
+        }
+        if (
+            this.operation == MoveControl.Operation.MOVE_TO
+            && dy > 0.75
+        ) {
+            final float SNEAK_SPEED_1 = 0.275f;
+            final float SNEAK_SPEED_2 = 0.25f;
+
+            this.operation = MoveControl.Operation.WAIT;
+            double d0 = this.wantedX - this.mob.getX();
+            double d1 = this.wantedZ - this.mob.getZ();
+            double d2 = this.wantedY - this.mob.getY();
+            double d3 = d0 * d0 + d2 * d2 + d1 * d1;
+            if (d3 < (double)2.5000003E-7F) {
+                this.mob.setZza(0.0F);
+                return;
+            }
+
+            float f9 = (float)(Mth.atan2(d1, d0) * (double)(180F / (float)Math.PI)) - 90.0F;
+            this.mob.setYRot(this.rotlerp(this.mob.getYRot(), f9, 90.0F));
+            float speed = dy > 1.75 ? SNEAK_SPEED_2 : SNEAK_SPEED_1;
+            this.mob.setSpeed(speed);
+            BlockPos blockpos = this.mob.blockPosition();
+            BlockState blockstate = this.mob.level.getBlockState(blockpos);
+            VoxelShape voxelshape = blockstate.getCollisionShape(this.mob.level, blockpos);
+            if (d2 > (double)this.mob.getStepHeight() && d0 * d0 + d1 * d1 < (double)Math.max(1.0F, this.mob.getBbWidth()) || !voxelshape.isEmpty() && this.mob.getY() < voxelshape.max(Direction.Axis.Y) + (double)blockpos.getY() && !blockstate.is(BlockTags.DOORS) && !blockstate.is(BlockTags.FENCES)) {
+                this.mob.getJumpControl().jump();
+                this.operation = MoveControl.Operation.JUMPING;
+            }
+            return;
+        }
+        super.tick();
+    }
+
+    // private boolean isMovingDiagonallyDownward() {
+    //     var dog_b0 = this.dog.blockPosition();
+    //     var target_b0 = new BlockPos(this.getWantedX(), this.getWantedY(), this.getWantedZ());
+    //     if (Math.abs(dog_b0.getY() - target_b0.getY()) < 1) return false;
+    //     var dx = Math.abs(dog_b0.getX() - target_b0.getX());
+    //     var dz = Math.abs(dog_b0.getZ() - target_b0.getZ());
+    //     if (dx != 1) return false;
+    //     if (dz != 1) return false;
+    //     return true;
+    // }
+
+    // private boolean riskyDiagonallyDownward() {
+    //     double v_dog_x = dog.position().x;
+    //     double v_dog_z = dog.position().z;
+
+    //     double v_target_x = this.getWantedX();
+    //     double v_target_z = this.getWantedZ();
+
+    //     double v_dog_target_x = v_target_x - v_dog_x;
+    //     double v_dog_target_z = v_target_z - v_dog_z;
+    //     double coeff_z_x_v_dog_target = v_dog_target_z / v_dog_target_x;
+
+    //     //intersect the first x = n line (n are integers)
+    //     double p_intersect_x = 
+    //         Mth.floor(v_dog_x) + (v_dog_target_x > 0 ? 1 : 0);
+    //     double p_intersect_z = 
+    //         v_dog_z + ((p_intersect_x - v_dog_x) * coeff_z_x_v_dog_target);
+        
+    //     //intersect point of the target blockPos and dog's pos
+    //     double p_node_intersect_z = 
+    //         Math.max(Mth.floor(v_dog_z),Mth.floor(v_target_z));
+
+    //     //sides of the triangle that result from the intersect of v_dog_target
+    //     //to the nearest x = n and y = n lines (n are integers)
+    //     double l_open_z = Math.abs(p_node_intersect_z - p_intersect_z);
+    //     double l_open_x = l_open_z / Math.abs(coeff_z_x_v_dog_target);
+
+    //     if (
+    //         l_open_z > dog.getBbWidth()/2
+    //         && l_open_x > dog.getBbWidth()/2
+    //     ) {
+    //         ChopinLogger.lwn(dog, "Dangerous : Went from "
+    //             + "[ " + v_dog_x  + " " + v_dog_z  + " ]" 
+    //             + "to"
+    //             + "[ " + v_target_x  + " " + v_target_z  + " ]"
+    //             + "open gap "
+    //             + "[ " + l_open_x  + " " + l_open_z  + " ]" 
+    //             );
+    //         return true;
+    //     }
+
+    //     return false;
+
+    // }
+
+
+    
+}
---
+++ src/main/java/doggytalents/common/entity/ai/nav/DogPathNavigation.java
@@ -0,0 +1,94 @@
+package doggytalents.common.entity.ai.nav;
+
+import doggytalents.ChopinLogger;
+import doggytalents.common.entity.Dog;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
+import net.minecraft.world.phys.Vec3;
+
+public class DogPathNavigation extends GroundPathNavigation {
+
+    private Dog dog;
+
+    public DogPathNavigation(Dog dog, Level level) {
+        super(dog, level);
+        this.dog = dog;
+    }
+
+    @Override
+    protected void followThePath() {
+        if (invalidateIfNextNodeIsTooHigh()) return;
+
+        var currentPos = this.getTempMobPos();
+        this.maxDistanceToWaypoint = 
+            this.mob.getBbWidth() > 0.75F ? 
+                this.mob.getBbWidth() / 2.0F 
+                : 0.75F - this.mob.getBbWidth() / 2.0F;
+
+        var nextPos = this.path.getNextNodePos();
+        double dx = Math.abs(this.mob.getX() - ((double)nextPos.getX() + 0.5));
+        double dy = Math.abs(this.mob.getY() - (double)nextPos.getY());
+        double dz = Math.abs(this.mob.getZ() - ((double)nextPos.getZ() + 0.5));
+
+        boolean isCloseEnough = 
+            dx <= (double)this.maxDistanceToWaypoint 
+            && dy < 1.0D
+            && dz <= (double)this.maxDistanceToWaypoint;
+        boolean canCutCorner = 
+            this.mob.canCutCorner(this.path.getNextNode().type) 
+            && this.shouldTargetNextNodeInDirection(currentPos);
+
+        if (isCloseEnough || canCutCorner) {
+            this.path.advance();
+        }
+  
+        this.doStuckDetection(currentPos);
+    }
+
+    protected boolean invalidateIfNextNodeIsTooHigh() {
+        var path = this.path;
+        if (path != null) {
+            var nextPos = path.getNextNodePos();
+            var dy = this.mob.getY() - (double)nextPos.getY();
+                    
+            if (dy < -1.75) {
+                this.stop();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean shouldTargetNextNodeInDirection(Vec3 current_pos) {
+        var path = this.path;
+        if (path == null) return false;
+        if (path.getNextNodeIndex() + 1 >= path.getNodeCount()) {
+           return false;
+        }
+        
+        var next_pos = Vec3.atBottomCenterOf(path.getNextNodePos());
+        if (!current_pos.closerThan(next_pos, 2.0D)) {
+            return false;
+        }
+
+        Vec3 next2th_node = Vec3.atBottomCenterOf(path.getNodePos(path.getNextNodeIndex() + 1));
+        Vec3 v_next_next2th = next2th_node.subtract(next_pos);
+        Vec3 v_next_current = current_pos.subtract(next_pos);
+        //small alpha
+        if (v_next_next2th.dot(v_next_current) <= 0.0D) return false;
+
+        Vec3 v_current_next2th = next2th_node.subtract(current_pos);
+        double v_current_next2th_lSqr = v_current_next2th.lengthSqr();
+        if (v_current_next2th_lSqr < 1) return true;
+        Vec3 v_add = v_current_next2th.normalize();
+        var check_b0 = new BlockPos(current_pos.add(v_add));
+        var type = WalkNodeEvaluator
+            .getBlockPathTypeStatic(level, check_b0.mutable());
+        return type == BlockPathTypes.WALKABLE;
+    }
+    
+}
