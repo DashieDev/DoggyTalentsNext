024ea3c0e8e62481c2aab9c14c7e37d484c68480
DashieDev <lamtinh.asian@gmail.com>
2023-07-09 22:07:32 +0700

    de-1.20 fix

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3091,7 +3091,7 @@ public class Dog extends AbstractDog {
             pushEachOther
             && otherEntity instanceof Dog dog
             && !dog.getNavigation().isDone()
-            && !dog.onGround()
+            && !dog.isOnGround()
         ) {
             ChopinLogger.l("Colliding with dog!");
             return false;
