5fd23eee5f16aa2aa006385f7dd5ed6795783bda
DashieDev <lamtinh.asian@gmail.com>
2023-07-18 22:11:15 +0700

    Feathered Mantle Artifact
    Solve Pillow Paw 5 annoying slow drop attack problem.
    
    Also remove Pillow 5 glide goal on debug side.

---
+++ src/main/java/doggytalents/common/artifacts/FeatheredMantleArtifact.java
@@ -0,0 +1,52 @@
+package doggytalents.common.artifacts;
+
+import java.util.UUID;
+
+import doggytalents.ChopinLogger;
+import doggytalents.DoggyItems;
+import doggytalents.api.inferface.AbstractDog;
+import doggytalents.common.entity.Dog;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraftforge.common.ForgeMod;
+
+public class FeatheredMantleArtifact extends DoggyArtifact {
+
+    private static final UUID PILLOW_PAW_BOOST_ID = UUID.fromString("1f002df0-9d35-49c6-a863-b8945caa4af4");
+
+    public FeatheredMantleArtifact() {
+        super(() -> DoggyItems.FEATHERED_MANTLE.get());
+    }
+
+    @Override
+    public void init(AbstractDog dogIn) {
+        startGliding(dogIn);
+        ChopinLogger.lwn((Dog) dogIn,"get glide!");
+    }
+
+    @Override
+    public void remove(AbstractDog dogIn) {
+        stopGliding(dogIn);
+        ChopinLogger.lwn((Dog) dogIn, "removed glide!");
+    }
+
+    @Override
+    public void tick(AbstractDog dogIn) {
+        if (dogIn.level.isClientSide) 
+            return;
+        dogIn.fallDistance = 0;
+    }
+
+    private void startGliding(AbstractDog dog) {
+        dog.setAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID, this::createSpeedModifier);
+        ChopinLogger.lwn(dog, "start gliding");
+    }
+
+    private void stopGliding(AbstractDog dog) {
+        dog.removeAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID);
+    }
+
+    public AttributeModifier createSpeedModifier(AbstractDog dogIn, UUID uuidIn) {
+        return new AttributeModifier(uuidIn, "Pillow Paw", -0.065D, AttributeModifier.Operation.ADDITION);
+    }
+    
+}
