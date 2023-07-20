cae9dc69fa8cbfd99fd2c0c1cc912776a7eb376c
DashieDev <lamtinh.asian@gmail.com>
2023-07-20 16:32:21 +0700

    PillowPawTalent sync

--- src/main/java/doggytalents/common/talent/PillowPawTalent.java
+++ src/main/java/doggytalents/common/talent/PillowPawTalent.java
@@ -1,16 +1,12 @@
 package doggytalents.common.talent;
 
-import doggytalents.ChopinLogger;
-import doggytalents.api.feature.DataKey;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.registry.Talent;
 import doggytalents.api.registry.TalentInstance;
-import doggytalents.common.entity.Dog;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ForgeMod;
 
@@ -20,25 +16,18 @@ public class PillowPawTalent extends TalentInstance {
 
     private static final UUID PILLOW_PAW_BOOST_ID = UUID.fromString("1f002df0-9d35-49c6-a863-b8945caa4af4");
 
-    private static DataKey<PillowPawGlideGoal> GLIDE_AI = DataKey.make();
-
     public PillowPawTalent(Talent talentIn, int levelIn) {
         super(talentIn, levelIn);
     }
 
     @Override
     public void init(AbstractDog dogIn) {
-        if (!(dogIn instanceof Dog)) return;
-        if (!dogIn.hasData(GLIDE_AI)) {
-            var glideGoal = new PillowPawGlideGoal((Dog)dogIn, this);
-            dogIn.goalSelector.addGoal(7, glideGoal);
-            dogIn.setData(GLIDE_AI, glideGoal);
-        }
+        dogIn.setAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID, this::createSpeedModifier);
     }
 
     @Override
     public void set(AbstractDog dogIn, int level) {
-        //dogIn.setAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID, this::createSpeedModifier);
+        dogIn.setAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID, this::createSpeedModifier);
     }
 
     public AttributeModifier createSpeedModifier(AbstractDog dogIn, UUID uuidIn) {
@@ -66,28 +55,5 @@ public class PillowPawTalent extends TalentInstance {
         }
 
         return InteractionResultHolder.pass(0F);
-    }
-
-    public static class PillowPawGlideGoal extends Goal{
-        
-        private Dog dog;
-        private PillowPawTalent talentInst;
-        
-        public PillowPawGlideGoal(Dog dog, PillowPawTalent talentInst) {
-            this.dog = dog;
-            this.talentInst = talentInst;
-        }
-
-        @Override
-        public boolean canUse() {
-            return this.dog.fallDistance > 3;
-        }
-
-        @Override
-        public boolean canContinueToUse() {
-            return this.dog.fallDistance > 3;
-        }
-
-
     }
 }
