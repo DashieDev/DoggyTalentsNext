582f54fe2a6d22042d9d324c6c67631c2324e771
DashieDev <lamtinh.asian@gmail.com>
2023-07-20 16:34:04 +0700

    Remove pillow paw glide side effect

--- src/main/java/doggytalents/common/talent/PillowPawTalent.java
+++ src/main/java/doggytalents/common/talent/PillowPawTalent.java
@@ -14,28 +14,8 @@ import java.util.UUID;
 
 public class PillowPawTalent extends TalentInstance {
 
-    private static final UUID PILLOW_PAW_BOOST_ID = UUID.fromString("1f002df0-9d35-49c6-a863-b8945caa4af4");
-
     public PillowPawTalent(Talent talentIn, int levelIn) {
         super(talentIn, levelIn);
-    }
-
-    @Override
-    public void init(AbstractDog dogIn) {
-        dogIn.setAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID, this::createSpeedModifier);
-    }
-
-    @Override
-    public void set(AbstractDog dogIn, int level) {
-        dogIn.setAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), PILLOW_PAW_BOOST_ID, this::createSpeedModifier);
-    }
-
-    public AttributeModifier createSpeedModifier(AbstractDog dogIn, UUID uuidIn) {
-        if (this.level() >= 5) {
-            return new AttributeModifier(uuidIn, "Pillow Paw", -0.065D, AttributeModifier.Operation.ADDITION);
-        }
-
-        return null;
     }
 
     @Override
