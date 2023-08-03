fb8db0a617e3fda372d6a590980a68c075a78070
DashieDev <lamtinh.asian@gmail.com>
2023-08-03 21:23:07 +0700

    1.20 disc tag fix

--- src/main/java/doggytalents/common/data/DTItemTagsProvider.java
+++ src/main/java/doggytalents/common/data/DTItemTagsProvider.java
@@ -38,6 +38,11 @@ public class DTItemTagsProvider extends ItemTagsProvider {
         createTag(DoggyTags.BREEDING_ITEMS, DoggyItems.BREEDING_BONE);
         createTag(DoggyTags.PACK_PUPPY_BLACKLIST, DoggyItems.THROW_BONE, DoggyItems.THROW_BONE_WET, DoggyItems.THROW_STICK, DoggyItems.THROW_STICK_WET);
         createTag(DoggyTags.TREATS, DoggyItems.TRAINING_TREAT, DoggyItems.SUPER_TREAT, DoggyItems.MASTER_TREAT, DoggyItems.DIRE_TREAT);
+        tag(ItemTags.MUSIC_DISCS).add(
+            DoggyItems.MUSIC_DISC_BWV_1080_FUGUE_11_KIMIKO.get(), 
+            DoggyItems.MUSIC_DISC_BWV_849_FUGUE_KIMIKO.get(),
+            DoggyItems.MUSIC_DISC_OKAMI_1.get()
+        );
     }
 
     @SafeVarargs
