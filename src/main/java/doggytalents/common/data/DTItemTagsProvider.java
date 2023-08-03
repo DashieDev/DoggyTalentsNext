56e632ce0bf3b3d4948642c2c9d0466cbd869efc
DashieDev <lamtinh.asian@gmail.com>
2023-08-04 02:49:41 +0700

    import?

--- src/main/java/doggytalents/common/data/DTItemTagsProvider.java
+++ src/main/java/doggytalents/common/data/DTItemTagsProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
