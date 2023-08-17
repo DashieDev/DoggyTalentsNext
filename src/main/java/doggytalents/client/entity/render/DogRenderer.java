5d8a06eb55a6161b23335111cabbd102651814e6
DashieDev <lamtinh.asian@gmail.com>
2023-08-17 09:58:30 +0700

    refactor

--- src/main/java/doggytalents/client/entity/render/DogRenderer.java
+++ src/main/java/doggytalents/client/entity/render/DogRenderer.java
@@ -101,7 +101,7 @@ public class DogRenderer extends MobRenderer<Dog, DogModel<Dog>> {
             this.model.setColor(f, f, f);
         }
 
-        if (modelNeedRefreshBeforeCurrentRender(dog)) {
+        if (this.model.modelNeedRefreshBeforeCurrentRender(dog)) {
             this.model.resetAllPose();
         }
 
@@ -114,25 +114,9 @@ public class DogRenderer extends MobRenderer<Dog, DogModel<Dog>> {
         if (this.model.hasAdditonalRendering())
             this.model.doAdditonalRendering(dog, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
 
-        if (modelNeedRefreshBeforeNextRender(dog)) {
+        if (this.model.modelNeedRefreshBeforeNextRender(dog)) {
             this.model.resetAllPose();
         }
-    }
-
-    private boolean modelNeedRefreshBeforeNextRender(Dog dog) {
-        if (dog.getAnim() != DogAnimation.NONE)
-            return true;
-        if (dog.getDogPose().needRenderRefresh)
-            return true;
-        return false;
-    }
-
-    private boolean modelNeedRefreshBeforeCurrentRender(Dog dog) {
-        if (dog.getAnim() != DogAnimation.NONE)
-            return true;
-        if (dog.getDogPose().needRenderRefresh)
-            return true;
-        return false;
     }
 
     private Component getNameUnknown(Dog dogIn) {
