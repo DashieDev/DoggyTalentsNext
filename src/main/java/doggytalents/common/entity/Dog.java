481679e77268f517b01a7b71c5f6d238d76b9316
DashieDev <lamtinh.asian@gmail.com>
2023-08-18 23:50:22 +0700

    If dog can move then dont faint.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3281,7 +3281,7 @@ public class Dog extends AbstractDog {
     }
 
     public void updateDogPose() {
-        if (this.isDefeated()) {
+        if (this.isDefeated() && !this.incapacitatedMananger.canMove()) {
             this.setDogPose(DogPose.FAINTED);
             return;
         }
