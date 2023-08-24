d9130ee50522710ba0b0b9fb13417a2638029ba9
DashieDev <lamtinh.asian@gmail.com>
2023-08-24 11:46:02 +0700

    Weaker jump when is defeated.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1793,6 +1793,12 @@ public class Dog extends AbstractDog {
     }
 
     @Override
+    public double getFluidJumpThreshold() {
+        float defeated_threshold = this.isInLava() ? 0.9f : 1;
+        return this.isDefeated() ? defeated_threshold : super.getFluidJumpThreshold();
+    }
+
+    @Override
     public void dropEquipment() {
         super.dropEquipment();
 
