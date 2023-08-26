d867964fb44f1b5cabe7fecc458e5ec56858098c
DashieDev <lamtinh.asian@gmail.com>
2023-08-26 18:22:19 +0700

    reset sit/standanim and stand quik as default.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3090,7 +3090,7 @@ public class Dog extends AbstractDog {
 
 
     private DogAnimation sitAnim = DogAnimation.SIT_DOWN;
-    private DogAnimation standAnim = DogAnimation.STAND_UP;
+    private DogAnimation standAnim = DogAnimation.STAND_QUICK;
 
     @Override
     public void setInSittingPose(boolean sit) {
@@ -3098,7 +3098,9 @@ public class Dog extends AbstractDog {
             boolean sit0 = this.isInSittingPose();
             if (sit0 != sit) {
                 this.setAnim(sit ? this.getSitAnim() : this.getStandAnim());
-            }   
+            }
+            this.sitAnim = DogAnimation.SIT_DOWN;
+            this.standAnim = DogAnimation.STAND_QUICK;
         }
         super.setInSittingPose(sit);
     }
