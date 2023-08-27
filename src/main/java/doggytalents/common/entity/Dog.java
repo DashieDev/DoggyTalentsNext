676f8d6e17704bf65f9cd790028af7edc6ecc7ed
DashieDev <lamtinh.asian@gmail.com>
2023-08-28 02:33:29 +0700

    immediately block flag when blockMove

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3356,8 +3356,10 @@ public class Dog extends AbstractDog {
         this.animAction = action;
         if (this.animAction != null) {
             this.animAction.onStart();
+            if (this.animAction.blockMove()) {
+                this.goalSelector.setControlFlag(Goal.Flag.MOVE, false);
+            }
         }
-            
     }
 
     protected void tickAnimAction() {
