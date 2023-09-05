88d2f6cebf3aff72e6452be146a40b9cb297be2a
DashieDev <lamtinh.asian@gmail.com>
2023-09-05 18:40:16 +0700

    Color to orb desc

--- src/main/java/doggytalents/common/item/LocatorOrbItem.java
+++ src/main/java/doggytalents/common/item/LocatorOrbItem.java
@@ -6,22 +6,29 @@ import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 import doggytalents.api.registry.Accessory;
+import doggytalents.common.entity.accessory.LocatorOrbAccessory;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class LocatorOrbItem extends AccessoryItem {
 
-    public LocatorOrbItem(Supplier<? extends Accessory> type, Properties properties) {
+    private Supplier<? extends LocatorOrbAccessory> orbType;
+
+    public LocatorOrbItem(Supplier<? extends LocatorOrbAccessory> type, Properties properties) {
         super(type, properties);
+        this.orbType = type;
     }
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
             TooltipFlag flags) {
         var desc_id = this.getDescriptionId(stack) + ".description";
-        components.add(Component.translatable(desc_id));
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withColor(orbType.get().getOrbColor())
+        ));
     }
     
 }
