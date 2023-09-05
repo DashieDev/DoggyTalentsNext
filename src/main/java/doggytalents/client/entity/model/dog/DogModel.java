ee00d55c3123b49c121335a9d71420d1b5a75ab2
DashieDev <lamtinh.asian@gmail.com>
2023-09-05 13:36:24 +0700

    allow some anim to static the tail

--- src/main/java/doggytalents/client/entity/model/dog/DogModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/DogModel.java
@@ -488,7 +488,7 @@ public class DogModel<T extends AbstractDog> extends EntityModel<T> {
     }
 
     public void resetPart(ModelPart part, Dog dog) {
-        if (part == this.tail) {
+        if (part == this.tail && dog.getAnim().freeTail()) {
             this.tail.resetPose();
             this.tail.xRot = dog.getTailRotation();
             return;
@@ -497,7 +497,7 @@ public class DogModel<T extends AbstractDog> extends EntityModel<T> {
     }
 
     public void adjustAnimatedPart(ModelPart part, Dog dog) {
-        if (part == this.tail) {
+        if (part == this.tail && dog.getAnim().freeTail()) {
             if (part.xRot > 3f) {
                 part.xRot = 3f;
             }
