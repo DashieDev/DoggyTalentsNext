7e4fb0900ee50d5d24f4ab3e121cbe2310d79d80
DashieDev <lamtinh.asian@gmail.com>
2023-09-06 22:18:40 +0700

    Starve incap state
    dont render anything only play anim

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1815,7 +1815,9 @@ public class Dog extends AbstractDog {
             type = DefeatedType.POISON;
         } else if (source.is(DamageTypes.DROWN))  {
             type = DefeatedType.DROWN;
-        } else {
+        } else if (source.is(DamageTypes.STARVE))  {
+            type = DefeatedType.STARVE;
+        }  else {
             type = DefeatedType.BLOOD;
         }
         
