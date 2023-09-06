2944cb4820c3ce5726967177e0c78d6d33c97104
DashieDev <lamtinh.asian@gmail.com>
2023-09-07 03:10:08 +0700

    Save and show cause of incap in incapScreen

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1776,6 +1776,9 @@ public class Dog extends AbstractDog {
 
         var owner = this.getOwner();
         if (owner != null) sendIncapacitatedMsg(owner, source);
+        this.incapacitatedMananger.setIncapMsg(
+            source.getLocalizedDeathMessage(this).getString()
+        );
     }
 
     private void sendIncapacitatedMsg(LivingEntity owner, DamageSource source) {
