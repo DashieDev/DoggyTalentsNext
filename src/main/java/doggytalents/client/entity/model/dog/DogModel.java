47a54446ef7a73620030f4fa2d212d173c98b36f
DashieDev <lamtinh.asian@gmail.com>
2023-09-08 14:46:19 +0700

    reset ear too

--- src/main/java/doggytalents/client/entity/model/dog/DogModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/DogModel.java
@@ -444,6 +444,8 @@ public class DogModel extends EntityModel<Dog> {
         this.root.getAllParts().forEach(x -> ((DogModelPart)x).resetPose());
         this.realHead.resetPose();
         this.realTail.resetPose();
+        this.earLeft.ifPresent(ear -> ear.resetPose());
+        this.earRight.ifPresent(ear -> ear.resetPose());
     }
 
     public void copyFrom(DogModel dogModel) {
