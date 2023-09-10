1205be757ae59acc72122011e56543d91773d391
DashieDev <lamtinh.asian@gmail.com>
2023-09-10 15:25:14 +0700

    Color and italize the dogbed tooltip.

--- src/api/java/doggytalents/api/impl/BeddingMaterial.java
+++ src/api/java/doggytalents/api/impl/BeddingMaterial.java
@@ -12,6 +12,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 
 public class BeddingMaterial extends IBeddingMaterial {
 
@@ -52,7 +53,10 @@ public class BeddingMaterial extends IBeddingMaterial {
         if (block == null) {
             return Component.empty();
         }
-        return Component.translatable(block.asItem().getDescriptionId());
+        return Component.translatable(block.asItem().getDescriptionId()).withStyle(
+            Style.EMPTY.withItalic(true)
+                .withColor(block.defaultMapColor().col)
+        );
     }
 
     /**
