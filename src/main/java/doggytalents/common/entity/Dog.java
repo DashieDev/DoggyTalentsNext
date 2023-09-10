49e67a6532cc820100ce49ae2865cd98c6b21f68
DashieDev <lamtinh.asian@gmail.com>
2023-09-11 02:49:36 +0700

    fix talent sync for real this time
    While not affecting the talent object life cycle.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2712,12 +2712,13 @@ public class Dog extends AbstractDog {
             inst.set(this, previousLevel);
 
             if (level <= 0) {
-                //Safely remove the talents.
-                final int isnt2_id = selected_id;
                 inst.remove(this);
-                this.modifyTalent(x -> {
-                    if (isnt2_id >= 0) x.remove(isnt2_id);
+                final int remove_id = selected_id;
+                modifyTalent(x -> {
+                    if (remove_id >= 0) x.remove(remove_id);
                 });
+            } else {
+                forceSyncTalents();
             }
         }
         return InteractionResult.SUCCESS;
@@ -2752,6 +2753,12 @@ public class Dog extends AbstractDog {
             artifacts.remove(indx);
         });
         return new ItemStack(removedArtifact);
+    }
+
+    public void forceSyncTalents() {
+        var copy_list = new ArrayList<>(this.getTalentMap());
+        this.getTalentMap().clear();
+        this.modifyTalent(x -> x.addAll(copy_list));
     }
 
     public void modifyTalent(Consumer<List<TalentInstance>> modify) {
