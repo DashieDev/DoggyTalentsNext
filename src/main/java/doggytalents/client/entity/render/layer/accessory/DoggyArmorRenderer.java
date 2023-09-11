58614ca43deb0f267abcfb48731357c4985d980f
DashieDev <lamtinh.asian@gmail.com>
2023-09-11 23:42:39 +0700

    Completely redo Dog Armor
    also added ass cover and modify legging cubes.

--- src/main/java/doggytalents/client/entity/render/layer/accessory/DoggyArmorRenderer.java
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/DoggyArmorRenderer.java
@@ -31,11 +31,11 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
 
-public class ArmorAccessoryRenderer extends RenderLayer<Dog, DogModel> {
+public class DoggyArmorRenderer extends RenderLayer<Dog, DogModel> {
 
     private DogArmorModel model;
 
-    public ArmorAccessoryRenderer(RenderLayerParent parentRenderer, EntityRendererProvider.Context ctx) {
+    public DoggyArmorRenderer(RenderLayerParent parentRenderer, EntityRendererProvider.Context ctx) {
         super(parentRenderer);
         this.model = new DogArmorModel(ctx.bakeLayer(ClientSetup.DOG_ARMOR));
     }
@@ -53,90 +53,33 @@ public class ArmorAccessoryRenderer extends RenderLayer<Dog, DogModel> {
         Optional<TalentInstance> inst = dog.getTalent(DoggyTalents.DOGGY_ARMOR);
         if (!inst.isPresent()) return;
 
-        if (this.model.modelNeedRefreshBeforeCurrentRender(dog)) {
-            this.model.resetAllPose();
-        }
-
-        this.getParentModel().copyPropertiesTo(this.model);
-        this.model.copyFrom(getParentModel());
-
-        this.model.setVisible(false);
+        var parentModel = this.getParentModel();
+        parentModel.copyPropertiesTo(this.model);
+        this.model.sync(parentModel);
 
         if (dog.getItemBySlot(EquipmentSlot.HEAD).getItem() instanceof ArmorItem) {
             var itemStack = dog.getItemBySlot(EquipmentSlot.HEAD);
-            this.model.head.visible = true;
+            this.model.setHelmet();
             this.renderArmorCutout(this.model, HelmetInteractHandler.getMappedResource(itemStack.getItem()).get().getModelTexture(), poseStack, buffer, packedLight, dog, 1.0F, 1.0F, 1.0F, itemStack.isEnchanted());
         }
-
-        this.model.setVisible(false);
 
         if (dog.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ArmorItem) {
             var itemStack = dog.getItemBySlot(EquipmentSlot.CHEST);
-            this.model.body.visible = true;
-            this.model.mane.visible = true;
-            this.model.tail.visible = true;
+            this.model.setChestplate();
             this.renderArmorCutout(this.model, HelmetInteractHandler.getMappedResource(itemStack.getItem()).get().getModelTexture(), poseStack, buffer, packedLight, dog, 1.0F, 1.0F, 1.0F, itemStack.isEnchanted());
         }
-
-        this.model.setVisible(false);
 
         if (dog.getItemBySlot(EquipmentSlot.LEGS).getItem() instanceof ArmorItem) {
             var itemStack = dog.getItemBySlot(EquipmentSlot.LEGS);
-            this.model.leggingFrontLeft.visible = true;
-            this.model.leggingFrontRight.visible = true;
-            this.model.leggingHindLeft.visible = true;
-            this.model.leggingHindRight.visible = true;
+            this.model.setLeggings();
             this.renderArmorCutout(this.model, HelmetInteractHandler.getMappedResource(itemStack.getItem()).get().getModelTexture(), poseStack, buffer, packedLight, dog, 1.0F, 1.0F, 1.0F, itemStack.isEnchanted());
         }
 
-        this.model.setVisible(false);
-
-        //TODO : Currently somehow boots which are not enchanted are rendered with flash
-        // If any other pieces is enchanted, but if the boot itself is enchanted, it
-        // get rendered with another flash layer making it brighter than usual :v
         if (dog.getItemBySlot(EquipmentSlot.FEET).getItem() instanceof ArmorItem) {
             var itemStack = dog.getItemBySlot(EquipmentSlot.FEET);
-            this.model.bootFrontLeft.visible = true;
-            this.model.bootFrontRight.visible = true;
-            this.model.bootHindLeft.visible = true;
-            this.model.bootHindRight.visible = true;
+            this.model.setBoot();
             this.renderArmorCutout(this.model, HelmetInteractHandler.getMappedResource(itemStack.getItem()).get().getModelTexture(), poseStack, buffer, packedLight, dog, 1.0F, 1.0F, 1.0F, itemStack.isEnchanted());
         }
-        if (this.model.modelNeedRefreshBeforeNextRender(dog)) {
-            this.model.resetAllPose();
-        }
-
-        // for (AccessoryInstance accessoryInst : dog.getAccessories()) {
-        //     if (accessoryInst instanceof ArmourAccessory.Instance armorInst) {
-
-        //         this.model.setVisible(false);
-
-        //         if (accessoryInst.ofType(DoggyAccessoryTypes.FEET)) {
-        //             this.model.legBackLeft.visible = true;
-        //             this.model.legBackRight.visible = true;
-        //             this.model.legFrontLeft.visible = true;
-        //             this.model.legFrontRight.visible = true;
-        //         } else if (accessoryInst.ofType(DoggyAccessoryTypes.HEAD)) {
-        //             
-        //         } else if (accessoryInst.ofType(DoggyAccessoryTypes.CLOTHING)) {
-        //             this.model.body.visible = true;
-        //             this.model.mane.visible = true;
-        //         } else if (accessoryInst.ofType(DoggyAccessoryTypes.TAIL)) {
-        //             this.model.tail.visible = true;
-        //         }
-
-        //         if (accessoryInst instanceof IColoredObject) {
-        //             float[] color = ((IColoredObject) armorInst).getColor();
-        //             this.renderArmorCutout(this.model, armorInst.getModelTexture(dog), poseStack, buffer, packedLight, dog, color[0], color[1], color[2], armorInst.hasEffect());
-        //         } else {
-        //             this.renderArmorCutout(this.model, armorInst.getModelTexture(dog), poseStack, buffer, packedLight, dog, 1.0F, 1.0F, 1.0F, armorInst.hasEffect());
-        //         }
-        //     }
-        // }
-    }
-
-    public void renderArmorPiece(PoseStack p_117119_, MultiBufferSource p_117120_, ItemStack p_117121_, EquipmentSlot p_117122_) {
-        
     }
 
     public static <T extends LivingEntity> void renderArmorCutout(EntityModel<T> modelIn, ResourceLocation textureLocationIn, PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn, T entityIn, float red, float green, float blue, boolean enchanted) {
