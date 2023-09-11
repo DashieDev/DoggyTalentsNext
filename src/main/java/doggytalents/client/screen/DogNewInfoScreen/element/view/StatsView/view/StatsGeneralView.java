c265a761f8adfcea6cc426f8947f84d5a3191654
DashieDev <lamtinh.asian@gmail.com>
2023-09-11 20:24:07 +0700

    Format stats number to human unit before render.

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StatsView/view/StatsGeneralView.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StatsView/view/StatsGeneralView.java
@@ -2,11 +2,13 @@ package doggytalents.client.screen.DogNewInfoScreen.element.view.StatsView.view;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
+import doggytalents.client.entity.render.DogScreenOverlays;
 import doggytalents.client.screen.framework.element.AbstractElement;
 import doggytalents.common.entity.stats.StatsTracker;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.stats.StatFormatter;
 
 public class StatsGeneralView extends AbstractElement {
 
@@ -31,28 +33,41 @@ public class StatsGeneralView extends AbstractElement {
         int pY = this.getRealY() + PADDING_TOP;
         String i18nPrefix = "doggui.stats.general.";
         String draw;
-        draw = I18n.get(i18nPrefix + "damageDealt") + ": " + stats.getDamageDealt();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+
+        draw = I18n.get(i18nPrefix + "damageDealt") + ": " + formatHealth(stats.getDamageDealt()) + " ";
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
+        graphics.blit(DogScreenOverlays.GUI_ICONS_LOCATION, startX
+            + font.width(draw), pY - 1, 16, 0 ,9, 9);
+        graphics.blit(DogScreenOverlays.GUI_ICONS_LOCATION, startX
+            + font.width(draw), pY - 1, 16 + 36, 0 ,9, 9);
         pY += font.lineHeight + LINE_SPACING;
-        draw = I18n.get(i18nPrefix + "distanceInWater") + ": " + stats.getDistanceInWater();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+        draw = I18n.get(i18nPrefix + "distanceInWater") + ": " + formatDistance(stats.getDistanceInWater());
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
         pY += font.lineHeight + LINE_SPACING;
-        draw = I18n.get(i18nPrefix + "distanceOnWater") + ": "+ stats.getDistanceOnWater();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+        draw = I18n.get(i18nPrefix + "distanceOnWater") + ": "+ formatDistance(stats.getDistanceOnWater());
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
         pY += font.lineHeight + LINE_SPACING;
-        draw = I18n.get(i18nPrefix + "distanceRidden") + ": "+ stats.getDistanceRidden();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+        draw = I18n.get(i18nPrefix + "distanceRidden") + ": "+ formatDistance(stats.getDistanceRidden());
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
         pY += font.lineHeight + LINE_SPACING;
-        draw = I18n.get(i18nPrefix + "distanceSneaking") + ": " + stats.getDistanceSneaking();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+        draw = I18n.get(i18nPrefix + "distanceSneaking") + ": " + formatDistance(stats.getDistanceSneaking());
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
         pY += font.lineHeight + LINE_SPACING;
-        draw = I18n.get(i18nPrefix + "distanceSprinting") + ": " + stats.getDistanceSprint();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+        draw = I18n.get(i18nPrefix + "distanceSprinting") + ": " + formatDistance(stats.getDistanceSprint());
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
         pY += font.lineHeight + LINE_SPACING;
-        draw = I18n.get(i18nPrefix + "distanceWalking") + ": " + stats.getDistanceWalk();
-        this.font.draw(stack, draw, startX, pY, 0xffffffff);
+        draw = I18n.get(i18nPrefix + "distanceWalking") + ": " + formatDistance(stats.getDistanceWalk());
+        graphics.drawString(font, draw, startX, pY, 0xffffffff);
         pY += font.lineHeight + LINE_SPACING;
 
+    }
+
+    private String formatDistance(int x_cm) {
+        return StatFormatter.DISTANCE.format(x_cm);
+    }
+
+    private String formatHealth(float h) {
+        return StatFormatter.DECIMAL_FORMAT.format(h);
     }
     
 }
