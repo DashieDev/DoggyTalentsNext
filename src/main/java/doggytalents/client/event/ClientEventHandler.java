f0887b8765814a528f1115fd3a3204d17979a841
DashieDev <lamtinh.asian@gmail.com>
2023-09-12 10:37:20 +0700

    cleanup ClientEventHandler

--- src/main/java/doggytalents/client/event/ClientEventHandler.java
+++ src/main/java/doggytalents/client/event/ClientEventHandler.java
@@ -1,7 +1,6 @@
 package doggytalents.client.event;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 
@@ -14,41 +13,31 @@ import doggytalents.client.screen.widget.DogInventoryButton;
 import doggytalents.client.screen.widget.DoggySpin;
 import doggytalents.common.config.ConfigHandler;
 import doggytalents.common.entity.Dog;
-import doggytalents.common.item.WhistleItem;
 import doggytalents.common.item.WhistleItem.WhistleMode;
 import doggytalents.common.network.PacketHandler;
 import doggytalents.common.network.packet.data.DogMountData;
 import doggytalents.common.network.packet.data.OpenDogScreenData;
 import doggytalents.common.network.packet.data.WhistleUseData;
 import doggytalents.common.util.InventoryUtil;
-import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.LevelLoadingScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
-import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.block.BlockModelShaper;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.BlockModelRotation;
-import net.minecraft.client.resources.model.Material;
-import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.client.settings.KeyConflictContext;
-import net.minecraftforge.client.settings.KeyModifier;
 import net.minecraftforge.client.event.MovementInputUpdateEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -56,11 +45,6 @@ import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-
-import org.jetbrains.annotations.Nullable;
-
 public class ClientEventHandler {
 
     public static void onModelBakeEvent(final ModelEvent.BakingCompleted event) {
@@ -194,38 +178,6 @@ public class ClientEventHandler {
             new WhistleUseData(mode_id));
     }
 
-
-
-// TODO Implement patrol item
-//    @SubscribeEvent
-//    public void onWorldRenderLast(RenderWorldLastEvent event) {
-//        Minecraft mc = Minecraft.getInstance();
-//        PlayerEntity player = mc.player;
-//
-//        if (player == null || player.getHeldItem(Hand.MAIN_HAND).getItem() != DoggyItems.PATROL.get()) {
-//            return;
-//        }
-//
-//        ItemStack stack = player.getHeldItem(Hand.MAIN_HAND);
-//
-//        RenderSystem.pushMatrix();
-//
-//        if (stack.hasTag() && stack.getTag().contains("patrolPos", Tag.TAG_LIST)) {
-//            ListNBT list = stack.getTag().getList("patrolPos", Tag.TAG_COMPOUND);
-//            List<BlockPos> poses = new ArrayList<>(list.size());
-//            for (int i = 0; i < list.size(); i++) {
-//                poses.add(NBTUtil.getBlockPos(list.getCompound(i)));
-//            }
-//
-//            for (BlockPos pos : poses) {
-//                this.drawSelectionBox(event.getMatrixStack(), player, event.getPartialTicks(), new AxisAlignedBB(pos));
-//            }
-//        }
-//
-//
-//        RenderSystem.popMatrix();
-//    }
-
     public void drawSelectionBox(PoseStack matrixStackIn, Player player, float particleTicks, AABB boundingBox) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
@@ -268,71 +220,4 @@ public class ClientEventHandler {
             }
         }
     }
-
-//    @SubscribeEvent
-//    public void onPreRenderGameOverlay(final RenderGameOverlayEvent.Post event) {
-//        // TODO
-//        label: if (event.getType() == RenderGameOverlayEvent.ElementType.HEALTHMOUNT) {
-//            Minecraft mc = Minecraft.getInstance();
-//
-//            if (mc.player == null || !(mc.player.getVehicle() instanceof DogEntity)) {
-//                break label;
-//            }
-//
-//            PoseStack stack = event.getMatrixStack();
-//
-//            DogEntity dog = (DogEntity) mc.player.getVehicle();
-//            int width = mc.getWindow().getGuiScaledWidth();
-//            int height = mc.getWindow().getGuiScaledHeight();
-//            RenderSystem.pushMatrix();
-//            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-//            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-//            RenderSystem.setShaderTexture(0, Screen.GUI_ICONS_LOCATION;
-//            RenderSystem.enableBlend();
-//            int left = width / 2 + 91;
-//            int top = height - ((ForgeIngameGui) mc.gui).right_height;
-//            ((ForgeIngameGui) mc.gui).right_height += 10;
-//            int level = Mth.ceil((dog.getDogHunger() / dog.getMaxHunger()) * 20.0D);
-//
-//            for (int i = 0; i < 10; ++i) {
-//                int idx = i * 2 + 1;
-//                int x = left - i * 8 - 9;
-//                int y = top;
-//                int icon = 16;
-//                byte backgound = 12;
-//
-//                mc.gui.blit(stack, x, y, 16 + backgound * 9, 27, 9, 9);
-//
-//
-//                if (idx < level) {
-//                    mc.gui.blit(stack, x, y, icon + 36, 27, 9, 9);
-//                } else if (idx == level) {
-//                    mc.gui.blit(stack, x, y, icon + 45, 27, 9, 9);
-//                }
-//            }
-//            RenderSystem.disableBlend();
-//
-//            RenderSystem.enableBlend();
-//            left = width / 2 + 91;
-//            top = height - ForgeIngameGui.right_height;
-//            RenderSystem.color4f(1.0F, 1.0F, 0.0F, 1.0F);
-//            int l6 = dog.getAirSupply();
-//            int j7 = dog.getMaxAirSupply();
-//
-//            if (dog.isEyeInFluid(FluidTags.WATER) || l6 < j7) {
-//                int air = dog.getAirSupply();
-//                int full = Mth.ceil((air - 2) * 10.0D / 300.0D);
-//                int partial = Mth.ceil(air * 10.0D / 300.0D) - full;
-//
-//                for (int i = 0; i < full + partial; ++i) {
-//                    mc.gui.blit(stack, left - i * 8 - 9, top, (i < full ? 16 : 25), 18, 9, 9);
-//                }
-//                ForgeIngameGui.right_height += 10;
-//            }
-//            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-//            RenderSystem.disableBlend();
-//
-//            RenderSystem.popMatrix();
-////        }
-//    }
 }
