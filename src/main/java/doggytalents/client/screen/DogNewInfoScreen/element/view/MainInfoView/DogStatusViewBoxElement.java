74c729e8bf9111b431fbaf07b24672fbd1bd0076
DashieDev <lamtinh.asian@gmail.com>
2023-09-17 02:03:10 +0700

    1.19.2 fix

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/MainInfoView/DogStatusViewBoxElement.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/MainInfoView/DogStatusViewBoxElement.java
@@ -6,7 +6,6 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 
-client.entity.render.DogScreenOverlays;
 import doggytalents.api.feature.DogSize;
 import doggytalents.api.feature.DogLevel.Type;
 import doggytalents.client.entity.render.DogScreenOverlays;
