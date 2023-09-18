112d80c1b09cf9f6920ea5ab8782ab1458a38876
DashieDev <lamtinh.asian@gmail.com>
2023-09-18 21:47:34 +0700

    Dont false on fail

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1087,8 +1087,6 @@ public class Dog extends AbstractDog {
 
             if (result.shouldSwing()) {
                 return true;
-            } else if (result == InteractionResult.FAIL) {
-                return false;
             }
         }
 
