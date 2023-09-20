4be44269a61042ffa3c6f57ffd31048a04a7e89e
DashieDev <lamtinh.asian@gmail.com>
2023-09-21 00:20:10 +0700

    Allow action to blopck sit/stand anim

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3397,7 +3397,8 @@ public class Dog extends AbstractDog {
 
     @Override
     public void setInSittingPose(boolean sit) {
-        if (!this.level().isClientSide) {
+        if (!this.level().isClientSide
+            && !this.animAction.blockSitStandAnim()) {
             boolean sit0 = this.isInSittingPose();
             if (sit0 != sit) {
                 var anim = sit ? this.getSitAnim() : this.getStandAnim();
