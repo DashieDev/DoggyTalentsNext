af2b29db3ed81e3e104a3bcd3633e6ce205045a6
DashieDev <lamtinh.asian@gmail.com>
2023-09-20 16:14:40 +0700

    Add msg to Heel By Look and Ride With Me.

--- src/main/java/doggytalents/common/item/WhistleItem.java
+++ src/main/java/doggytalents/common/item/WhistleItem.java
@@ -384,9 +384,11 @@ public class WhistleItem extends Item implements IDogItem {
         if (vehicle == null) {
             return;
         }
-
+        
         dog.authorizeRiding();
-        dog.startRiding(vehicle);
+        var result = dog.startRiding(vehicle);
+        if (result)
+        player.sendSystemMessage(Component.translatable("dogcommand.ride_with_me", dog.getName().getString()));
     }
 
     private void heelByLook(Level level, Player player) {
@@ -410,6 +412,7 @@ public class WhistleItem extends Item implements IDogItem {
         if (!(entity instanceof Dog dog))
             return;
         DogUtil.dynamicSearchAndTeleportToOwnwer(dog, player, 2);
+        player.sendSystemMessage(Component.translatable("dogcommand.heel_by_name", dog.getName().getString()));
         dog.setOrderedToSit(false);
     }
 
