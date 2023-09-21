9270c8415a2847e0b8e4dbd6f4c18db47b957b67
DashieDev <lamtinh.asian@gmail.com>
2023-09-22 04:07:11 +0700

    Non BedFinder dog can be ride on head too.

--- src/main/java/doggytalents/common/talent/BedFinderTalent.java
+++ src/main/java/doggytalents/common/talent/BedFinderTalent.java
@@ -3,12 +3,6 @@ package doggytalents.common.talent;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.registry.Talent;
 import doggytalents.api.registry.TalentInstance;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
 
 public class BedFinderTalent extends TalentInstance {
 
@@ -18,40 +12,9 @@ public class BedFinderTalent extends TalentInstance {
 
     @Override
     public void set(AbstractDog dog, int levelBefore) {
-        if (dog.level().isClientSide) return;
-        if (levelBefore > 0 && this.level() <= 0) {
-            dog.unRide();
-        }
     }
 
     @Override
     public void livingTick(AbstractDog dog) {
-
-    }
-
-    @Override
-    public InteractionResult processInteract(AbstractDog dog, Level level, Player player, InteractionHand hand) {
-        if (this.level() <= 0)
-            return InteractionResult.PASS;
-        if (player.hasPassenger(dog)) {
-            if (!dog.level().isClientSide)
-                dog.unRide();
-            return InteractionResult.SUCCESS;
-        }
-        var item = player.getItemInHand(hand).getItem();
-        if (item != Items.BONE)
-            return InteractionResult.PASS;
-        if (dog.isVehicle())
-            return InteractionResult.PASS;
-        if (!dog.canInteract(player))
-            return InteractionResult.PASS;
-        if (!dog.level().isClientSide) {
-            if (dog.startRiding(player))
-            player.displayClientMessage(
-                Component.translatable(
-                    "talent.doggytalents.bed_finder.dog_mount", 
-                    dog.getGenderPronoun()), true);
-        }
-        return InteractionResult.SUCCESS;
     }
 }
