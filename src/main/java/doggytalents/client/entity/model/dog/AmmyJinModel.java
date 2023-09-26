0c5209f2d7c4228fa402375a0bba0edd8eca41ae
MashXP <realsteelphong@gmail.com>
2023-09-26 23:03:31 +0700

    A ton of Ammy variations!!!

---
+++ src/main/java/doggytalents/client/entity/model/dog/AmmyJinModel.java
@@ -0,0 +1,101 @@
+package doggytalents.client.entity.model.dog;
+
+import doggytalents.common.entity.Dog;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.RenderType;
+
+public class AmmyJinModel extends DogModel {
+
+	
+
+    public AmmyJinModel(ModelPart box) {
+		super(box, RenderType::entityTranslucent);
+	}
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create(), PartPose.offset(-1.0F, 12.75F, -7.0F));
+
+		PartDefinition real_head = head.addOrReplaceChild("real_head", CubeListBuilder.create().texOffs(23, 1).addBox(-0.5F, 0.43F, -4.5F, 3.0F, 2.0F, 4.0F, new CubeDeformation(-0.4F))
+		.texOffs(23, 0).addBox(-0.5F, -0.32F, -4.5F, 3.0F, 2.0F, 4.0F, new CubeDeformation(-0.3F))
+		.texOffs(24, 13).addBox(-2.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, new CubeDeformation(-0.1F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition right_ear = real_head.addOrReplaceChild("right_ear", CubeListBuilder.create(), PartPose.offset(-0.6753F, -3.65F, -0.2839F));
+
+		PartDefinition head_r1 = right_ear.addOrReplaceChild("head_r1", CubeListBuilder.create().texOffs(0, 1).mirror().addBox(-0.9F, -1.2F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.1F)).mirror(false)
+		.texOffs(0, 1).mirror().addBox(-1.2F, -0.7F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.1F)).mirror(false)
+		.texOffs(0, 1).mirror().addBox(-0.6F, -0.7F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.1F)).mirror(false)
+		.texOffs(18, 13).mirror().addBox(-1.5F, -1.2F, -0.75F, 3.0F, 3.0F, 1.0F, new CubeDeformation(-0.7F)).mirror(false)
+		.texOffs(0, 0).mirror().addBox(-1.3F, -0.3F, -0.5F, 2.0F, 2.0F, 1.0F, new CubeDeformation(-0.1F)).mirror(false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.6545F, 0.0F));
+
+		PartDefinition left_ear = real_head.addOrReplaceChild("left_ear", CubeListBuilder.create(), PartPose.offset(2.6753F, -3.65F, -0.2839F));
+
+		PartDefinition head_r2 = left_ear.addOrReplaceChild("head_r2", CubeListBuilder.create().texOffs(0, 1).addBox(-0.1F, -1.2F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.1F))
+		.texOffs(0, 1).addBox(0.2F, -0.7F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.1F))
+		.texOffs(0, 1).addBox(-0.4F, -0.7F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.1F))
+		.texOffs(18, 13).addBox(-1.5F, -1.2F, -0.75F, 3.0F, 3.0F, 1.0F, new CubeDeformation(-0.7F))
+		.texOffs(0, 0).addBox(-0.7F, -0.3F, -0.5F, 2.0F, 2.0F, 1.0F, new CubeDeformation(-0.1F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, -0.6545F, 0.0F));
+
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, 14.0F, 2.0F));
+
+		PartDefinition body_rotation = body.addOrReplaceChild("body_rotation", CubeListBuilder.create().texOffs(0, 13).addBox(-3.0F, -3.0F, -4.0F, 6.0F, 9.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 1.5708F, 0.0F, 0.0F));
+
+		PartDefinition upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create(), PartPose.offset(-1.0F, 14.0F, 2.0F));
+
+		PartDefinition mane_rotation = upper_body.addOrReplaceChild("mane_rotation", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -5.5F, -2.0F, 8.0F, 6.0F, 7.0F, new CubeDeformation(-0.35F))
+		.texOffs(0, 32).addBox(3.1F, -5.5F, -4.5F, 1.0F, 11.0F, 13.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 32).mirror().addBox(-4.1F, -5.5F, -4.5F, 1.0F, 11.0F, 13.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(1.0F, 2.5F, -2.5F, 1.5708F, 0.0F, 0.0F));
+
+		PartDefinition right_hind_leg = partdefinition.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(24, 23).addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(33, 43).mirror().addBox(-0.1F, 1.7F, -1.4F, 1.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, 16.0F, 6.0F));
+
+		PartDefinition left_hind_leg = partdefinition.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(24, 23).addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(33, 43).addBox(1.1F, 1.7F, -1.4F, 1.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.5F, 16.0F, 6.0F));
+
+		PartDefinition right_front_leg = partdefinition.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(24, 23).addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(33, 43).mirror().addBox(-0.1F, 1.7F, -1.4F, 1.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, 16.0F, -4.0F));
+
+		PartDefinition left_front_leg = partdefinition.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(24, 23).addBox(0.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(33, 43).addBox(1.1F, 1.7F, -1.4F, 1.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.5F, 16.0F, -4.0F));
+
+		PartDefinition tail = partdefinition.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 12.13F, 6.54F, 2.0071F, 0.0F, 0.0F));
+
+		PartDefinition real_tail = tail.addOrReplaceChild("real_tail", CubeListBuilder.create().texOffs(0, 28).addBox(-1.0F, 0.62F, -2.29F, 2.0F, 4.0F, 2.0F, new CubeDeformation(0.1F))
+		.texOffs(0, 30).addBox(-1.0F, 3.77F, 1.26F, 2.0F, 3.0F, 2.0F, new CubeDeformation(-0.1F))
+		.texOffs(33, 24).mirror().addBox(-1.25F, -0.43F, -2.94F, 1.0F, 8.0F, 7.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(33, 24).addBox(0.25F, -0.43F, -2.94F, 1.0F, 8.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition tail_r1 = real_tail.addOrReplaceChild("tail_r1", CubeListBuilder.create().texOffs(0, 30).addBox(-1.0F, -2.75F, -1.0F, 2.0F, 3.0F, 2.0F, new CubeDeformation(-0.3F)), PartPose.offsetAndRotation(0.0F, 4.37F, 2.76F, 1.0036F, 0.0F, 0.0F));
+
+		PartDefinition tail_r2 = real_tail.addOrReplaceChild("tail_r2", CubeListBuilder.create().texOffs(0, 30).addBox(-1.0F, -2.25F, -1.5F, 2.0F, 4.0F, 2.0F, new CubeDeformation(0.2F)), PartPose.offsetAndRotation(0.0F, 5.17F, -0.14F, 0.6109F, 0.0F, 0.0F));
+
+		PartDefinition tail_r3 = real_tail.addOrReplaceChild("tail_r3", CubeListBuilder.create().texOffs(0, 30).addBox(-1.0F, -1.75F, -1.5F, 2.0F, 3.0F, 2.0F, new CubeDeformation(0.05F)), PartPose.offsetAndRotation(0.0F, 6.07F, 1.16F, 1.9635F, 0.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 64, 64);
+	}
+
+
+	public static void createAugment() {
+		
+	}
+
+	@Override
+    public boolean useDefaultModelForAccessories() {
+        return true;
+    }
+
+	@Override
+	public boolean tickClient() { return true; }
+
+	@Override
+    public void doTickClient(Dog dog) {
+	}
+    
+}
--- src/main/java/doggytalents/client/entity/model/dog/AmmyShiranuiModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/AmmyShiranuiModel.java
@@ -1,7 +1,6 @@
 package doggytalents.client.entity.model.dog;
 
 import doggytalents.common.entity.Dog;
-import net.minecraft.client.model.ListModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
@@ -10,16 +9,14 @@ import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.particles.ParticleTypes;
 
-public class AmaterasuModel extends DogModel {
+public class AmmyShiranuiModel extends DogModel {
 
 	
 
-    public AmaterasuModel(ModelPart box) {
+    public AmmyShiranuiModel(ModelPart box) {
 		super(box, RenderType::entityTranslucent);
 	}
-
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
@@ -47,9 +44,10 @@ public class AmaterasuModel extends DogModel {
 
 		PartDefinition upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create(), PartPose.offset(-1.0F, 14.0F, -4.0F));
 
-		PartDefinition mane_rotation = upper_body.addOrReplaceChild("mane_rotation", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -5.5F, -0.5F, 8.0F, 6.0F, 7.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 32).addBox(3.3F, -5.5F, -4.5F, 1.0F, 11.0F, 13.0F, new CubeDeformation(-0.2F))
-		.texOffs(0, 32).mirror().addBox(-4.3F, -5.5F, -4.5F, 1.0F, 11.0F, 13.0F, new CubeDeformation(-0.2F)).mirror(false), PartPose.offsetAndRotation(1.0F, 2.5F, 3.5F, 1.5708F, 0.0F, 0.0F));
+		PartDefinition mane_rotation = upper_body.addOrReplaceChild("mane_rotation", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -5.5F, -0.5F, 8.0F, 6.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(1.0F, 2.5F, 3.5F, 1.5708F, 0.0F, 0.0F));
+
+		PartDefinition Swirly_r1 = mane_rotation.addOrReplaceChild("Swirly_r1", CubeListBuilder.create().texOffs(0, 32).mirror().addBox(-4.1F, -11.5F, 0.75F, 1.0F, 11.0F, 13.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(0, 32).addBox(3.1F, -11.5F, 0.75F, 1.0F, 11.0F, 13.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 7.5F, 0.5F, 0.2618F, 0.0F, 0.0F));
 
 		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, 14.0F, 2.0F));
 
@@ -57,10 +55,8 @@ public class AmaterasuModel extends DogModel {
 
 		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create(), PartPose.offset(-1.0F, 13.5F, -7.0F));
 
-		PartDefinition real_head = head.addOrReplaceChild("real_head", CubeListBuilder.create().texOffs(23, 0).addBox(-0.5F, -0.07F, -4.5F, 3.0F, 3.0F, 4.0F, new CubeDeformation(-0.3F))
-		.texOffs(33, 24).addBox(0.5F, -0.77F, -2.3F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.35F))
-		.texOffs(32, 24).addBox(0.5F, -0.53F, -2.35F, 1.0F, 1.0F, 1.0F, new CubeDeformation(-0.4F))
-		.texOffs(24, 13).addBox(-2.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, new CubeDeformation(-0.1F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition real_head = head.addOrReplaceChild("real_head", CubeListBuilder.create().texOffs(24, 13).addBox(-2.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, new CubeDeformation(-0.2F))
+		.texOffs(23, 0).addBox(-0.5F, -0.07F, -4.5F, 3.0F, 3.0F, 4.0F, new CubeDeformation(-0.3F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition left_ear = real_head.addOrReplaceChild("left_ear", CubeListBuilder.create(), PartPose.offset(-1.0F, -3.0F, 0.0F));
 
@@ -78,8 +74,23 @@ public class AmaterasuModel extends DogModel {
 		.texOffs(18, 13).addBox(-1.5F, -1.2F, -0.75F, 3.0F, 3.0F, 1.0F, new CubeDeformation(-0.7F))
 		.texOffs(0, 0).addBox(-0.7F, -0.3F, -0.5F, 2.0F, 2.0F, 1.0F, new CubeDeformation(-0.1F)), PartPose.offsetAndRotation(-0.3247F, -0.9F, -0.2839F, 0.0F, -0.6545F, 0.0F));
 
+		PartDefinition headfur = real_head.addOrReplaceChild("headfur", CubeListBuilder.create().texOffs(11, 10).addBox(1.75F, -7.3F, -8.25F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(11, 10).addBox(0.75F, -8.05F, -8.5F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(10, 9).addBox(1.5F, -9.05F, -8.75F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(11, 10).addBox(3.0F, -9.55F, -8.6F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.1F))
+		.texOffs(13, 9).addBox(1.5F, -10.8F, -8.5F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(10, 9).addBox(0.25F, -11.55F, -8.5F, 3.0F, 1.0F, 2.0F, new CubeDeformation(-0.2F))
+		.texOffs(11, 10).mirror().addBox(-2.75F, -7.3F, -8.25F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(11, 10).mirror().addBox(-3.75F, -8.05F, -8.5F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(10, 9).mirror().addBox(-4.5F, -9.05F, -8.75F, 3.0F, 1.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(11, 10).mirror().addBox(-4.0F, -9.55F, -8.6F, 1.0F, 1.0F, 2.0F, new CubeDeformation(0.1F)).mirror(false)
+		.texOffs(13, 9).mirror().addBox(-3.5F, -10.8F, -8.5F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(13, 7).mirror().addBox(-2.5F, -11.8F, -8.65F, 1.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(10, 9).mirror().addBox(-3.25F, -11.55F, -8.5F, 3.0F, 1.0F, 2.0F, new CubeDeformation(-0.2F)).mirror(false), PartPose.offset(1.0F, 9.5F, 7.25F));
+
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
+
 
 	public static void createAugment() {
 		
