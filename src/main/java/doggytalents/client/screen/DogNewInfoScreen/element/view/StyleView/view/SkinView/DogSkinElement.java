9e27b0e954416532d286cc8f5752cd6868801f40
DashieDev <lamtinh.asian@gmail.com>
2023-09-29 03:40:24 +0700

    scrollable more info skin

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/DogSkinElement.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/DogSkinElement.java
@@ -10,6 +10,9 @@ import doggytalents.client.screen.DogNewInfoScreen.element.view.MainInfoView.Dog
 import doggytalents.client.screen.DogNewInfoScreen.store.slice.ActiveSkinSlice;
 import doggytalents.client.screen.framework.Store;
 import doggytalents.client.screen.framework.element.AbstractElement;
+import doggytalents.client.screen.framework.element.DivElement;
+import doggytalents.client.screen.framework.element.ScrollView;
+import doggytalents.client.screen.framework.element.ElementPosition.PosType;
 import doggytalents.common.entity.Dog;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -19,6 +22,7 @@ import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.util.Mth;
 
 public class DogSkinElement extends AbstractElement {
@@ -45,8 +49,70 @@ public class DogSkinElement extends AbstractElement {
             Store.get(getScreen()).getStateOrDefault(
                 ActiveSkinSlice.class, ActiveSkinSlice.class, 
                 new ActiveSkinSlice()).showInfo;
-            
+        if (!this.showInfo) 
+            return this;
+        var manifestSkin = this.locList.get(activeSkinId);
+
+        if (!manifestSkin.hasExtraInfo()) {
+            return this;
+        }
+        var scrollView = new ScrollView(this, getScreen());
+        scrollView
+            .setPosition(PosType.ABSOLUTE, Mth.floor(getParent().getSizeX()*0.45f), 0)
+            .setSize(0.55f, 1f)
+            .init();
+        this.addChildren(scrollView);
+        
+        var scroll = scrollView.getContainer();
+        scroll.addChildren(new DivElement(scroll, getScreen()).setSize(1f, Math.max(10, this.getSizeY()/2 - 40)).init()); 
+        scroll.addChildren(new SkinStrEntry(scroll, getScreen(), Component.literal("Name: "), 
+            Component.literal(manifestSkin.getName())).init()); 
+        scroll.addChildren(new SkinStrEntry(scroll, getScreen(), Component.literal("Based On: "), 
+            Component.literal(manifestSkin.getBasedOn())).init()); 
+        scroll.addChildren(new SkinStrEntry(scroll, getScreen(), Component.literal("Author: "), 
+            Component.literal(manifestSkin.getAuthor())).init()); 
+        
         return this;
+    }
+
+    public static class SkinStrEntry extends AbstractElement {
+        
+        private Component title, content;
+        private List<FormattedCharSequence> components;
+        private Font font;
+
+        public SkinStrEntry(AbstractElement parent, Screen screen, Component title, Component content) {
+            super(parent, screen);
+            this.title = title;
+            this.content = content;
+            font = Minecraft.getInstance().font;
+        }
+
+        @Override
+        public AbstractElement init() {
+            this.setPosition(PosType.RELATIVE, 0, 0);
+            title = title.copy().withStyle(Style.EMPTY.withBold(true));
+            components = this.font.split(content, this.getParent().getSizeX() - 5);
+            int totalH = LINE_SPACING + font.lineHeight + LINE_SPACING + components.size()*(LINE_SPACING + font.lineHeight)
+                + LINE_SPACING;
+            this.setSize(1f, totalH);
+
+            return this;
+        }
+
+        @Override
+        public void renderElement(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
+            int pX = this.getRealX();
+            int pY = this.getRealY() + LINE_SPACING;
+            graphics.drawString(font, title, pX, pY, 0xffffffff);
+            
+            pY += font.lineHeight + LINE_SPACING;
+            for (var line : components) {
+                graphics.drawString(font, line, pX, pY, 0xffffffff);
+                pY += font.lineHeight + LINE_SPACING;
+            }
+        }
+
     }
 
     @Override
@@ -112,47 +178,6 @@ public class DogSkinElement extends AbstractElement {
         if (!manifestSkin.hasExtraInfo()) {
             renderNoInfo(graphics, mouseX, mouseY, partialTicks);
             return;
-        }
-        
-        //MockData
-        Component basedOn = Component.literal(manifestSkin.getBasedOn());
-        Component authors = Component.literal(manifestSkin.getAuthor());
-        //END
-
-        var split_basedOn = this.font.split(basedOn, 140);
-        var split_authors = this.font.split(authors, 140);
-
-        int lines = 6 + split_authors.size() + split_basedOn.size();
-        int size = Mth.floor(lines * font.lineHeight + (lines - 1) * LINE_SPACING);
-
-        int tX = this.getRealX() + mX - 20;
-        int tY = this.getRealY() + mY - size/2;
-
-        graphics.drawString(font, "Name: ", tX, tY, 0xffffffff);
-        tY += font.lineHeight + LINE_SPACING;
-        graphics.drawString(font, manifestSkin.getName(), tX, tY, 0xffffffff);
-        tY += font.lineHeight + LINE_SPACING;
-
-
-        tY += font.lineHeight + LINE_SPACING;
-        
-        graphics.drawString(font, "Based On: ", tX, tY, 0xffffffff);
-        tY += font.lineHeight + LINE_SPACING;
-        
-        for (var c : split_basedOn) {
-            graphics.drawString(font, c, tX, tY, 0xffffffff);
-            tY += font.lineHeight + LINE_SPACING;
-        }
-
-        
-        tY += font.lineHeight + LINE_SPACING;
-
-        graphics.drawString(font, "Author: ", tX, tY, 0xffffffff);
-        tY += font.lineHeight + LINE_SPACING;
-        
-        for (var c : split_authors) {
-            graphics.drawString(font, c, tX, tY, 0xffffffff);
-            tY += font.lineHeight + LINE_SPACING;
         }
         
     }
