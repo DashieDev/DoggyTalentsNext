a4e9001d54cdc13dfc32b02e078c16280df1567f
DashieDev <lamtinh.asian@gmail.com>
2023-10-01 00:03:35 +0700

    center skin number str

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/SkinButtonElement.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/StyleView/view/SkinView/SkinButtonElement.java
@@ -119,12 +119,11 @@ public class SkinButtonElement extends AbstractElement {
     @Override
     public void renderElement(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
         
-        int tX = this.getRealX() + 38;
+        int tX = this.getRealX() + 52;
         int tY = this.getRealY() + this.getSizeY()/2 - font.lineHeight/2;
-        if (this.activeSkinId+1 < 10) tX += 2;  
         String str = (this.activeSkinId+1) + "/" + this.locList.size();
-        this.font.draw(stack, str, tX, tY, 0xffffffff);
-        
+        tX -= font.width(str)/2;
+        graphics.drawString(font, str, tX, tY, 0xffffffff);
     }
 
     public void applyAndRequestSkinChange(int id) {
