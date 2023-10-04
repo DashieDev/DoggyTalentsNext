c29eba2df6487bf4887f7c3af961d53167a24139
DashieDev <lamtinh.asian@gmail.com>
2023-10-05 02:59:31 +0700

    sync canconinute todo

--- src/main/java/doggytalents/common/entity/ai/DogRandomSitIdleGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogRandomSitIdleGoal.java
@@ -35,7 +35,7 @@ public class DogRandomSitIdleGoal extends Goal {
     public boolean canContinueToUse() {
         if (!this.dog.isOrderedToSit())
             return false;
-        if (!this.dog.isInSittingPose())
+        if (this.dog.getDogPose() != DogPose.SIT)
             return false;
         if (!dog.onGround()) return false;
 
