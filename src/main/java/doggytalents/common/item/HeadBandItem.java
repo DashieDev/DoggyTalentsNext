61cf1e53021db27bf0022a5c3a28c4a85d597cc8
DashieDev <lamtinh.asian@gmail.com>
2023-10-04 19:54:33 +0700

    save1

--- src/main/java/doggytalents/common/item/HeadBandItem.java
+++ src/main/java/doggytalents/common/item/HeadBandItem.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 
 import javax.annotation.Nullable;
 
+import doggytalents.DoggyItems;
 import doggytalents.api.registry.Accessory;
 import doggytalents.common.entity.accessory.HeadBandAccessory;
 import doggytalents.common.entity.accessory.LocatorOrbAccessory;
@@ -21,6 +22,8 @@ public class HeadBandItem extends AccessoryItem{
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
             TooltipFlag flags) {
+        if (this == DoggyItems.HEAD_BAND_BLANK.get())
+            return;
         var desc_id = this.getDescriptionId(stack) + ".description";
         components.add(Component.translatable(desc_id));
     }
