dfdcbcf34ec0e873697cc8f358c6d03b7d44f73d
MashXP <realsteelphong@gmail.com>
2023-10-05 10:25:14 +0700

    Houndstone 🐺✝

--- src/main/java/doggytalents/client/ClientSetup.java
+++ src/main/java/doggytalents/client/ClientSetup.java
@@ -46,6 +46,7 @@ import doggytalents.client.entity.model.dog.DogModel;
 import doggytalents.client.entity.model.dog.EnglishBulldogModel;
 import doggytalents.client.entity.model.dog.FrenchBulldogModel;
 import doggytalents.client.entity.model.dog.GermanShepherdModel;
+import doggytalents.client.entity.model.dog.HoundstoneModel;
 import doggytalents.client.entity.model.dog.HungarianPuliModel;
 import doggytalents.client.entity.model.dog.IwankoModel;
 import doggytalents.client.entity.model.dog.JackModel;
@@ -158,6 +159,7 @@ public class ClientSetup {
     public static final ModelLayerLocation INU_AKITA = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "akita_inu"), "main");
     public static final ModelLayerLocation INU_SHIBA = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "shiba_inu"), "main");
     public static final ModelLayerLocation INU_SHIKOKU = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "shikoku_inu"), "main");
+    public static final ModelLayerLocation DOG_HOUNDSTONE = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "houndstone"), "main");
 
     public static final ModelLayerLocation DOG_ARMOR = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "dog"), "armor");
     public static final ModelLayerLocation DOG_FRONT_LEGS_SEPERATE = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "dog_hind_leg_diff_tex"), "main");
@@ -232,6 +234,7 @@ public class ClientSetup {
         event.registerLayerDefinition(INU_AKITA, AkitaModel::createBodyLayer);
         event.registerLayerDefinition(INU_SHIBA, ShibaModel::createBodyLayer);
         event.registerLayerDefinition(INU_SHIKOKU, ShikokuModel::createBodyLayer);
+        event.registerLayerDefinition(DOG_HOUNDSTONE, HoundstoneModel::createBodyLayer);
 
         event.registerLayerDefinition(DOG_ARMOR, DogArmorModel::createBodyLayer);
         event.registerLayerDefinition(DOG_FRONT_LEGS_SEPERATE, DogFrontLegsSeperate::createBodyLayer);
