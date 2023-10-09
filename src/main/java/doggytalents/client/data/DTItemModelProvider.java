7ab49f74a1760cb282c6213bd524b5ec2dd50f10
MashXP <realsteelphong@gmail.com>
2023-10-09 12:47:01 +0700

    (remove) HeadBand disable to run datagen

--- src/main/java/doggytalents/client/data/DTItemModelProvider.java
+++ src/main/java/doggytalents/client/data/DTItemModelProvider.java
@@ -104,8 +104,8 @@ public class DTItemModelProvider extends ItemModelProvider {
         generated(DoggyItems.STARTER_BUNDLE);
 
         generated(DoggyItems.HEAD_BAND_BLANK);
-        generated(DoggyItems.HEAD_BAND_MYSTERY);
-        generated(DoggyItems.HEAD_BAND_HIGHHH);
+        // generated(DoggyItems.HEAD_BAND_MYSTERY);
+        // generated(DoggyItems.HEAD_BAND_HIGHHH);
 
         generated(DoggyItems.MUSIC_DISC_BWV_849_FUGUE_KIMIKO);
         generated(DoggyItems.MUSIC_DISC_BWV_1080_FUGUE_11_KIMIKO);
