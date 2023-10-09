2d2b01f17e8907dc17fe7b793c92d6c1165bde90
MashXP <realsteelphong@gmail.com>
2023-10-09 12:47:01 +0700

    (remove) HeadBand disable to run datagen

--- src/main/java/doggytalents/common/data/DTRecipeProvider.java
+++ src/main/java/doggytalents/common/data/DTRecipeProvider.java
@@ -319,11 +319,11 @@ public class DTRecipeProvider extends RecipeProvider {
             .requires(DoggyItems.KITSUNE_SIDE.get(),1)
             .unlockedBy("has_string", has(Items.STRING))
             .save(consumer);
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, DoggyItems.KITSUNE_FRONT.get(), 1)
-            .requires(DoggyItems.HEAD_BAND_BLANK.get(),1)
-            .requires(Items.RED_DYE)  
-            .requires(Items.WHITE_DYE)              
-            .save(consumer);
+        // ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, DoggyItems.KITSUNE_FRONT.get(), 1)
+        //     .requires(DoggyItems.HEAD_BAND_BLANK.get(),1)
+        //     .requires(Items.RED_DYE)  
+        //     .requires(Items.WHITE_DYE)              
+        //     .save(consumer);
         ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, DoggyItems.KITSUNE_SIDE.get(), 1)
             .requires(DoggyItems.KITSUNE_FRONT.get(),1)
             .unlockedBy("has_string", has(Items.STRING))
