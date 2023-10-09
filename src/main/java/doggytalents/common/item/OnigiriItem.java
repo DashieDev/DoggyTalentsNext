3d5e32dbd1edbb52d8508d130816fdba57759f70
MashXP <realsteelphong@gmail.com>
2023-10-09 11:53:01 +0700

    ONIGIRI SENSEIII!!!

---
+++ src/main/java/doggytalents/common/item/OnigiriItem.java
@@ -0,0 +1,37 @@
+package doggytalents.common.item;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+public class OnigiriItem extends Item {
+
+    public OnigiriItem() {
+        super(
+            (new Properties()).food(
+                (new FoodProperties.Builder())
+                    .nutrition(5)
+                    .saturationMod(0.4F)
+                    .build()
+            )
+        );
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
+    }
+}
