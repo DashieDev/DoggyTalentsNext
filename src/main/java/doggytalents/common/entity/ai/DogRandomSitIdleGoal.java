13a2a3aaaaa7c95a9af7231ce5daf95cf5db56c1
DashieDev <lamtinh.asian@gmail.com>
2023-10-09 23:04:26 +0700

    Kusodesne mistake.
    i put this here to test the idle anim but commit it aliong with the
    data gen.

--- src/main/java/doggytalents/common/entity/ai/DogRandomSitIdleGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogRandomSitIdleGoal.java
@@ -29,7 +29,7 @@ public class DogRandomSitIdleGoal extends Goal {
         if (!dog.canDoIdileAnim()) return false;
         if (!dog.onGround()) return false;
 
-        return true;
+        return this.dog.getRandom().nextFloat() < 0.02;
     }
 
     @Override
