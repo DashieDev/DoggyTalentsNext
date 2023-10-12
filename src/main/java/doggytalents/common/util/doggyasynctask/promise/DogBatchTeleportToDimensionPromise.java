9c6d7170cf0ddd48450196351f72e8eb4f1c3990
DashieDev <lamtinh.asian@gmail.com>
2023-10-12 08:20:33 +0700

    Prevent passanger duplicate when tp.

--- src/main/java/doggytalents/common/util/doggyasynctask/promise/DogBatchTeleportToDimensionPromise.java
+++ src/main/java/doggytalents/common/util/doggyasynctask/promise/DogBatchTeleportToDimensionPromise.java
@@ -83,6 +83,7 @@ public class DogBatchTeleportToDimensionPromise extends AbstractPromise {
 
         for (var dog : tp_dogs) {
             int index = dog.getRandom().nextInt(safePosList.size());
+            dog.unRide();
             teleportDog(dog, targetLevel, safePosList.get(index));
         }
 
@@ -115,7 +116,6 @@ public class DogBatchTeleportToDimensionPromise extends AbstractPromise {
         dog.setPortalCooldown();
         targetLevel.addDuringTeleport(dog);
         
-        dog0.unRide();
         dog0.remove(RemovalReason.CHANGED_DIMENSION);
         targetLevel.resetEmptyTime();
         if (dog0.level instanceof ServerLevel sLevel) 
