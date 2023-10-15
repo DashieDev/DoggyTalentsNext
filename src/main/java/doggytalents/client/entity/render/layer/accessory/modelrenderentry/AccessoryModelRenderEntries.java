8621eaf8ffa173fc47992a3b034de7955cef2d0d
MashXP <realsteelphong@gmail.com>
2023-10-16 00:29:53 +0700

    demon horns

--- src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/AccessoryModelRenderEntries.java
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/AccessoryModelRenderEntries.java
@@ -17,6 +17,7 @@ public class AccessoryModelRenderEntries {
     public static Entry SNORKEL = new SnorkelRenderEntry();
     public static Entry TENGU_MASK = new TenguMaskRenderEmtry();
     public static Entry KITSUNE_MASK = new KitsuneMaskRenderEntry();
+    public static Entry DEMON_HORNS = new DemonHornsRenderEntry();
     
     public static void registerEntries() {
         AccessoryModelManager.register(BOWTIE);
@@ -31,6 +32,7 @@ public class AccessoryModelRenderEntries {
         AccessoryModelManager.register(SNORKEL);
         AccessoryModelManager.register(TENGU_MASK);
         AccessoryModelManager.register(KITSUNE_MASK);
+        AccessoryModelManager.register(DEMON_HORNS);
     }
 
 }
---
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/DemonHornsRenderEntry.java
@@ -0,0 +1,40 @@
+package doggytalents.client.entity.render.layer.accessory.modelrenderentry;
+
+import doggytalents.api.registry.AccessoryInstance;
+import doggytalents.client.entity.model.DemonHornsModel;
+import doggytalents.client.entity.model.SnorkelModel;
+import doggytalents.client.entity.model.SyncedAccessoryModel;
+import doggytalents.client.entity.render.AccessoryModelManager;
+import doggytalents.common.entity.accessory.DemonHornsAccessory;
+import doggytalents.common.lib.Constants;
+import doggytalents.common.lib.Resources;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.EntityRenderersEvent.RegisterLayerDefinitions;
+
+public class DemonHornsRenderEntry extends AccessoryModelManager.Entry{
+    public static final ModelLayerLocation DOG_DEMON_HORNS = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "dog_demon_horns"), "main");
+    
+    public DemonHornsModel model;
+    @Override
+    public void initModel(Context ctx) {
+        this.model = new DemonHornsModel(ctx.bakeLayer(DOG_DEMON_HORNS));
+    }
+
+    @Override
+    public SyncedAccessoryModel getModel() {
+        return this.model;
+    }
+
+    @Override
+    public void registerLayerDef(RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(DOG_DEMON_HORNS, DemonHornsModel::createBodyLayer);
+    }
+
+    @Override
+    public ResourceLocation getResources(AccessoryInstance inst) {
+        return Resources.DEMON_HORNS;
+    }
+    
+}
