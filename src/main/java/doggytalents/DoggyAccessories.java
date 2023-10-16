dc233f7dcef3e84c3f972320c2ea5ee38298eb85
DashieDev <lamtinh.asian@gmail.com>
2023-10-16 16:58:53 +0700

    Remove obselete Armor Accessory

--- src/main/java/doggytalents/DoggyAccessories.java
+++ src/main/java/doggytalents/DoggyAccessories.java
@@ -57,41 +57,6 @@ public class DoggyAccessories {
     public static final RegistryObject<BirthdayHatAccessory> BIRTHDAY_HAT_RED_WHITE = register("birthday_hat_red_white", () -> new BirthdayHatAccessory(DoggyItems.BIRTHDAY_HAT_RED_WHITE).setModelTexture(Resources.BIRTHDAY_HAT_RED_WHITE).setAccessoryRenderType(AccessoryRenderType.MODEL));
     public static final RegistryObject<BirthdayHatAccessory> BIRTHDAY_HAT_YELLOW_BLUE = register("birthday_hat_yellow_blue", () -> new BirthdayHatAccessory(DoggyItems.BIRTHDAY_HAT_YELLOW_BLUE).setModelTexture(Resources.BIRTHDAY_HAT_YELLOW_BLUE).setAccessoryRenderType(AccessoryRenderType.MODEL));
 
-    public static final RegistryObject<Helmet> IRON_HELMET = registerHelmet("iron_helmet", () -> Items.IRON_HELMET, Resources.IRON_HELMET);
-    public static final RegistryObject<Helmet> DIAMOND_HELMET = registerHelmet("diamond_helmet", () -> Items.DIAMOND_HELMET, Resources.DIAMOND_HELMET);
-    public static final RegistryObject<Helmet> GOLDEN_HELMET = registerHelmet("golden_helmet", () -> Items.GOLDEN_HELMET, Resources.GOLDEN_HELMET);
-    public static final RegistryObject<Helmet> CHAINMAIL_HELMET = registerHelmet("chainmail_helmet", () -> Items.CHAINMAIL_HELMET, Resources.CHAINMAIL_HELMET);
-    public static final RegistryObject<Helmet> TURTLE_HELMET = registerHelmet("turtle_helmet", () -> Items.TURTLE_HELMET, Resources.TURTLE_HELMET);
-    public static final RegistryObject<Helmet> NETHERITE_HELMET = registerHelmet("netherite_helmet", () -> Items.NETHERITE_HELMET, Resources.NETHERITE_HELMET);
-
-    public static final RegistryObject<ArmourAccessory> IRON_BODY_PIECE = registerBodyPiece("iron_body_piece", () -> Items.IRON_CHESTPLATE, Resources.IRON_BODY_PIECE);
-    public static final RegistryObject<ArmourAccessory> DIAMOND_BODY_PIECE = registerBodyPiece("diamond_body_piece", () -> Items.DIAMOND_CHESTPLATE, Resources.DIAMOND_BODY_PIECE);
-    public static final RegistryObject<ArmourAccessory> GOLDEN_BODY_PIECE = registerBodyPiece("golden_body_piece", () -> Items.GOLDEN_CHESTPLATE, Resources.GOLDEN_BODY_PIECE);
-    public static final RegistryObject<ArmourAccessory> CHAINMAIL_BODY_PIECE = registerBodyPiece("chainmail_body_piece", () -> Items.CHAINMAIL_CHESTPLATE, Resources.CHAINMAIL_BODY_PIECE);
-    public static final RegistryObject<ArmourAccessory> NETHERITE_BODY_PIECE = registerBodyPiece("netherite_body_piece", () -> Items.NETHERITE_CHESTPLATE, Resources.NETHERITE_BODY_PIECE);
-
-    public static final RegistryObject<ArmourAccessory> IRON_BOOTS = registerBoots("iron_boots", () -> Items.IRON_BOOTS, Resources.IRON_BOOTS);
-    public static final RegistryObject<ArmourAccessory> DIAMOND_BOOTS = registerBoots("diamond_boots", () -> Items.DIAMOND_BOOTS, Resources.DIAMOND_BOOTS);
-    public static final RegistryObject<ArmourAccessory> GOLDEN_BOOTS = registerBoots("golden_boots", () -> Items.GOLDEN_BOOTS, Resources.GOLDEN_BOOTS);
-    public static final RegistryObject<ArmourAccessory> CHAINMAIL_BOOTS = registerBoots("chainmail_boots", () -> Items.CHAINMAIL_BOOTS, Resources.CHAINMAIL_BOOTS);
-    public static final RegistryObject<ArmourAccessory> NETHERITE_BOOTS = registerBoots("netherite_boots", () -> Items.NETHERITE_BOOTS, Resources.NETHERITE_BOOTS);
-
-    public static final RegistryObject<LeatherArmourAccessory> LEATHER_HELMET = register("leather_helmet", () -> new LeatherArmourAccessory(DoggyAccessoryTypes.HEAD, () -> Items.LEATHER_HELMET).setModelTexture(Resources.LEATHER_HELMET));
-    public static final RegistryObject<LeatherArmourAccessory> LEATHER_BODY_PIECE = register("leather_body_piece", () -> new LeatherArmourAccessory(DoggyAccessoryTypes.CLOTHING, () -> Items.LEATHER_CHESTPLATE).setModelTexture(Resources.LEATHER_BODY_PIECE));
-    public static final RegistryObject<LeatherArmourAccessory> LEATHER_BOOTS = register("leather_boots", () -> new LeatherArmourAccessory(DoggyAccessoryTypes.FEET, () -> Items.LEATHER_BOOTS).setModelTexture(Resources.LEATHER_BOOTS));
-
-    private static RegistryObject<Helmet> registerHelmet(final String name, final Supplier<? extends ItemLike> itemIn, ResourceLocation modelLocation) {
-        return ACCESSORIES.register(name, () -> new Helmet(itemIn).setModelTexture(modelLocation));
-    }
-
-    private static RegistryObject<ArmourAccessory> registerBoots(final String name, final Supplier<? extends ItemLike> itemIn, ResourceLocation modelLocation) {
-        return ACCESSORIES.register(name, () -> new ArmourAccessory(DoggyAccessoryTypes.FEET, itemIn).setModelTexture(modelLocation));
-    }
-
-    private static RegistryObject<ArmourAccessory> registerBodyPiece(final String name, final Supplier<? extends ItemLike> itemIn, ResourceLocation modelLocation) {
-        return ACCESSORIES.register(name, () -> new ArmourAccessory(DoggyAccessoryTypes.CLOTHING, itemIn).setModelTexture(modelLocation));
-    }
-
     private static <T extends Accessory> RegistryObject<T> register(final String name, final Supplier<T> sup) {
         return ACCESSORIES.register(name, sup);
     }
