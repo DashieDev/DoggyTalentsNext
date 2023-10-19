bcc10f25948079dda3adb765ee9457261262f688
DashieDev <lamtinh.asian@gmail.com>
2023-10-19 09:02:17 +0700

    New food items
    + RIce Bowl
    + Salmon Sushi
    + Onigiri

---
+++ src/main/java/doggytalents/common/item/RiceBowlItem.java
@@ -0,0 +1,24 @@
+package doggytalents.common.item;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BowlFoodItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+
+public class RiceBowlItem extends BowlFoodItem {
+    
+    public RiceBowlItem() {
+        super(
+            (new Properties()).food(
+                (new FoodProperties.Builder())
+                    .nutrition(3)
+                    .saturationMod(0.6F)
+                    .build()
+            ).stacksTo(1).craftRemainder(Items.BOWL)
+        ); 
+    }
+
+}
