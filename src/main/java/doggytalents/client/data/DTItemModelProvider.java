21dfc90b36a5d81019363684e96fc375e87e2116
MashXP <realsteelphong@gmail.com>
2023-10-20 21:52:54 +0700

    Sausage 🌭🌭🌭

--- src/main/java/doggytalents/client/data/DTItemModelProvider.java
+++ src/main/java/doggytalents/client/data/DTItemModelProvider.java
@@ -92,6 +92,7 @@ public class DTItemModelProvider extends ItemModelProvider {
         generated(DoggyItems.PLAGUE_DOC_MASK);
         generated(DoggyItems.BAKER_HAT);
         generated(DoggyItems.CHEF_HAT);
+        generated(DoggyItems.SAUSAGE);
 
         
         generated(DoggyItems.RICE_BOWL);
