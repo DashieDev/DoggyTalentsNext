9a09550b3ca9461746ce71f9fc69a68dc83d08b5
DashieDev <lamtinh.asian@gmail.com>
2023-10-24 02:53:08 +0700

    preoperly reset field when init.
    Keep in mind that remove only notice the instance being
    unlinked from the dog, the instance itself may be added back
    right after it.

--- src/main/java/doggytalents/common/talent/SwimmerDogTalent.java
+++ src/main/java/doggytalents/common/talent/SwimmerDogTalent.java
@@ -57,6 +57,7 @@ public class SwimmerDogTalent extends TalentInstance {
                 dog.getMaxHeadYRot(), 1, 1, false);
         this.navigator = 
             new DogWaterBoundNavigation(dog, dog.level());
+        swimming = false;
         // if (!(dogIn instanceof Dog)) return;
         // if (!dogIn.hasData(SWIM_AI)) {
         //     SwimmerDogGoal swimmerDogGoal = new SwimmerDogGoal((Dog)dogIn);
@@ -93,6 +94,7 @@ public class SwimmerDogTalent extends TalentInstance {
     @Override
     public void remove(AbstractDog abstractDog) {
         if (this.swimming && abstractDog instanceof Dog dog) {
+            this.swimming = false;
             stopSwimming(dog);
         }
     }
