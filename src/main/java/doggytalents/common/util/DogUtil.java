39597ce3a862185c73f89cbdfb0171c150e00b98
DashieDev <lamtinh.asian@gmail.com>
2023-10-28 01:22:05 +0700

    If dog can fly then tp to air as well.

--- src/main/java/doggytalents/common/util/DogUtil.java
+++ src/main/java/doggytalents/common/util/DogUtil.java
@@ -368,6 +368,8 @@ public class DogUtil {
                 break;
             }
         }
+        if (dog.canDogFly() && pathnodetype == BlockPathTypes.OPEN)
+            alterationWalkable = true;
         if (pathnodetype != BlockPathTypes.WALKABLE && !alterationWalkable) {
             return false;
         } else {
