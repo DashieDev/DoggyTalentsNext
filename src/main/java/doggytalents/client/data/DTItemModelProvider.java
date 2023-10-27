b764e0fdca0e9e6d42de52a38af8e38fdd80681e
MashXP <realsteelphong@gmail.com>
2023-10-27 14:57:59 +0700

    completely remove the old bd hat

--- src/main/java/doggytalents/client/data/DTItemModelProvider.java
+++ src/main/java/doggytalents/client/data/DTItemModelProvider.java
@@ -84,8 +84,6 @@ public class DTItemModelProvider extends ItemModelProvider {
         generated(DoggyItems.SNORKEL);
         generated(DoggyItems.TENGU_MASK);
         generated(DoggyItems.KITSUNE_MASK);
-        generated(DoggyItems.BIRTHDAY_HAT_YELLOW_BLUE);
-        generated(DoggyItems.BIRTHDAY_HAT_RED_WHITE);
         generated(DoggyItems.DEMON_HORNS);
         generated(DoggyItems.WITCH_HAT);
         generated(DoggyItems.PLAGUE_DOC_MASK);
