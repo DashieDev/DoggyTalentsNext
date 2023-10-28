83aa3f9c6b4b774addb9035ad4fce3e0e8496d58
DashieDev <lamtinh.asian@gmail.com>
2023-10-28 22:48:42 +0700

    Dog cannot be leashed so no need to render

--- src/main/java/doggytalents/client/entity/render/DogRenderer.java
+++ src/main/java/doggytalents/client/entity/render/DogRenderer.java
@@ -263,70 +263,8 @@ public class DogRenderer extends MobRenderer<Dog, DogModel> {
 
      public void MobRenderer_render(Dog p_115455_, float p_115456_, float p_115457_, PoseStack p_115458_, MultiBufferSource p_115459_, int p_115460_) {
         LivingEntityRenderer_render(p_115455_, p_115456_, p_115457_, p_115458_, p_115459_, p_115460_);
-        var entity = p_115455_.getLeashHolder();
-        if (entity != null) {
-           this.MobRenderer_renderLeash(p_115455_, p_115457_, p_115458_, p_115459_, entity);
-        }
      }
-
-    private void MobRenderer_renderLeash(Dog p_115462_, float p_115463_, PoseStack p_115464_, MultiBufferSource p_115465_, Entity p_115466_) {
-        p_115464_.pushPose();
-        Vec3 vec3 = p_115466_.getRopeHoldPosition(p_115463_);
-        double d0 = (double)(Mth.lerp(p_115463_, p_115462_.yBodyRotO, p_115462_.yBodyRot) * ((float)Math.PI / 180F)) + (Math.PI / 2D);
-        Vec3 vec31 = p_115462_.getLeashOffset();
-        double d1 = Math.cos(d0) * vec31.z + Math.sin(d0) * vec31.x;
-        double d2 = Math.sin(d0) * vec31.z - Math.cos(d0) * vec31.x;
-        double d3 = Mth.lerp((double)p_115463_, p_115462_.xo, p_115462_.getX()) + d1;
-        double d4 = Mth.lerp((double)p_115463_, p_115462_.yo, p_115462_.getY()) + vec31.y;
-        double d5 = Mth.lerp((double)p_115463_, p_115462_.zo, p_115462_.getZ()) + d2;
-        p_115464_.translate(d1, vec31.y, d2);
-        float f = (float)(vec3.x - d3);
-        float f1 = (float)(vec3.y - d4);
-        float f2 = (float)(vec3.z - d5);
-        float f3 = 0.025F;
-        VertexConsumer vertexconsumer = p_115465_.getBuffer(RenderType.leash());
-        Matrix4f matrix4f = p_115464_.last().pose();
-        float f4 = Mth.fastInvSqrt(f * f + f2 * f2) * 0.025F / 2.0F;
-        float f5 = f2 * f4;
-        float f6 = f * f4;
-        BlockPos blockpos = new BlockPos(p_115462_.getEyePosition(p_115463_));
-        BlockPos blockpos1 = new BlockPos(p_115466_.getEyePosition(p_115463_));
-        int i = this.getBlockLightLevel(p_115462_, blockpos);
-        int j = EntityRenderer_getBlockLightLevel(p_115466_, blockpos1);
-        int k = p_115462_.level.getBrightness(LightLayer.SKY, blockpos);
-        int l = p_115462_.level.getBrightness(LightLayer.SKY, blockpos1);
-
-        for(int i1 = 0; i1 <= 24; ++i1) {
-            MobRenderer_addVertexPair(vertexconsumer, matrix4f, f, f1, f2, i, j, k, l, 0.025F, 0.025F, f5, f6, i1, false);
-        }
-
-        for(int j1 = 24; j1 >= 0; --j1) {
-            MobRenderer_addVertexPair(vertexconsumer, matrix4f, f, f1, f2, i, j, k, l, 0.025F, 0.0F, f5, f6, j1, true);
-        }
-
-        p_115464_.popPose();
-    }
-
-    private static void MobRenderer_addVertexPair(VertexConsumer p_174308_, Matrix4f p_174309_, float p_174310_, float p_174311_, float p_174312_, int p_174313_, int p_174314_, int p_174315_, int p_174316_, float p_174317_, float p_174318_, float p_174319_, float p_174320_, int p_174321_, boolean p_174322_) {
-        float f = (float)p_174321_ / 24.0F;
-        int i = (int)Mth.lerp(f, (float)p_174313_, (float)p_174314_);
-        int j = (int)Mth.lerp(f, (float)p_174315_, (float)p_174316_);
-        int k = LightTexture.pack(i, j);
-        float f1 = p_174321_ % 2 == (p_174322_ ? 1 : 0) ? 0.7F : 1.0F;
-        float f2 = 0.5F * f1;
-        float f3 = 0.4F * f1;
-        float f4 = 0.3F * f1;
-        float f5 = p_174310_ * f;
-        float f6 = p_174311_ > 0.0F ? p_174311_ * f * f : p_174311_ - p_174311_ * (1.0F - f) * (1.0F - f);
-        float f7 = p_174312_ * f;
-        p_174308_.vertex(p_174309_, f5 - p_174319_, f6 + p_174318_, f7 + p_174320_).color(f2, f3, f4, 1.0F).uv2(k).endVertex();
-        p_174308_.vertex(p_174309_, f5 + p_174319_, f6 + p_174317_ - p_174318_, f7 - p_174320_).color(f2, f3, f4, 1.0F).uv2(k).endVertex();
-    }
-
-    private static int EntityRenderer_getBlockLightLevel(Entity p_114496_, BlockPos p_114497_) {
-        return p_114496_.isOnFire() ? 15 : p_114496_.level.getBrightness(LightLayer.BLOCK, p_114497_);
-    }
-
+     
     public void LivingEntityRenderer_render(Dog p_115308_, float p_115309_, float p_115310_, PoseStack p_115311_, MultiBufferSource p_115312_, int p_115313_) {
        // if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.client.event.RenderLivingEvent.Pre<Dog, DogModel<Dog>>(p_115308_, this, p_115310_, p_115311_, p_115312_, p_115313_))) return;
         p_115311_.pushPose();
