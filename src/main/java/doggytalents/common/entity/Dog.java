6b709d77ac4d6b4ab0e76f949662dba5113c4629
DashieDev <lamtinh.asian@gmail.com>
2023-10-29 11:15:31 +0700

    Set anim to none as default when hurt affect anim

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1539,6 +1539,8 @@ public class Dog extends AbstractDog {
                 this.setAnim(DogAnimation.HURT_1);
             } else if (source.getEntity() != null) {
                 this.setAnim(DogAnimation.HURT_2);
+            } else {
+                this.setAnim(DogAnimation.NONE);
             }
             this.setStandAnim(DogAnimation.NONE);
             this.setOrderedToSit(false);
