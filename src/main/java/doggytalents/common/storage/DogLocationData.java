fb1e886f32d5c661127504735994722aeb8d819d
DashieDev <lamtinh.asian@gmail.com>
2023-10-31 22:03:32 +0700

    Alsos save orbLocateColor.

--- src/main/java/doggytalents/common/storage/DogLocationData.java
+++ src/main/java/doggytalents/common/storage/DogLocationData.java
@@ -127,6 +127,7 @@ public class DogLocationData implements IDogData {
         if (compound.hasUUID("sessionUUID")) {
             this.sessionUUID = compound.getUUID("sessionUUID");
         }
+        this.locateColor = compound.getInt("locateOrbColor");
     }
 
     public CompoundTag write(CompoundTag compound) {
@@ -141,6 +142,7 @@ public class DogLocationData implements IDogData {
         if (this.sessionUUID != null) {
             compound.putUUID("sessionUUID", this.sessionUUID);
         }
+        compound.putInt("locateOrbColor", locateColor);
         return compound;
     }
 
