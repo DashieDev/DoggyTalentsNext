8fea99c3cc3542cf23c8be721e4c35ed2d2b5ad7
DashieDev <lamtinh.asian@gmail.com>
2023-10-31 22:35:43 +0700

    duplication detection only work if uuid preserve

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2603,6 +2603,8 @@ public class Dog extends AbstractDog {
     }
 
     private boolean detectDuplicate(CompoundTag tag) {
+        if (!ConfigHandler.SERVER.PRESERVE_UUID.get())
+            return false;
         if (!tag.contains("DTN_DupeDetect_UUID", Tag.TAG_COMPOUND))
             return false;
         var backupUUIDTag = tag.getCompound("DTN_DupeDetect_UUID");
