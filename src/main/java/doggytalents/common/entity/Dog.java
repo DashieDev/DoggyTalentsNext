86ded2619f992f2ea9c77765b4a196faedd1e58d
DashieDev <lamtinh.asian@gmail.com>
2023-11-02 02:02:00 +0700

    Exclude an odd case for now.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2621,6 +2621,12 @@ public class Dog extends AbstractDog {
         }
         if (uuid == null || ownerUUID == null)
             return false;
+
+        // Only detect if dog is not added to world while having the same uuid
+        // for now. This is a pretty unlikely case though.
+        if (this.isAddedToWorld() && uuid.equals(this.getUUID())) {
+            return false;
+        }
         
         boolean isDuplicate = false;
         
