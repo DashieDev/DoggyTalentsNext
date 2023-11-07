0c3856b39d70d772cb8b75526dc57486365beed4
DashieDev <lamtinh.asian@gmail.com>
2023-11-08 02:15:22 +0700

    Seperate STAND_IDLE Anim and the the rest
    So that the dog can natually beg and look at owner while
    slight moving body.

---
+++ src/main/java/doggytalents/common/entity/ai/DogCommonStandIdleGoal.java
@@ -0,0 +1,53 @@
+package doggytalents.common.entity.ai;
+
+import java.util.EnumSet;
+
+import doggytalents.common.entity.Dog;
+import doggytalents.common.entity.anim.DogAnimation;
+import net.minecraft.world.entity.ai.goal.Goal;
+
+public class DogCommonStandIdleGoal extends Goal {
+    
+    private Dog dog;
+    private int stopTick;
+
+    public DogCommonStandIdleGoal(Dog dog) {
+        this.dog = dog;
+        this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+    }
+
+    @Override
+    public boolean canUse() {
+        if (!dog.canDoIdileAnim()) return false;
+        if (dog.isLowHunger()) return false;
+        if (!this.dog.onGround()) return false;
+        double use_chance = 0.02f;
+        return this.dog.getRandom().nextFloat() < use_chance;
+    }
+
+    @Override
+    public boolean canContinueToUse() {
+        if (!this.dog.onGround())
+            return false;
+        if (dog.isLowHunger()) return false;
+        if (!dog.canContinueDoIdileAnim()) return false;
+        return this.dog.tickCount < this.stopTick;
+    }
+
+    @Override
+    public void start() {
+        var currentAnimation = DogAnimation.STAND_IDLE_2;
+        this.stopTick = dog.tickCount + currentAnimation.getLengthTicks();
+        this.dog.setAnimForIdle(currentAnimation);
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void stop() {
+        if (dog.getAnim() == DogAnimation.STAND_IDLE_2)
+            dog.setAnim(DogAnimation.NONE);
+    }
+}
