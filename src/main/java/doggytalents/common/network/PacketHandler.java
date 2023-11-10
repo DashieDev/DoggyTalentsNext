b6ada59cb4271acf596b81cdf61adc1ae7761b96
DashieDev <lamtinh.asian@gmail.com>
2023-11-10 15:40:35 +0700

    In game option to disable doggy torch placing torch.

--- src/main/java/doggytalents/common/network/PacketHandler.java
+++ src/main/java/doggytalents/common/network/PacketHandler.java
@@ -59,6 +59,7 @@ public final class PacketHandler {
         registerPacket(new PatrolTargetLockPacket(), PatrolTargetLockData.class);
         registerPacket(new HideArmorPacket(), HideArmorData.class);
         registerPacket(new CombatReturnStrategyPacket(), CombatReturnStrategyData.class);
+        registerPacket(new DoggyTorchPlacingTorchPacket(), DoggyTorchPlacingTorchData.class);
     }
 
     public static <MSG> void send(PacketDistributor.PacketTarget target, MSG message) {
