a2ad3e15d822b8b3dcfea9cf3bffd94ab23dce4f
DashieDev <lamtinh.asian@gmail.com>
2023-11-12 22:14:13 +0700

    Open Tools/Armor straight from DogScreen.

--- src/main/java/doggytalents/common/network/packet/data/OpenDogScreenData.java
+++ src/main/java/doggytalents/common/network/packet/data/OpenDogScreenData.java
@@ -2,8 +2,40 @@ package doggytalents.common.network.packet.data;
 
 public class OpenDogScreenData {
 
+    public enum ScreenType {
+        INVENTORY(0),
+        TOOL(1),
+        ARMOR(2);
+
+        private int id;
+
+        private ScreenType(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public static ScreenType byId(int id) {
+            return ScreenType.values()[id];
+        }
+    }
+
+    public ScreenType type = ScreenType.INVENTORY; 
+    public int dogId = -1;
+
     public OpenDogScreenData() {
         super();
+        type = ScreenType.INVENTORY;
+        dogId = -1;
     }
+
+    public OpenDogScreenData(ScreenType type, int id) {
+        this.type = type;
+        this.dogId = id;
+    }
+
+
 
 }
