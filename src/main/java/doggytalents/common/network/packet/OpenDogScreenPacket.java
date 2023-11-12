7ab8d861cade44f6874ca748e18559cdf0465bf1
DashieDev <lamtinh.asian@gmail.com>
2023-11-12 21:34:58 +0700

    Limit inventory dog view.

--- src/main/java/doggytalents/common/network/packet/OpenDogScreenPacket.java
+++ src/main/java/doggytalents/common/network/packet/OpenDogScreenPacket.java
@@ -5,11 +5,15 @@ import doggytalents.common.entity.Dog;
 import doggytalents.common.network.IPacket;
 import doggytalents.common.network.packet.data.OpenDogScreenData;
 import doggytalents.common.talent.PackPuppyTalent;
+import doggytalents.common.util.EntityUtil;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.network.NetworkEvent.Context;
 
+
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -34,6 +38,10 @@ public class OpenDogScreenPacket implements IPacket<OpenDogScreenData>  {
                 List<Dog> dogs = player.level.getEntitiesOfClass(Dog.class, player.getBoundingBox().inflate(12D, 12D, 12D),
                     (dog) -> dog.canInteract(player) && PackPuppyTalent.hasInventory(dog)
                 );
+                Collections.sort(dogs, new EntityUtil.Sorter(player.position()));
+                if (dogs.size() > PackPuppyTalent.MAX_DOG_INV_VIEW) {
+                    dogs = dogs.subList(0, PackPuppyTalent.MAX_DOG_INV_VIEW);
+                }
 				if (!dogs.isEmpty()) {
 				    Screens.openDogInventoriesScreen(player, dogs);
 				}
