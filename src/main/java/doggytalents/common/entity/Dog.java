a9af1c69c3304c4eb313638476bca6b7bce15b88
DashieDev <lamtinh.asian@gmail.com>
2023-11-22 17:21:06 +0700

    mobInteract return PASS instead of FAIL.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1093,7 +1093,7 @@ public class Dog extends AbstractDog {
             this.displayToastIfNoPermission(player);
         }
 
-        return InteractionResult.FAIL;
+        return InteractionResult.PASS;
     }
 
     private InteractionResult handleDogSitStand(Player player) {
