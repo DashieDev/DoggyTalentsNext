e7a02037d1013668a8ad30dbe4e3ec5519c16902
DashieDev <lamtinh.asian@gmail.com>
2023-12-01 23:52:40 +0700

    IDogEddible instead of tags.

--- src/main/java/doggytalents/common/item/GoldenAFiveWagyuItem.java
+++ src/main/java/doggytalents/common/item/GoldenAFiveWagyuItem.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import com.mojang.datafixers.util.Pair;
+
 import doggytalents.DoggyItemGroups;
+import doggytalents.api.inferface.AbstractDog;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -17,20 +20,20 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import doggytalents.api.enu.forward_imitate.ComponentUtil;
 
-public class GoldenAFiveWagyuItem extends Item {
+public class GoldenAFiveWagyuItem extends Item implements IDogEddible {
+
+    public static FoodProperties FOOD_PROPS = 
+        (new FoodProperties.Builder())
+            .nutrition(8)
+            .saturationMod(10F)
+            .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 1200, 1), 1)
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 1200, 1), 1)
+            .effect(() -> new MobEffectInstance(MobEffects.SATURATION, 1200, 1), 1)
+            .build();
 
     public GoldenAFiveWagyuItem() {
         super(
-            (new Properties()).food(
-                (new FoodProperties.Builder())
-                    .nutrition(8)
-                    .saturationMod(10F)
-                    .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 1200, 1), 1)
-                    .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 1200, 1), 1)
-                    .effect(() -> new MobEffectInstance(MobEffects.SATURATION, 1200, 1), 1)
-                    .build()
-            )
-            .tab(DoggyItemGroups.GENERAL)
+            (new Properties()).food(FOOD_PROPS)
         );
     }
     @Override
@@ -44,5 +47,14 @@ public class GoldenAFiveWagyuItem extends Item {
     @Override
     public Rarity getRarity(ItemStack stack) {
         return Rarity.UNCOMMON;
+    }
+    @Override
+    public float getAddedHunger(ItemStack useStack, AbstractDog dog) {
+        return FOOD_PROPS.getNutrition() * 5;
+    }
+
+    @Override
+    public List<Pair<MobEffectInstance, Float>> getAdditionalEffects(ItemStack useStack, AbstractDog dog) {
+        return FOOD_PROPS.getEffects();
     }
 }
--- src/main/java/doggytalents/common/item/SausageItem.java
+++ src/main/java/doggytalents/common/item/SausageItem.java
@@ -1,21 +1,31 @@
 package doggytalents.common.item;
 
-import doggytalents.DoggyItemGroups;
+import doggytalents.api.inferface.AbstractDog;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
-public class SausageItem extends Item {
+public class SausageItem extends Item implements IDogEddible {
+
+    public static FoodProperties FOOD_PROPS = 
+        (new FoodProperties.Builder())
+            .nutrition(3)
+            .saturationMod(0.6F)
+            .meat()
+            .build();
+
     public SausageItem() {
         super(
             (new Properties()).food(
-                (new FoodProperties.Builder())
-                    .nutrition(3)
-                    .saturationMod(0.6F)
-                    .meat()
-                    .build()
+                FOOD_PROPS
             )
             .tab(DoggyItemGroups.GENERAL)
         );
+    }
+
+    @Override
+    public float getAddedHunger(ItemStack useStack, AbstractDog dog) {
+        return FOOD_PROPS.getNutrition() * 5;
     }
     
 }
