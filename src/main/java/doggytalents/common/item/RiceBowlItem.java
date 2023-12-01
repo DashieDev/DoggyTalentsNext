b5d7497d89725861bbda5bb4ef25d2ac0bc08405
DashieDev <lamtinh.asian@gmail.com>
2023-12-02 00:00:54 +0700

    Rice bowl eating mechanic
    Dog eat and return bowl
    
    also decouple from BowlFoodItem.

--- src/main/java/doggytalents/common/item/RiceBowlItem.java
+++ src/main/java/doggytalents/common/item/RiceBowlItem.java
@@ -1,26 +1,45 @@
 package doggytalents.common.item;
 
-import doggytalents.DoggyItemGroups;
+import doggytalents.api.inferface.AbstractDog;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
-public class RiceBowlItem extends BowlFoodItem {
+public class RiceBowlItem extends Item implements IDogEddible {
+
+    public static final FoodProperties FOOD_PROPS =
+        (new FoodProperties.Builder())
+            .nutrition(3)
+            .saturationMod(0.5F)
+            .build();
     
     public RiceBowlItem() {
         super(
             (new Properties()).food(
-                (new FoodProperties.Builder())
-                    .nutrition(3)
-                    .saturationMod(0.5F)
-                    .build()
+                FOOD_PROPS
             ).stacksTo(1).craftRemainder(Items.BOWL)
             .tab(DoggyItemGroups.GENERAL)
         ); 
+    }
+
+    @Override
+    public float getAddedHunger(ItemStack useStack, AbstractDog dog) {
+        return FOOD_PROPS.getNutrition() * 5;
+    }
+
+    @Override
+    public ItemStack getReturnStackAfterDogConsume(ItemStack useStack, AbstractDog dog) {
+        return new ItemStack(Items.BOWL);
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity living) {
+        ItemStack itemstack = super.finishUsingItem(stack, level, living);
+        return living instanceof Player && ((Player)living).getAbilities().instabuild ? itemstack : new ItemStack(Items.BOWL);
     }
 
 }
