62752e913c11d8d15667cba24ebdb9b811c3a371
DashieDev <lamtinh.asian@gmail.com>
2023-12-06 18:04:14 +0700

    No natto bite render.

--- src/main/java/doggytalents/common/effects/NattoBiteEffect.java
+++ src/main/java/doggytalents/common/effects/NattoBiteEffect.java
@@ -1,11 +1,14 @@
 package doggytalents.common.effects;
 
+import java.util.function.Consumer;
+
 import doggytalents.common.entity.Dog;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.client.extensions.common.IClientMobEffectExtensions;
 
 public class NattoBiteEffect extends MobEffect {
     
@@ -19,6 +22,21 @@ public class NattoBiteEffect extends MobEffect {
 
     public void doAdditionalAttackEffects(Dog dog, LivingEntity target) {
         target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 30 * 20, 3));
+    }
+
+    @Override
+    public void initializeClient(Consumer<IClientMobEffectExtensions> consumer) {
+        consumer.accept(new IClientMobEffectExtensions() {
+            @Override
+            public boolean isVisibleInGui(MobEffectInstance instance) {
+                return false;
+            }
+
+            @Override
+            public boolean isVisibleInInventory(MobEffectInstance instance) {
+                return false;
+            }
+        });
     }
 
 }
