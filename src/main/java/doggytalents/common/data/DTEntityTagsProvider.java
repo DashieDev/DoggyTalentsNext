c445d30a8fd8ac2074aec091c68eaeb752a95424
DashieDev <lamtinh.asian@gmail.com>
2023-12-07 17:36:03 +0700

    Tags signifying entity Bm and Gb should ignore.

---
+++ src/main/java/doggytalents/common/data/DTEntityTagsProvider.java
@@ -0,0 +1,45 @@
+package doggytalents.common.data;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+import org.jetbrains.annotations.Nullable;
+
+import doggytalents.DoggyTags;
+import doggytalents.common.lib.Constants;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class DTEntityTagsProvider extends EntityTypeTagsProvider {
+
+    public DTEntityTagsProvider(PackOutput p_256095_, CompletableFuture<Provider> p_256572_,
+            @Nullable ExistingFileHelper existingFileHelper) {
+        super(p_256095_, p_256572_, Constants.MOD_ID, existingFileHelper);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getName() {
+        return "DoggyTalents Entity Tags";
+    }
+
+    @Override
+    public void addTags(HolderLookup.Provider provider) {
+        createTag(DoggyTags.DOG_SHOULD_IGNORE, () -> EntityType.ENDERMAN);
+    }
+
+    @SafeVarargs
+    private final void createTag(TagKey<EntityType<?>> tag, Supplier<? extends EntityType<?>>... entities) {
+        tag(tag).add(
+            Arrays.stream(entities)
+                .map(Supplier::get)
+                .toArray(EntityType<?>[]::new));
+    }
+    
+}
