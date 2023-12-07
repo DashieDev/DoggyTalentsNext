5b956240d6ea64667a5f47cc55bb0e9c825dba99
DashieDev <lamtinh.asian@gmail.com>
2023-12-07 12:50:08 +0700

    init the store before start the screen.

--- src/main/java/doggytalents/client/screen/AmnesiaBoneScreen/AmneisaBoneScreen.java
+++ src/main/java/doggytalents/client/screen/AmnesiaBoneScreen/AmneisaBoneScreen.java
@@ -33,8 +33,8 @@ public class AmneisaBoneScreen extends StoreConnectedScreen {
     public static void open(Dog dog) {
         var mc = Minecraft.getInstance();
         var screen = new AmneisaBoneScreen(dog);
-        mc.setScreen(screen);
         Store.get(screen);
+        mc.setScreen(screen);
     }
 
     @Override
