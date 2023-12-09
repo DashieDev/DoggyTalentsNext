42d086a1fcfea5c0a65690a8df4c29528453132f
DashieDev <lamtinh.asian@gmail.com>
2023-12-09 17:16:49 +0700

    Refactor and modify water path get
    If dog cannot breathe underwater but
    canSwim, omit only when not low on air.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4155,13 +4155,8 @@ public class Dog extends AbstractDog {
         default:
             break;
         case WATER:
-            if (isDogFollowingSomeone())
-                return 0;
-            if (this.canSwimUnderwater() && this.isInWater())
-                return 0;
-            break;
         case WATER_BORDER:
-            if (this.canSwimUnderwater() && this.isInWater())
+            if (shouldDogOmitWaterPathWeight())
                 return 0;
             break;
         case LAVA:
@@ -4172,6 +4167,18 @@ public class Dog extends AbstractDog {
             break;
         }
         return super.getPathfindingMalus(type);
+    }
+
+    private boolean shouldDogOmitWaterPathWeight() {
+        if (isDogFollowingSomeone())
+            return true;
+        if (!this.isInWater())
+            return false;
+        if (this.canBreatheUnderwater())
+            return true;
+        if (this.canSwimUnderwater() && !this.isLowAirSupply())
+            return true;
+        return false;
     }
 
     public boolean shouldDogBlockFloat() {
