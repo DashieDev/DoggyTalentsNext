aaee82315d61df764c4b5a5630fbb16663a1f636
DashieDev <lamtinh.asian@gmail.com>
2023-12-12 00:34:25 +0700

    Thread safe just be sure.

--- src/main/java/doggytalents/DoggyTalentsNext.java
+++ src/main/java/doggytalents/DoggyTalentsNext.java
@@ -129,8 +129,9 @@ public class DoggyTalentsNext {
         FoodHandler.registerDynPredicate(HappyEaterTalent.INNER_DYN_PRED);
         //InteractHandler.registerHandler(new HelmetInteractHandler());
         ConfigHandler.initTalentConfig();
-        Dog.initDataParameters();
+            
         event.enqueueWork(() -> {
+            Dog.initDataParameters();
             GarbageChunkCollector.init();
         });
     }
