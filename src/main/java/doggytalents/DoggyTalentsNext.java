a45983eb34f8de3a059b9143d4caa63ac28e4232
DashieDev <lamtinh.asian@gmail.com>
2023-12-14 01:24:49 +0700

    Sake brew 🥴

--- src/main/java/doggytalents/DoggyTalentsNext.java
+++ src/main/java/doggytalents/DoggyTalentsNext.java
@@ -134,6 +134,7 @@ public class DoggyTalentsNext {
         event.enqueueWork(() -> {
             Dog.initDataParameters();
             DoggyAdvancementTriggers.registerAll();
+            DoggyBrewingRecipes.registerAll();
             ConfigHandler.initTalentConfig();
             GarbageChunkCollector.init();
         });
