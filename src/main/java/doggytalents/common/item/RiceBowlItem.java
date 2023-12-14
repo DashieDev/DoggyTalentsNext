a80c627632fa7b42c72e848953eccfa98fcd344e
MashXP <realsteelphong@gmail.com>
2023-12-14 16:50:28 +0700

    improve clarity to rice items

--- src/main/java/doggytalents/common/item/RiceBowlItem.java
+++ src/main/java/doggytalents/common/item/RiceBowlItem.java
@@ -1,12 +1,19 @@
 package doggytalents.common.item;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import doggytalents.api.inferface.AbstractDog;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 public class RiceBowlItem extends DogEddibleBowlFoodItem {
@@ -23,5 +30,12 @@ public class RiceBowlItem extends DogEddibleBowlFoodItem {
     public boolean alwaysEatWhenDogConsume(AbstractDog dog) {
         return true;
     }
-
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
+    }
 }
