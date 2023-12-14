35b78788550eaef5b448ca0ef92f02f962430881
MashXP <realsteelphong@gmail.com>
2023-12-14 16:50:28 +0700

    improve clarity to rice items

--- src/main/java/doggytalents/common/item/UncookedRiceBowlItem.java
+++ src/main/java/doggytalents/common/item/UncookedRiceBowlItem.java
@@ -1,11 +1,28 @@
 package doggytalents.common.item;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 public class UncookedRiceBowlItem extends Item {
 
     public UncookedRiceBowlItem() {
         super(new Properties());
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
     }
     
 }
