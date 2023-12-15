31ea989c325ad71ab45bee1e8db5faa11bf63e8f
DashieDev <lamtinh.asian@gmail.com>
2023-12-16 02:35:14 +0700

    remove configurable noDogCurious,.

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/MainInfoView/view/EditInfoView.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/element/view/MainInfoView/view/EditInfoView.java
@@ -20,7 +20,6 @@ import doggytalents.common.network.packet.data.CrossOriginTpData;
 import doggytalents.common.network.packet.data.DogAutoMountData;
 import doggytalents.common.network.packet.data.DogForceSitData;
 import doggytalents.common.network.packet.data.DogNameData;
-import doggytalents.common.network.packet.data.DogNoCuriousData;
 import doggytalents.common.network.packet.data.DogObeyData;
 import doggytalents.common.network.packet.data.DogRegardTeamPlayersData;
 import doggytalents.common.network.packet.data.FriendlyFireData;
@@ -231,30 +230,6 @@ public class EditInfoView extends AbstractElement {
 
         scroll.addChildren(
             new ButtonOptionEntry(scroll, getScreen(), 
-                new FlatButton(
-                    0, 0,
-                    40, 20, Component.literal("" + this.dog.noDogCurious()), 
-                    b -> {
-                        Boolean newVal = !this.dog.noDogCurious();
-                        b.setMessage(Component.literal("" + newVal));
-                        this.requestNoDogCurious(newVal);
-                    }     
-                ) {
-                    @Override
-                    public void render(GuiGraphics graphics, int mouseX, int mouseY, float pTicks) {
-                        super.render(graphics, mouseX, mouseY, pTicks);
-                        if (this.isHovered) {
-                            ToolTipOverlayManager.get().setComponents(ScreenUtil.splitInto(I18n.get("doggui.no_dog_curious.help"), 150, font));
-                        }
-                    }
-                },
-                I18n.get("doggui.no_dog_curious")
-            )
-            .init()
-        );
-
-        scroll.addChildren(
-            new ButtonOptionEntry(scroll, getScreen(), 
                 new LowHealthStrategySwitch(
                     0, 0, 
                     100, 20, dog, font, getScreen()
@@ -339,12 +314,6 @@ public class EditInfoView extends AbstractElement {
         PacketHandler
             .send(PacketDistributor.SERVER.noArg(), 
             new DogAutoMountData(this.dog.getId(), val));
-    }
-
-    private void requestNoDogCurious(boolean val) {
-        PacketHandler
-            .send(PacketDistributor.SERVER.noArg(), 
-            new DogNoCuriousData(this.dog.getId(), val));
     }
 
     private static class NewnameEntry extends AbstractElement {
