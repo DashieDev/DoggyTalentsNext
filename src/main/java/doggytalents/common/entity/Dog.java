bf0e5a838c5df730c18825c6fc9d4edcdc231799
DashieDev <lamtinh.asian@gmail.com>
2023-12-15 13:47:37 +0700

    stooped getArmorSlot bug.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4279,7 +4279,7 @@ public class Dog extends AbstractDog {
     }
 
     public Iterable<ItemStack> getArmorSlots() {
-        if (!this.canDogUseTools())
+        if (!this.canDogWearArmor())
             return List.of();
         return this.dogArmors.armors();
     }
