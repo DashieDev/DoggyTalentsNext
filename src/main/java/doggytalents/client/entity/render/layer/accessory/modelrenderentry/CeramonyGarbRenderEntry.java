03c342479e455ed266d93ca13a4c1ecdc8d33b97
DashieDev <lamtinh.asian@gmail.com>
2023-12-16 23:28:35 +0700

    Finalize ceragarb.

--- src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/CeramonyGarbRenderEntry.java
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/CeramonyGarbRenderEntry.java
@@ -1,14 +1,23 @@
 package doggytalents.client.entity.render.layer.accessory.modelrenderentry;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import doggytalents.api.inferface.IColoredObject;
 import doggytalents.api.registry.AccessoryInstance;
 import doggytalents.client.entity.model.CeramonyGarbModel;
 import doggytalents.client.entity.model.LabCoatModel;
 import doggytalents.client.entity.model.SyncedAccessoryModel;
+import doggytalents.client.entity.model.dog.DogModel;
+import doggytalents.client.entity.render.AccessoryModelManager;
 import doggytalents.client.entity.render.AccessoryModelManager.Entry;
+import doggytalents.client.entity.render.layer.accessory.DefaultAccessoryRenderer;
+import doggytalents.common.entity.Dog;
 import doggytalents.common.lib.Constants;
 import doggytalents.common.lib.Resources;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.event.EntityRenderersEvent.RegisterLayerDefinitions;
 
@@ -38,6 +47,28 @@ public class CeramonyGarbRenderEntry extends Entry{
     @Override
     public boolean isDyable() {
         return true;
+    }
+
+    @Override
+    public void renderAccessory(RenderLayer<Dog, DogModel> layer, PoseStack poseStack, MultiBufferSource buffer,
+            int packedLight, Dog dog, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks,
+            float netHeadYaw, float headPitch, AccessoryInstance inst) {
+        
+        var model = this.getModel();
+            var dogModel = layer.getParentModel();
+            dogModel.copyPropertiesTo(model);
+            model.prepareMobModel(dog, limbSwing, limbSwingAmount, partialTicks);
+            model.setupAnim(dog, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+            model.sync(dogModel);
+            
+            float[] color = new float[]{1.0f, 1.0f, 1.0f};
+            if (inst instanceof IColoredObject coloredObject)
+                color = coloredObject.getColor();
+            
+            DefaultAccessoryRenderer.renderTranslucentModel(model, Resources.CERA_GARB, 
+                poseStack, buffer, packedLight, dog, 1f, 1f, 1f, 1f);
+            DefaultAccessoryRenderer.renderTranslucentModel(model, Resources.CERA_GARB_OVERLAY, 
+                poseStack, buffer, packedLight, dog, color[0], color[1], color[2], 1f);
     }
 
     
