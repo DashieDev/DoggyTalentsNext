d1b2202b83ea61443b658ec9d969d1646fd374d1
DashieDev <lamtinh.asian@gmail.com>
2023-12-17 21:42:04 +0700

    remove debug.

--- src/main/java/doggytalents/common/entity/anim/DogAnimationManager.java
+++ src/main/java/doggytalents/common/entity/anim/DogAnimationManager.java
@@ -1,7 +1,5 @@
 package doggytalents.common.entity.anim;
 
-
-import doggytalents.ChopinLogger;
 import doggytalents.common.config.ConfigHandler;
 import doggytalents.common.entity.Dog;
 import net.minecraft.util.Mth;
@@ -91,8 +89,7 @@ public class DogAnimationManager {
         int latencyAbs = Mth.abs(correctTime - currentTime);
         if (latencyAbs <= maxLatencyAllowed)
             return;
-
-        ChopinLogger.lwn(dog, "Resolving a latency of " + latencyAbs + " ticks");
+            
         var anim = dog.getAnim();
         this.animationTime = Mth.clamp(correctTime, 0, anim.getLengthTicks());
         int correctPassedTime = 
