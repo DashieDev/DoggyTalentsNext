b4580812763e410fb00c91ae50d08d2fc082ccda
DashieDev <lamtinh.asian@gmail.com>
2023-12-18 17:08:34 +0700

    reset wetsource when incap too!

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2315,6 +2315,9 @@ public class Dog extends AbstractDog {
         this.incapacitatedMananger.setIncapMsg(
             source.getLocalizedDeathMessage(this).getString()
         );
+
+        this.wetSource = WetSource.NONE;
+        this.finishShaking();
     }
 
     private void sendIncapacitatedMsg(LivingEntity owner, DamageSource source) {
