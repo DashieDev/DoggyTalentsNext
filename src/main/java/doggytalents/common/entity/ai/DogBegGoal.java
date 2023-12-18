a25a330d18160f8533dd5da1d6a98dce8a43077b
DashieDev <lamtinh.asian@gmail.com>
2023-12-18 21:53:04 +0700

    Dog only beg at owner.

--- src/main/java/doggytalents/common/entity/ai/DogBegGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogBegGoal.java
@@ -6,6 +6,7 @@ import doggytalents.common.entity.Dog;
 import doggytalents.common.entity.Dog.RestingState;
 import doggytalents.common.entity.anim.DogAnimation;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
@@ -17,45 +18,44 @@ import java.util.EnumSet;
 public class DogBegGoal extends Goal {
 
     private final Dog dog;
-    private Player player;
-    private final Level world;
     private final float minPlayerDistance;
+    private LivingEntity owner;
     private int timeoutCounter;
-    private final TargetingConditions playerPredicate;
 
     public DogBegGoal(Dog wolf, float minDistance) {
         this.dog = wolf;
-        this.world = wolf.level;
+
         this.minPlayerDistance = minDistance;
-        this.playerPredicate = TargetingConditions.forNonCombat().range(minDistance); // TODO check
         this.setFlags(EnumSet.of(Goal.Flag.LOOK));
     }
 
     @Override
     public boolean canUse() {
-        this.player = this.world.getNearestPlayer(this.playerPredicate, this.dog);
-        if (this.player == null) 
-            return false;
-        if (!this.player.isAlive() || this.player.isSpectator()) {
-            return false;
-        }
         if (dog.isDogResting())
-            return false;
-        if (!this.hasTemptationItemInHand(player))
             return false;
         if (!this.dog.getAnim().freeHead() && this.dog.getAnim() != DogAnimation.NONE)
             return false;
+        var owner = dog.getOwner();
+        if (owner == null)
+            return false;
+        if (!owner.isAlive() || owner.isSpectator())
+            return false;
+        if (owner.distanceToSqr(dog) > minPlayerDistance * minPlayerDistance)
+            return false;
+        if (!this.hasTemptationItemInHand(owner))
+            return false;
+        this.owner = owner;
         return true;
     }
 
     @Override
     public boolean canContinueToUse() {
-        if (!this.player.isAlive() || this.player.isSpectator()) {
+        if (!this.owner.isAlive() || this.owner.isSpectator()) {
             return false;
-        } else if (this.dog.distanceToSqr(this.player) > this.minPlayerDistance * this.minPlayerDistance) {
+        } else if (this.dog.distanceToSqr(this.owner) > this.minPlayerDistance * this.minPlayerDistance) {
             return false;
         } else {
-            return this.timeoutCounter > 0 && this.hasTemptationItemInHand(this.player);
+            return this.timeoutCounter > 0 && this.hasTemptationItemInHand(this.owner);
         }
     }
 
@@ -68,16 +68,16 @@ public class DogBegGoal extends Goal {
     @Override
     public void stop() {
         this.dog.setBegging(false);
-        this.player = null;
+        this.owner = null;
     }
 
     @Override
     public void tick() {
-        this.dog.getLookControl().setLookAt(this.player.getX(), this.player.getEyeY(), this.player.getZ(), 10.0F, this.dog.getMaxHeadXRot());
+        this.dog.getLookControl().setLookAt(this.owner.getX(), this.owner.getEyeY(), this.owner.getZ(), 10.0F, this.dog.getMaxHeadXRot());
         --this.timeoutCounter;
     }
 
-    private boolean hasTemptationItemInHand(Player player) {
+    private boolean hasTemptationItemInHand(LivingEntity player) {
         for (InteractionHand hand : InteractionHand.values()) {
             ItemStack itemstack = player.getItemInHand(hand);
             if (itemstack.is(this.dog.isTame() ? DoggyTags.BEG_ITEMS_TAMED : DoggyTags.BEG_ITEMS_UNTAMED)) {
