5148924c6db7f6ab4da8ed061df5dfbdc3b1965d
MashXP <realsteelphong@gmail.com>
2023-12-18 11:14:46 +0700

    Egg sandwich description tamago SANDOOOO!!!

--- src/main/java/doggytalents/common/item/EggSandwichItem.java
+++ src/main/java/doggytalents/common/item/EggSandwichItem.java
@@ -2,15 +2,21 @@ package doggytalents.common.item;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.mojang.datafixers.util.Pair;
 
 import doggytalents.DoggyItemGroups;
 import doggytalents.api.inferface.AbstractDog;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 public class EggSandwichItem extends DogEddibleItem {
 
@@ -21,6 +27,14 @@ public class EggSandwichItem extends DogEddibleItem {
                 .saturationMod(0.6F)
                 .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 60, 1), 1)
         );
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
     }
 
     @Override
