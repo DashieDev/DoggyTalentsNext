48a505c1d3672e4d2006b9271ceb765569a80135
DashieDev <lamtinh.asian@gmail.com>
2023-12-20 01:01:48 +0700

    Fix non-owner be able to change dog Name thru nametags.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2066,6 +2066,22 @@ public class Dog extends AbstractDog {
         }
     }
 
+    private boolean authorizedChangingName = false;
+
+    @Override
+    public void setCustomName(@Nullable Component name) {
+        if (!authorizedChangingName)
+            return;
+        authorizedChangingName = false;
+        super.setCustomName(name);
+    }    
+
+    public void setDogCustomName(@Nullable Component name) {
+        this.authorizedChangingName = true;
+        this.setCustomName(name);
+        this.authorizedChangingName = false;
+    }
+
     @Override // blockAttackFromPlayer
     public boolean skipAttackInteraction(Entity entityIn) {
 
@@ -2535,6 +2551,7 @@ public class Dog extends AbstractDog {
     public void load(CompoundTag compound) {
 
         this.authorizedChangingOwner = true;
+        this.authorizedChangingName = true;
 
         // DataFix uuid entries and attribute ids
         try {
@@ -2618,6 +2635,7 @@ public class Dog extends AbstractDog {
         super.load(compound);
 
         this.authorizedChangingOwner = false;
+        this.authorizedChangingName = false;
     }
 
     @Override
@@ -3799,7 +3817,7 @@ public class Dog extends AbstractDog {
             .map(goal -> { goal.stop(); return goal; } );
         this.setOrderedToSit(false);
         this.setHealth(8);
-        this.setCustomName(null);
+        this.setDogCustomName(null);
 
         this.modifyTalent(x -> x.clear());
 
