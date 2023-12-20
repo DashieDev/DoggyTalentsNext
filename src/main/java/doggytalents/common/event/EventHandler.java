9ac7d67c6f3190f1041686249bb707f8cc6850dd
DashieDev <lamtinh.asian@gmail.com>
2023-12-20 13:32:46 +0700

    Only listen to projectile on serverside to prevent null.

--- src/main/java/doggytalents/common/event/EventHandler.java
+++ src/main/java/doggytalents/common/event/EventHandler.java
@@ -195,6 +195,13 @@ public class EventHandler {
 
     @SubscribeEvent
     public void onProjectileHit(final ProjectileImpactEvent event) {
+        var levelChecker = event.getProjectile();
+        if (levelChecker == null)
+            return;
+        var level = levelChecker.level();
+        if (level.isClientSide)
+            return;
+
         var hitResult = event.getRayTraceResult();
         if (!(hitResult instanceof EntityHitResult)) return;
 
