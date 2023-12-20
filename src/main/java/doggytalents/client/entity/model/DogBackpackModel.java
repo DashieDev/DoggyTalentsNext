d4ad3e81a2a10d008e6c4807bd8888a016a8ce20
DashieDev <lamtinh.asian@gmail.com>
2023-12-20 20:48:50 +0700

    Resolve texture conflict backpack model.

--- src/main/java/doggytalents/client/entity/model/DogBackpackModel.java
+++ src/main/java/doggytalents/client/entity/model/DogBackpackModel.java
@@ -30,8 +30,8 @@ public class DogBackpackModel extends SyncedAccessoryModel {
         var var2 = var1.addOrReplaceChild("body", CubeListBuilder.create()
                 , PartPose.offsetAndRotation(0.0F, 14.0F, 2.0F, 1.5707964F, 0.0F, 0.0F));
 
-        var2.addOrReplaceChild("right_chest", CubeListBuilder.create().texOffs(52, 0).addBox(2.0F, -1F, 0F, 2, 7, 4), PartPose.ZERO);
-        var2.addOrReplaceChild("left_chest", CubeListBuilder.create().texOffs(52, 0).addBox(-4.0F, -1F, 0F, 2F, 7F, 4F), PartPose.ZERO);
+        var2.addOrReplaceChild("right_chest", CubeListBuilder.create().texOffs(52, 0).addBox(2.0F - 0.01F, -1F, 0F, 2, 7, 4), PartPose.ZERO);
+        var2.addOrReplaceChild("left_chest", CubeListBuilder.create().texOffs(52, 0).addBox(-4.0F - 0.01F, -1F, 0F, 2F, 7F, 4F), PartPose.ZERO);
 
         return LayerDefinition.create(var0, 64, 32);
     }
