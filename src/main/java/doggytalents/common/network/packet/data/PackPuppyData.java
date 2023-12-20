0787a783a75fa282ee0f43b1dc7fb5a093853bad
DashieDev <lamtinh.asian@gmail.com>
2023-12-20 20:51:39 +0700

    In game option to make pack pup offer food and pickup items.

---
+++ src/main/java/doggytalents/common/network/packet/data/PackPuppyData.java
@@ -0,0 +1,35 @@
+package doggytalents.common.network.packet.data;
+
+public class PackPuppyData extends DogData {
+    
+    public Type type;
+    public boolean val;
+
+    public PackPuppyData(int entityId, Type type, boolean val) {
+        super(entityId);
+        this.type = type == null ? Type.RENDER_CHEST : type;
+        this.val = val;
+    }
+
+    public static enum Type {
+        RENDER_CHEST(0),
+        PICKUP_NEARBY(1),
+        OFFER_FOOD(2);
+
+        private int id;
+
+        private Type(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public static Type fromId(int id) {
+            var values = Type.values();
+            return values[id];
+        }
+    }
+
+}
