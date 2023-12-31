f966cfb0f7ff94b34f3224f229cfbebe5d32a3ee
MashXP <realsteelphong@gmail.com>
2023-12-31 08:34:15 +0700

    Doggy Contacts 👁👁👁👁👁👁👁👁

---
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/ContactsRenderEntry.java
@@ -0,0 +1,42 @@
+package doggytalents.client.entity.render.layer.accessory.modelrenderentry;
+
+import doggytalents.api.registry.AccessoryInstance;
+import doggytalents.client.entity.model.BowTieModel;
+import doggytalents.client.entity.model.ContactsModel;
+import doggytalents.client.entity.model.SyncedAccessoryModel;
+import doggytalents.client.entity.render.AccessoryModelManager.Entry;
+import doggytalents.common.lib.Constants;
+import doggytalents.common.lib.Resources;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.EntityRenderersEvent.RegisterLayerDefinitions;
+
+public class ContactsRenderEntry extends Entry{
+    public static final ModelLayerLocation DOG_CONTACTS = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "dog_contacts"), "main");
+
+    private ContactsModel model;
+    @Override
+    public void initModel(Context ctx) {
+        this.model = new ContactsModel(ctx.bakeLayer(DOG_CONTACTS));
+    }
+
+    @Override
+    public SyncedAccessoryModel getModel() {
+        return this.model;
+    }
+
+    @Override
+    public void registerLayerDef(RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(DOG_CONTACTS, ContactsModel::createBodyLayer);
+    }
+
+    @Override
+    public ResourceLocation getResources(AccessoryInstance inst) {
+        return Resources.DOGGY_CONTACTS;
+    }
+    @Override
+    public boolean isDyable() {
+        return true;
+    }
+}
