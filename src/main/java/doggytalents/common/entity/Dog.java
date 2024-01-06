3fb98ff29b9f9739992c9f8801011805cf98d38e
DashieDev <lamtinh.asian@gmail.com>
2024-01-06 17:33:24 +0700

    do inital attack Effects.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1805,6 +1805,9 @@ public class Dog extends AbstractDog {
     @Override
     public boolean doHurtTarget(Entity target) {
         
+        for (IDogAlteration alter : this.alterations) {
+            alter.doInitialAttackEffects(this, target);
+        }
 
         var attackDamageInst = this.getAttribute(Attributes.ATTACK_DAMAGE);
         var critDamageInst = this.getAttribute(DoggyAttributes.CRIT_CHANCE.get());
