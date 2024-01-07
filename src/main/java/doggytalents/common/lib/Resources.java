4a1b4ad181bc618790a9a655ba87c2dfc5215927
DashieDev <lamtinh.asian@gmail.com>
2024-01-07 18:50:56 +0700

    Propeollar 👒

--- src/main/java/doggytalents/common/lib/Resources.java
+++ src/main/java/doggytalents/common/lib/Resources.java
@@ -66,6 +66,7 @@ public class Resources {
     public static final ResourceLocation CERE_GARB = getEntity("dog", "ceremonial_garb");
     public static final ResourceLocation CERE_GARB_OVERLAY = getEntity("dog", "ceremonial_garb_overlay");
     public static final ResourceLocation DOGGY_CONTACTS = getEntity("dog", "doggy_contacts");
+    public static final ResourceLocation DOG_PROPELLAR = getEntity("dog", "dog_propeller_hat");
 
     public static final ResourceLocation SUPERDOG_SUIT = getEntity("dog", "superdog_suit");
     public static final ResourceLocation FLYING_CAPE = getEntity("dog", "flying_cape");
