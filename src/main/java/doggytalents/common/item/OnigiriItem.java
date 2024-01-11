d693e52140b61b849fed7daacddc3eeb2628ee5a
MashXP <realsteelphong@gmail.com>
2024-01-11 15:47:12 +0700

    update desc for onigiri

--- src/main/java/doggytalents/common/item/OnigiriItem.java
+++ src/main/java/doggytalents/common/item/OnigiriItem.java
@@ -1,9 +1,17 @@
 package doggytalents.common.item;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import doggytalents.api.inferface.AbstractDog;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 public class OnigiriItem extends DogEddibleItem {
 
@@ -13,6 +21,14 @@ public class OnigiriItem extends DogEddibleItem {
                 .nutrition(7)
                 .saturationMod(0.5F)
         );
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
     }
     
 }
