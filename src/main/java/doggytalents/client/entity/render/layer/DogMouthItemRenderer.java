ccc66846436f69303de0b693e4ba0bf0af27938e
DashieDev <lamtinh.asian@gmail.com>
2024-01-15 14:43:07 +0700

    SyncedHeadFunctionRender.

--- src/main/java/doggytalents/client/entity/render/layer/DogMouthItemRenderer.java
+++ src/main/java/doggytalents/client/entity/render/layer/DogMouthItemRenderer.java
@@ -6,7 +6,7 @@ import com.mojang.math.Vector3f;
 import doggytalents.DoggyTalents;
 import doggytalents.api.inferface.IThrowableItem;
 import doggytalents.client.ClientSetup;
-import doggytalents.client.entity.model.SyncedItemModel;
+import doggytalents.client.entity.model.SyncedRenderFunctionWithHeadModel;
 import doggytalents.client.entity.model.dog.DogModel;
 import doggytalents.client.entity.render.DogRenderer;
 import doggytalents.common.config.ConfigHandler;
@@ -18,6 +18,8 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.DiggerItem;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.SwordItem;
@@ -25,12 +27,12 @@ import net.minecraft.world.item.SwordItem;
 public class DogMouthItemRenderer extends RenderLayer<Dog, DogModel> {
     
     private ItemInHandRenderer itemInHandRenderer;
-    private SyncedItemModel itemSyncer;
+    private SyncedRenderFunctionWithHeadModel itemSyncer;
 
     public DogMouthItemRenderer(RenderLayerParent dogRendererIn, EntityRendererProvider.Context ctx) {
         super(dogRendererIn);
         this.itemInHandRenderer = ctx.getItemInHandRenderer();
-        itemSyncer = new SyncedItemModel(ctx.bakeLayer(ClientSetup.DOG_MOUTH_ITEM), itemInHandRenderer);
+        itemSyncer = new SyncedRenderFunctionWithHeadModel(ctx.bakeLayer(ClientSetup.DOG_SYNCED_FUNCTION_WITH_HEAD));
     }
 
     @Override
@@ -53,6 +55,23 @@ public class DogMouthItemRenderer extends RenderLayer<Dog, DogModel> {
 
         model.copyPropertiesTo(itemSyncer);
         itemSyncer.sync(model);
-        itemSyncer.startRenderFromRoot(matrixStack, bufferSource, packedLight, dog, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, stack);
+        itemSyncer.startRenderFromRoot(matrixStack, matrixStack1 -> {
+            renderItem(matrixStack1, bufferSource, packedLight, dog, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, stack);
+        });
+    }
+
+    public void renderItem(PoseStack stack, MultiBufferSource bufferSource, int packedLight, Dog dog, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack itemStack) {
+        stack.pushPose();
+        stack.translate(-0.025F, 0.125F, -0.32F);
+        var item = itemStack.getItem();
+
+        if (item instanceof SwordItem || item instanceof DiggerItem) {
+            stack.translate(0.25, 0, 0);
+        }
+        stack.mulPose(Axis.YP.rotationDegrees(45.0F));
+        stack.mulPose(Axis.XP.rotationDegrees(90.0F));
+
+        this.itemInHandRenderer.renderItem(dog, itemStack, ItemDisplayContext.GROUND, false, stack, bufferSource, packedLight);
+        stack.popPose();
     }
 }
