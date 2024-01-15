18de74c9b0e2e082368bfe56a065d6201aeff843
DashieDev <lamtinh.asian@gmail.com>
2024-01-15 16:33:51 +0700

    alternative armor models and option to use player helmet model.

---
+++ src/main/java/doggytalents/client/entity/render/layer/DogArmorHelmetAltModel.java
@@ -0,0 +1,24 @@
+package doggytalents.client.entity.render.layer;
+
+import doggytalents.common.entity.Dog;
+import net.minecraft.client.model.HumanoidArmorModel;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
+public class DogArmorHelmetAltModel {
+ 
+    private HumanoidArmorModel<Dog> helmetModel;
+
+    public DogArmorHelmetAltModel(EntityRendererProvider.Context ctx) {
+        this.helmetModel = new HumanoidArmorModel<>(ctx.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR));
+        this.helmetModel.setAllVisible(false);
+        this.helmetModel.head.visible = true;
+        this.helmetModel.young = false;
+    }
+
+    public Model getModel() {
+        return helmetModel;
+    }    
+
+}
