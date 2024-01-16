88feb914b94ecfeefaa3db6841a10d15f5eb5381
DashieDev <lamtinh.asian@gmail.com>
2024-01-16 15:27:58 +0700

    🎩🎩Added Fedora and FlatCap!

---
+++ src/main/java/doggytalents/client/entity/render/layer/accessory/modelrenderentry/FlatCapRenderEntry.java
@@ -0,0 +1,45 @@
+package doggytalents.client.entity.render.layer.accessory.modelrenderentry;
+
+import doggytalents.api.registry.AccessoryInstance;
+import doggytalents.client.entity.model.FlatCapModel;
+import doggytalents.client.entity.model.SyncedAccessoryModel;
+import doggytalents.client.entity.render.AccessoryModelManager.Entry;
+import doggytalents.common.lib.Constants;
+import doggytalents.common.lib.Resources;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.EntityRenderersEvent.RegisterLayerDefinitions;
+
+public class FlatCapRenderEntry extends Entry {
+    
+    public static final ModelLayerLocation DOG_FLATCAP = new ModelLayerLocation(new ResourceLocation(Constants.MOD_ID, "dog_flatcap"), "main");
+    
+    public FlatCapModel model;
+
+    @Override
+    public void initModel(Context ctx) {
+        this.model = new FlatCapModel(ctx.bakeLayer(DOG_FLATCAP));
+    }
+
+    @Override
+    public SyncedAccessoryModel getModel() {
+        return this.model;
+    }
+
+    @Override
+    public void registerLayerDef(RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(DOG_FLATCAP, FlatCapModel::createLayer);
+    }
+
+    @Override
+    public ResourceLocation getResources(AccessoryInstance inst) {
+        return Resources.DOG_FLATCAP;
+    }
+
+    @Override
+    public boolean isDyable() {
+        return true;
+    }
+
+}
