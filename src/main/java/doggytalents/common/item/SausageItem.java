9f864f04d0d8ab19858e5024ee78f7bbd3f6c072
MashXP <realsteelphong@gmail.com>
2024-01-18 14:10:57 +0700

    Mistake in sausage

--- src/main/java/doggytalents/common/item/SausageItem.java
+++ src/main/java/doggytalents/common/item/SausageItem.java
@@ -10,7 +10,7 @@ public class SausageItem extends DogEddibleItem {
     public SausageItem() {
         super(
             b -> b
-                .nutrition(3)
+                .nutrition(8)
                 .saturationMod(0.6F)
         );
     }
