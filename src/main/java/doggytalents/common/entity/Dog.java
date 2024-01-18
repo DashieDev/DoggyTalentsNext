add60cfc3c7c92fa605d64ce21999c6ed1c21909
DashieDev <lamtinh.asian@gmail.com>
2024-01-18 23:50:32 +0700

    remove untame() expose.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3868,7 +3868,6 @@ public class Dog extends AbstractDog {
         return this.objects.containsKey(key.getIndex());
     }
 
-    @Override
     public void untame() {
         this.navigation.stop();
         this.clearTriggerableAction();
