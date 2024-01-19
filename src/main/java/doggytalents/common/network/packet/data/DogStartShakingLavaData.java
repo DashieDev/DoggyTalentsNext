190a51f8acf9094d693131814c644f44bc198c14
DashieDev <lamtinh.asian@gmail.com>
2024-01-19 22:58:40 +0700

    Use packets to handle dog shaking instead of entity events.

--- src/main/java/doggytalents/common/network/packet/data/DogStartShakingLavaData.java
+++
@@ -1,8 +0,0 @@
-package doggytalents.common.network.packet.data;
-
-public class DogStartShakingLavaData {
-    public int dogId;
-    public DogStartShakingLavaData(int dogId) {
-        this.dogId = dogId;
-    }
-}
