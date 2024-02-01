ac52093827da153282a0cfef7be6120641c0ec1a
DashieDev <lamtinh.asian@gmail.com>
2024-02-01 16:57:02 +0700

    Intention on allowing incap to below -64

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2428,7 +2428,7 @@ public class Dog extends AbstractDog {
         this.setHealth(1);
         this.setMode(EnumMode.INCAPACITATED);
         this.setDogHunger(0);
-        this.setDogIncapValue(this.getMinDogIncapValue());
+        this.setDogIncapValue(this.getDefaultInitIncapVal());
         
         this.unRide();
         createAndSetIncapSyncState(source);
@@ -3412,7 +3412,7 @@ public class Dog extends AbstractDog {
     }
 
     @Override
-    public int getMinDogIncapValue() {
+    public int getDefaultInitIncapVal() {
         return -64;
     }
 
