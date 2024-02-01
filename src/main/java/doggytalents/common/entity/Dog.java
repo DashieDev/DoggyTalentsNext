bbf1dfde08d28e416d62df24f1098a0599d838b6
DashieDev <lamtinh.asian@gmail.com>
2024-02-01 20:57:38 +0700

    stop nav when dog is defeated.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2448,6 +2448,7 @@ public class Dog extends AbstractDog {
         this.setDogHunger(0);
         this.setDogIncapValue(this.getDefaultInitIncapVal());
         
+        this.getNavigation().stop();
         this.unRide();
         createAndSetIncapSyncState(source);
         if (this.isInWater() || this.isInLava()) {
