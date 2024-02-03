fcd39878eecdcedbd8eec7ec02f83f87ab23444a
DashieDev <lamtinh.asian@gmail.com>
2024-02-04 00:31:39 +0700

    remove all Effect when dog is incap.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2457,6 +2457,7 @@ public class Dog extends AbstractDog {
         this.setHealth(1);
         this.setMode(EnumMode.INCAPACITATED);
         this.setDogHunger(0);
+        this.removeAllEffects();
         this.setDogIncapValue(this.getInitalDogIncapVal(source));
         
         this.getNavigation().stop();
