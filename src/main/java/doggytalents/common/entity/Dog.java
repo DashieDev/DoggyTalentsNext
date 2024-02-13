20b44c227b8cc5edbcc4af8250640c44ac7027a8
DashieDev <lamtinh.asian@gmail.com>
2024-02-13 18:01:26 +0700

    reimplement recipe mechanic for mill.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -338,6 +338,7 @@ public class Dog extends AbstractDog {
     @Override
     protected void registerGoals() {
         int trivial_p = 0, non_trivial_p = 0;
+        ItemStack stack = new ItemStack(Items.DIAMOND);
         DogSitWhenOrderedGoal sitGoal = null;
         
         int p = 1;
