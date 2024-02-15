f82ec0965b5efc87ba21bc5c15523566e32396f0
DashieDev <lamtinh.asian@gmail.com>
2024-02-15 22:10:47 +0700

    also get respAwn storage thru getServer.

--- src/main/java/doggytalents/common/storage/DogRespawnStorage.java
+++ src/main/java/doggytalents/common/storage/DogRespawnStorage.java
@@ -8,6 +8,7 @@ import doggytalents.common.util.NBTUtil;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.datafix.DataFixTypes;
 import net.minecraft.world.level.Level;
@@ -34,6 +35,13 @@ public class DogRespawnStorage extends SavedData {
 
         DimensionDataStorage storage = overworld.getDataStorage();
         return storage.computeIfAbsent(storageFactory(), Constants.STORAGE_DOG_RESPAWN);
+    }
+
+    public static DogRespawnStorage get(MinecraftServer server) {
+        ServerLevel overworld = server.getLevel(Level.OVERWORLD);
+
+        DimensionDataStorage storage = overworld.getDataStorage();
+        return storage.computeIfAbsent(DogRespawnStorage::load, DogRespawnStorage::new, Constants.STORAGE_DOG_RESPAWN);
     }
 
     public Stream<DogRespawnData> getDogs(@Nonnull UUID ownerId) {
