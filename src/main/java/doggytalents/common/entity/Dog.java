1e1e158bcd6176971e162ba55b3f4f53d71f5218
DashieDev <lamtinh.asian@gmail.com>
2024-02-16 13:19:03 +0700

    Forgot to remove temp line,.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2492,7 +2492,6 @@ public class Dog extends AbstractDog {
         this.setMode(EnumMode.INCAPACITATED);
         this.setDogHunger(0);
         this.removeAllEffects();
-        this.addEffect(null)
         this.setDogIncapValue(this.getInitalDogIncapVal(source));
         
         this.getNavigation().stop();
