3cb32e2fdbbe7db13b68af7a39384375ce2dcf00
DashieDev <lamtinh.asian@gmail.com>
2024-02-18 16:59:38 +0700

    Add config to enable starting item by defeault on client.

--- src/main/java/doggytalents/common/event/EventHandler.java
+++ src/main/java/doggytalents/common/event/EventHandler.java
@@ -3,6 +3,7 @@ package doggytalents.common.event;
 import java.util.UUID;
 
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import doggytalents.ChopinLogger;
 import doggytalents.DoggyAccessories;
@@ -52,6 +53,7 @@ import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.ServerTickEvent;
@@ -69,6 +71,7 @@ import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class EventHandler {
@@ -190,7 +193,7 @@ public class EventHandler {
         if (event.getEntity().level().isClientSide)
             return;
 
-        if (ConfigHandler.ServerConfig.getConfig(ConfigHandler.SERVER.STARTING_ITEMS)) {
+        if (isEnableStarterBundle()) {
 
             Player player = event.getEntity();
 
@@ -208,6 +211,18 @@ public class EventHandler {
                 player.getInventory().add(new ItemStack(DoggyItems.STARTER_BUNDLE.get()));
             }
         }
+    }
+
+    private boolean isEnableStarterBundle() {
+        final var retMut = new MutableBoolean(false);
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            if (ConfigHandler.ClientConfig
+                .getConfig(ConfigHandler.CLIENT.ENABLE_STARTER_BUNDLE_BY_DEFAULT))
+                retMut.setTrue();
+        });
+        if (retMut.getValue())
+            return true;
+        return ConfigHandler.ServerConfig.getConfig(ConfigHandler.SERVER.STARTING_ITEMS);
     }
 
     @SubscribeEvent
