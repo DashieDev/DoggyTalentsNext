8f404255c7b86991369e43e32d155ef29f05fbf4
DashieDev <lamtinh.asian@gmail.com>
2024-02-19 21:56:43 +0700

    TreatBag inv overview re-work.

--- src/main/java/doggytalents/common/util/ItemUtil.java
+++ src/main/java/doggytalents/common/util/ItemUtil.java
@@ -1,31 +1,53 @@
 package doggytalents.common.util;
 
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.items.IItemHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemUtil {
 
-    public static List<ItemStack> getContentOverview(IItemHandler inventory) {
-        List<ItemStack> items = new ArrayList<>(inventory.getSlots());
-
-        SLOT: for (int i = 0; i < inventory.getSlots(); i++) {
-            ItemStack itemstack = inventory.getStackInSlot(i).copy();
-
-            if (!itemstack.isEmpty()) {
-                for (int j = 0; j < items.size(); j++) {
-                    ItemStack stack = items.get(j);
-                    //TODO ?? 1.19.3
-                    if (ItemStack.matches(stack, itemstack)) {
-                        stack.grow(itemstack.getCount());
-                        continue SLOT;
-                    }
-                }
+    private static int MAX_OVERVIEW = 3;
+    public static ContentOverview getContentOverview(IItemHandler inventory) {
+        var retMap = new HashMap<Item, Integer>(MAX_OVERVIEW);
+        int isMore = 0;
+        for (int i = 0; i < inventory.getSlots(); ++i) {
+            var stack = inventory.getStackInSlot(i);
+            if (stack.isEmpty())
+                continue;
+            var item = stack.getItem();
+            var existing = retMap.get(item);
+            if (existing != null) {
+                retMap.put(item, existing + stack.getCount());
+                continue;
             }
+            if (retMap.size() >= MAX_OVERVIEW) {
+                ++isMore;
+                continue;
+            }
+            retMap.put(item, stack.getCount());
+        }
+        return new ContentOverview(retMap, isMore);        
+    }
+
+    public static class ContentOverview {
+        
+        private final Map<Item, Integer> contents;
+        private int isMore = 0;
+
+        private ContentOverview(Map<Item, Integer> contents, int isMore) {
+            this.contents = contents;
+            this.isMore = isMore;
         }
 
-        return items;
+        public int isMore() {
+            return isMore;
+        }
+
+        public Map<Item, Integer> contents() {
+            return this.contents;
+        }
+
     }
 }
