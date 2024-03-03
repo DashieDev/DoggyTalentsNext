ce27be766382888a011aa865dacd38d123566acb
DashieDev <lamtinh.asian@gmail.com>
2024-03-03 16:31:15 +0700

    refactor and extract double dyable mechanic

---
+++ src/main/java/doggytalents/common/item/DoubleDyableAccessoryItem.java
@@ -0,0 +1,82 @@
+package doggytalents.common.item;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import doggytalents.api.inferface.AbstractDog;
+import doggytalents.api.registry.AccessoryInstance;
+import doggytalents.common.entity.accessory.DoubleDyableAccessory;
+import doggytalents.common.util.Util;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.ItemStack;
+
+public class DoubleDyableAccessoryItem extends AccessoryItem {
+    
+    private Supplier<? extends DoubleDyableAccessory> accessory;
+
+    public DoubleDyableAccessoryItem(Supplier<? extends DoubleDyableAccessory> accessoryIn, Properties properties) {
+        super(accessoryIn, properties);
+        this.accessory = accessoryIn;
+    }
+
+    @Override
+    public AccessoryInstance createInstance(AbstractDog dogIn, ItemStack stack, Player playerIn) {
+        return this.accessory.get().createFromStack(stack);
+    }
+
+    public int getBgColor(ItemStack stack) { 
+        var defColor = getDefaultBgColor();
+        var tag = stack.getTag();
+        if (tag == null)
+            return defColor;
+        if (tag.contains("doggytalents_bg_color", Tag.TAG_INT)) {
+            return tag.getInt("doggytalents_bg_color");
+        } else if (tag.contains("color", Tag.TAG_INT)) {
+            return tag.getInt("color");
+        }
+        return defColor;
+    }
+
+    public int getFgColor(ItemStack stack) { 
+        var defColor = getDefaultFgColor();
+        var tag = stack.getTag();
+        if (tag == null)
+            return defColor;
+        if (tag.contains("doggytalents_fg_color", Tag.TAG_INT)) {
+            return tag.getInt("doggytalents_fg_color");
+        } else if (tag.contains("dtn_bdhat_foreground", Tag.TAG_INT)) {
+            return tag.getInt("dtn_bdhat_foreground");
+        }
+        return defColor;
+    }
+
+    public void setBgColor(ItemStack stack, int color) {
+        var tag = stack.getOrCreateTag();
+        tag.putInt("doggytalents_bg_color", color);
+    }
+
+    public void setFgColor(ItemStack stack, int color) {
+        var tag = stack.getOrCreateTag();
+        tag.putInt("doggytalents_fg_color", color);
+    }
+
+    public static ItemStack copyAndSetColorForStack(ItemStack stack, List<DyeColor> dye, boolean fg_color) {
+        var item = stack.getItem();
+        if (!(item instanceof DoubleDyableAccessoryItem accessory))
+            return ItemStack.EMPTY;
+        stack = stack.copyWithCount(1);
+        int color = Util.colorDye(-1, dye);
+        if (fg_color) {
+            accessory.setFgColor(stack, color);
+        } else {
+            accessory.setBgColor(stack, color);
+        }
+        return stack;
+    }
+
+    public int getDefaultBgColor() { return 0; }
+    public int getDefaultFgColor() { return getDefaultBgColor(); }
+
+}
--- src/main/java/doggytalents/common/item/DyableBirthdayHatItem.java
+++ src/main/java/doggytalents/common/item/DyableBirthdayHatItem.java
@@ -8,6 +8,7 @@ import javax.annotation.Nullable;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.registry.Accessory;
 import doggytalents.api.registry.AccessoryInstance;
+import doggytalents.common.entity.accessory.DoubleDyableAccessory;
 import doggytalents.common.entity.accessory.DyeableAccessory;
 import doggytalents.common.util.Util;
 import net.minecraft.network.chat.Component;
@@ -19,55 +20,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
-public class DyableBirthdayHatItem extends AccessoryItem implements IDyeableArmorItem {
+public class DyableBirthdayHatItem extends DoubleDyableAccessoryItem {
     
-    private Supplier<? extends Accessory> type;
-    
-    public DyableBirthdayHatItem(Supplier<? extends Accessory> type, Properties properties) {
+    public DyableBirthdayHatItem(Supplier<? extends DoubleDyableAccessory> type, Properties properties) {
         super(type, properties);
-        this.type = type;
     }
 
-    @Override
-    public AccessoryInstance createInstance(AbstractDog dogIn, ItemStack stack, Player playerIn) {
-        return this.type.get().createFromStack(stack);
-    }
-
-    public int getForegroundColor(ItemStack stack) {
-        var color = this.getDefaultColor(stack);
-        var tag = stack.getTag();
-        if (tag == null)
-            return color;
-        if (!tag.contains("dtn_bdhat_foreground", 99))
-            return color;
-        return tag.getInt("dtn_bdhat_foreground");
-    }
-
-    public void setForegroundColor(ItemStack stack, int color) {
-        var tag = stack.getOrCreateTag();
-        tag.putInt("dtn_bdhat_foreground", color);
-    }
-
-    @Override
-    public void clearColor(ItemStack stack) {
-        IDyeableArmorItem.super.clearColor(stack);
-        var tag = stack.getTag();
-        if (tag == null)
-            return;
-        if (tag.contains("dtn_bdhat_foreground", 99)) {
-            tag.remove("dtn_bdhat_foreground");
-        }
-    }
-
-    public static ItemStack dyeForegroundColorStack(ItemStack stack, List<DyeColor> dye) {
-        var newStack = stack.copyWithCount(1);
-        var item = stack.getItem();
-        if (!(item instanceof DyableBirthdayHatItem birthdayHat))
-            return ItemStack.EMPTY;
-        int color = Util.colorDye(-1, dye);
-        birthdayHat.setForegroundColor(newStack, color);
-        return newStack;
-    }
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components,
             TooltipFlag flags) {
@@ -75,6 +33,11 @@ public class DyableBirthdayHatItem extends AccessoryItem implements IDyeableArmo
         components.add(Component.translatable(desc_id).withStyle(
             Style.EMPTY.withItalic(true)
         ));
+    }
+
+    @Override
+    public int getDefaultBgColor() {
+        return 0xffffffff;
     }
     
 }
