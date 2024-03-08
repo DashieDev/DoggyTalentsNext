012523b6d81941af68a5689b2f0ecf4ec53e9d34
DashieDev <lamtinh.asian@gmail.com>
2024-03-09 00:26:49 +0700

    only play HURT_2 when actually does >= 1 hp

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1833,7 +1833,7 @@ public class Dog extends AbstractDog {
             return;
         }
 
-        if (source.getEntity() != null) {
+        if (source.getEntity() != null && real_hurt_amount >= 2) {
             this.setAnim(DogAnimation.HURT_2);
             return;
         }
