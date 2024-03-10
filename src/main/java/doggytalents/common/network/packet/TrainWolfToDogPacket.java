e65711d4db5873915eeb2832b0cca21f9a1b11b2
DashieDev <lamtinh.asian@gmail.com>
2024-03-10 17:15:18 +0700

    turn wolf to dog training to client->server request
    increase the flluency between transoforming a wolf into dog.
    allowing client to pass in metadata.

---
+++ src/main/java/doggytalents/common/network/packet/TrainWolfToDogPacket.java
@@ -0,0 +1,46 @@
+package doggytalents.common.network.packet;
+
+import java.util.function.Supplier;
+
+import doggytalents.common.event.EventHandler;
+import doggytalents.common.network.IPacket;
+import doggytalents.common.network.packet.data.TrainWolfToDogData;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.network.NetworkEvent.Context;
+
+public class TrainWolfToDogPacket implements IPacket<TrainWolfToDogData> {
+
+    @Override
+    public void encode(TrainWolfToDogData data, FriendlyByteBuf buf) {
+        buf.writeInt(data.wolfId);
+        buf.writeFloat(data.wolfYBodyRot);
+        buf.writeFloat(data.wolfYHeadRot);
+    }
+
+    @Override
+    public TrainWolfToDogData decode(FriendlyByteBuf buf) {
+        int id = buf.readInt();
+        float body = buf.readFloat();
+        float head = buf.readFloat();
+        return new TrainWolfToDogData(id, body, head);
+    }
+
+    @Override
+    public void handle(TrainWolfToDogData data, Supplier<Context> ctx) {
+        ctx.get().enqueueWork(() -> {
+            if (!ctx.get().getDirection().getReceptionSide().isServer())
+                return;
+            
+            var sender = ctx.get().getSender();
+            var level = sender.level();
+            var e = level.getEntity(data.wolfId);
+            if (!(e instanceof Wolf wolf))
+                return;
+            EventHandler.checkAndTrainWolf(sender, wolf, data.wolfYBodyRot, data.wolfYHeadRot);
+        });
+
+        ctx.get().setPacketHandled(true);
+    }
+}
