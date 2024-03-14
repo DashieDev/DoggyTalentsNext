e315a485ba0ff0558e3080e934ebe24652c652cd
DashieDev <lamtinh.asian@gmail.com>
2024-03-14 19:49:14 +0700

    Fix faint anim breaking when incap
    Dont play any hurt anim if hurt to incap.
    Force stop all goal and immediately update all goal flags upon
    enter.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1761,6 +1761,8 @@ public class Dog extends AbstractDog {
     }
 
     private void mayStandUpAndPlayHurtAnim(DamageSource source, float real_hurt_amount, float health0) {
+        if (this.isDefeated())
+            return;
         if (this.isDeadOrDying()) {
             this.setAnim(DogAnimation.HURT_1);
             return;
@@ -2461,6 +2463,8 @@ public class Dog extends AbstractDog {
         this.setDogHunger(0);
         this.removeAllEffects();
         this.setDogIncapValue(this.getInitalDogIncapVal(source));
+        this.updateControlFlags();
+        this.goalSelector.getRunningGoals().forEach(x -> { x.stop(); });
         
         this.getNavigation().stop();
         this.unRide();
