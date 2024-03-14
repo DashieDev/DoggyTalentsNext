08f41ee95f61d60e302e5bff8139c42ce96e2594
DashieDev <lamtinh.asian@gmail.com>
2024-03-15 00:10:31 +0700

    Change knockback behaviour in Ookamikaze
    + Knockback allies a bit too
    + Knockback increase when haii lvl

--- src/main/java/doggytalents/common/network/packet/data/DogExplosionData.java
+++ src/main/java/doggytalents/common/network/packet/data/DogExplosionData.java
@@ -1,11 +1,23 @@
 package doggytalents.common.network.packet.data;
 
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.world.phys.Vec3;
+
 public class DogExplosionData {
     
     public final int dogId;
+    private Vec3 knockback;
     
-    public DogExplosionData(int dogId) {
+    public DogExplosionData(int dogId, @Nullable Vec3 knockback) {
         this.dogId = dogId;
+        this.knockback = knockback;
+    }
+
+    public Optional<Vec3> knockback() {
+        return Optional.ofNullable(knockback);
     }
 
 }
