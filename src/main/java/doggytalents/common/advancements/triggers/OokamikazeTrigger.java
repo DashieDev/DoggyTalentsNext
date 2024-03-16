bb037c0b06225004e99de441bc8b43d8a6611381
DashieDev <lamtinh.asian@gmail.com>
2024-03-16 16:02:57 +0700

    Ookamikaze advancement

---
+++ src/main/java/doggytalents/common/advancements/triggers/OokamikazeTrigger.java
@@ -0,0 +1,45 @@
+package doggytalents.common.advancements.triggers;
+
+import com.google.gson.JsonObject;
+
+import doggytalents.api.inferface.AbstractDog;
+import doggytalents.common.util.Util;
+import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
+import net.minecraft.advancements.critereon.ContextAwarePredicate;
+import net.minecraft.advancements.critereon.DeserializationContext;
+import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+
+public class OokamikazeTrigger extends SimpleCriterionTrigger<OokamikazeTrigger.TriggerInstance> {
+    
+    public static ResourceLocation ID = Util.getResource("ookamikaze_trigger");
+
+    @Override
+    public ResourceLocation getId() {
+        return ID;
+    }
+
+    @Override
+    protected TriggerInstance createInstance(JsonObject json, ContextAwarePredicate player,
+            DeserializationContext context) {
+        return new TriggerInstance(player);
+    }
+
+    public void trigger(AbstractDog dog, ServerPlayer player) {
+        this.trigger(player, x -> true);
+    }
+
+    public static TriggerInstance getInstance() {
+        return new TriggerInstance(ContextAwarePredicate.ANY);
+    }
+
+    public static class TriggerInstance extends AbstractCriterionTriggerInstance {
+
+        public TriggerInstance(ContextAwarePredicate player) {
+            super(ID, player);
+        }
+        
+    }
+
+}
