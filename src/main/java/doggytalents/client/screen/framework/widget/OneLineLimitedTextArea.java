f8d989e7aa87ee4784fae7f1c03a4680c54494ad
DashieDev <lamtinh.asian@gmail.com>
2024-03-17 14:49:15 +0700

    Dont click on text area

--- src/main/java/doggytalents/client/screen/framework/widget/OneLineLimitedTextArea.java
+++ src/main/java/doggytalents/client/screen/framework/widget/OneLineLimitedTextArea.java
@@ -59,4 +59,20 @@ public class OneLineLimitedTextArea extends AbstractWidget {
     @Override
     public void updateNarration(NarrationElementOutput p_169152_) {
     }
+    
+    @Override
+    public boolean keyPressed(int p_94745_, int p_94746_, int p_94747_) {
+        return false;
+    }
+
+    @Override
+    public boolean keyReleased(int p_94750_, int p_94751_, int p_94752_) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseClicked(double p_93641_, double p_93642_, int p_93643_) {
+        return false;
+    }
+
 }
