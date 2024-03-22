63d8e93c8d29f8fbb6b343ed60a2b29a4c3b84b4
DashieDev <lamtinh.asian@gmail.com>
2024-03-22 21:41:27 +0700

    fix dropdown menu.

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/DogNewInfoScreen.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/DogNewInfoScreen.java
@@ -169,9 +169,9 @@ public class DogNewInfoScreen extends StoreConnectedScreen {
     }
 
     @Override
-    public void init() {
+    public void reRenderRoot() {
+        super.reRenderRoot();
         this.setupDropdown();
-        super.init();
     }
 
     @Override
