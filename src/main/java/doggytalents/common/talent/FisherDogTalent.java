dec159773aa7c03250efefc181c2314757aba78f
DashieDev <lamtinh.asian@gmail.com>
2024-03-24 03:48:17 +0700

    refactor and modify fisher dog
    + Fisher dog don't cook when cannot generate heat.

--- src/main/java/doggytalents/common/talent/FisherDogTalent.java
+++ src/main/java/doggytalents/common/talent/FisherDogTalent.java
@@ -5,6 +5,7 @@ import doggytalents.api.enu.WetSource;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.registry.Talent;
 import doggytalents.api.registry.TalentInstance;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 public class FisherDogTalent extends TalentInstance {
@@ -19,11 +20,31 @@ public class FisherDogTalent extends TalentInstance {
             return;
         }
 
-        if (source.isWaterBlock()) {
-            if (dogIn.getRandom().nextInt(15) < this.level() * 2) {
-                int lvlHellHound = dogIn.getDogLevel(DoggyTalents.HELL_HOUND);
-                dogIn.spawnAtLocation(dogIn.getRandom().nextInt(15) < lvlHellHound * 2 ? Items.COOKED_COD : Items.COD);
-            }
-        }
+        if (!source.isWaterBlock())
+            return;
+        
+        int r_fish = dogIn.getRandom().nextInt(15);
+        if (r_fish >= this.level() * 2)
+            return;
+        
+        var fishItem = Items.COD;
+        fishItem = mayCookFish(dogIn, fishItem);
+
+        dogIn.spawnAtLocation(fishItem);
     }
+
+    private Item mayCookFish(AbstractDog dog, Item fish_raw) {
+        var hellhound_optional = dog.getTalent(DoggyTalents.HELL_HOUND);
+        if (!hellhound_optional.isPresent())
+            return fish_raw;
+        if (!(hellhound_optional.get() instanceof HellHoundTalent hellhound))
+            return fish_raw;
+        if (!hellhound.canGenerateHeat())
+            return fish_raw;
+
+        int r_cooked = dog.getRandom().nextInt(15);
+        if (r_cooked >= hellhound.level() * 2)
+            return fish_raw;
+        return Items.COOKED_COD;
+    } 
 }
