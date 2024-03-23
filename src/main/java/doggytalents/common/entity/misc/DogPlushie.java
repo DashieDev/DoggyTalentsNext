80925d24022402bbe0116712b6ef50763c1448c3
DashieDev <lamtinh.asian@gmail.com>
2024-03-24 03:05:34 +0700

    invol clarify for misc entities.

--- src/main/java/doggytalents/common/entity/misc/DogPlushie.java
+++ src/main/java/doggytalents/common/entity/misc/DogPlushie.java
@@ -8,6 +8,7 @@ import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
@@ -77,12 +78,18 @@ public class DogPlushie extends Entity {
     
     @Override
     public boolean isInvulnerableTo(DamageSource source) {
+        if (this.isRemoved())
+            return true;
+        if (source.is(DamageTypeTags.BYPASSES_INVULNERABILITY))
+            return false;
+        if (source.is(DamageTypeTags.IS_FIRE))
+            return false;
         return !(source.getDirectEntity() instanceof Player);
     }
 
     @Override
     public boolean fireImmune() {
-        return true;
+        return false;
     }
 
     @Override
