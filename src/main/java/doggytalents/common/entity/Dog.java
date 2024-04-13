a6e4f47356c82db9b03246eb618221bde4e0c2bb
DashieDev <lamtinh.asian@gmail.com>
2024-04-13 14:56:04 +0700

    Fix unable to untame dog bug amnesia bone.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3846,11 +3846,13 @@ public class Dog extends AbstractDog {
         this.dogSyncedDataManager.talents().clear();
         this.dogSyncedDataManager.setTalentsDirty();
 
+        this.authorizedChangingOwner = true;
         this.setTame(false);
         this.setOwnerUUID(null);
         this.setWillObeyOthers(false);
         this.setCanPlayersAttack(true);
         this.setMode(EnumMode.DOCILE);
+        this.authorizedChangingOwner = false;
     }
 
     public void migrateOwner(UUID newOwnerUUID) {
