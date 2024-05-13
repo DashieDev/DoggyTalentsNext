7937cc909d40bda30dbee920067e55cf2ab0aeb3
DashieDev <lamtinh.asian@gmail.com>
2024-05-13 15:57:38 +0700

    Now advancements!
    When player apply bandaid to injured dog
    When player get a dog to heal (not in best condition)
    When player get a dog to heal (in best condition)

--- src/main/java/doggytalents/DoggyAdvancementTriggers.java
+++ src/main/java/doggytalents/DoggyAdvancementTriggers.java
@@ -4,7 +4,9 @@ import java.util.Map;
 
 import com.google.common.collect.Maps;
 
+import doggytalents.common.advancements.triggers.DogBandaidApplyTrigger;
 import doggytalents.common.advancements.triggers.DogDrunkTrigger;
+import doggytalents.common.advancements.triggers.DogRecoveredTrigger;
 import doggytalents.common.advancements.triggers.OokamikazeTrigger;
 import doggytalents.common.lib.Constants;
 import doggytalents.common.util.Util;
@@ -20,6 +22,8 @@ public class DoggyAdvancementTriggers {
 ;
     public static final DogDrunkTrigger DOG_DRUNK_TRIGGER = register("get_dog_drunk", new DogDrunkTrigger());
     public static final OokamikazeTrigger OOKAMIKAZE_TRIGGER  = register("ookamikaze_trigger", new OokamikazeTrigger());
+    public static final DogBandaidApplyTrigger DOG_BANDAID_APPLY_TRIGGER  = register("dog_bandaid_apply_trigger", new DogBandaidApplyTrigger());
+    public static final DogRecoveredTrigger DOG_RECOVERED_TRIGGER  = register("dog_recovered_trigger", new DogRecoveredTrigger());
 
     public static <T extends CriterionTrigger<?>> T register(String name, T val) {
         var reg_val = val;
