c3ec468439d7cc4c235897515bb43eb7581ed831
DashieDev <lamtinh.asian@gmail.com>
2024-05-15 12:53:41 +0700

    Dyable Orb

--- src/main/java/doggytalents/client/entity/model/dog/kusa/LocatorOrbModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/kusa/LocatorOrbModel.java
@@ -12,6 +12,9 @@ import net.minecraft.client.model.geom.builders.MeshDefinition;
 
 public class LocatorOrbModel extends SyncedAccessoryModel {
 
+    public ModelPart scarf;
+    public ModelPart orb;
+
     public LocatorOrbModel(ModelPart root) {
         super(root);
     }
@@ -19,6 +22,8 @@ public class LocatorOrbModel extends SyncedAccessoryModel {
     @Override
     protected void populatePart(ModelPart box) {
         this.mane = Optional.of(box.getChild("upper_body"));
+        this.scarf = this.mane.get().getChild("scarf");
+        this.orb = this.mane.get().getChild("orb");
     }
 
     public static LayerDefinition createOrbLayer() {
@@ -27,7 +32,9 @@ public class LocatorOrbModel extends SyncedAccessoryModel {
 
         var upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create(), PartPose.offsetAndRotation(0F, 14.0F, -3.0F, 1.5707964F, 0.0F, 0.0F));
         upper_body.addOrReplaceChild("scarf", CubeListBuilder.create().texOffs(0, 43).addBox(-4.0F, -13.0F, -1.0F, 8.0F, 13.0F, 7.0F, new CubeDeformation(0.5F))
-		.texOffs(28, 27).addBox(-4.0F, -13.2F, -7.0F, 8.0F, 6.0F, 10.0F, new CubeDeformation(-0.2F))
+		.texOffs(28, 27).addBox(-4.0F, -13.2F, -7.0F, 8.0F, 6.0F, 10.0F, new CubeDeformation(-0.2F)), 
+        PartPose.offset(0F, 10F, -2F));
+        upper_body.addOrReplaceChild("orb", CubeListBuilder.create()
 		.texOffs(0, 38).addBox(-1.0F, -14.0F, -3.7F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.1F)), 
         PartPose.offset(0F, 10F, -2F));
 
