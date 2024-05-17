0ad2b874afca9c4be3c893cf7cec37457df31a56
DashieDev <lamtinh.asian@gmail.com>
2024-05-17 23:10:49 +0700

    Only set equipslot stack serverside

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3232,9 +3232,11 @@ public class Dog extends AbstractDog {
     }
 
     private void onPropsUpdated() {
+        if (this.level().isClientSide)
+            return;
         this.dogArmors.onPropsUpdated(alterationProps);
         if (!alterationProps.canUseTools())
-            this.mouthStack = ItemStack.EMPTY;
+            this.setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
     }
 
     public IDogRangedAttackManager getDogRangedAttack() {
