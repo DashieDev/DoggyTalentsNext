7e2e3bfba195e6c1349715a901bea7aef37dcee5
DashieDev <lamtinh.asian@gmail.com>
2024-05-17 23:20:35 +0700

    Fix injured swimmer dog don't float.

--- src/main/java/doggytalents/common/talent/SwimmerDogTalent.java
+++ src/main/java/doggytalents/common/talent/SwimmerDogTalent.java
@@ -106,6 +106,7 @@ public class SwimmerDogTalent extends TalentInstance {
         if (
             (!dog.isInWater() && dog.isOnGround())
             || dog.isLowAirSupply()
+            || dog.isDefeated()
         ) {
             this.swimming = false;
             stopSwimming(dog);
@@ -115,6 +116,7 @@ public class SwimmerDogTalent extends TalentInstance {
     private void updateNotSwimming(Dog dog) {
         if (
             dog.isInWater()
+            && !dog.isDefeated()
             && readyToBeginSwimming(dog)
             && !dog.isDogSwimming()
         ) {
