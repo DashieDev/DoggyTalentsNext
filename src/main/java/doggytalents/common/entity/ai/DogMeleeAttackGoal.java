534e42664a6200ef01b769fcd6d56fc5f40e4472
DashieDev <lamtinh.asian@gmail.com>
2024-05-17 17:50:51 +0700

    Also dog can dash thru target if hellhound in lava

--- src/main/java/doggytalents/common/entity/ai/DogMeleeAttackGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogMeleeAttackGoal.java
@@ -304,6 +304,8 @@ public class DogMeleeAttackGoal extends Goal {
 
    protected boolean isTargetInSafeArea(Dog dog, LivingEntity target) {
       var type = WalkNodeEvaluator.getPathTypeStatic(dog, target.blockPosition().mutable());
+      if (dog.fireImmune() && type == PathType.LAVA)
+         return true;
       for (var x : dog.getAlterations()) {
          var type_result = x.inferType(dog, type);
          if (type_result.getResult().shouldSwing() 
