e1a84e1e9e357a243cdef9899a7779f66ad68000
DashieDev <lamtinh.asian@gmail.com>
2024-05-28 04:08:56 +0700

    Bambooo!!!!!

--- src/main/java/doggytalents/common/variants/DTNWolfVariantsProvider.java
+++ src/main/java/doggytalents/common/variants/DTNWolfVariantsProvider.java
@@ -1,6 +1,8 @@
 package doggytalents.common.variants;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import doggytalents.common.lib.Constants;
 import doggytalents.common.util.Util;
@@ -10,71 +12,106 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class DTNWolfVariantsProvider {
     
-    // public static void start(GatherDataEvent event) {
-    //     var wolf_variant_set = new RegistrySetBuilder()
-    //         .add(Registries.WOLF_VARIANT, DTNWolfVariants::bootstrap)
-    //         .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, DTNWolfVariantsProvider::registerWolfModifier);
+    public static void start(GatherDataEvent event) {
+        var wolf_variant_set = new RegistrySetBuilder()
+            .add(Registries.WOLF_VARIANT, DTNWolfVariants::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, DTNWolfVariantsProvider::registerWolfModifier);
             
-    //     var datagen = event.getGenerator();
-    //     datagen.addProvider(event.includeServer(),
-    //         new DatapackBuiltinEntriesProvider(
-    //             datagen.getPackOutput(), event.getLookupProvider(), 
-    //             wolf_variant_set, Set.of(Constants.MOD_ID) 
-    //         )
-    //     );
-    // }
+        var datagen = event.getGenerator();
+        datagen.addProvider(event.includeServer(),
+            new DatapackBuiltinEntriesProvider(
+                datagen.getPackOutput(), event.getLookupProvider(), 
+                wolf_variant_set, Set.of(Constants.MOD_ID) 
+            )
+        );
+    }
 
-    // private static void registerWolfModifier(BootstrapContext<BiomeModifier> ctx) {
-    //     registerCherryWolfModifier(ctx);
-    //     registerLemonyLimeWolfModifier(ctx);
-    //     registerHimalayanSaltWolfModifier(ctx);
-    // }
+    private static void registerWolfModifier(BootstrapContext<BiomeModifier> ctx) {
+        registerCherryWolfModifier(ctx);
+        registerLemonyLimeWolfModifier(ctx);
+        registerHimalayanSaltWolfModifier(ctx);
+        registerBambooWolfModifier(ctx);
+    }
 
-    // private static void registerCherryWolfModifier(BootstrapContext<BiomeModifier> ctx) {
-    //     var cherry_wolf_spawn_id = ResourceKey.create(
-    //         NeoForgeRegistries.Keys.BIOME_MODIFIERS, 
-    //         Util.getResource("cherry_wolf_spawn_modifier")) ;
-    //     var cherry_wolf_spawn_biome = HolderSet.direct(
-    //         ctx.lookup(Registries.BIOME).getOrThrow(Biomes.CHERRY_GROVE)
-    //     );
-    //     var cherry_wolf_spawner_data = new MobSpawnSettings
-    //         .SpawnerData(EntityType.WOLF, 1, 1, 1);
-    //     var cherry_wolf_spawn_modifier = BiomeModifiers.AddSpawnsBiomeModifier
-    //         .singleSpawn(cherry_wolf_spawn_biome, cherry_wolf_spawner_data);
-    //     ctx.register(cherry_wolf_spawn_id, cherry_wolf_spawn_modifier);
-    // }
+    private static void registerCherryWolfModifier(BootstrapContext<BiomeModifier> ctx) {
+        registerSingleSpawnModifier(
+            ctx, "wolf_cherry", 
+            Biomes.CHERRY_GROVE, 
+            new MobSpawnSettings
+                .SpawnerData(EntityType.WOLF, 1, 1, 1)
+        );
+    }
 
-    // private static void registerLemonyLimeWolfModifier(BootstrapContext<BiomeModifier> ctx) {
-    //     var lemony_lime_wolf_spawn_id = ResourceKey.create(
-    //         NeoForgeRegistries.Keys.BIOME_MODIFIERS, 
-    //         Util.getResource("lemony_lime_wolf_spawn_modifier")) ;
-    //     var lemony_lime_wolf_spawn_biome = HolderSet.direct(
-    //         ctx.lookup(Registries.BIOME).getOrThrow(Biomes.BEACH)
-    //     );
-    //     var lemony_lime_wolf_spawner_data = new MobSpawnSettings
-    //         .SpawnerData(EntityType.WOLF, 1, 1, 1);
-    //     var lemony_lime_wolf_spawn_modifier = BiomeModifiers.AddSpawnsBiomeModifier
-    //         .singleSpawn(lemony_lime_wolf_spawn_biome, lemony_lime_wolf_spawner_data);
-    //     ctx.register(lemony_lime_wolf_spawn_id, lemony_lime_wolf_spawn_modifier);
-    // }
+    private static void registerLemonyLimeWolfModifier(BootstrapContext<BiomeModifier> ctx) {
+        registerSingleSpawnModifier(
+            ctx, "wolf_lemony_lime", 
+            Biomes.BEACH, 
+            new MobSpawnSettings
+                .SpawnerData(EntityType.WOLF, 1, 1, 1)
+        );
+    }
 
-    // private static void registerHimalayanSaltWolfModifier(BootstrapContext<BiomeModifier> ctx) {
-    //     var himalayan_salt_wolf_spawn_id = ResourceKey.create(
-    //         NeoForgeRegistries.Keys.BIOME_MODIFIERS, 
-    //         Util.getResource("himalayan_salt_wolf_spawn_modifier")) ;
-    //     var himalayan_salt_wolf_spawn_biome = HolderSet.direct(
-    //         ctx.lookup(Registries.BIOME).getOrThrow(Biomes.JAGGED_PEAKS)
-    //     );
-    //     var himalayan_salt_wolf_spawner_data = new MobSpawnSettings
-    //         .SpawnerData(EntityType.WOLF, 1, 1, 1);
-    //     var himalayan_salt_wolf_spawn_modifier = BiomeModifiers.AddSpawnsBiomeModifier
-    //         .singleSpawn(himalayan_salt_wolf_spawn_biome, himalayan_salt_wolf_spawner_data);
-    //     ctx.register(himalayan_salt_wolf_spawn_id, himalayan_salt_wolf_spawn_modifier);
-    // }
+    private static void registerHimalayanSaltWolfModifier(BootstrapContext<BiomeModifier> ctx) {
+        registerSingleSpawnModifier(
+            ctx, "wolf_himalayan_salt", 
+            Biomes.JAGGED_PEAKS, 
+            new MobSpawnSettings
+                .SpawnerData(EntityType.WOLF, 1, 1, 1)
+        );
+    }
+
+    private static void registerBambooWolfModifier(BootstrapContext<BiomeModifier> ctx) {
+        registerSingleSpawnModifier(
+            ctx, "wolf_bamboo", 
+            Biomes.JUNGLE, 
+            new MobSpawnSettings
+                .SpawnerData(EntityType.WOLF, 1, 1, 1)
+        );
+        registerSingleSpawnModifier(
+            ctx, "wolf_bamboo_dedicated", 
+            Biomes.BAMBOO_JUNGLE,
+            new MobSpawnSettings
+                .SpawnerData(EntityType.WOLF, 60, 1, 1)
+        );
+    }
+
+    private static void registerSingleSpawnModifier(BootstrapContext<BiomeModifier> ctx,
+        String name, ResourceKey<Biome> biome, MobSpawnSettings.SpawnerData spawner_data) {
+        
+        registerSingleSpawnModifier(ctx, name, List.of(biome), spawner_data);
+    }
+
+    private static void registerSingleSpawnModifier(BootstrapContext<BiomeModifier> ctx,
+        String name, List<ResourceKey<Biome>> biomes, MobSpawnSettings.SpawnerData spawner_data) {
+        
+        var spawn_id = ResourceKey.create(
+            NeoForgeRegistries.Keys.BIOME_MODIFIERS, 
+            Util.getResource(name));
+        
+        var biome_reg = ctx.lookup(Registries.BIOME);
+        var biome_holders = biomes.stream()
+            .map(x -> biome_reg.get(x))
+            .filter(x -> x.isPresent())
+            .map(x -> x.get())
+            .collect(Collectors.toList());
+        if (biome_holders.isEmpty())
+            return;
+        var spawn_biomes = HolderSet.direct(biome_holders);
+        var spawn_modifier = BiomeModifiers.AddSpawnsBiomeModifier
+            .singleSpawn(spawn_biomes, spawner_data);
+        
+        ctx.register(spawn_id, spawn_modifier);
+    }
 
 }
