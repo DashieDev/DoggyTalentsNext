025a919d7b106286d37d7c876f72b9371f89023a
DashieDev <lamtinh.asian@gmail.com>
2024-05-29 16:12:40 +0700

    postupdate dataprov sync
    Wont affect anything in production.

--- src/main/java/doggytalents/common/data/DTRecipeProvider.java
+++ src/main/java/doggytalents/common/data/DTRecipeProvider.java
@@ -90,7 +90,7 @@ public class DTRecipeProvider extends RecipeProvider {
             .requires(Items.BONE)
             .requires(Items.GUNPOWDER)
             .requires(Items.SUGAR)
-            .requires(DoggyItems.UNCOOKED_RICE.get())
+            .requires(DoggyItems.RICE_BOWL.get())
             .unlockedBy("has_dtn_rice_grains", has(DoggyItems.RICE_GRAINS.get()))
             .save(consumer);
             
