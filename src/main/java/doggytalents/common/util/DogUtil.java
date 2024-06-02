57785ef68972b4b374b96a20bcff9a9f7c647086
DashieDev <lamtinh.asian@gmail.com>
2024-06-03 02:16:14 +0700

    unified check for trident.

--- src/main/java/doggytalents/common/util/DogUtil.java
+++ src/main/java/doggytalents/common/util/DogUtil.java
@@ -38,6 +38,7 @@ import net.minecraft.world.entity.animal.WolfVariant;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
@@ -709,6 +710,10 @@ public class DogUtil {
             .normalize();
         var dot = v_look_wanted.dot(v_look_owner);
         return dot > 0.7;
+    }
+
+    public static boolean isTrident(ItemStack stack) {
+        return stack.is(Items.TRIDENT);
     }
 
     public static boolean isDangerPathType(PathType pathType) {
