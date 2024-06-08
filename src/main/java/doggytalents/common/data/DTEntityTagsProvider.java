c149fc581d23b10cd3b94d236eea57f90edbff37
DashieDev <lamtinh.asian@gmail.com>
2024-06-08 17:16:27 +0700

    Dog Arrow Tags

--- src/main/java/doggytalents/common/data/DTEntityTagsProvider.java
+++ src/main/java/doggytalents/common/data/DTEntityTagsProvider.java
@@ -6,12 +6,14 @@ import java.util.function.Supplier;
 
 import org.jetbrains.annotations.Nullable;
 
+import doggytalents.DoggyEntityTypes;
 import doggytalents.DoggyTags;
 import doggytalents.common.lib.Constants;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -39,6 +41,9 @@ public class DTEntityTagsProvider extends EntityTypeTagsProvider {
             () -> EntityType.SPIDER);
         createTag(DoggyTags.MOB_RETRIEVER_MUST_IGNORE, 
             () -> EntityType.CREEPER);
+        createTag(EntityTypeTags.IMPACT_PROJECTILES, 
+            DoggyEntityTypes.DOG_ARROW_PROJ, DoggyEntityTypes.DOG_TRIDENT_PROJ);
+        createTag(EntityTypeTags.ARROWS, DoggyEntityTypes.DOG_ARROW_PROJ);
     }
 
     @SafeVarargs
