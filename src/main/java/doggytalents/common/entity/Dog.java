c542bfe192be4731b0da1070d9a0d962b788361a
DashieDev <lamtinh.asian@gmail.com>
2024-06-21 01:32:21 +0700

    integrate lava floating and lava/fire infertype to Dog class itself
    Applicable if dog isFireImmune()

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -150,6 +150,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -160,6 +161,12 @@ import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 import net.minecraft.world.level.portal.DimensionTransition;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.neoforge.common.NeoForgeMod;
+import net.neoforged.neoforge.fluids.FluidType;
+import doggytalents.common.network.PacketDistributor;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -917,6 +924,10 @@ public class Dog extends AbstractDog {
                 --this.pushFromOtherDogResistTick;
         } else {
             this.pushFromOtherDogResistTick = 20;
+        }
+
+        if (!this.level().isClientSide && this.fireImmune()) {
+            mayFloatDogInLava();
         }
 
         if (this.navigationLock != null)
@@ -4125,6 +4136,18 @@ public class Dog extends AbstractDog {
         this.setDeltaMovement(new Vec3(moveX, move.y(), moveZ));
     }
 
+    private void mayFloatDogInLava() {
+        if (!this.isInLava()) return;
+        var collisioncontext = CollisionContext.of(this);
+        if (collisioncontext.isAbove(LiquidBlock.STABLE_SHAPE, 
+            this.blockPosition(), true) 
+            && !this.level().getFluidState(this.blockPosition().above()).is(FluidTags.LAVA)) {
+            this.setOnGround(true);
+        } else {
+            this.setDeltaMovement(this.getDeltaMovement().add(0.0D, 0.085D, 0.0D));
+        }
+    }
+
     @Override
     protected float getFlyingSpeed() {
         return this.isDogFlying() ? 0.49f : super.getFlyingSpeed();
@@ -4954,6 +4977,14 @@ public class Dog extends AbstractDog {
     }
 
     public PathType inferType(PathType type) {
+        if (this.fireImmune()) {
+            if (type == PathType.DANGER_FIRE) {
+                return PathType.WALKABLE;
+            }
+            if (type == PathType.LAVA) {
+                return PathType.BLOCKED;
+            }
+        }
         for (var alt : this.alterations) {
             var result = alt.inferType(this, type);
             if (result.getResult().shouldSwing()) {
--- src/main/java/doggytalents/common/talent/HellHoundTalent.java
+++ src/main/java/doggytalents/common/talent/HellHoundTalent.java
@@ -193,7 +193,6 @@ public class HellHoundTalent extends TalentInstance {
         };
         if (!(d instanceof Dog dog))
             return;
-        floatHellhound(dog);
         if (dog != null) {
             if (dog.isShakingLava()) {
                 if (dog.getTimeDogIsShaking() > 0.8) {
@@ -216,18 +215,6 @@ public class HellHoundTalent extends TalentInstance {
         }
         if (this.fireResistTickLeft > 0)
             --this.fireResistTickLeft;
-    }
-
-    private void floatHellhound(AbstractDog dog) {
-        if (!dog.isInLava()) return;
-        var collisioncontext = CollisionContext.of(dog);
-        if (collisioncontext.isAbove(LiquidBlock.STABLE_SHAPE, 
-            dog.blockPosition(), true) 
-            && !dog.level().getFluidState(dog.blockPosition().above()).is(FluidTags.LAVA)) {
-            dog.setOnGround(true);
-        } else {
-            dog.setDeltaMovement(dog.getDeltaMovement().add(0.0D, 0.085D, 0.0D));
-        }
     }
 
     private void fireSpreadToEnermies(AbstractDog dog) {
@@ -293,23 +280,5 @@ public class HellHoundTalent extends TalentInstance {
                     0.1 //TODO Tune
                 );
         }
-    }
-
-    @Override
-    public InteractionResultHolder<PathType> inferType(AbstractDog dog, PathType type) {
-        if (level < 5) return super.inferType(dog, type);
-        // CAUTION : MAGMA_BLOCK also returns DAMAGE_FIRE instead of BLOCKED
-        // so the dog may suffocate.
-        // if (type == PathType.DAMAGE_FIRE) {
-        //     return InteractionResult.SUCCESS;
-        // }
-        //Won't push owner due to A.I check
-        if (type == PathType.DANGER_FIRE) {
-            return InteractionResultHolder.success(PathType.WALKABLE);
-        }
-        if (type == PathType.LAVA) {
-            return InteractionResultHolder.success(PathType.BLOCKED);
-        }
-        return super.inferType(dog, type);
     }
 }
