abc45923c156d56afdcf70e8b9b607e7db508e3a
DashieDev <lamtinh.asian@gmail.com>
2024-06-21 21:20:09 +0700

    common updateHover with works on all version and fix 1.20.6+

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/widget/ModeSwitch.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/widget/ModeSwitch.java
@@ -34,6 +34,7 @@ public class ModeSwitch extends AbstractWidget {
     Font font;
     Screen screen;
 
+    boolean localIsHovered = false;
     boolean hoveredLeft = false;
     boolean hoveredRight = false;
     boolean isHolding = false;
@@ -161,8 +162,9 @@ public class ModeSwitch extends AbstractWidget {
     }
 
     private void updateHover(int mouseX, int mouseY) {
-        boolean isHovered0 = this.isHovered;
+        boolean isHovered0 = this.localIsHovered;
         this.isHovered = mouseX >= this.getX() && mouseY >= this.getY() && mouseX < this.getX() + this.width && mouseY < this.getY() + this.height;
+        this.localIsHovered = this.isHovered;
         if (isHovered0 != this.isHovered) {
             this.stillHovered = this.isHovered;
             if (this.isHovered) {
