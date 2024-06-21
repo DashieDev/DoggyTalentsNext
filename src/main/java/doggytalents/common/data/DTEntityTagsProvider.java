c477d6d459a7a6de5035819b20bfd3db7e7f1e6f
DashieDev <lamtinh.asian@gmail.com>
2024-06-22 00:56:57 +0700

    capturing block tag.

--- src/main/java/doggytalents/common/data/DTEntityTagsProvider.java
+++ src/main/java/doggytalents/common/data/DTEntityTagsProvider.java
@@ -14,6 +14,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.neoforged.neoforge.common.Tags;
 
 public class DTEntityTagsProvider /*extends EntityTypeTagsProvider*/ {
 
