8358b313abde41979e0f125591f9001a01acdbbd
DashieDev <lamtinh.asian@gmail.com>
2024-06-26 19:31:28 +0700

    configurable ammy spinna

--- src/main/java/doggytalents/client/screen/widget/DoggySpin.java
+++ src/main/java/doggytalents/client/screen/widget/DoggySpin.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import doggytalents.api.enu.forward_imitate.ComponentUtil;
+import doggytalents.common.config.ConfigHandler;
 import doggytalents.common.lib.Resources;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
@@ -137,7 +138,7 @@ public class DoggySpin extends AbstractWidget {
         } else if (r >= 0.04f) {
             selected_style = Style.SIT;
         } else {
-            selected_style = Style.AMMY;
+            selected_style = ConfigHandler.CLIENT.AMMY_SPINNA.get() ? Style.AMMY : Style.BACKFLIP;
         }
         this.style = selected_style;
         indx = 0;
