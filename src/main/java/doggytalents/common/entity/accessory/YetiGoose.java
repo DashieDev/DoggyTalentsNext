7f78dba478b49d8eb883cbbd3de09c1b1aa1b2f3
DashieDev <lamtinh.asian@gmail.com>
2024-06-28 02:19:44 +0700

    1.20 fix

--- src/main/java/doggytalents/common/entity/accessory/YetiGoose.java
+++ src/main/java/doggytalents/common/entity/accessory/YetiGoose.java
@@ -18,6 +18,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 
 public class YetiGoose extends Accessory implements IAccessoryHasModel{
 
@@ -42,7 +43,7 @@ public class YetiGoose extends Accessory implements IAccessoryHasModel{
             super(type, properties);
         }
         @Override
-        public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
+        public void appendHoverText(ItemStack stack, Level level, List<Component> components,
                 TooltipFlag flags) {
             var desc_id = this.getDescriptionId(stack) + ".description";
             components.add(Component.translatable(desc_id).withStyle(
