cbd18e1c0e1267f1582d5b77d45c022d88781413
MashXP <realsteelphong@gmail.com>
2024-06-27 13:51:06 +0700

    MIDI Keyboard 🎹🎹🎹

--- src/main/java/doggytalents/DoggyAccessoryTypes.java
+++ src/main/java/doggytalents/DoggyAccessoryTypes.java
@@ -22,6 +22,7 @@ public class DoggyAccessoryTypes {
     public static final RegistryObject<AccessoryType> TAIL = register("tail");
     public static final RegistryObject<AccessoryType> WINGS = register("wings");
     public static final RegistryObject<AccessoryType> BODY = register("body");
+    public static final RegistryObject<AccessoryType> UPPER_BODY = register("upper_body");
 
     private static RegistryObject<AccessoryType> register(final String name) {
         return register(name, () -> new AccessoryType());
