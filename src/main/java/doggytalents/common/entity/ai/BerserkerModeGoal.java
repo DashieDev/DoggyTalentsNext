1f7d24fa0160aaf3218665d33dca7c903ae1395b
DashieDev <lamtinh.asian@gmail.com>
2024-07-22 03:32:01 +0700

    move minor attack logic to central dog.

--- src/main/java/doggytalents/common/entity/ai/BerserkerModeGoal.java
+++ src/main/java/doggytalents/common/entity/ai/BerserkerModeGoal.java
@@ -20,21 +20,7 @@ public class BerserkerModeGoal extends NearestAttackableTargetGoal<Mob> {
     public BerserkerModeGoal(Dog dog) {
         super(dog, Mob.class, false , (e) -> {
             if (!(e instanceof Enemy)) return false;
-            if (dog.isMode(EnumMode.BERSERKER_MINOR)) {
-                if (e instanceof ZombifiedPiglin) return false;
-                if (e instanceof AbstractPiglin) {
-                    var owner = dog.getOwner();
-                    if (owner != null) {
-                        for (var stack : owner.getArmorSlots()) {
-                            if (stack.makesPiglinsNeutral(owner)) {
-                                return false;
-                            }
-                        }
-                    }
-                }
-                if (e.getType().is(DoggyTags.DOG_SHOULD_IGNORE))
-                    return false;
-            }
+            
             return true;
         });
         this.dog = dog;
