a4ca345c354454310dcb5a1ed71563bd87158124
DashieDev <lamtinh.asian@gmail.com>
2024-07-24 00:35:55 +0700

    Fix fabric ItemStackHandler

--- src/api/java/doggytalents/api/forge_imitate/inventory/ItemStackHandler.java
+++ src/api/java/doggytalents/api/forge_imitate/inventory/ItemStackHandler.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerListener;
 import net.minecraft.world.SimpleContainer;
@@ -18,9 +20,38 @@ public class ItemStackHandler extends SimpleContainer {
         this.stacks = this.items;
     }
 
-    public CompoundTag serializeNBT(HolderLookup.Provider prov) { return new CompoundTag(); }
+    public CompoundTag serializeNBT(HolderLookup.Provider prov) {
+        var itemsList = new ListTag();
 
-    public void deserializeNBT(HolderLookup.Provider prov, CompoundTag compound) {}
+        for(int i = 0; i < this.stacks.size(); i++) {
+           ItemStack stack = this.stacks.get(i);
+           if (!stack.isEmpty()) {
+              CompoundTag itemTag = new CompoundTag();
+              itemTag.putByte("Slot", (byte) i);
+              stack.save(itemTag);
+              itemsList.add(itemTag);
+           }
+        }
+
+        CompoundTag compound = new CompoundTag();
+        compound.put("item_list", itemsList);
+
+        return compound;
+    }
+
+    public void deserializeNBT(HolderLookup.Provider prov, CompoundTag compound) {
+        if (!compound.contains("item_list", Tag.TAG_LIST)) return;
+        ListTag tagList = compound.getList("item_list", Tag.TAG_COMPOUND);
+        for (int i = 0; i < tagList.size(); i++) {
+            CompoundTag itemTag = tagList.getCompound(i);
+            int slot = itemTag.getInt("Slot");
+
+            if (slot >= 0 && slot < this.stacks.size()) {
+                this.stacks.set(slot, ItemStack.of(itemTag));
+            }
+        }
+        this.onLoad();
+    }
 
     public void onLoad() {}
 
