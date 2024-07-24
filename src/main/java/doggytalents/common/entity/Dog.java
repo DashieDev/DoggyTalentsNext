dde61ddd1af23a9855232f3315bc678065c8cfbe
DashieDev <lamtinh.asian@gmail.com>
2024-07-24 17:29:09 +0700

    Allow player petting dog to clip in the dog bounding box without push a bit
    for certain type to be able to get closer to the dog.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4910,6 +4910,8 @@ public class Dog extends AbstractDog {
 
     @Override
     protected void doPush(Entity pushTarget) {
+        if (this.pettingManager.checkPush(pushTarget))
+            return;
         if (shouldBlockPush(pushTarget))
             return;
         if (pushTarget.getVehicle() == this
@@ -4961,6 +4963,8 @@ public class Dog extends AbstractDog {
     public void push(Entity source) {
         if (source.getVehicle() == this
             || this.getVehicle() == source)
+            return;
+        if (this.pettingManager.checkPush(source))
             return;
         if (this.isVehicle() && !this.hasControllingPassenger())
             Entity_push(source);
