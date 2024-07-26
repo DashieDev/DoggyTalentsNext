1686ec273838d57600af04c94f3ee6099b88dfdf
DashieDev <lamtinh.asian@gmail.com>
2024-07-26 16:34:21 +0700

    fabric 1.20 fix

--- src/main/java/doggytalents/common/data/fabric_data/FabricDTRecipeProvider.java
+++ src/main/java/doggytalents/common/data/fabric_data/FabricDTRecipeProvider.java
@@ -1,24 +1,25 @@
 package doggytalents.common.data.fabric_data;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 import doggytalents.DoggyItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.Items;
 
 public class FabricDTRecipeProvider extends FabricRecipeProvider {
 
     public FabricDTRecipeProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-        super(output, registriesFuture);
+        super(output);
     }
 
     @Override
-    public void buildRecipes(RecipeOutput consumer) {
+    public void buildRecipes(Consumer<FinishedRecipe> consumer) {
         //Fabric
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, DoggyItems.SAKE.get(), 1)
             .requires(Items.POTION, 1)
