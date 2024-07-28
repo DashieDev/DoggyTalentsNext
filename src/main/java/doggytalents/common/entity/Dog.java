32da83b6f8de113baeac13ad797eab1097c181dd
DashieDev <lamtinh.asian@gmail.com>
2024-07-28 23:59:07 +0700

    Allow server to configure and disable certain customSkin to be applied in dog

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -3603,6 +3603,8 @@ public class Dog extends AbstractDog {
         if (data == null) {
             data = DogSkinData.NULL;
         }
+        if (!DogUtil.vertifySkinData(data.getHash()))
+            data = DogSkinData.NULL;
         this.entityData.set(CUSTOM_SKIN.get(), data);
     }
 
