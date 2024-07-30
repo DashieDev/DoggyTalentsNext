e29a64d2117019092893ade9a097d9394ff35018
DashieDev <lamtinh.asian@gmail.com>
2024-07-30 13:59:54 +0700

    1.20 fix

--- src/main/java/doggytalents/common/talent/doggy_tools/ShootHandler.java
+++ src/main/java/doggytalents/common/talent/doggy_tools/ShootHandler.java
@@ -162,7 +162,7 @@ public interface ShootHandler {
                 
             arrowStack = select_arrowStack;
 
-            var arrow_proj = createDogArrow(dog, arrow, arrowStack, bow_stack);
+            var arrow_proj = createDogArrow(dog, arrow, arrowStack);
             if (arrow_proj == null)
                 return Optional.empty();
     
