c996be7daf3b12bed00ceeeb95ee56071e6e8a87
DashieDev <lamtinh.asian@gmail.com>
2024-08-03 22:24:17 +0700

    default movecontrol or nav if null

--- src/api/java/doggytalents/api/inferface/AbstractDog.java
+++ src/api/java/doggytalents/api/inferface/AbstractDog.java
@@ -114,6 +114,7 @@ public abstract class AbstractDog extends TamableAnimal implements IDog {
     }
 
     public void setNavigation(PathNavigation p) {
+        if (p == null) p = this.getDefaultNavigation();
         if (this.navigation == p) return;
         this.navigation.stop();
         this.navigation = p;
@@ -121,6 +122,7 @@ public abstract class AbstractDog extends TamableAnimal implements IDog {
 
     //TODO try to replicate the bug and check if moveControl.haveWantedPosition using debug magic
     public void setMoveControl(MoveControl m) {
+        if (m == null) m = this.getDefaultMoveControl();
         breakMoveControl();
 
         this.moveControl = m;
