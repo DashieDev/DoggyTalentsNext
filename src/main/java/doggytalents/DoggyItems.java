51b1fddf4d551019b63c46553aa3ecd5f1870c2e
DashieDev <lamtinh.asian@gmail.com>
2024-08-03 23:45:37 +0700

    remove DTN disc but chopin

--- src/main/java/doggytalents/DoggyItems.java
+++ src/main/java/doggytalents/DoggyItems.java
@@ -186,15 +186,6 @@ public class DoggyItems {
         props -> new DoggyArtifactItem(
             () -> new FeatheredMantleArtifact(), props), 1);
 
-    public static final RegistryObject<Item> MUSIC_DISC_BWV_1080_FUGUE_11_KIMIKO = register("disc_bwv_1080_fugue_11", 
-        () -> new RecordItem(13, () -> DoggySounds.BWV_1080_FUGUE_11_KIMIKO.get() , 
-        (new Item.Properties()).stacksTo(1).tab(DoggyItemGroups.GENERAL).rarity(Rarity.RARE), 292));
-    public static final RegistryObject<Item> MUSIC_DISC_BWV_849_FUGUE_KIMIKO = register("disc_bwv_849_fugue", 
-        () -> new RecordItem(13, () -> DoggySounds.BWV_849_FUGUE_KIMIKO.get() , 
-        (new Item.Properties()).stacksTo(1).tab(DoggyItemGroups.GENERAL).rarity(Rarity.RARE), 160));
-    public static final RegistryObject<Item> MUSIC_DISC_OKAMI_1 = register("disc_okami_ryoshima_coast_arr", 
-        () -> new RecordItem(13, () -> DoggySounds.OKAMI_RYOSHIMA_COAST_ARR.get() , 
-        (new Item.Properties()).stacksTo(1).rarity(Rarity.RARE), 111));
     public static final RegistryObject<Item> MUSIC_DISC_CHOPIN_OP64_NO1 = register("disc_chopin_op64_no1", 
         () -> new ChopinRecordItem(13, () -> DoggySounds.CHOPIN_OP64_NO1.get() , 
         (new Item.Properties()).stacksTo(1).rarity(Rarity.RARE), 132*20));
