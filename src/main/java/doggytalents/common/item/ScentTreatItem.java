2ed213599a87406ea3a14d74233e525fbb629e0f
DashieDev <lamtinh.asian@gmail.com>
2024-08-07 17:53:35 +0700

    add color to scent treat item desc

--- src/main/java/doggytalents/common/item/ScentTreatItem.java
+++ src/main/java/doggytalents/common/item/ScentTreatItem.java
@@ -91,10 +91,11 @@ public class ScentTreatItem extends Item {
         var block = NBTUtil.getRegistryValue(tag, SCENT_BLOCK_ID, ForgeRegistries.BLOCKS);
         if (block == null)
             return;
-        components.add(ComponentUtil.translatable(this.getDescriptionId() + ".scented_block"));
+        components.add(Component.translatable(this.getDescriptionId() + ".scented_block")
+            .withStyle(Style.EMPTY.withColor(0xffffea2e).withBold(true)));
         components.add(
-            ComponentUtil.translatable(block.asItem().getDescriptionId()).withStyle(
-                Style.EMPTY.withItalic(true)
+            Component.translatable(block.asItem().getDescriptionId()).withStyle(
+                Style.EMPTY.withItalic(true).withColor(0xff6fe86b)
             )
         );
     }
