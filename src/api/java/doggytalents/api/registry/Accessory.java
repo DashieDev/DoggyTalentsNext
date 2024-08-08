9de8197650a82d67feed447672bb041d14f3b90d
DashieDev <lamtinh.asian@gmail.com>
2024-08-08 14:48:38 +0700

    (Configurable) Overlay accessories now render as translucent

--- src/api/java/doggytalents/api/registry/Accessory.java
+++ src/api/java/doggytalents/api/registry/Accessory.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 import doggytalents.api.DoggyTalentsAPI;
+import doggytalents.common.config.ConfigHandler;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
@@ -120,6 +121,9 @@ public class Accessory {
     private boolean renderTranslucent = false;
 
     public boolean renderTranslucent() {
+        if (ConfigHandler.CLIENT.TRANSLUCENT_ALL_OVERLAY.get()) {
+            return true;
+        }
         return renderTranslucent;
     }
 
