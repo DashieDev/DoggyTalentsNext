72c1cf3b9b4d09d657a5d15a357ae2ebdb38722c
DashieDev <lamtinh.asian@gmail.com>
2024-08-09 22:31:32 +0700

    (Configurable) Dog don't push owner

--- src/main/java/doggytalents/common/config/ConfigHandler.java
+++ src/main/java/doggytalents/common/config/ConfigHandler.java
@@ -304,6 +304,7 @@ ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CONFIG_CLIENT_SPEC
         public ForgeConfigSpec.BooleanValue ALLOW_TRACK_ANY_DOG;
         public ForgeConfigSpec.BooleanValue LOG_WHEN_DOG_GO_OFFLINE;
         public ForgeConfigSpec.BooleanValue BG_MODE_LESS_STRICT;
+        public ForgeConfigSpec.BooleanValue DOG_DONT_PUSH_OWNER;
 
         //(Non 1.20.5+)
         public ForgeConfigSpec.BooleanValue RANDOM_VAR_ON_TRAIN;
@@ -525,6 +526,10 @@ ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CONFIG_CLIENT_SPEC
                 .comment("targetting the player a potential danger instead of just 'Enemy'(s)")
                 .translation("doggytalents.besserker_less_strict")
                 .define("b_g_mode_less_strict", false);
+            DOG_DONT_PUSH_OWNER = builder
+                .comment("Enable this to prevent Dogs from pushing their Owners")
+                .translation("doggytalents.dog_dont_push_owner")
+                .define("dog_dont_push_owner", false);
 
             //(Non 1.20.5+)
             RANDOM_VAR_ON_TRAIN = builder
