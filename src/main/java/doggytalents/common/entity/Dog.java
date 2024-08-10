4bc9e972b40e1fb309b49bcedcb4497266f76df8
DashieDev <lamtinh.asian@gmail.com>
2024-08-10 22:47:59 +0700

    dont canBreatheUnderwater using vanila !

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4671,7 +4671,7 @@ public class Dog extends AbstractDog {
             return true;
         if (!this.isInWater())
             return false;
-        if (this.canBreatheUnderwater())
+        if (this.alterationProps.canBreatheUnderwater())
             return true;
         if (this.canSwimUnderwater() && !this.isLowAirSupply())
             return true;
