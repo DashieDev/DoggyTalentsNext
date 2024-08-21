444adef4d1d92666b8fc5ddbbfc886f6b02770c7
DashieDev <lamtinh.asian@gmail.com>
2024-08-21 17:32:11 +0700

    stoopid invert again, drop bandage if incap val reset

--- src/main/java/doggytalents/common/entity/DogIncapacitatedMananger.java
+++ src/main/java/doggytalents/common/entity/DogIncapacitatedMananger.java
@@ -218,7 +218,7 @@ public class DogIncapacitatedMananger {
     }
 
     public void onHurt() {
-        if (!ConfigHandler.SERVER.INCAP_VAL_RESET_WHEN_HURT.get()) {
+        if (ConfigHandler.SERVER.INCAP_VAL_RESET_WHEN_HURT.get()) {
             this.dropBandages();
         }
     }
