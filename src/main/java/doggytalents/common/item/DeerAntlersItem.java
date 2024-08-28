4ca3febfeabbf2393a5e7dd67d5883a9982feca9
DashieDev <lamtinh.asian@gmail.com>
2024-08-28 16:05:09 +0700

    deer antlers description and en_us name

--- src/main/java/doggytalents/common/item/DeerAntlersItem.java
+++ src/main/java/doggytalents/common/item/DeerAntlersItem.java
@@ -1,13 +1,27 @@
 package doggytalents.common.item;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import doggytalents.api.registry.Accessory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 
 public class DeerAntlersItem extends AccessoryItem {
 
     public DeerAntlersItem(Supplier<? extends Accessory> type, Properties properties) {
         super(type, properties);
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
     }
     
 }
