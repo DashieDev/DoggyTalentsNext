ca5cb73d37a9d147f82d397802aa683cfb0694bf
DashieDev <lamtinh.asian@gmail.com>
2024-08-29 17:32:32 +0700

    refactor doggy serializer being uneccesary complicated

--- src/main/java/doggytalents/DoggySerializers.java
+++ src/main/java/doggytalents/DoggySerializers.java
@@ -1,32 +1,41 @@
 package doggytalents;
 
+import doggytalents.api.feature.DogLevel;
+import doggytalents.api.feature.DogSize;
+import doggytalents.api.feature.EnumGender;
+import doggytalents.api.feature.EnumMode;
+import doggytalents.common.entity.DogIncapacitatedMananger.IncapacitatedSyncState;
+import doggytalents.common.entity.DogPettingManager.DogPettingState;
 import doggytalents.common.entity.serializers.*;
+import doggytalents.common.entity.texture.DogSkinData;
+import doggytalents.common.item.DoggyArtifactItem;
 import doggytalents.common.lib.Constants;
+import doggytalents.common.variant.DogVariant;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataSerializer;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class DoggySerializers {
 
     //public static final DeferredRegister<EntityDataSerializer<?>> SERIALIZERS = DeferredRegister.create(ForgeRegistries.Keys.ENTITY_DATA_SERIALIZERS, Constants.MOD_ID);
 
-    public static final EntityDataSerializer DOG_VARIANT_SERIALIZER = register2("dog_variant", DogVariantSerializer::new);
-    //public static final EntityDataSerializer COLLAR_TYPE_SERIALIZER = register2("collar", CollarSerializer::new);
-    public static final EntityDataSerializer GENDER_SERIALIZER = register2("gender", GenderSerializer::new);
-    public static final EntityDataSerializer MODE_SERIALIZER = register2("mode", ModeSerializer::new);
-    public static final EntityDataSerializer DOG_LEVEL_SERIALIZER = register2("dog_level", DogLevelSerializer::new);
-    public static final EntityDataSerializer BED_LOC_SERIALIZER = register2("dog_bed_location", BedLocationsSerializer::new);
-    public static final EntityDataSerializer INCAP_SYNC_SERIALIZER = register2("incap_sync", IncapacitatedSyncSerializer::new);
-    public static final EntityDataSerializer ARTIFACTS_SERIALIZER = register2("doggy_artifacts", DoggyArtifactsSerializer::new);
-    public static final EntityDataSerializer DOG_SIZE_SERIALIZER = register2("dog_size", DogSizeSerializer::new);
-    public static final EntityDataSerializer DOG_SKIN_DATA_SERIALIZER = register2("dog_skin_data", DogSkinDataSerializer::new);
-    public static final EntityDataSerializer DOG_PETTING_STATE = register2("dog_petting_state", PettingStateSerializer::new);
+    public static final EntityDataSerializer<DogVariant> DOG_VARIANT_SERIALIZER = register("dog_variant", DogVariantSerializer::new);
+    public static final EntityDataSerializer<EnumGender> GENDER_SERIALIZER = register("gender", GenderSerializer::new);
+    public static final EntityDataSerializer<EnumMode> MODE_SERIALIZER = register("mode", ModeSerializer::new);
+    public static final EntityDataSerializer<DogLevel> DOG_LEVEL_SERIALIZER = register("dog_level", DogLevelSerializer::new);
+    public static final EntityDataSerializer<DimensionDependantArg<Optional<BlockPos>>> BED_LOC_SERIALIZER = register("dog_bed_location", BedLocationsSerializer::new);
+    public static final EntityDataSerializer<IncapacitatedSyncState> INCAP_SYNC_SERIALIZER = register("incap_sync", IncapacitatedSyncSerializer::new);
+    public static final EntityDataSerializer<List<DoggyArtifactItem>> ARTIFACTS_SERIALIZER = register("doggy_artifacts", DoggyArtifactsSerializer::new);
+    public static final EntityDataSerializer<DogSize> DOG_SIZE_SERIALIZER = register("dog_size", DogSizeSerializer::new);
+    public static final EntityDataSerializer<DogSkinData> DOG_SKIN_DATA_SERIALIZER = register("dog_skin_data", DogSkinDataSerializer::new);
+    public static final EntityDataSerializer<DogPettingState> DOG_PETTING_STATE = register("dog_petting_state", PettingStateSerializer::new);
 
-    private static <X extends EntityDataSerializer<?>> EntityDataSerializer register2(final String name, final Supplier<X> factory) {
-        return register(name, () -> factory.get());
-    }
-
-    private static EntityDataSerializer register(final String name, final Supplier<EntityDataSerializer> sup) {
-        return sup.get();
+    private static <T> EntityDataSerializer<T> register(final String name, final Supplier<EntityDataSerializer<T>> sup) {
+        final var captured_value = sup.get();
+        //SERIALIZERS.register(name, () -> captured_value);
+        return captured_value;
     }
 }
