00ad919cdb0414ce01da11c6115595c98e11ee2e
DashieDev <lamtinh.asian@gmail.com>
2024-09-28 03:33:32 +0700

    Improper usage of PathComputation.LAND check

--- src/main/java/doggytalents/common/entity/ai/DogGoAwayFromFireGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogGoAwayFromFireGoal.java
@@ -152,7 +152,7 @@ public class DogGoAwayFromFireGoal extends Goal {
         var pos_below = new BlockPos(pos).below();
         var state_below = dog.level().getBlockState(pos_below);
         if (WalkNodeEvaluator.isBurningBlock(state_below)
-            && !state_below.isPathfindable(dog.level(), pos_below, PathComputationType.LAND))
+            && state_below.isCollisionShapeFullBlock(dog.level(), pos_below))
             return 1;
 
         // {
