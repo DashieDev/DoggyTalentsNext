1d7f105ae99969471203c0d779dfac7db763f654
DashieDev <lamtinh.asian@gmail.com>
2024-09-30 07:21:49 +0700

    1.19.2 fix

--- src/main/java/doggytalents/common/talent/FisherDogTalent.java
+++ src/main/java/doggytalents/common/talent/FisherDogTalent.java
@@ -12,7 +12,6 @@ import doggytalents.api.registry.TalentOption;
 import doggytalents.api.registry.Talent;
 import doggytalents.api.registry.TalentInstance;
 import doggytalents.common.entity.Dog;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
