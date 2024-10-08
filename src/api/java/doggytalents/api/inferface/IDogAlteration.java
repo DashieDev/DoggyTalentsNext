119ab77287da5ab94149f9317e799bd2d0bb0a23
DashieDev <lamtinh.asian@gmail.com>
2024-10-09 02:45:47 +0700

    comment clean up

--- src/api/java/doggytalents/api/inferface/IDogAlteration.java
+++ src/api/java/doggytalents/api/inferface/IDogAlteration.java
@@ -24,12 +24,6 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 
-/**
- * Defines objects which may alter the dog's properties or functions.
- * 
- * @author ProPercivalalb
- * compL by DashieDev
- */
 public interface IDogAlteration {
 
     /**
