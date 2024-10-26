e1138e2cf567855926faa5c07c6a05ec80bd468c
DashieDev <lamtinh.asian@gmail.com>
2024-10-26 15:35:23 +0700

    correctSittingPose only in serverside no clietnside since AI make no sense in clientside.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4594,6 +4594,8 @@ public class Dog extends AbstractDog {
     }
 
     private void mayDogCorrectSittingPose(boolean sit_ordered) {
+        if (this.level().isClientSide)
+            return;
         if (sit_ordered)
             return;
         if (this.dogAi == null)
