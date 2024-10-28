7686a4df5ae6a0ea2e2d6db859baf73d91987fdd
DashieDev <lamtinh.asian@gmail.com>
2024-10-28 14:37:45 +0700

    stopLocating when player logged out clientside

--- src/main/java/doggytalents/client/event/ClientEventHandler.java
+++ src/main/java/doggytalents/client/event/ClientEventHandler.java
@@ -12,6 +12,7 @@ import doggytalents.client.DoggyKeybinds;
 import doggytalents.client.block.model.DogBedModel;
 import doggytalents.client.entity.model.animation.DogAnimationRegistry;
 import doggytalents.client.entity.model.animation.DogKeyframeAnimations;
+import doggytalents.client.entity.render.world.CanineTrackerLocateRenderer;
 import doggytalents.client.screen.DogNewInfoScreen.DogNewInfoScreen;
 import doggytalents.client.screen.DogNewInfoScreen.store.UIActionTypes;
 import doggytalents.client.screen.DogNewInfoScreen.store.slice.TalentChangeHandlerSlice;
@@ -314,6 +315,11 @@ public class ClientEventHandler {
         if (infoScr.dog != dog)
             return;
         Store.get(infoScr).dispatch(TalentChangeHandlerSlice.class, new UIAction(UIActionTypes.Talents.TALENT_UPDATE, null));
+    }
+
+    @SubscribeEvent
+    public void onPlayerLoggingOut(LoggingOut event) {
+        CanineTrackerLocateRenderer.onWorldLogOut();
     }
 
 }
