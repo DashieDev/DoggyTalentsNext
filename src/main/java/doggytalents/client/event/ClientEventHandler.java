d56f20d98297ef1aca57d6cf0f6788b9924a3a55
DashieDev <lamtinh.asian@gmail.com>
2024-10-28 14:38:35 +0700

    clearer event

--- src/main/java/doggytalents/client/event/ClientEventHandler.java
+++ src/main/java/doggytalents/client/event/ClientEventHandler.java
@@ -324,7 +324,7 @@ public class ClientEventHandler {
     }
 
     @SubscribeEvent
-    public void onPlayerLoggingOut(LoggingOut event) {
+    public void onPlayerLoggingOut(ClientPlayerNetworkEvent.LoggingOut event) {
         CanineTrackerLocateRenderer.onWorldLogOut();
     }
 
