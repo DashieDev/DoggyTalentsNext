0dfbad7525b86e5017cc29bd6fb7be4db913d2bc
DashieDev <lamtinh.asian@gmail.com>
2024-10-29 13:38:50 +0700

    log error instead when dog failed to update location upon going offline

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2330,6 +2330,7 @@ public class Dog extends AbstractDog {
         return false;
     }
 
+    public boolean locationUpdatedUponRemove = false;
     //@Override
     public void onRemovedFromWorld() {
         if (this.level() instanceof ServerLevel serverLevel && this.isAlive()) {
@@ -2338,6 +2339,7 @@ public class Dog extends AbstractDog {
             var data = DogLocationStorage.get(serverLevel).getData(this);
             
             if (data != null) data.update(this);
+            locationUpdatedUponRemove = true;
         }
         //super.onRemovedFromWorld();
     }
