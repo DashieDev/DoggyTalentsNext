e96a314df4d23a886c12042ba71925096c45cf60
DashieDev <lamtinh.asian@gmail.com>
2024-10-29 16:35:00 +0700

    option to disable wolf armor protection

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1739,6 +1739,8 @@ public class Dog extends AbstractDog {
     }
 
     // private boolean canWolfArmorAbsorb(DamageSource source) {
+    //     if (ConfigHandler.SERVER.DISABLE_WOLF_ARMOR_FUNCTION.get())
+    //         return false;
     //     return this.hasWolfArmor() && !source.is(DamageTypeTags.BYPASSES_WOLF_ARMOR);
     // }
 
