d56a16e1ffc05031939c60bb2e7350b5117e9658
DashieDev <lamtinh.asian@gmail.com>
2024-11-03 23:54:56 +0700

    anim debug stick item and front end

--- src/main/java/doggytalents/client/data/DTItemModelProvider.java
+++ src/main/java/doggytalents/client/data/DTItemModelProvider.java
@@ -191,6 +191,8 @@ public class DTItemModelProvider extends ItemModelProvider {
         blockItem(DoggyBlocks.DOG_BED);
         blockItem(DoggyBlocks.FOOD_BOWL);
         generated(DoggyBlocks.RICE_MILL);
+
+        generated(DoggyItems.DOG_ANIM_DEBUG);
     }
 
     private ResourceLocation itemTexture(Supplier<? extends ItemLike> item) {
