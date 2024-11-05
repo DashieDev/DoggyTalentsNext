a53c4eaa79ded1566855155786f8e5581f86b38b
DashieDev <lamtinh.asian@gmail.com>
2024-11-05 21:00:45 +0700

    unecessary synchonization in ClientPettingManager
    since everything need to be interact with the clientside game state
    to be able to inrteract with this class, they already needed
    to be in the client thread.

--- src/main/java/doggytalents/client/DTNClientPettingManager.java
+++ src/main/java/doggytalents/client/DTNClientPettingManager.java
@@ -54,9 +54,9 @@ public class DTNClientPettingManager {
     private float pet_camera_xRot0_add = 0;
     private float pet_camera_yRot0_add = 0;
     
-    private final Map<UUID, Dog> petterMap = Maps.newConcurrentMap();
+    private final Map<UUID, Dog> petterMap = Maps.newHashMap();
 
-    public synchronized void setPetMode(DogPettingType type) {
+    public void setPetMode(DogPettingType type) {
         this.selectedType = type;
     }
 
@@ -64,7 +64,7 @@ public class DTNClientPettingManager {
         return this.selectedType;
     }
 
-    public synchronized void setPetting(Dog dog) {
+    public void setPetting(Dog dog) {
         isPetting = dog != null;
         this.dog = dog;
         if (!isPetting)
