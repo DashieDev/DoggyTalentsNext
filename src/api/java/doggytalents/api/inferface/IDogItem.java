7d45481a2e626571bb6da4a6f73994289a944055
DashieDev <lamtinh.asian@gmail.com>
2024-11-05 22:35:40 +0700

    remove InteractHandler and refactor IDogItem handling

--- src/api/java/doggytalents/api/inferface/IDogItem.java
+++ src/api/java/doggytalents/api/inferface/IDogItem.java
@@ -1,24 +1,21 @@
 package doggytalents.api.inferface;
 
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.level.Level;
 
 public interface IDogItem {
 
-    /**
-     * Implement on item class called when player interacts with a dog
-     * @param dogIn The dog the item is being used on
-     * @param worldIn The world the dog is in
-     * @param playerIn The player who clicked
-     * @param handIn The hand used
-     * @return The result of the interaction
-     */
     public InteractionResult processInteract(AbstractDog dogIn, Level worldIn, Player playerIn, InteractionHand handIn);
 
-    @Deprecated
-    default InteractionResult onInteractWithDog(AbstractDog dogIn, Level worldIn, Player playerIn, InteractionHand handIn) {
-        return processInteract(dogIn, worldIn, playerIn, handIn);
+    public static InteractionResult getMatch(AbstractDog dog, ItemStack stack, Player player, InteractionHand hand) {
+        var item = stack.getItem();
+        if (!(item instanceof IDogItem dog_item))
+            return InteractionResult.PASS;
+        var result = dog_item.processInteract(dog, dog.level(), player, hand);
+        return result;
     }
+
 }
