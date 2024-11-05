7568427d33023139c8d414a1dc8860d117618ec8
DashieDev <lamtinh.asian@gmail.com>
2024-11-05 22:35:40 +0700

    remove InteractHandler and refactor IDogItem handling

--- src/main/java/doggytalents/DoggyTalentsNext.java
+++ src/main/java/doggytalents/DoggyTalentsNext.java
@@ -1,7 +1,6 @@
 package doggytalents;
 
 import doggytalents.api.feature.FoodHandler;
-import doggytalents.api.feature.InteractHandler;
 import doggytalents.client.ClientSetup;
 import doggytalents.client.DogBlockRenderTypeSetup;
 import doggytalents.client.DoggyKeybinds;
