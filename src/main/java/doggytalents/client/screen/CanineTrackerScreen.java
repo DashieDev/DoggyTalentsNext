4f11cbc1fd9816fe7fd70bc02fcc4faa65881c17
DashieDev <lamtinh.asian@gmail.com>
2024-11-08 18:15:37 +0700

    Also copy the style and string select screen set selected color via Component

--- src/main/java/doggytalents/client/screen/CanineTrackerScreen.java
+++ src/main/java/doggytalents/client/screen/CanineTrackerScreen.java
@@ -119,7 +119,8 @@ public class CanineTrackerScreen extends StringEntrySelectScreen {
         if (this.showUuid) {
             entryText = Component.literal(
                 entryText.getString() +
-                " ( " + this.dogIdList.get(entryId) + " ) ");
+                " ( " + this.dogIdList.get(entryId) + " ) ")
+            .setStyle(entryText.getStyle());
         }
 
         return entryText;
--- src/main/java/doggytalents/client/screen/ConductingBoneScreen.java
+++ src/main/java/doggytalents/client/screen/ConductingBoneScreen.java
@@ -109,7 +109,8 @@ public class ConductingBoneScreen extends StringEntrySelectScreen {
         if (this.showUuid) {
             entryText = Component.literal(
                 entryText.getString() +
-                " ( " + this.dogIdList.get(entryId) + " ) ");
+                " ( " + this.dogIdList.get(entryId) + " ) ")
+                .setStyle(entryText.getStyle());
         }
 
         return entryText;
