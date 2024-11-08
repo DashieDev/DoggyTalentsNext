c1a5257a311ad6dfdfa10f9469b00a5ddc5b5c5e
DashieDev <lamtinh.asian@gmail.com>
2024-11-08 16:35:17 +0700

    StringScreen API reinforce visibly and method name change

--- src/main/java/doggytalents/client/screen/StringEntrySelectScreen.java
+++ src/main/java/doggytalents/client/screen/StringEntrySelectScreen.java
@@ -20,9 +20,9 @@ public class StringEntrySelectScreen extends Screen {
     private List<String> entries;
     private List<Integer> filteredIndexes;
 
-    protected MouseUpdate mouseUpdate = new MouseUpdate(this);
-    protected EntryView entryView = new EntryView(this);
-    protected TextField searchField = new TextField(this);
+    protected final MouseUpdate mouseUpdate = new MouseUpdate(this);
+    protected final EntryView entryView = new EntryView(this);
+    protected final TextField searchField = new TextField(this);
 
     private TextOnlyButton prevPageButton;
     private TextOnlyButton nextPageButton;
@@ -166,7 +166,7 @@ public class StringEntrySelectScreen extends Screen {
     @Override
     public boolean mouseClicked(double x, double y, int p_94697_) {
         var clicked_id = this.entryView
-            .getClickedEntry(x, y, filteredIndexes);
+            .getHoveringEntry(x, y, filteredIndexes);
         if (clicked_id.isPresent()) {
             onEntrySelected(clicked_id.get());
             return true;
@@ -256,7 +256,7 @@ public class StringEntrySelectScreen extends Screen {
         return false;
     }
 
-    private static class EntryView {
+    public static class EntryView {
         private final StringEntrySelectScreen parent;
         private int pageCount = 1;
         private int activePage = 0;
@@ -289,7 +289,7 @@ public class StringEntrySelectScreen extends Screen {
             this.updatePages(filtered_indexes);
         }
 
-        public Optional<Integer> getClickedEntry(double x, double y,
+        public Optional<Integer> getHoveringEntry(double x, double y,
             List<Integer> filtered_indexes) {
             if (filtered_indexes.isEmpty())
                 return Optional.empty();
@@ -408,7 +408,7 @@ public class StringEntrySelectScreen extends Screen {
 
     }
 
-    private static class MouseUpdate {
+    public static class MouseUpdate {
         private final StringEntrySelectScreen parent;
         private boolean startedUpdate = false;
         private int mouseX0 = 0;
@@ -451,13 +451,13 @@ public class StringEntrySelectScreen extends Screen {
 
     }
 
-    private static class TextField {
+    public static class TextField {
         private final StringEntrySelectScreen parent;
         private long blockCharInputMillis = 0;
         private long prevMillis = 0;
         private String searchString = "";
 
-        public TextField(StringEntrySelectScreen parent) {
+        private TextField(StringEntrySelectScreen parent) {
             this.parent = parent;
         }
 
