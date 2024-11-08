de9a35077691004a453f3dc806390df5c9bcb904
DashieDev <lamtinh.asian@gmail.com>
2024-11-08 13:35:50 +0700

    dogAnimDebugScreen search isgnore Case

--- src/main/java/doggytalents/client/screen/DogAnimDebugScreen.java
+++ src/main/java/doggytalents/client/screen/DogAnimDebugScreen.java
@@ -155,4 +155,9 @@ public class DogAnimDebugScreen extends StringEntrySelectScreen {
         }
     }
 
+    @Override
+    protected boolean matchIgnoreCaseSearch() {
+        return true;
+    }
+
 }
