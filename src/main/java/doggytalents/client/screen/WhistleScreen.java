7c5e776ec79014c7e8d337fb33555e373426f325
DashieDev <lamtinh.asian@gmail.com>
2024-11-08 17:26:46 +0700

    refactor WhistleScreen

--- src/main/java/doggytalents/client/screen/WhistleScreen.java
+++ src/main/java/doggytalents/client/screen/WhistleScreen.java
@@ -1,52 +1,29 @@
 package doggytalents.client.screen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import com.mojang.blaze3d.platform.InputConstants;
-import com.mojang.blaze3d.vertex.PoseStack;
+import java.util.stream.Collectors;
 
 import doggytalents.client.screen.framework.widget.FlatButton;
-import doggytalents.client.screen.widget.CustomButton;
 import doggytalents.DoggyItems;
-import doggytalents.common.entity.Dog;
-import doggytalents.common.item.WhistleItem;
 import doggytalents.common.item.WhistleItem.WhistleMode;
 import doggytalents.common.network.PacketHandler;
-import doggytalents.common.network.packet.data.HeelByNameData;
 import doggytalents.common.network.packet.data.WhisltleEditHotKeyData;
 import doggytalents.common.network.packet.data.WhistleRequestModeData;
-import net.minecraft.SharedConstants;
+import doggytalents.common.util.ItemUtil;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Tooltip;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.player.Player;
-import doggytalents.forge_imitate.network.PacketDistributor;
+import doggytalents.common.network.PacketDistributor;
 
 
-public class WhistleScreen extends Screen{
+public class WhistleScreen extends StringEntrySelectScreen {
 
-   private List<WhistleMode> modeFilterList;
-   private int selectedId;
-   private String value = "";
-   
-   private final int HLC_SELECTED = 0xFF10F9; 
-
-   private int hightlightTextColor = HLC_SELECTED;
-
-    private final int MAX_BUFFER_SIZE = 64;
-
-    private int mouseX0;
-    private int mouseY0;
+    private final List<WhistleMode> modeList;
 
     private boolean settingKeysMode = false;
     private int pKey = 0;
@@ -54,11 +31,11 @@ public class WhistleScreen extends Screen{
 
     public WhistleScreen() {
         super(Component.translatable("doggytalents.screen.whistler.title"));
-        this.modeFilterList = new ArrayList<WhistleMode>();
-        for (var i : WhistleMode.VALUES) {
-            this.modeFilterList.add(i);
-        }
-        this.selectedId = 0;
+        this.modeList = Arrays.stream(WhistleMode.VALUES)
+            .collect(Collectors.toList());
+        this.updateEntries(this.modeList.stream()
+            .map(x -> I18n.get(x.getUnlocalisedTitle()))
+            .collect(Collectors.toList()));
     }
 
     public static void open() { 
@@ -67,10 +44,7 @@ public class WhistleScreen extends Screen{
     }
 
     @Override
-    public void init() {
-        super.init();
-        //this.minecraft.keyboardHandler.setSendRepeatsToGui(true);
-
+    protected void addUtilitiesButton() {
         int mX = this.width/2;
         int mY = this.height/2;
         int pY = mY - 100;
@@ -116,119 +90,84 @@ public class WhistleScreen extends Screen{
             }
         };
 
-        
-        
         this.addRenderableWidget(help);
         this.addRenderableWidget(setKey);
     }
 
- 
     @Override
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-
-        if (this.mouseX0 != mouseX || this.mouseY0 != mouseY) {
-            this.onMouseMoved(mouseX, mouseY);
-            this.mouseX0 = mouseX;
-            this.mouseY0 = mouseY;
-        }
-
-        super.render(graphics, mouseX, mouseY, partialTicks);
-
-        int half_width = this.width >> 1;
-        int half_height = this.height >> 1; 
-      
-        graphics.fill( half_width - 100, half_height - 100, half_width + 100, half_height + 100, Integer.MIN_VALUE);
-        graphics.fill( half_width - 100, half_height + 105, half_width + 100, half_height + 117, Integer.MIN_VALUE);
-
+    protected void drawEntry(GuiGraphics graphics, int entry_x, int entry_y, int start_indx, int render_indx,
+            int selected_entry_in_page) {
         if (this.settingKeysMode) {
-            renderModeListSetHotkey(graphics, mouseX, mouseY, partialTicks);
+            drawSetMode(graphics, entry_x, entry_y, start_indx, render_indx, selected_entry_in_page);
         } else {
-            renderModeListWhistleUse(graphics, mouseX, mouseY, partialTicks);
+            drawNonSetMode(graphics, entry_x, entry_y, start_indx, render_indx, selected_entry_in_page);
         }
-
-        
-        int txtorgx = half_width - 90;
-        int txtorgy = half_height + 107;
-        
-        graphics.drawString(font, this.value + "_", txtorgx, txtorgy,  0xffffffff);
-         
     }
 
-    public void renderModeListWhistleUse(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-        int half_width = this.width >> 1;
-        int half_height = this.height >> 1; 
+    private void drawNonSetMode(GuiGraphics graphics, int entry_x, int entry_y, int start_indx, int render_indx,
+        int selected_entry_in_page) {
         
-        int offset = 0;
-        int textx = half_width - 100 + 2;
-        int texty = half_height - 100 + 2;
+        int color = 0xffffffff;
+        boolean is_selected_entry =
+            render_indx == start_indx + selected_entry_in_page;
+        if (is_selected_entry) 
+            color = getHightlightSelectedColor();
+        var text = Component.translatable(this.modeList.get(render_indx).getUnlocalisedTitle());
+        text.withStyle(
+            Style.EMPTY
+            .withBold(false)
+            .withColor(color)
+        );
+        graphics.drawString(font, text, entry_x, entry_y, color);
+    }
 
-        for (int i = 0; i < this.modeFilterList.size(); ++i) {
-            int color = 0xffffffff;
-            if (i == this.selectedId) color = this.hightlightTextColor;
-            var text = Component.translatable(this.modeFilterList.get(i).getUnlocalisedTitle());
-            text.withStyle(
-                Style.EMPTY
-                .withBold(false)
-                .withColor(color)
+    private void drawSetMode(GuiGraphics graphics, int entry_x, int entry_y, int start_indx, int render_indx,
+        int selected_entry_in_page) {
+            
+        int color = 0xffffffff;
+        boolean is_selected_entry =
+            render_indx == start_indx + selected_entry_in_page;
+        if (is_selected_entry) 
+            color = getHightlightSelectedColor();
+        var mode = this.modeList.get(render_indx);
+        int hotkey_indx = findHotkeyForMode(mode.getIndex());
+        int prefix_color = 0xff6f00;
+        MutableComponent text; 
+        if (is_selected_entry) {
+            boolean remove = false;
+            prefix_color = 0x0aff43;
+            if (hotkey_indx >= 0) { 
+                prefix_color = 0xff3636;
+                remove = true;
+            }
+            text = Component.literal(
+                remove ? "- " : pKey + " "
             );
-            graphics.drawString(font, text, textx, texty + offset, color);
-            offset+=10;
-            if (offset > 190) break;
+        } else if (hotkey_indx >= 0) {
+            prefix_color = 0xff6f00;
+            text = Component.literal(hotkey_indx + " ");
+        } else {
+            text = Component.literal("  ");
         }
+        text.withStyle(
+            Style.EMPTY
+                .withBold(true)
+                .withColor(prefix_color)
+        );
+        var title = Component.translatable(mode.getUnlocalisedTitle());
+        title.withStyle(
+            Style.EMPTY
+            .withBold(false)
+            .withColor(color)
+        );
+        text.append(title);
+        graphics.drawString(font, text, entry_x, entry_y, color);
     }
 
-    public void renderModeListSetHotkey(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-
+    public void tick() {
         updateCurrentHotKeys();
         this.pKey = findEmptyHotkey();
-
-        int half_width = this.width >> 1;
-        int half_height = this.height >> 1; 
-        
-        int offset = 0;
-        int textx = half_width - 100 + 2;
-        int texty = half_height - 100 + 2;
-
-        for (int i = 0; i < this.modeFilterList.size(); ++i) {
-            int color = 0xffffffff;
-            if (i == this.selectedId) color = this.hightlightTextColor;
-            int mode_id = this.modeFilterList.get(i).getIndex();
-            int hotkey_indx = findHotkeyForMode(mode_id);
-            int prefix_color = 0xff6f00;
-            MutableComponent text; 
-            if (i == this.selectedId) {
-                boolean remove = false;
-                prefix_color = 0x0aff43;
-                if (hotkey_indx >= 0) { 
-                    prefix_color = 0xff3636;
-                    remove = true;
-                }
-                text = Component.literal(
-                    remove ? "- " : pKey + " "
-                );
-            } else if (hotkey_indx >= 0) {
-                prefix_color = 0xff6f00;
-                text = Component.literal(hotkey_indx + " ");
-            } else {
-                text = Component.literal("  ");
-            }
-            text.withStyle(
-                Style.EMPTY
-                    .withBold(true)
-                    .withColor(prefix_color)
-            );
-            var title = Component.translatable(this.modeFilterList.get(i).getUnlocalisedTitle());
-            title.withStyle(
-                Style.EMPTY
-                .withBold(false)
-                .withColor(color)
-            );
-            text.append(title);
-            graphics.drawString(font, text, textx, texty + offset, color);
-            offset+=10;
-            if (offset > 190) break;
-        }
-    }
+    };
 
     private void updateCurrentHotKeys() {
         for (int i = 0; i < this.hotkeysModeArr.length; ++i) {
@@ -265,66 +204,20 @@ public class WhistleScreen extends Screen{
         }
         return -1;
     }
-
+    
     private void sendHotKeyEdits(int hotkey_id, int mode_id) {
         PacketHandler.send(PacketDistributor.SERVER.noArg(), 
             new WhisltleEditHotKeyData(hotkey_id, mode_id));
     }
 
-    private int getHoveredIndex(double x, double y, int entry_size) {
-        int mX = this.width/2;
-        int mY = this.height/2;
-        if (Math.abs(x - mX) > 100) return -1;
-        if (Math.abs(y - mY) > 100) return -1;
-        int baseY = mY - 100;
-        int indx = ( Mth.floor(y - baseY) )/10;
-        if (indx >= entry_size) return -1;
-        return indx;
-    }
-
-    private void onMouseMoved(double x, double y) {
-        int newIndx = getHoveredIndex(x, y, this.modeFilterList.size());
-        if (newIndx < 0) return;
-        this.selectedId = newIndx;
-    }
-
     @Override
-    public boolean mouseClicked(double x, double y, int p_94697_) {
-        boolean ret = super.mouseClicked(x, y, p_94697_);
-        int mX = this.width/2;
-        int mY = this.height/2;
-        if (Math.abs(x - mX) > 100) return ret;
-        if (Math.abs(y - mY) > 100) return ret;
-        int indx = getHoveredIndex(x, y, this.modeFilterList.size());
-        if (indx >= 0) {
-            proccessSelectIndx(indx);
-        }
-        return ret;
+    protected void onEntrySelected(int id) {
+        proccessSelectIndx(id);
     }
 
-    @Override
-    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
-        InputConstants.Key mouseKey = InputConstants.getKey(keyCode, scanCode);
-
-        if (keyCode == 264) {
-            if (this.modeFilterList.isEmpty()) return false; 
-            this.selectedId = Mth.clamp(this.selectedId +1, 0, this.modeFilterList.size()-1);
-        } else if (keyCode == 265) {
-            if (this.modeFilterList.isEmpty()) return false; 
-            this.selectedId = Mth.clamp(this.selectedId -1, 0,  this.modeFilterList.size()-1);
-        } else if (keyCode == 257) {
-            if (this.modeFilterList.isEmpty()) return false; 
-            proccessSelectIndx(this.selectedId);
-        } else if (keyCode == 259) {
-            this.popCharInText();
-        } 
-
-        return super.keyReleased(keyCode, scanCode, modifiers);
-    }
-
-    public void proccessSelectIndx(int indx) {
+    private void proccessSelectIndx(int indx) {
         if (this.settingKeysMode) {
-            int new_mode_id = this.modeFilterList.get(indx).getIndex();
+            int new_mode_id = this.modeList.get(indx).getIndex();
             int send_key = this.pKey;
             int occupied_key = this.findHotkeyForMode(new_mode_id);
             if (occupied_key >= 0) {
@@ -334,60 +227,13 @@ public class WhistleScreen extends Screen{
                 
             this.sendHotKeyEdits(send_key, new_mode_id);
         } else {
-            this.requestMode(this.modeFilterList.get(indx).getIndex());
+            this.requestMode(this.modeList.get(indx).getIndex());
             Minecraft.getInstance().setScreen(null);
         }
     }
 
-
-    @Override
-    public boolean charTyped(char code, int p_231042_2_) {
-
-        if (SharedConstants.isAllowedChatCharacter(code)) {
-            this.insertText(Character.toString(code));
-            return true;
-        } else {
-            return false;
-        }
-        
-    }
-
-    private void updateFilter() {
-        this.modeFilterList.clear();
-        this.selectedId = 0;
-
-        if (this.value == "") {
-            for (var i : WhistleMode.VALUES) {
-                this.modeFilterList.add(i);
-            }
-        } else {
-            for (var i : WhistleMode.VALUES) {
-                String text = I18n.get(i.getUnlocalisedTitle());
-                if (text.length() < this.value.length()) continue; 
-                if (text.contains(this.value)) {
-                    this.modeFilterList.add(i);
-                }
-            }
-        }
-    } 
-
-    private void insertText(String x) {
-        if (this.value.length() < MAX_BUFFER_SIZE) {
-            this.value = this.value + x;
-        }
-        this.updateFilter();
-    }
-
-    private void popCharInText() {
-        if (this.value.length() <= 0) return;
-        this.value = this.value.substring(0, this.value.length()-1);
-        this.updateFilter();
-    }
-    
-    @Override
-    public void removed() {
-        super.removed();
-        //this.minecraft.keyboardHandler.setSendRepeatsToGui(false);
+    private void requestMode(int id) {
+        PacketHandler.send(PacketDistributor.SERVER.noArg(), new WhistleRequestModeData(id));
     }
 
     @Override
@@ -395,8 +241,4 @@ public class WhistleScreen extends Screen{
         return false;
     }
 
-
-    private void requestMode(int id) {
-        PacketHandler.send(PacketDistributor.SERVER.noArg(), new WhistleRequestModeData(id));
-    }
 }
