8838aa357bc13e61c3e08aad8ed40ed7f807c367
DashieDev <lamtinh.asian@gmail.com>
2024-11-09 12:14:22 +0700

    fix hotdog costume

--- src/main/java/doggytalents/client/entity/model/HotDogModel.java
+++ src/main/java/doggytalents/client/entity/model/HotDogModel.java
@@ -25,25 +25,25 @@ public class HotDogModel extends SyncedAccessoryModel{
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create(), PartPose.offsetAndRotation(0F, 14.0F, -3.0F, 1.5708F, 0.0F, 0.0F));
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 14.0F, 2.0F, 1.5708F, 0.0F, 0.0F));
 
-		PartDefinition hotdogmane = upper_body.addOrReplaceChild("hotdogmane", CubeListBuilder.create().texOffs(46, 40).addBox(3.0F, -4.1564F, -4.401F, 2.0F, 7.0F, 7.0F, new CubeDeformation(0.3F))
-		.texOffs(46, 40).addBox(-3.0F, -4.1564F, -4.401F, 2.0F, 7.0F, 7.0F, new CubeDeformation(0.3F)), PartPose.offset(-1.0F, 2.0F, -1.5F));
+		PartDefinition hottodoggubody = body.addOrReplaceChild("hottodoggubody", CubeListBuilder.create().texOffs(46, 46).addBox(-4.0F, -3.2F, -0.1F, 2.0F, 11.0F, 7.0F, new CubeDeformation(0.1F))
+		.texOffs(46, 46).addBox(2.0F, -3.2F, -0.1F, 2.0F, 11.0F, 7.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 1.4287F, -2.8968F));
 
-		PartDefinition body_rotation_r1 = hotdogmane.addOrReplaceChild("body_rotation_r1", CubeListBuilder.create().texOffs(46, 47).addBox(-8.1F, -7.5F, -3.5F, 2.0F, 7.0F, 7.0F, new CubeDeformation(0.2F)), PartPose.offsetAndRotation(1.0F, 3.3436F, 3.749F, 0.0F, -1.5708F, 0.0F));
+		PartDefinition body_rotation_r1 = hottodoggubody.addOrReplaceChild("body_rotation_r1", CubeListBuilder.create().texOffs(46, 47).addBox(4.3F, -0.25F, -3.5F, 2.0F, 7.0F, 7.0F, new CubeDeformation(-0.1F))
+		.texOffs(46, 47).addBox(-5.6F, 1.5F, -3.5F, 2.0F, 3.0F, 7.0F, new CubeDeformation(0.1F)), PartPose.offsetAndRotation(0.0F, 2.0F, 0.25F, 1.5708F, 0.0F, -1.5708F));
 
-		PartDefinition body_rotation_r2 = hotdogmane.addOrReplaceChild("body_rotation_r2", CubeListBuilder.create().texOffs(46, 13).addBox(2.1F, -9.25F, -2.5F, 2.0F, 6.0F, 7.0F, new CubeDeformation(0.3F)), PartPose.offsetAndRotation(1.0F, 5.3436F, -1.401F, 0.0F, -1.5708F, 0.0F));
+		PartDefinition body_rotation_r2 = hottodoggubody.addOrReplaceChild("body_rotation_r2", CubeListBuilder.create().texOffs(46, 19).addBox(4.9F, -3.75F, -3.5F, 2.0F, 11.0F, 7.0F, new CubeDeformation(0.2F))
+		.texOffs(46, 46).addBox(0.2F, -2.25F, -3.5F, 2.0F, 10.0F, 7.0F, new CubeDeformation(0.2F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, -1.5708F, 0.0F));
 
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 14.0F, 2.75F, 1.5708F, 0.0F, 0.0F));
+		PartDefinition upper_body = partdefinition.addOrReplaceChild("upper_body", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 14.0F, -3.0F, 1.5708F, 0.0F, 0.0F));
 
-		PartDefinition hottodoggubody = body.addOrReplaceChild("hottodoggubody", CubeListBuilder.create().texOffs(46, 46).addBox(-4.0F, -1.7F, -3.35F, 2.0F, 11.0F, 7.0F, new CubeDeformation(0.1F))
-		.texOffs(46, 46).addBox(2.0F, -1.7F, -3.35F, 2.0F, 11.0F, 7.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 0.8968F, -2.3213F));
+		PartDefinition hotdogmane = upper_body.addOrReplaceChild("hotdogmane", CubeListBuilder.create().texOffs(46, 40).addBox(3.0F, -4.1564F, -1.401F, 2.0F, 7.0F, 7.0F, new CubeDeformation(0.3F))
+		.texOffs(46, 40).addBox(-3.0F, -4.1564F, -1.401F, 2.0F, 7.0F, 7.0F, new CubeDeformation(0.3F)), PartPose.offset(-1.0F, 2.0F, -1.5F));
 
-		PartDefinition body_rotation_r3 = hottodoggubody.addOrReplaceChild("body_rotation_r3", CubeListBuilder.create().texOffs(46, 47).addBox(4.3F, -3.25F, -3.5F, 2.0F, 7.0F, 7.0F, new CubeDeformation(-0.1F))
-		.texOffs(46, 47).addBox(-5.6F, -1.5F, -3.5F, 2.0F, 3.0F, 7.0F, new CubeDeformation(0.1F)), PartPose.offsetAndRotation(0.0F, 3.5F, 0.0F, 1.5708F, 0.0F, -1.5708F));
+		PartDefinition body_rotation_r3 = hotdogmane.addOrReplaceChild("body_rotation_r3", CubeListBuilder.create().texOffs(46, 47).addBox(-5.1F, -7.5F, -3.5F, 2.0F, 7.0F, 7.0F, new CubeDeformation(0.2F)), PartPose.offsetAndRotation(1.0F, 3.3436F, 3.749F, 0.0F, -1.5708F, 0.0F));
 
-		PartDefinition body_rotation_r4 = hottodoggubody.addOrReplaceChild("body_rotation_r4", CubeListBuilder.create().texOffs(46, 19).addBox(1.9F, -3.75F, -3.5F, 2.0F, 11.0F, 7.0F, new CubeDeformation(0.2F))
-		.texOffs(46, 46).addBox(-2.8F, -2.25F, -3.5F, 2.0F, 10.0F, 7.0F, new CubeDeformation(0.2F)), PartPose.offsetAndRotation(0.0F, 1.5F, -0.25F, 0.0F, -1.5708F, 0.0F));
+		PartDefinition body_rotation_r4 = hotdogmane.addOrReplaceChild("body_rotation_r4", CubeListBuilder.create().texOffs(46, 13).addBox(5.1F, -9.25F, -2.5F, 2.0F, 6.0F, 7.0F, new CubeDeformation(0.3F)), PartPose.offsetAndRotation(1.0F, 5.3436F, -1.401F, 0.0F, -1.5708F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
     }
