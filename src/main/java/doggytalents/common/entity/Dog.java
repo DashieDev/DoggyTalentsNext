ea942c43a28975cae8aa6ac69f325a2b52eae9b7
DashieDev <lamtinh.asian@gmail.com>
2024-11-11 23:09:45 +0700

    save auto mount

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2662,6 +2662,7 @@ public class Dog extends AbstractDog {
         compound.putBoolean("friendlyFire", this.canOwnerAttack());
         compound.putBoolean("regardTeamPlayers", this.regardTeamPlayers());
         compound.putBoolean("forceSit", this.forceSit());
+        compound.putBoolean("dogAutoMount", this.dogAutoMount());
         compound.putByte("lowHealthStrategy", this.getLowHealthStrategy().getId());
         compound.putByte("combatReturnStrategy", this.getCombatReturnStrategy().getId());
         compound.putBoolean("crossOriginTp", this.crossOriginTp());
@@ -2831,6 +2832,7 @@ public class Dog extends AbstractDog {
             this.setWillObeyOthers(compound.getBoolean("willObey"));
             this.setCanPlayersAttack(compound.getBoolean("friendlyFire"));
             this.setRegardTeamPlayers(compound.getBoolean("regardTeamPlayers"));
+            this.setDogAutoMount(compound.getBoolean("dogAutoMount"));
             this.setForceSit(compound.getBoolean("forceSit"));
             this.setCrossOriginTp(compound.getBoolean("crossOriginTp"));
             this.setPatrolTargetLock(compound.getBoolean("patrolTargetLock")); 
