a5c60016b8212266dd0785bec69fde193841499a
DashieDev <lamtinh.asian@gmail.com>
2024-11-11 23:41:21 +0700

    Completely remove legacy DTN WolfVariants

--- src/main/java/doggytalents/common/variant/util/VanillaToClassicalMapping.java
+++ src/main/java/doggytalents/common/variant/util/VanillaToClassicalMapping.java
@@ -7,7 +7,6 @@ import com.google.common.collect.ImmutableMap;
 
 import doggytalents.DogVariants;
 import doggytalents.common.variant.DogVariant;
-import doggytalents.common.variants_legacy.DTNWolfVariants;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.animal.WolfVariant;
 import net.minecraft.world.entity.animal.WolfVariants;
@@ -25,15 +24,6 @@ public class VanillaToClassicalMapping {
         .put(WolfVariants.SNOWY, DogVariants.SNOWY)
         .put(WolfVariants.ASHEN, DogVariants.ASHEN)
         .put(WolfVariants.SPOTTED, DogVariants.SPOTTED)
-        .put(DTNWolfVariants.CHERRY, DogVariants.CHERRY)
-        .put(DTNWolfVariants.LEMONY_LIME, DogVariants.LEMONY_LIME)
-        .put(DTNWolfVariants.HIMALAYAN_SALT, DogVariants.HIMALAYAN_SALT)
-        .put(DTNWolfVariants.BAMBOO, DogVariants.BAMBOO)
-        .put(DTNWolfVariants.CRIMSON, DogVariants.CRIMSON)
-        .put(DTNWolfVariants.WARPED, DogVariants.WARPED)
-        .put(DTNWolfVariants.BIRCH, DogVariants.BIRCH)
-        .put(DTNWolfVariants.PISTACHIO, DogVariants.PISTACHIO)
-        .put(DTNWolfVariants.GUACAMOLE, DogVariants.GUACAMOLE)
         .build();
 
     public static Supplier<DogVariant> fromVanilla(ResourceKey<WolfVariant> variant) {
