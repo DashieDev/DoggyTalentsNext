c1b5dd915f9b3dd46a3cdf215ad00f905506aab8
DashieDev <lamtinh.asian@gmail.com>
2024-11-12 17:37:21 +0700

    option to hide wolf mount status

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1403,6 +1403,14 @@ public class Dog extends AbstractDog {
     }
 
     @Override
+    public boolean showVehicleHealth() {
+        if (this.level().isClientSide) {
+            return !ConfigHandler.CLIENT.HIDE_WOLF_MOUNT_STATUS.get();
+        }
+        return super.showVehicleHealth();
+    }
+
+    @Override
     public boolean canTrample(BlockState state, BlockPos pos, float fallDistance) {
         //Temporary to avoid wolf mount bug when trampling crops.
         return false;
