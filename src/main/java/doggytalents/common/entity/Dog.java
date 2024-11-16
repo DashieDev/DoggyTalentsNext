da86636ddfcee1972cf64eb22e989069ec3d440c
DashieDev <lamtinh.asian@gmail.com>
2024-11-16 15:59:52 +0700

    Dog running away from fire push resist liquid

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -300,6 +300,7 @@ public class Dog extends AbstractDog {
     protected float jumpPower;
 
     protected boolean isDogSwimming;
+    protected boolean isDogRunningAwayFromFire;
 
     public int lastOrderedToSitTick;
     private int tickChopinTail;
@@ -4521,6 +4522,8 @@ public class Dog extends AbstractDog {
             return false;
         if (this.alterationProps.resistWaterPush())
             return false;
+        if (this.isDogRunningAwayFromFire())
+            return false;
         for (var alter : this.alterations) {
             InteractionResult result = alter.canResistPushFromFluidType();
 
@@ -4938,6 +4941,14 @@ public class Dog extends AbstractDog {
 
     public boolean isDogSwimming() {
         return this.isDogSwimming;
+    }
+
+    public void setDogRunningAwayFromFire(boolean val) {
+        this.isDogRunningAwayFromFire = val;
+    }
+
+    public boolean isDogRunningAwayFromFire() {
+        return this.isDogRunningAwayFromFire;
     }
 
     private boolean isDogCurious;
