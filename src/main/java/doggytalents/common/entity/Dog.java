fc68e14d77e980d95ab70b54c7234fd8d7365467
DashieDev <lamtinh.asian@gmail.com>
2024-11-17 17:33:18 +0700

    standUp when dead

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1792,6 +1792,8 @@ public class Dog extends AbstractDog {
         if (this.isDefeated())
             return;
         if (this.isDeadOrDying()) {
+            this.setOrderedToSit(false);
+            this.setInSittingPose(false);
             this.setAnim(DogAnimation.HURT_1);
             return;
         }
