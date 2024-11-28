122c0d2c6d8c28b6fa02b7e6194fbb3bd9d98299
DashieDev <lamtinh.asian@gmail.com>
2024-11-28 14:40:32 +0700

    generalize going infront of owner feature

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -245,8 +245,8 @@ public class Dog extends AbstractDog {
         = new DogDataSyncManager(this);
     public final DogOwnerDistanceManager dogOwnerDistanceManager 
         = new DogOwnerDistanceManager(this);
-    public final DogMiningCautiousManager dogMiningCautiousManager
-        = new DogMiningCautiousManager(this);
+    public final DogAvoidGoingInFrontOfOwnerManager avoidGoInFrontOfOwnerManager
+        = new DogAvoidGoingInFrontOfOwnerManager(this);
     public final DogGroupsManager dogGroupsManager
         = new DogGroupsManager();
     public final DogIncapacitatedMananger incapacitatedMananger
@@ -846,7 +846,7 @@ public class Dog extends AbstractDog {
             this.hungerManager.tick();
             this.tickDogHealing();
             this.dogOwnerDistanceManager.tick();
-            this.dogMiningCautiousManager.tick();
+            this.avoidGoInFrontOfOwnerManager.tick();
         }
 
         if (!this.level().isClientSide)
@@ -5126,10 +5126,6 @@ public class Dog extends AbstractDog {
 
     public DogGroupsManager getGroups() {
         return this.dogGroupsManager;
-    }
-
-    public boolean isMiningCautious() {
-        return this.dogMiningCautiousManager.isMiningCautious();
     }
 
     public void setAnim(DogAnimation animation) {
