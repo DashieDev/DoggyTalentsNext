06bca87dc46d7cd7e8e67f83fb1fc9c171a62171
DashieDev <lamtinh.asian@gmail.com>
2024-11-29 17:16:30 +0700

    remove excess loclist logic in skinslice

--- src/main/java/doggytalents/client/screen/DogNewInfoScreen/store/slice/ActiveSkinSlice.java
+++ src/main/java/doggytalents/client/screen/DogNewInfoScreen/store/slice/ActiveSkinSlice.java
@@ -6,7 +6,6 @@ import doggytalents.DoggyEntityTypes;
 import doggytalents.client.DogTextureManager;
 import doggytalents.client.entity.skin.DogSkin;
 import doggytalents.client.screen.DogNewInfoScreen.store.UIActionTypes;
-import doggytalents.client.screen.DogNewInfoScreen.store.payload.InitSkinIndexPayload;
 import doggytalents.client.screen.DogNewInfoScreen.store.payload.interfaces.TabChange;
 import doggytalents.client.screen.DogNewInfoScreen.store.slice.ActiveTabSlice.Tab;
 import doggytalents.client.screen.framework.AbstractSlice;
@@ -19,56 +18,26 @@ import net.minecraft.resources.ResourceLocation;
 
 public class ActiveSkinSlice implements CleanableSlice {
 
-    public static List<DogSkin> locList;
     public static Dog DUMMY_DOG_OBJ;
 
-    public int activeSkinId;
     public boolean showInfo;
 
     @Override
     public Object getInitalState() {
         var ret = new ActiveSkinSlice();
-        ret.activeSkinId = 0;
         ret.showInfo = false;
         return ret;
     }
 
     @Override
     public Object reducer(Object oldData, UIAction action) {
-        if (locList == null) return oldData;
-        if (locList.isEmpty()) return oldData;
-        if (oldData instanceof ActiveSkinSlice oldActiveSkin) {
-            if (action.type == UIActionTypes.Skins.ACTIVE_INC) {
+        if (oldData instanceof ActiveSkinSlice) {
+            if (action.type == UIActionTypes.Skins.SHOW_INFO) {
                 var ret = new ActiveSkinSlice();
-                    ret.activeSkinId = oldActiveSkin.activeSkinId;
-                    ret.showInfo = oldActiveSkin.showInfo;
-                    ++ret.activeSkinId;
-                    if (ret.activeSkinId >= locList.size()) {
-                        ret.activeSkinId = locList.size() - 1;
-                    }
-                    return ret;
-            } else if (action.type == UIActionTypes.Skins.ACTIVE_DEC) {
-                var ret = new ActiveSkinSlice();
-                    ret.activeSkinId = oldActiveSkin.activeSkinId;
-                    ret.showInfo = oldActiveSkin.showInfo;
-                    --ret.activeSkinId;
-                    if (ret.activeSkinId < 0) {
-                        ret.activeSkinId = 0;
-                    }
-                    return ret;  
-            } else if (action.type == CommonUIActionTypes.CHANGE_TAB
-                || action.type == CommonUIActionTypes.SWITCH_TAB) {
-                if (action.payload instanceof InitSkinIndexPayload initSkin && initSkin.getTab() == Tab.STYLE) {
-                    return initSkin.getInitSkinIndex();
-                }
-            } else if (action.type == UIActionTypes.Skins.SHOW_INFO) {
-                var ret = new ActiveSkinSlice();
-                ret.activeSkinId = oldActiveSkin.activeSkinId;
                 ret.showInfo = true;
                 return ret;
             } else if (action.type == UIActionTypes.Skins.HIDE_INFO) {
                 var ret = new ActiveSkinSlice();
-                ret.activeSkinId = oldActiveSkin.activeSkinId;
                 ret.showInfo = false;
                 return ret;
             }
@@ -77,13 +46,11 @@ public class ActiveSkinSlice implements CleanableSlice {
     }
 
     public static void initLocList() {
-        locList = DogTextureManager.INSTANCE.getAll();
         setupDummyDog();
     }
 
     @Override
     public void cleanUpSlice() {
-        locList = null;
         DUMMY_DOG_OBJ = null;
     }
 
