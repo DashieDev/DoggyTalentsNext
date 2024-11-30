bf9948bd919f85a4c957f96eeaf6d3739788c8b3
DashieDev <lamtinh.asian@gmail.com>
2024-11-30 17:42:27 +0700

    prevent dog upsidedown name

--- src/main/java/doggytalents/common/util/DogUtil.java
+++ src/main/java/doggytalents/common/util/DogUtil.java
@@ -649,6 +649,7 @@ public class DogUtil {
         if (str.isEmpty())
             return name;
         
+        str = checkFlipName(str);
         str = checkInvalidChar(str);
         str = stripIfNeccessary(str);
 
@@ -695,6 +696,13 @@ public class DogUtil {
 
         return str;
     } 
+
+    public static String checkFlipName(String string) {
+        if ("Dinnerbone".equals(string) || "Grumm".equals(string)) {
+            return "_" + string;
+        }
+        return string;
+    }
 
     public static boolean vertifySkinData(String hash) {
         if (hash == null || hash.isEmpty())
