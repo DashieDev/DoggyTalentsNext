68789bbcbba367ffd4f4740c0f2dc2e0ba456cdc
DashieDev <lamtinh.asian@gmail.com>
2024-11-30 14:25:37 +0700

    diff push behaviour for injured dogs
    Apply custom push and collide behaviour when a defeated dog is being pushed by or pushing or colliding
    any other than another defeated dog:
    + Collide is blocked
    + The defeated dog doesn't cause any changes in movement for the other entity when being pushed and doesn't do any active push on the other entity.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -5001,6 +5001,8 @@ public class Dog extends AbstractDog {
             return;
         if (this.dogPushAvoidManager.shouldBlockPush(pushTarget))
             return;
+        if (this.isDefeated() && this.incapacitatedMananger.shouldApplyCustomPushBehaviour(pushTarget))
+            return;
         if (pushTarget.getVehicle() == this
             || this.getVehicle() == pushTarget) {
             return;        
@@ -5018,6 +5020,10 @@ public class Dog extends AbstractDog {
             return;
         if (this.pettingManager.checkPush(source))
             return;
+        if (this.isDefeated() && this.incapacitatedMananger.shouldApplyCustomPushBehaviour(source)) {
+            this.incapacitatedMananger.customBeingPushed(source, this::calcDogPushVec);
+            return;
+        }
         if (this.isVehicle() && !this.hasControllingPassenger())
             pushDogAsVehicle(source);
         else {
@@ -5074,6 +5080,8 @@ public class Dog extends AbstractDog {
         if (this.dogPushAvoidManager.shouldBlockPush(otherEntity)) {
             return false;
         }
+        if (this.isDefeated() && this.incapacitatedMananger.shouldApplyCustomPushBehaviour(otherEntity))
+            return false;
 
         if (otherEntity.getVehicle() == this
             || this.getVehicle() == otherEntity)
