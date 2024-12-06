d1837bc043c83add02195d5344e9b7b0316a7896
DashieDev <lamtinh.asian@gmail.com>
2024-12-06 21:08:27 +0700

    rename the rest of teleportInternal

--- src/main/java/doggytalents/common/util/DogUtil.java
+++ src/main/java/doggytalents/common/util/DogUtil.java
@@ -103,7 +103,7 @@ public class DogUtil {
         if (target == null) {
             return false;
         }
-        teleportInternal(dog, target);
+        teleportDogAbs(dog, target);
         
         return true;
     }
@@ -127,7 +127,7 @@ public class DogUtil {
    
         for (var dog : dogs) {
             int r_indx = dog.getRandom().nextInt(safePosList.size());
-            teleportInternal(dog, safePosList.get(r_indx));
+            teleportDogAbs(dog, safePosList.get(r_indx));
         }
 
         //long stopTime = System.nanoTime();
@@ -163,7 +163,7 @@ public class DogUtil {
         for (var dog : sorted_dogs) {
             if (safePosList.isEmpty()) break;
             int r_indx = dog.getRandom().nextInt(safePosList.size());
-            teleportInternal(dog, safePosList.get(r_indx));
+            teleportDogAbs(dog, safePosList.get(r_indx));
             int density_count = densityMap.get(r_indx) + 1;
             if (density_count >= max_density) {
                 densityMap.remove(r_indx);
@@ -198,7 +198,7 @@ public class DogUtil {
         if (target == null) {
             return false;
         }
-        teleportInternal(dog, target);
+        teleportDogAbs(dog, target);
         
         return true;
     }
@@ -260,7 +260,7 @@ public class DogUtil {
             var b0 = new BlockPos(randX, randY, randZ);
 
             if (wantToTeleportToThePosition(dog, owner, b0)) {
-                teleportInternal(dog, b0);
+                teleportDogAbs(dog, b0);
                 return true;
             }
         }
@@ -277,7 +277,7 @@ public class DogUtil {
             var b0 = new BlockPos(randX, randY, randZ);
 
             if (isTeleportSafeBlock(dog, b0, null)) {
-                teleportInternal(dog, b0);
+                teleportDogAbs(dog, b0);
                 return true;
             }
         }
@@ -603,7 +603,7 @@ public class DogUtil {
         var owner = dog.getOwner();
         if (dog.level.hasChunk(chunkpos.x, chunkpos.z)) {
             if (isTeleportSafeBlockMidAir(dog, bedPos.get().above())) {
-                teleportInternal(dog, bedPos.get().above());
+                teleportDogAbs(dog, bedPos.get().above());
                 dog.setOrderedToSit(true);
             }
         } else if (owner != null && owner instanceof ServerPlayer sP) {
