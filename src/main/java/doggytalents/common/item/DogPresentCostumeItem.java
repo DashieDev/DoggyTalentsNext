3ef23f69e7c4172ebc5bca840d299aff8a4b5358
MashXP | マッシーXP <realsteelphong@gmail.com>
2024-12-23 13:43:58 +0700

    Description for the Present

--- src/main/java/doggytalents/common/item/DogPresentCostumeItem.java
+++ src/main/java/doggytalents/common/item/DogPresentCostumeItem.java
@@ -1,7 +1,13 @@
 package doggytalents.common.item;
 
 import java.util.function.Supplier;
+import java.util.List;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import doggytalents.common.entity.accessory.DoubleDyableAccessory;
 
 public class DogPresentCostumeItem extends DoubleDyableAccessoryItem {
@@ -10,14 +16,14 @@ public class DogPresentCostumeItem extends DoubleDyableAccessoryItem {
         super(type, properties);
     }
 
-    // @Override
-    // public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
-    //         TooltipFlag flags) {
-    //     var desc_id = this.getDescriptionId(stack) + ".description";
-    //     components.add(Component.translatable(desc_id).withStyle(
-    //         Style.EMPTY.withItalic(true)
-    //     ));
-    // }
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
+            TooltipFlag flags) {
+        var desc_id = this.getDescriptionId(stack) + ".description";
+        components.add(Component.translatable(desc_id).withStyle(
+            Style.EMPTY.withItalic(true)
+        ));
+    }
 
     @Override
     public int getDefaultBgColor() {
