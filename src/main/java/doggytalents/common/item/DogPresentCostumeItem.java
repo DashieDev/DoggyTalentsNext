7e19f993e6a53c56fbd3f67de81c9e2a7455b70e
DashieDev <lamtinh.asian@gmail.com>
2024-12-24 22:03:40 +0700

    1.20 fix

--- src/main/java/doggytalents/common/item/DogPresentCostumeItem.java
+++ src/main/java/doggytalents/common/item/DogPresentCostumeItem.java
@@ -8,6 +8,7 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import doggytalents.common.entity.accessory.DoubleDyableAccessory;
 
 public class DogPresentCostumeItem extends DoubleDyableAccessoryItem {
@@ -17,7 +18,7 @@ public class DogPresentCostumeItem extends DoubleDyableAccessoryItem {
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
+    public void appendHoverText(ItemStack stack, Level context, List<Component> components,
             TooltipFlag flags) {
         var desc_id = this.getDescriptionId(stack) + ".description";
         components.add(Component.translatable(desc_id).withStyle(
