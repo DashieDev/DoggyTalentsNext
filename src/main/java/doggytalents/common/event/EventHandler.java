5590df5bb08a78c9b32f87f9db190bb3a02e95e1
DashieDev <lamtinh.asian@gmail.com>
2025-01-14 16:28:47 +0700

    cooldown and penalty BedDog

--- src/main/java/doggytalents/common/event/EventHandler.java
+++ src/main/java/doggytalents/common/event/EventHandler.java
@@ -604,4 +604,9 @@ public class EventHandler {
     public void canPlayerContinueSleeping(CanContinueSleepingEvent event) {
         DogSleepOnManager.canPlayerContinueSleeping(event);
     }
+
+    @SubscribeEvent
+    public void beforeAllPlayerWakeUp(SleepFinishedTimeEvent event) {
+        DogSleepOnManager.beforeSleepFinishedForAllPlayer(event);
+    }
 }
