880800f6c6f018fdeddfdc9689fc61c14a7398db
DashieDev <lamtinh.asian@gmail.com>
2025-01-26 01:05:09 +0700

    New Doggy Spin
    No longer rely on huge spritesheet.
    Use an actual model to render the dog
    This allows much more flexibility regarding customization without wasting spaces (more variant, more collar colors.)

---
+++ src/main/java/doggytalents/client/screen/widget/DoggySpin/DoggySpin.java
@@ -0,0 +1,72 @@
+package doggytalents.client.screen.widget.DoggySpin;
+
+import doggytalents.common.config.ConfigHandler;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.screens.LevelLoadingScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
+import net.neoforged.neoforge.client.event.ScreenEvent;
+
+public class DoggySpin extends AbstractWidget {
+
+    private int size;
+    private long accumulatedTime = 0;
+    private long lastAccumulate = 0;
+
+    public DoggySpin(int x, int y, int size) {
+        super(x, y, size, size, Component.empty());
+        this.size = size;
+        this.lastAccumulate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float pTicks) {
+        updateAcculumlatedMillis();
+        // graphics.fill(this.getX(), this.getY(), 
+        //     this.getX() + this.getWidth(), 
+        //     this.getY() + this.getHeight(), 0xff576f96);
+        int mx = this.getX() + this.getWidth()/2;
+        int my = this.getY() + this.getHeight()/2;
+        DoggySpinModel.get().prepareRender(this.accumulatedTime);
+        DoggySpinModel.get().renderGui(graphics, mx, my);
+    }
+
+    private void updateAcculumlatedMillis() {
+        long current_time = System.currentTimeMillis();
+        long time_since = current_time - this.lastAccumulate;
+        if (time_since > 0) {
+            this.accumulatedTime += Mth.clamp(time_since * 0.9, 0, 70);
+            this.lastAccumulate = current_time;
+        }
+    }
+
+    public void chooseStyle() {
+        DoggySpinModel.get().configureRandomStyle();
+        this.accumulatedTime = 0;
+        this.lastAccumulate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void updateWidgetNarration(NarrationElementOutput p_259858_) {
+    }
+    
+    private static final DoggySpin spinWidget = new DoggySpin(0, 0, 128);
+
+    public static void onScreenInit(final ScreenEvent.Init.Post event) {
+        if (event.getScreen() instanceof LevelLoadingScreen) {
+            spinWidget.chooseStyle();
+        }
+    }
+
+    public static void onScreenRenderForeground(final ScreenEvent.Render.Post event) {
+        if (!ConfigHandler.CLIENT.WORD_LOAD_ICON.get())
+            return;
+        if (!(event.getScreen() instanceof LevelLoadingScreen))
+            return;
+        spinWidget.setY(event.getScreen().height - spinWidget.getHeight());
+        spinWidget.render(event.getGuiGraphics(), event.getMouseX(), event.getMouseY(), event.getPartialTick());
+    }
+
+}
--- src/main/java/doggytalents/common/lib/Resources.java
+++ src/main/java/doggytalents/common/lib/Resources.java
@@ -27,6 +27,7 @@ public class Resources {
 
     public static final ResourceLocation DOG_CLASSICAL = getEntity("dog", "custom/classical/wolf");
     public static final ResourceLocation DOG_MYSTERY = getEntity("dog", "custom/classical/wolf_nani");
+    public static final ResourceLocation OKAMI_AMATERASU = getEntity("dog", "custom/ammy_divine_mouthopen");
 
     public static final ResourceLocation COLLAR_DEFAULT = getEntity("dog", "doggy_collar");
     public static final ResourceLocation COLLAR_THICC = getEntity("dog", "doggy_collar_thicc");
@@ -171,11 +172,6 @@ public class Resources {
     public static final ResourceLocation KANJI_INCAP_STARVE = getGui("fatalkanji/starve");
     public static final ResourceLocation KANJI_MYSTERY = getGui("kanji/mystery/kanji");
     public static final ResourceLocation KANJI_MYSTERY_BKG = getGui("kanji/mystery/background");
-
-    public static final ResourceLocation SPIN = getGui("spin");
-    public static final ResourceLocation SPIN2 = getGui("spin2");
-    public static final ResourceLocation SPIN3 = getGui("spin3");
-    public static final ResourceLocation SPIN4 = getGui("spin4");
 
     public static final ResourceLocation BANDAID_OVERLAY_HALF = getEntity("dog", "doggy_hurt/bandaid/overlay_half");
     public static final ResourceLocation BANDAID_OVERLAY_FULL = getEntity("dog", "doggy_hurt/bandaid/overlay_full");
