82858d0c8880fe220ad52b4eb8d835ccf50f9563
DashieDev <lamtinh.asian@gmail.com>
2025-01-27 01:41:38 +0700

    Dog Bed dont show missing texture particle when step on
    They will now show the wrapped default model.

--- src/main/java/doggytalents/client/block/model/DogBedModel.java
+++ src/main/java/doggytalents/client/block/model/DogBedModel.java
@@ -69,6 +69,8 @@ private BlockModel model;
     }
 
     public BakedModel getModelVariant(ICasingMaterial casing, IBeddingMaterial bedding, Direction facing) {
+        if (casing == null || bedding == null)
+            return bakedModel;
         Triple<ICasingMaterial, IBeddingMaterial, Direction> key =
                 ImmutableTriple.of(casing != null ? casing : null, bedding != null ? bedding : null, facing != null ? facing : Direction.NORTH);
 
