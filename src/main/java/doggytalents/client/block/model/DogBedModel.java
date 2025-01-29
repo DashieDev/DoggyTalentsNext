6d0b258d0c081a39bd85b43df82f542af69a3d44
DashieDev <61554666+DashieDev@users.noreply.github.com>
2025-01-29 16:05:55 +0700

    Only cache exactly direction.length nani variant.
    and return them if one of the material is nani.

--- src/main/java/doggytalents/client/block/model/DogBedModel.java
+++ src/main/java/doggytalents/client/block/model/DogBedModel.java
@@ -8,6 +8,8 @@ import doggytalents.api.registry.IBeddingMaterial;
 import doggytalents.api.registry.ICasingMaterial;
 import doggytalents.common.block.DogBedBlock;
 import doggytalents.common.block.DogBedMaterialManager;
+import doggytalents.common.block.DogBedMaterialManager.NaniBedding;
+import doggytalents.common.block.DogBedMaterialManager.NaniCasing;
 import doggytalents.common.block.tileentity.DogBedTileEntity;
 import doggytalents.common.lib.Constants;
 import doggytalents.common.util.Util;
@@ -43,6 +45,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 @OnlyIn(Dist.CLIENT)
@@ -54,6 +57,7 @@ public class DogBedModel implements BakedModel {
     private ModelBakery modelLoader;
     private BlockModel unbakedModel;
     private BakedModel defaultModelVariant;
+    private final Map<Direction, BakedModel> missingModelVariant = new ConcurrentHashMap<>(Direction.values().length);
 
     private final Map<Triple<ICasingMaterial, IBeddingMaterial, Direction>, BakedModel> cache = Maps.newConcurrentMap();
     private final int maxCacheSize;
@@ -63,6 +67,14 @@ public class DogBedModel implements BakedModel {
         this.unbakedModel = model;
         this.defaultModelVariant = bakedModel;
         this.maxCacheSize = maxCacheSize;
+        bakeMissingVariants();
+    }
+
+    private void bakeMissingVariants() {
+        for (var dir : Direction.values()) {
+            var model = bakeModelVariant(NaniCasing.NULL, NaniBedding.NULL, dir);
+            this.missingModelVariant.put(dir, model);
+        }
     }
 
     public BakedModel getModelVariant(@Nonnull ModelData data) {
@@ -72,6 +84,8 @@ public class DogBedModel implements BakedModel {
     public BakedModel getModelVariant(ICasingMaterial casing, IBeddingMaterial bedding, Direction facing) {
         if (casing == null || bedding == null)
             return defaultModelVariant;
+        if (casing.isNani() || bedding.isNani())
+            return missingModelVariant.get(facing);
         
         if (facing == null)
             facing = Direction.NORTH;
