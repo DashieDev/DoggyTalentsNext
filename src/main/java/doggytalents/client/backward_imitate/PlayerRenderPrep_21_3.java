c84b3b499f9d858231f3c5c30279a333128303a9
DashieDev <lamtinh.asian@gmail.com>
2025-02-14 17:50:08 +0700

    1.21.3 port: misc and bug fix

--- src/main/java/doggytalents/client/backward_imitate/PlayerRenderPrep_21_3.java
+++ src/main/java/doggytalents/client/backward_imitate/PlayerRenderPrep_21_3.java
@@ -1,7 +1,12 @@
 package doggytalents.client.backward_imitate;
 
 import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.client.event.RenderPlayerEvent;
 
 public class PlayerRenderPrep_21_3 {
     public static Player player = null;
+
+    public static void afterPlayerRender(RenderPlayerEvent.Post event) {
+        player = null;
+    }
 }
--- src/main/java/doggytalents/client/event/ClientEventHandler.java
+++ src/main/java/doggytalents/client/event/ClientEventHandler.java
@@ -87,7 +87,7 @@ public class ClientEventHandler {
 
             var model = modelRegistry.get(ModelResourceLocation.standalone(bakedModelLoc));
 
-            var modelUnbaked = (BlockModel) event.getModelBakery().unbakedModels.get(ModelResourceLocation.standalone(bakedModelLoc));
+            var modelUnbaked = (BlockModel) event.getModelBakery().topModels.get(ModelResourceLocation.standalone(bakedModelLoc));
 
             BakedModel customModel = new DogBedModel(event.getModelBakery(), modelUnbaked, model, ConfigHandler.CLIENT.MAX_DOG_BED_MODEL_CACHE.get());
 
