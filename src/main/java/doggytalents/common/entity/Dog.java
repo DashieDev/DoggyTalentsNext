41cf3c9fddad041e8cb7191596a9f8d65967ea87
DashieDev <lamtinh.asian@gmail.com>
2025-02-19 15:57:50 +0700

    Dog Ally check logic refactor and revamp
    Ally can now be detected even if owner is offline.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1654,7 +1654,7 @@ public class Dog extends AbstractDog {
             return false;
         }
 
-        return !EventHandler.isAlliedToDog(target, owner);
+        return !DogAllyCheck.isAlliedToDog(this, target, owner);
     }
 
     // TODO
@@ -1722,9 +1722,8 @@ public class Dog extends AbstractDog {
         // Must be checked here too as hitByEntity only applies to when the dog is
         // directly hit not indirect damage like sweeping effect etc
         if (!this.canOwnerAttack()) {
-            var owner = this.getOwner();
             boolean flag = 
-                this.checkIfAttackedFromOwnerOrTeam(owner, attacker);
+                DogAllyCheck.isAlliedToDog(this, attacker);
             if (flag) return false;
         }
 
@@ -1823,16 +1822,6 @@ public class Dog extends AbstractDog {
             this.setAnim(DogAnimation.HURT_2);
             return;
         }
-    }
-
-    public boolean checkIfAttackedFromOwnerOrTeam(LivingEntity owner, Entity attacker) {
-        if (owner == null || attacker == null) 
-            return false;
-        if (owner == attacker) 
-            return true;
-        if (attacker.isAlliedTo(owner))
-            return true;
-        return false;
     }
 
     @Override
@@ -2201,7 +2190,7 @@ public class Dog extends AbstractDog {
         } 
 
         if (!this.canOwnerAttack() 
-            && this.checkIfAttackedFromOwnerOrTeam(this.getOwner(), entityIn)) {
+            && DogAllyCheck.isAlliedToDog(this, entityIn)) {
             return true;
         }
 
