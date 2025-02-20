b80c60bfb455992846a0ba24ab58966bf592d713
DashieDev <lamtinh.asian@gmail.com>
2025-02-20 13:57:32 +0700

    increase default bed model cache

--- src/main/java/doggytalents/common/config/ConfigHandler.java
+++ src/main/java/doggytalents/common/config/ConfigHandler.java
@@ -297,7 +297,7 @@ ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CONFIG_CLIENT_SPEC
                 .comment("other variants will be rendered as the default variant.")
                 .comment("To reset the cache, do a resource reload (F3+T).")
                 .comment("Set this option to any value less than zero to remove the limit.")
-                .defineInRange("max_dog_bed_model_cache", 16384, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                .defineInRange("max_dog_bed_model_cache", 65536, Integer.MIN_VALUE, Integer.MAX_VALUE);
             builder.pop();
         }
 
