db49994a2ce249764b094fb1fe803829b0a04268
DashieDev <lamtinh.asian@gmail.com>
2025-02-20 13:56:54 +0700

    declare all DTN tags final

--- src/main/java/doggytalents/DoggyTags.java
+++ src/main/java/doggytalents/DoggyTags.java
@@ -11,17 +11,17 @@ import net.minecraft.world.item.Item;
 
 public class DoggyTags {
 
-    public static TagKey<Item> BEG_ITEMS_TAMED = tag("beg_items_tamed");
-    public static TagKey<Item> BEG_ITEMS_UNTAMED = tag("beg_items_untamed");
-    public static TagKey<Item> BREEDING_ITEMS = tag("breeding_items");
-    public static TagKey<Item> PACK_PUPPY_BLACKLIST = tag("pack_puppy_blacklist");
-    public static TagKey<Item> TREATS = tag("treats");
-    public static TagKey<Item> WHITELIST_FOOD = tag("whitelist_food");
-    public static TagKey<Item> DOGGY_TOOLS_BLACKLIST = tag("doggy_tools_blacklist");
+    public static final TagKey<Item> BEG_ITEMS_TAMED = tag("beg_items_tamed");
+    public static final TagKey<Item> BEG_ITEMS_UNTAMED = tag("beg_items_untamed");
+    public static final TagKey<Item> BREEDING_ITEMS = tag("breeding_items");
+    public static final TagKey<Item> PACK_PUPPY_BLACKLIST = tag("pack_puppy_blacklist");
+    public static final TagKey<Item> TREATS = tag("treats");
+    public static final TagKey<Item> WHITELIST_FOOD = tag("whitelist_food");
+    public static final TagKey<Item> DOGGY_TOOLS_BLACKLIST = tag("doggy_tools_blacklist");
 
-    public static TagKey<EntityType<?>> DOG_SHOULD_IGNORE = tagEntity("dog_should_ignore");
-    public static TagKey<EntityType<?>> DROP_SOY_WHEN_DOG_KILL = tagEntity("drop_soy_when_dog_kill");
-    public static TagKey<EntityType<?>> MOB_RETRIEVER_MUST_IGNORE = tagEntity("mob_retriever_must_ignore");
+    public static final TagKey<EntityType<?>> DOG_SHOULD_IGNORE = tagEntity("dog_should_ignore");
+    public static final TagKey<EntityType<?>> DROP_SOY_WHEN_DOG_KILL = tagEntity("drop_soy_when_dog_kill");
+    public static final TagKey<EntityType<?>> MOB_RETRIEVER_MUST_IGNORE = tagEntity("mob_retriever_must_ignore");
 
     private static TagKey<Item> tag(String name) {
         return ItemTags.create(Util.getResource(name));
