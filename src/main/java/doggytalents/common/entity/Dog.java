69d47fd28d68505d0f9d3a4a17b67f1c34eba72d
DashieDev <lamtinh.asian@gmail.com>
2025-02-27 16:26:03 +0700

    making use of dogSoundManager in Dog's Howling and Shaking
    Dog Howling Action will now block ambient voice the howl sound will
    interrupt if the action is interrupted
    Shaking sound will also being interrupted if the dog shaking is interrupted.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -480,6 +480,9 @@ public class Dog extends AbstractDog {
             this.finishShaking();
             break;
         }
+        if (this.level().isClientSide) {
+            this.dogSoundManager.interuptPlaying();
+        }
     }
 
     public float getTailRotation() {
@@ -735,8 +738,8 @@ public class Dog extends AbstractDog {
         if (!this.isShaking)
             return;
 
-        if (this.timeWolfIsShaking == 0.0F) {
-            if (!this.shakeFire) this.playSound(SoundEvents.WOLF_SHAKE, this.getSoundVolume(), (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+        if (this.timeWolfIsShaking == 0.0F && this.level().isClientSide) {
+            if (!this.shakeFire) this.dogSoundManager.playInterruptible(SoundEvents.WOLF_SHAKE, this.getSoundVolume(), (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
         }
 
         this.prevTimeWolfIsShaking = this.timeWolfIsShaking;
