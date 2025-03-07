7d1dbd35dbcb9a3f57c66ee721227254996ca7ae
DashieDev <lamtinh.asian@gmail.com>
2025-03-08 00:00:11 +0700

    On Duty System
    Allow whistle to only target dogs who are
    "On Duty" for certain modes (e.g Stand, Sit, .....)
    Dogs can be set "On Duty" via its Dog Menu or using the "Duty Whistle".
    "Duty Whistle" : Whistle mode that will, upon use, set every dog
    in radius who are standing to be "On Duty" and vice versa.
    You can configure the whistle anytime to either target all
    dogs in radius or only Dogs who are On Duty.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -186,7 +186,7 @@ public class Dog extends AbstractDog {
      *     5               32                  LOW_HEALTH_STRATEGY_MSB
      *     6               64                  CROSS_ORIGIN_TP
      *     7               128                 REGARD_TEAM_PLAYERS
-     *     8               256                 <Reserved>
+     *     8               256                 ON_DUTY
      *     9               512                 PATROL_TARGET_LOCK
      *     10              1024                FLYING
      *     11              2048                SHOW_ARMOR
@@ -2683,6 +2683,7 @@ public class Dog extends AbstractDog {
         compound.putBoolean("crossOriginTp", this.crossOriginTp());
         compound.putBoolean("patrolTargetLock", this.patrolTargetLock());
         compound.putBoolean("hideDogArmor", this.hideArmor());
+        compound.putBoolean("dogOnDuty", this.dogOnDuty());
         compound.putInt("dogSize", this.getDogSize().getId());
         compound.putInt("level_normal", this.getDogLevel().getLevel(Type.NORMAL));
         compound.putInt("level_kami", this.getDogLevel().getLevel(Type.KAMI));
@@ -2852,6 +2853,7 @@ public class Dog extends AbstractDog {
             this.setCrossOriginTp(compound.getBoolean("crossOriginTp"));
             this.setPatrolTargetLock(compound.getBoolean("patrolTargetLock")); 
             this.setHideArmor(compound.getBoolean("hideDogArmor"));
+            this.setDogOnDuty(compound.getBoolean("dogOnDuty"));
             var low_health_strategy_id = compound.getByte("lowHealthStrategy");
             this.setLowHealthStrategy(LowHealthStrategy.fromId(low_health_strategy_id));
             var combat_return_strategy_id = compound.getByte("combatReturnStrategy");
@@ -3864,6 +3866,14 @@ public class Dog extends AbstractDog {
 
     public boolean forcedWhenNoneAnim() {
         return getDogFlag(262144);
+    }
+
+    public void setDogOnDuty(boolean val) {
+        this.setDogFlag(256, val);
+    }
+
+    public boolean dogOnDuty() {
+        return getDogFlag(256);
     }
 
     public void setDrunkTicks(int ticks) {
