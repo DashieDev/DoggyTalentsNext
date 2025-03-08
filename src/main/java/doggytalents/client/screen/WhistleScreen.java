c04a23b54026b360a48a3ce6c7a2da59ff3f0e34
DashieDev <lamtinh.asian@gmail.com>
2025-03-08 23:36:02 +0700

    Update and refactor I18n for entry select screen help

--- src/main/java/doggytalents/client/screen/WhistleScreen.java
+++ src/main/java/doggytalents/client/screen/WhistleScreen.java
@@ -119,7 +119,7 @@ public class WhistleScreen extends StringEntrySelectScreen {
                 List<Component> list = new ArrayList<>();
                 list.add(Component.translatable("doggytalents.screen.whistler.screen.help_title")
                     .withStyle(Style.EMPTY.withBold(true)));
-                String str = I18n.get("doggytalents.screen.whistler.screen.help");
+                String str = I18n.get("doggytalents.screen.general.entry_select.help");
                 list.addAll(ScreenUtil.splitInto(str, 150, WhistleScreen.this.font));
 
                 graphics.renderComponentTooltip(font, list, mouseX, mouseY);
