1f4499dd7778cf895ecf86f30d5f5b25baf3b4c9
DashieDev <lamtinh.asian@gmail.com>
2025-03-13 16:13:16 +0700

    Allow addon models to render only model accessories.

--- src/main/java/doggytalents/client/entity/model/dog/DogModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/DogModel.java
@@ -651,7 +651,8 @@ public class DogModel extends EntityModel<Dog> {
         NON_COMPATIBLE,
         SOME_WILL_FIT,
         HAVE_NOT_TESTED,
-        RECOMMENDED
+        RECOMMENDED,
+        MODEL_ONLY
     }
 
     private AccessoryState accessoryState = AccessoryState.HAVE_NOT_TESTED;
