535b5ddde17f7e6b96ba46daff995b03c7897a51
DashieDev <lamtinh.asian@gmail.com>
2025-03-13 15:41:44 +0700

    EnumGender -> DogGender

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -319,7 +319,7 @@ public class Dog extends AbstractDog {
     public Dog(EntityType<? extends Dog> type, Level worldIn) {
         super(type, worldIn);
         this.setTame(false);
-        this.setGender(EnumGender.random(this.getRandom()));
+        this.setGender(DogGender.random(this.getRandom()));
         this.setLowHealthStrategy(LowHealthStrategy.STICK_TO_OWNER);
         this.authorizedChangingOwner = true;
         this.setOwnerUUID(null); //Just to be sure
@@ -2837,7 +2837,7 @@ public class Dog extends AbstractDog {
 		}
 
         try {
-            this.setGender(EnumGender.bySaveName(compound.getString("dogGender")));
+            this.setGender(DogGender.bySaveName(compound.getString("dogGender")));
 
             if (compound.contains("mode", Tag.TAG_STRING)) {
             this.setMode(DogMode.bySaveName(compound.getString("mode")));
@@ -3476,11 +3476,11 @@ public class Dog extends AbstractDog {
         this.dogFabricHelper.setDogVariant(val);
     }
 
-    public EnumGender getGender() {
+    public DogGender getGender() {
         return this.dogFabricHelper.getDogGender();
     }
 
-    public void setGender(EnumGender collar) {
+    public void setGender(DogGender collar) {
         this.dogFabricHelper.setDogGender(collar);
     }
 
@@ -4532,8 +4532,8 @@ public class Dog extends AbstractDog {
     }
 
     @Override
-    public MutableComponent getTranslationKey(Function<EnumGender, String> function) {
-        return Component.translatable(function.apply(!ConfigHandler.ServerConfig.getConfig(ConfigHandler.SERVER.DISABLE_GENDER) ? this.getGender() : EnumGender.UNISEX));
+    public MutableComponent getTranslationKey(Function<DogGender, String> function) {
+        return Component.translatable(function.apply(!ConfigHandler.ServerConfig.getConfig(ConfigHandler.SERVER.DISABLE_GENDER) ? this.getGender() : DogGender.UNISEX));
     }
 
     @Override
