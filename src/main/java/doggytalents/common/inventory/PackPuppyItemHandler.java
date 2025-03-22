f9a4df7ec047bf1d4fef28d4e35ab8a39878c4b6
DashieDev <lamtinh.asian@gmail.com>
2025-03-22 17:21:15 +0700

    Fix Pack Puppy having access to the spots belonging to higher levels of the talent when picking nearby item

--- src/main/java/doggytalents/common/inventory/PackPuppyItemHandler.java
+++ src/main/java/doggytalents/common/inventory/PackPuppyItemHandler.java
@@ -1,5 +1,7 @@
 package doggytalents.common.inventory;
 
+import doggytalents.common.talent.PackPuppyTalent;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
@@ -8,8 +10,11 @@ import net.minecraftforge.items.ItemStackHandler;
 
 public class PackPuppyItemHandler extends ItemStackHandler {
 
-    public PackPuppyItemHandler() {
+    private PackPuppyTalent inst;
+
+    public PackPuppyItemHandler(PackPuppyTalent inst) {
         super(15);
+        this.inst = inst;
     }
 
     @Override
@@ -61,5 +66,12 @@ public class PackPuppyItemHandler extends ItemStackHandler {
             }
             this.onLoad();
         }
+    }
+
+    @Override
+    public boolean isItemValid(int slot, ItemStack stack) {
+        if (slot >= inst.level() * 3)
+            return false;
+        return super.isItemValid(slot, stack);
     }
 }
