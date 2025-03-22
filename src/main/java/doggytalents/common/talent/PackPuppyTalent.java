4f59ea5a9af38d1cd886b1c8fc2557659cfc0449
DashieDev <lamtinh.asian@gmail.com>
2025-03-22 17:21:15 +0700

    Fix Pack Puppy having access to the spots belonging to higher levels of the talent when picking nearby item

--- src/main/java/doggytalents/common/talent/PackPuppyTalent.java
+++ src/main/java/doggytalents/common/talent/PackPuppyTalent.java
@@ -83,7 +83,7 @@ public class PackPuppyTalent extends TalentInstance {
 
     public PackPuppyTalent(Talent talentIn, int levelIn) {
         super(talentIn, levelIn);
-        PackPuppyItemHandler handler = new PackPuppyItemHandler();
+        PackPuppyItemHandler handler = new PackPuppyItemHandler(this);
         this.packPuppyHandler = handler;
     }
 
