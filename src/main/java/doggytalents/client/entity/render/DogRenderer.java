bec89dbb41d74bf266297e75f6c34fa28784ac7b
DashieDev <lamtinh.asian@gmail.com>
2025-03-28 15:28:15 +0700

    DoggySpinModel init at clientSetup and no longer depends on the context.
     To avoid bugs caused by DogRender not being initialized when World Load Screen is opened.

--- src/main/java/doggytalents/client/entity/render/DogRenderer.java
+++ src/main/java/doggytalents/client/entity/render/DogRenderer.java
@@ -70,7 +70,6 @@ public class DogRenderer extends MobRenderer<Dog, DogRenderState_21_3, DogModel>
 //        this.addLayer(new DogTalentLayer(this, ctx));
 //        this.addLayer(new DogAccessoryLayer(this, ctx));
         DogModelRegistry.resolve(ctx);
-        //DoggySpinModel.init(ctx); //Fabric initialized this in ClientEntry. We needs to be extra careful wWhen porting a change here.
         this.defaultModel = DogModelRegistry.getDogModelHolder("default").getValue();
         for (LayerFactory<DogRenderState_21_3, DogModel> layer : CollarRenderManager.getLayers()) {
             this.addLayer(layer.createLayer(this, ctx));
