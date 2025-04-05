0737b5fe3b0de6590a5744ba4e3819d27a76ed6a
DashieDev <lamtinh.asian@gmail.com>
2025-04-05 21:27:26 +0700

    1.21.5 port: fix mixin

--- src/main/java/doggytalents/mixin/PlayerRendererMixin_1_21_5.java
+++ src/main/java/doggytalents/mixin/PlayerRendererMixin_1_21_5.java
@@ -7,8 +7,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import doggytalents.client.PettingArmPose;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
@@ -16,13 +18,13 @@ import net.minecraft.world.item.ItemStack;
 public class PlayerRendererMixin_1_21_5 {
 
     @Inject(at = @At("HEAD"),  method = "getArmPose", cancellable = true)
-    public void dtn__getArmPose(Player player, ItemStack stack, InteractionHand hand, CallbackInfoReturnable<HumanoidModel.ArmPose> info) {
-        if (hand == InteractionHand.MAIN_HAND && PettingArmPose.activeRight_1_21_5) {
+    private static void dtn__getArmPose(AbstractClientPlayer player, HumanoidArm arm, CallbackInfoReturnable<HumanoidModel.ArmPose> info) {
+        if (arm == HumanoidArm.RIGHT && PettingArmPose.activeRight_1_21_5) {
             PettingArmPose.activeRight_1_21_5 = false;
             info.setReturnValue(PettingArmPose.VALUE);
             return;
         }
-        if (hand == InteractionHand.OFF_HAND && PettingArmPose.activeLeft_1_21_5) {
+        if (arm == HumanoidArm.LEFT && PettingArmPose.activeLeft_1_21_5) {
             PettingArmPose.activeLeft_1_21_5 = false;
             info.setReturnValue(PettingArmPose.VALUE);
             return;
