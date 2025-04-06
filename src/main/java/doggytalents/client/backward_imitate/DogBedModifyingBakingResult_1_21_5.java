c82993b813bf507f73a19902712193ca78bfdb8f
DashieDev <lamtinh.asian@gmail.com>
2025-04-07 03:10:55 +0700

    1.21.5 port: Fix DogBedItemModel ItemTransforms

--- src/main/java/doggytalents/client/backward_imitate/DogBedModifyingBakingResult_1_21_5.java
+++ src/main/java/doggytalents/client/backward_imitate/DogBedModifyingBakingResult_1_21_5.java
@@ -7,6 +7,8 @@ import doggytalents.common.config.ConfigHandler;
 import doggytalents.common.util.Util;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.renderer.item.ModelRenderProperties;
+import net.minecraft.client.resources.model.ResolvedModel;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
@@ -20,8 +22,9 @@ public class DogBedModifyingBakingResult_1_21_5 {
         var default_baked_model = baking_result.blockStateModels().get(default_bed_state);
         
         var unbaked_model_loc = getBlockModelLocation(DoggyBlocks.DOG_BED.get());
-        var unbaked_model = (BlockModel) event.getModelBakery().resolvedModels.get(unbaked_model_loc).wrapped();
-        
+        var resolved_model = event.getModelBakery().resolvedModels.get(unbaked_model_loc);
+        var unbaked_model = (BlockModel) resolved_model.wrapped();
+
         var dog_bed_model = new DogBedModel(event.getModelBakery(), unbaked_model, default_baked_model, ConfigHandler.CLIENT.MAX_DOG_BED_MODEL_CACHE.get());
     
         DoggyBlocks.DOG_BED.get().getStateDefinition().getPossibleStates().forEach(state -> {
@@ -29,12 +32,20 @@ public class DogBedModifyingBakingResult_1_21_5 {
         });
 
         //ItemStackModel
-        baking_result.itemStackModels().put(DogBedItemProps_1_21_5.dogBedModelLocation(), new DogBedItemModel_1_21_5(dog_bed_model));
+        var render_props = renderPopsForDogBedItemModel(resolved_model, default_baked_model);
+        baking_result.itemStackModels().put(DogBedItemProps_1_21_5.dogBedModelLocation(), 
+            new DogBedItemModel_1_21_5(dog_bed_model, render_props));
     }
 
     private static ResourceLocation getBlockModelLocation(Block block) {
         var block_id = BuiltInRegistries.BLOCK.getKey(block);
         return Util.modifyPath(block_id, x -> "block/" + x);
+    }
+
+    private static ModelRenderProperties renderPopsForDogBedItemModel(
+        ResolvedModel resolved, BlockStateModel default_baked) {
+        return new ModelRenderProperties(resolved.getTopGuiLight().lightLikeBlock(), 
+            default_baked.particleIcon(), resolved.getTopTransforms());
     }
 
 }
