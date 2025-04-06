7ff9183e9f0917ef5b1f25672ce77318944d5d6c
DashieDev <lamtinh.asian@gmail.com>
2025-04-07 03:11:44 +0700

    1.21.5 port: Data gen port II
    forgot to stage the import lol

--- src/main/java/doggytalents/DoggyTalentsNext.java
+++ src/main/java/doggytalents/DoggyTalentsNext.java
@@ -11,6 +11,7 @@ import doggytalents.client.entity.render.DoggyArmorMapping;
 import doggytalents.client.entity.render.world.BedFinderRenderer;
 import doggytalents.client.entity.render.world.CanineTrackerLocateRenderer;
 import doggytalents.client.event.ClientEventHandler;
+import doggytalents.common.backward_imitate.DTNModelProvider_1_21_5;
 import doggytalents.common.backward_imitate.DTNRecipeProvider_21_3;
 import doggytalents.common.backward_imitate.EventRegister_21_3;
 import doggytalents.client.screen.widget.DoggySpin.DoggySpinModel;
