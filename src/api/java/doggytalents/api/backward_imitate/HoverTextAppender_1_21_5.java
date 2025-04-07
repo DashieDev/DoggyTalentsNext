342d44732f83156195e969419e1fe04fd333029f
DashieDev <lamtinh.asian@gmail.com>
2025-04-08 01:01:10 +0700

    1.21.5 port: Fix Dog Bed ItemStack missing component tooltip

--- src/api/java/doggytalents/api/backward_imitate/HoverTextAppender_1_21_5.java
+++ src/api/java/doggytalents/api/backward_imitate/HoverTextAppender_1_21_5.java
@@ -1,8 +1,10 @@
 package doggytalents.api.backward_imitate;
 
 import java.util.List;
+import java.util.Optional;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -11,5 +13,19 @@ public interface HoverTextAppender_1_21_5 {
     
     default void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
         TooltipFlag flags) {};
+
+    public static Optional<HoverTextAppender_1_21_5> findFromStack(ItemStack stack) {
+        var item = stack.getItem();
+        if (item instanceof HoverTextAppender_1_21_5 appender) {
+            return Optional.of(appender);
+        }
+        if (
+            item instanceof BlockItem block_item
+            && block_item.getBlock() instanceof HoverTextAppender_1_21_5 appender
+        ) {
+            return Optional.of(appender);
+        }
+        return Optional.empty();
+    }
 
 }
