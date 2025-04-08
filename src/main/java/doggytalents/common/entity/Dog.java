9451b687c2852007f68dc46280935445d13b1975
DashieDev <lamtinh.asian@gmail.com>
2025-04-08 15:58:38 +0700

    Fix flying furball unable to fly when riding

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4291,7 +4291,7 @@ public class Dog extends AbstractDog {
     }
 
     public boolean canDogFly() {
-        return alterationProps.canFly() && this.getNavigation() instanceof DogFlyingNavigation;
+        return alterationProps.canFly() && alterationProps.willFly();
     }
 
     // @Override
