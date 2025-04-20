599bb4c40fd7c3cabfb2637149ce50bff7b7bfa5
DashieDev <lamtinh.asian@gmail.com>
2025-04-20 16:55:52 +0700

    Bunny Ears drop egg candy

--- src/main/java/doggytalents/common/entity/accessory/BunnyEars.java
+++ src/main/java/doggytalents/common/entity/accessory/BunnyEars.java
@@ -3,10 +3,16 @@ package doggytalents.common.entity.accessory;
 import java.util.function.Supplier;
 
 import doggytalents.DoggyAccessoryTypes;
+import doggytalents.DoggyItems;
+import doggytalents.api.inferface.AbstractDog;
+import doggytalents.api.inferface.IDogAlteration;
 import doggytalents.api.registry.Accessory;
+import doggytalents.api.registry.AccessoryInstance;
 import doggytalents.client.entity.render.AccessoryModelManager.Entry;
 import doggytalents.client.entity.render.layer.accessory.modelrenderentry.AccessoryModelRenderEntries;
 import doggytalents.client.entity.render.layer.accessory.modelrenderentry.IAccessoryHasModel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 
 public class BunnyEars extends Accessory implements IAccessoryHasModel{
@@ -17,5 +23,31 @@ public class BunnyEars extends Accessory implements IAccessoryHasModel{
     @Override
     public Entry getRenderEntry() {
         return AccessoryModelRenderEntries.BUNNY_EARS;
+    }
+
+    public AccessoryInstance getDefault() {
+        return new Inst(this);
+    }
+
+    public static class Inst extends AccessoryInstance implements IDogAlteration {
+
+        public Inst(Accessory typeIn) {
+            super(typeIn);
+        }
+
+        @Override
+        public void doAdditionalAttackEffects(AbstractDog dogIn, Entity target) {
+            if (target == null || target.isAlive()) return;
+            mayDropCandy(dogIn, target);
+        }
+
+        public void mayDropCandy(AbstractDog dog, Entity target) {
+            float r = dog.getRandom().nextFloat();
+            final float chance = 0.15f;
+            if (r > chance) return;
+            var item = DoggyItems.EASTER_EGG_CANDY.get();
+            dog.spawnAtLocation(new ItemStack(item), 0.0F);
+        }
+        
     }
 }
