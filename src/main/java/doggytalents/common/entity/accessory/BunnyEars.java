32709281158f789c07f2bdeb567e8a63e38c8f3e
DashieDev <lamtinh.asian@gmail.com>
2025-04-20 16:32:50 +0700

    Bunny Ears

---
+++ src/main/java/doggytalents/common/entity/accessory/BunnyEars.java
@@ -0,0 +1,21 @@
+package doggytalents.common.entity.accessory;
+
+import java.util.function.Supplier;
+
+import doggytalents.DoggyAccessoryTypes;
+import doggytalents.api.registry.Accessory;
+import doggytalents.client.entity.render.AccessoryModelManager.Entry;
+import doggytalents.client.entity.render.layer.accessory.modelrenderentry.AccessoryModelRenderEntries;
+import doggytalents.client.entity.render.layer.accessory.modelrenderentry.IAccessoryHasModel;
+import net.minecraft.world.level.ItemLike;
+
+public class BunnyEars extends Accessory implements IAccessoryHasModel{
+    public BunnyEars(Supplier<? extends ItemLike> itemIn) {
+        super(DoggyAccessoryTypes.HORN, itemIn);
+    }
+
+    @Override
+    public Entry getRenderEntry() {
+        return AccessoryModelRenderEntries.BUNNY_EARS;
+    }
+}
