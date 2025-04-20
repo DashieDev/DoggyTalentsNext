c3297d1c2dfbf16f2490b9522b8f27122207da7f
DashieDev <lamtinh.asian@gmail.com>
2025-04-20 18:31:40 +0700

    Reset the mode to docile if a wandering/patrol dog change dim somehow.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2356,8 +2356,11 @@ public class Dog extends AbstractDog {
         this.DTN_dogChangingDim = true;
         Entity transportedEntity = super.changeDimension(tansition);
         this.DTN_dogChangingDim = false;
-        if (transportedEntity instanceof Dog) {
-            DogLocationStorage.get(this.level()).getOrCreateData(this).update((Dog) transportedEntity);
+        if (transportedEntity instanceof Dog dog) {
+            DogLocationStorage.get(this.level()).getOrCreateData(this).update(dog);
+            if (dog.getMode().canWander()) {
+                dog.setMode(DogMode.DOCILE);
+            }
         }
         return transportedEntity;
     }
