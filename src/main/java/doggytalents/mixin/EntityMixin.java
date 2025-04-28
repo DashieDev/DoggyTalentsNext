58252b896c5ef9c69d2f3cb88f1e19eac2f7225e
DashieDev <lamtinh.asian@gmail.com>
2025-04-29 01:00:29 +0700

    apply DogAllyCheck when calling isAlliedTo on a player with a Dog passed in.
    Workaround to apply DogAllyCheck to sweep attack too.

---
+++ src/main/java/doggytalents/mixin/EntityMixin.java
@@ -0,0 +1,24 @@
+package doggytalents.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import doggytalents.common.entity.DogAllyCheck;
+import net.minecraft.world.entity.Entity;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    
+    @Inject(at = @At("HEAD"),  method = "isAlliedTo", cancellable = true)
+    protected void dtn__isAlliedTo(Entity entity, CallbackInfoReturnable<Boolean> info) {
+        var self = (Entity)(Object)this;
+        if (self.level().isClientSide)
+            return;
+        boolean result = DogAllyCheck.onEntityIsAlliedToServer(self, entity);
+        if (result)
+            info.setReturnValue(true);
+    }
+    
+}
