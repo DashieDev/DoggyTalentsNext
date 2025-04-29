d1680217b8df46de9ebffabe5a66330665a3d647
DashieDev <lamtinh.asian@gmail.com>
2025-04-29 17:22:05 +0700

    missing Dog Variant retaining mechanic
    When a Dog is loaded with a missing DogVariant id
     + Its variant will be "Missing"
     + the missing id will be retained in the Dog and saved in place
     of the "Missing" variant until either
     it switches variant or load in with the variant that the id refers to
     being present again.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -243,7 +243,7 @@ public class Dog extends AbstractDog {
     private ArrayList<AccessoryInstance> clientAccessories
         = new ArrayList<AccessoryInstance>();
 
-    
+    private ResourceLocation missingDogVariant = null;
         
     public final StatsTracker statsTracker = new StatsTracker();
     public final DogDataSyncManager dogSyncedDataManager
@@ -2726,7 +2726,9 @@ public class Dog extends AbstractDog {
         }
         compound.put("doggy_artifacts", artifactsListTag);
 
-        compound.putString("classicalVariant", DogVariantUtil.toSaveString(this.dogVariant()));
+        compound.putString("classicalVariant", DogVariantUtil.toSaveString(
+            this.dogVariant(), () -> Optional.ofNullable(this.missingDogVariant)));
+        
         compound.putString("mode", this.getMode().getSaveName());
         compound.putString("dogGender", this.getGender().getSaveName());
         compound.putFloat("dogHunger", this.getDogHunger());
@@ -2885,7 +2887,8 @@ public class Dog extends AbstractDog {
 
         try {
             this.setDogVariant(
-                DogVariantUtil.fromSaveString(compound.getString("classicalVariant"))
+                DogVariantUtil.fromSaveString(compound.getString("classicalVariant"), 
+                    id -> { this.missingDogVariant = id; } )
             );
         } catch (Exception e) {
             DoggyTalentsNext.LOGGER.error("Failed to load Dog Variant: " + e.getMessage());
--- src/main/java/doggytalents/common/variant/util/DogVariantUtil.java
+++ src/main/java/doggytalents/common/variant/util/DogVariantUtil.java
@@ -1,6 +1,9 @@
 package doggytalents.common.variant.util;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import doggytalents.DogVariants;
@@ -17,7 +20,15 @@ public class DogVariantUtil {
         return DogVariant.PALE;
     }
 
+    public static DogVariant getMissing() {
+        return DogVariant.MISSING;
+    }
+
     public static DogVariant fromSaveString(String string) {
+        return fromSaveString(string, id -> {});
+    }
+
+    public static DogVariant fromSaveString(String string, Consumer<ResourceLocation> missingSetter) {
         ResourceLocation id = null;
         try {
             id = ResourceLocation.parse(string);
@@ -26,17 +37,50 @@ public class DogVariantUtil {
         }
         if (id == null)
             return getDefault();
+        
         var variant = DoggyRegistries.DOG_VARIANT.get().get(id);
         if (variant == null)
             return getDefault();
+
+        boolean handle_missing = 
+            variant == getMissing() && !id.equals(getMissing().id());
+        if (handle_missing) {
+            missingSetter.accept(id);
+            return getMissing();
+        }
+
+        if (variant == getMissing())
+            variant = getDefault();
         return variant;
     }
 
     public static String toSaveString(DogVariant variant) {
+        return toSaveString(variant, () -> Optional.empty());
+    }
+
+    public static String toSaveString(DogVariant variant, Supplier<Optional<ResourceLocation>> missingGetter) {
+        if (variant == null)
+            variant = getDefault();
+        if (variant == getMissing()) {
+            var missing = missingGetter.get();
+            if (missing.isPresent()) {
+                return missing.get().toString();
+            } else {
+                return getDefault().id().toString();
+            }
+        }
         return variant.id().toString();
     }
 
     public static List<DogVariant> getAll() {
+        var variant_reg = DoggyRegistries.DOG_VARIANT.get();
+        var entries = variant_reg.stream()
+            .filter(x -> x != getMissing())
+            .collect(Collectors.toList());
+        return entries;
+    }
+
+    public static List<DogVariant> getAllWithMissing() {
         var variant_reg = DoggyRegistries.DOG_VARIANT.get();
         var entries = variant_reg.stream()
             .collect(Collectors.toList());
