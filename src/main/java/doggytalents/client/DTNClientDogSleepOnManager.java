8d79fc0684bad550d5226a58644b4eb9c58af03e
DashieDev <lamtinh.asian@gmail.com>
2025-05-02 00:28:54 +0700

    clear client petter and sleeper (DogSleepOn) map when player log out clientside.

--- src/main/java/doggytalents/client/DTNClientDogSleepOnManager.java
+++ src/main/java/doggytalents/client/DTNClientDogSleepOnManager.java
@@ -46,6 +46,11 @@ public class DTNClientDogSleepOnManager {
         invalidateSleeperCache();
     }
 
+    @SubscribeEvent
+    public void onPlayerLoggingOut(ClientPlayerNetworkEvent.LoggingOut event) {
+        this.sleeperMap.clear();
+    }
+
     public void invalidateSleeperCache() {
         if (this.sleeperMap.isEmpty())
             return;
--- src/main/java/doggytalents/client/DTNClientPettingManager.java
+++ src/main/java/doggytalents/client/DTNClientPettingManager.java
@@ -91,6 +91,11 @@ public class DTNClientPettingManager {
         invalidatePetterCache();
     }
 
+    @SubscribeEvent
+    public void onPlayerLoggingOut(ClientPlayerNetworkEvent.LoggingOut event) {
+        clearPetterMap();
+    }
+
     private final List<UUID> toRemoveForPetter = new ArrayList<>();
     public void invalidatePetterCache() {
         if (this.petterMap.isEmpty())
@@ -424,6 +429,11 @@ public class DTNClientPettingManager {
         this.petterMap.remove(petter);
         if (isSelfUUID(petter))
             this.setPetting(null);
+    }
+
+    private void clearPetterMap() {
+        this.petterMap.clear();
+        this.setPetting(null);
     }
 
     private boolean isSelfUUID(UUID petter) {
