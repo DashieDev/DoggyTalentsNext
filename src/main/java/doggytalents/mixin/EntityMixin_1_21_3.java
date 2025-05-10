6a495f5258772eca244b8109e1d5d2bed816b20b
DashieDev <lamtinh.asian@gmail.com>
2025-05-10 15:49:14 +0700

    1.21.5 fix: Change @Redirect into @WrapOperation
    so other mod can mixin here as well.

--- src/main/java/doggytalents/mixin/EntityMixin_1_21_3.java
+++ src/main/java/doggytalents/mixin/EntityMixin_1_21_3.java
@@ -2,7 +2,9 @@ package doggytalents.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import doggytalents.DoggyEntityTypes;
 import net.minecraft.world.entity.Entity;
@@ -13,21 +15,21 @@ public class EntityMixin_1_21_3 {
     
     //Workaround for player no longer be able to force the Dog on their head in 1.21.3+
     //thanks to the canSerialize check.
-    @Redirect(
+    @WrapOperation(
         method = "startRiding(Lnet/minecraft/world/entity/Entity;Z)Z", 
         at = @At(
             value = "INVOKE", 
             target = "Lnet/minecraft/world/entity/EntityType;canSerialize()Z"
         )
     )
-    private boolean dtn__startRiding__canSerialize_bypass(EntityType<?> vehicle_type) {
+    private boolean dtn__startRiding__canSerialize_bypass(EntityType<?> vehicle_type, Operation<Boolean> original) {
         var self = (Entity)(Object)this;
         var rider_type = self.getType();
         boolean dog_attempting_to_ride_player = 
             rider_type == DoggyEntityTypes.DOG.get()
             && vehicle_type == EntityType.PLAYER;
         if (!dog_attempting_to_ride_player)
-            return vehicle_type.canSerialize();
+            return original.call(vehicle_type);
         return true;
     }
 
