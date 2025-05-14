1bfce2ea1a44b4b994c452dd59cae2e09c4146e3
DashieDev <lamtinh.asian@gmail.com>
2025-05-15 02:21:16 +0700

    Dog dont go to trapdoor pos

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4788,6 +4788,9 @@ public class Dog extends AbstractDog {
         case DANGER_POWDER_SNOW:
         case POWDER_SNOW:
             return -1;
+        case DANGER_TRAPDOOR:
+        case TRAPDOOR:
+            return -1;
         }
         return super.getPathfindingMalus(type);
     }
