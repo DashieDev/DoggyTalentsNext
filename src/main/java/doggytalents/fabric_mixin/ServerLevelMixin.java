f2becc2fe1639ad83a7e8f6a2d46a7b54090b4a5
DashieDev <lamtinh.asian@gmail.com>
2025-05-17 17:40:12 +0700

    fabric fix: misinterpreted SleepFinishedEvent port causing BedDogs not be "notified" when sleep succeed.

--- src/main/java/doggytalents/fabric_mixin/ServerLevelMixin.java
+++ src/main/java/doggytalents/fabric_mixin/ServerLevelMixin.java
@@ -1,8 +1,11 @@
 package doggytalents.fabric_mixin;
 
+import java.util.function.BooleanSupplier;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import doggytalents.forge_imitate.event.EventCallbacksRegistry;
@@ -12,11 +15,28 @@ import net.minecraft.server.level.ServerLevel;
 @Mixin(ServerLevel.class)
 public class ServerLevelMixin {
     
-    @Inject(method = "wakeUpAllPlayers()V", at = @At("TAIL"))
-    public void dtn__wakeUpAllPlayers(CallbackInfo info) {
+    @Inject(
+        method = "tick(Ljava/util/function/BooleanSupplier;)V", 
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/level/ServerLevel;setDayTime(J)V",
+            ordinal = 0
+        ),
+        slice = @Slice(
+            from = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/server/players/SleepStatus;areEnoughDeepSleeping(ILjava/util/List;)Z",
+                ordinal = 0
+            ),
+            to = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/server/level/ServerLevel;wakeUpAllPlayers()V",
+                ordinal = 0
+            )
+        )
+    )
+    public void dtn__tick(BooleanSupplier booleanSupplier, CallbackInfo info) {
         var self = (ServerLevel)(Object)this;
-        if (!self.isDay())
-            return;
         var event = new SleepFinishedTimeEvent(self);
         EventCallbacksRegistry.postEvent(event);
     }
