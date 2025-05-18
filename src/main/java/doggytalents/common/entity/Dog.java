457ad7c497a6c5c5a077a2d65691140289362bfa
DashieDev <lamtinh.asian@gmail.com>
2025-05-18 15:45:23 +0700

    Dont rely on forge canDrownInFluidType to determine if dog canBreathUnderwater

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -1571,6 +1571,11 @@ public class Dog extends AbstractDog {
     }
 
     @Override
+    public boolean canBreatheUnderwater() {
+        return alterationProps.canBreatheUnderwater();
+    }
+
+    @Override
     public boolean canDrownInFluidType(FluidType type) {
         return !alterationProps.canBreatheUnderwater();
     }
