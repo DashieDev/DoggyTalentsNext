b38c8032f64548625b9185d5bf442ac31a45c3d3
DashieDev <lamtinh.asian@gmail.com>
2025-05-25 01:22:16 +0700

    Inflate rice mill render bb a bit

--- src/main/java/doggytalents/common/block/tileentity/RiceMillBlockEntity.java
+++ src/main/java/doggytalents/common/block/tileentity/RiceMillBlockEntity.java
@@ -518,13 +518,13 @@ public class RiceMillBlockEntity extends BlockEntity {
         var aabb = new AABB(pos/*, pos.offset(1, 1, 1)*/);
         var facing = RiceMillBlock.getFacing(state);
         var facing_norm = facing.getNormal();
-        var expand_vec = new Vec3(facing_norm.getX(), 1, facing_norm.getZ());
+        var expand_vec = new Vec3(facing_norm.getX(), 1.5, facing_norm.getZ());
         aabb = aabb.expandTowards(expand_vec);
         var side_axis = facing.getClockWise().getAxis();
         if (side_axis == Axis.X) {
-            aabb = aabb.inflate(1, 0, 0);
+            aabb = aabb.inflate(1.5, 0, 0);
         } else {
-            aabb = aabb.inflate(0, 0, 1);
+            aabb = aabb.inflate(0, 0, 1.5);
         }
 
         return aabb;
