2e9f3813ed2bad09a7d480749744a3ebce04344a
DashieDev <lamtinh.asian@gmail.com>
2025-05-25 15:25:57 +0700

    Common function to getFood in ItemUtil

--- src/main/java/doggytalents/common/util/ItemUtil.java
+++ src/main/java/doggytalents/common/util/ItemUtil.java
@@ -7,10 +7,6 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.DamageTypeTags;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -158,6 +154,10 @@ public class ItemUtil {
         if (!stack.has(DataComponents.TRIM) || !isValidTrim_1_21_3(stack))
             return Optional.empty();
         return Optional.ofNullable(stack.get(DataComponents.TRIM));
+    }
+
+    public static FoodProperties food(ItemStack stack) {
+        return stack.getFoodProperties(null);
     }
 
     public static CustomData getWrappedTag(ItemStack stack) {
