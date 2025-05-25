d6b4b6b9554f844830a4699fe2e5b38d24356a4c
DashieDev <lamtinh.asian@gmail.com>
2025-05-25 14:42:23 +0700

    reorder conifg building to match initialization

--- src/main/java/doggytalents/common/config/ConfigHandler.java
+++ src/main/java/doggytalents/common/config/ConfigHandler.java
@@ -302,6 +302,10 @@ ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CONFIG_CLIENT_SPEC
                 .comment("status. This will takes up two health lines and will render where")
                 .comment("the Vanilla Vehicle Health is and instead of it.")
                 .define("dtn_wolf_mount_overlay", true);
+            SHOW_DOG_NAME_THRU_WALL = builder
+                .comment("By default, DTN will render your Dogs' Names through walls. Disable")
+                .comment("this to make Owned Dogs' Names no longer visible through walls.")
+                .define("show_dog_name_thru_wall", true);
             MAX_DOG_BED_MODEL_CACHE = builder
                 .comment("Configure the maximum value of Dog Bed variants that will be rendered.")
                 .comment("If the amount of Dog Bed variants rendered exceeds this limit,")
@@ -309,10 +313,6 @@ ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CONFIG_CLIENT_SPEC
                 .comment("To reset the cache, do a resource reload (F3+T).")
                 .comment("Set this option to any value less than zero to remove the limit.")
                 .defineInRange("max_dog_bed_model_cache", 65536, Integer.MIN_VALUE, Integer.MAX_VALUE);
-            SHOW_DOG_NAME_THRU_WALL = builder
-                .comment("By default, DTN will render your Dogs' Names through walls. Disable")
-                .comment("this to make Owned Dogs' Names no longer visible through walls.")
-                .define("show_dog_name_thru_wall", true);
             builder.pop();
         }
 
