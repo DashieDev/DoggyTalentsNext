6cee3b66d4f667036e72a21ea1a1ae94ecfb6f7f
DashieDev <lamtinh.asian@gmail.com>
2025-05-25 15:42:52 +0700

    Dog will now eat uncooked food from pack puppy and food bowl
    They will still prefer cooked one.

--- src/main/java/doggytalents/common/util/DogFoodUtil.java
+++ src/main/java/doggytalents/common/util/DogFoodUtil.java
@@ -23,18 +23,7 @@ import net.neoforged.neoforge.items.ItemStackHandler;
 
 public class DogFoodUtil {
     
-    private static final MeatFoodHandler meat_food_handler_limited = new MeatFoodHandler() {
-
-        @Override
-        public boolean isFood(ItemStack stack) {
-            var props = ItemUtil.food_1_21_3(stack);
-
-            if (props == null) return false;
-            return isMeat(stack) && stack.getItem() != Items.ROTTEN_FLESH
-                && props.nutrition() >= 6;
-        }
-        
-    };
+    private static final MeatFoodHandler meat_food_handler_limited = new MeatFoodHandler();
 
     public static boolean isMeat(ItemStack stack) {
         return stack.is(ItemTags.MEAT) || (
@@ -110,13 +99,22 @@ public class DogFoodUtil {
         if (inventory == null)
             return -1;
 
+        int minor_meat_id = -1;
+
         for (int i = 0; i < inventory.getSlots(); i++) {
             var stack = inventory.getStackInSlot(i);
-            if (meat_food_handler_limited.canConsume(target, stack, finder)) {
+            if (!meat_food_handler_limited.canConsume(target, stack, finder))
+                continue;
+            var food = ItemUtil.food(stack);
+            if (food == null)
+                continue;
+                
+            if (food.nutrition() >= 6)
                 return i;
-            }
+            if (minor_meat_id < 0) minor_meat_id = i;
         }
-        return -1;
+
+        return minor_meat_id;
     }
 
     public static boolean tryFeed(Dog dog, boolean findHealingFood,
