c8bbe7a7ff71de7ef3483cb911a5a80225c85595
DashieDev <lamtinh.asian@gmail.com>
2025-05-25 23:42:45 +0700

    Add some randomness to ring search iterator.

--- src/main/java/doggytalents/common/entity/ai/DogFindWaterGoal.java
+++ src/main/java/doggytalents/common/entity/ai/DogFindWaterGoal.java
@@ -105,7 +105,7 @@ public class DogFindWaterGoal extends Goal {
     }
 
     private BlockPos searchForWaterPos() {
-        for (var pos : RingSearchIterator.create(this.dog.blockPosition(), 4, SEARCH_RANGE, true)) {
+        for (var pos : RingSearchIterator.createWithRandom(this.dog.blockPosition(), 4, SEARCH_RANGE, true, this.dog.getRandom())) {
             if (this.isWaterPos(pos)) {
                 return pos;
             }
