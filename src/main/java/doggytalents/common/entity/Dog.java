925f689aca20033c4dc793b520852f470bb39c22
DashieDev <lamtinh.asian@gmail.com>
2025-05-26 17:12:13 +0700

    fix dog owner push avoid dont work
    Because push check is done serverside only and owner is only being
    pushed clientside.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -209,6 +209,7 @@ public class Dog extends AbstractDog {
      *     16              65536               REST_BELLY
      *     17              131072              DRUNK_POSE
      *     18              262144              ACTION_FORCED_NONE_ANIM
+     *     19              524288              FORCE_PUSH_AVOID
      *     .
      *     31              2^31                <Reserved>
      */
@@ -315,7 +316,6 @@ public class Dog extends AbstractDog {
     protected float jumpPower;
 
     protected boolean isDogSwimming;
-    protected boolean isDogRunningAwayFromFire;
 
     public int lastOrderedToSitTick;
     private int tickChopinTail;
@@ -4601,7 +4601,7 @@ public class Dog extends AbstractDog {
             return false;
         if (this.alterationProps.resistWaterPush())
             return false;
-        if (this.isDogRunningAwayFromFire())
+        if (this.isDogForcePushAvoid())
             return false;
         for (var alter : this.alterations) {
             InteractionResult result = alter.canResistPushFromFluidType();
@@ -5041,12 +5041,12 @@ public class Dog extends AbstractDog {
         return this.isDogSwimming;
     }
 
-    public void setDogRunningAwayFromFire(boolean val) {
-        this.isDogRunningAwayFromFire = val;
+    public void setDogForcePushAvoid(boolean val) {
+        this.setDogFlag(524288, val);
     }
 
-    public boolean isDogRunningAwayFromFire() {
-        return this.isDogRunningAwayFromFire;
+    public boolean isDogForcePushAvoid() {
+        return this.getDogFlag(524288);
     }
 
     private boolean isDogCurious;
