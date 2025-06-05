22245f4d1a8ec16a8571304a4cc8995f30567a70
DashieDev <lamtinh.asian@gmail.com>
2025-06-05 16:17:08 +0700

    option to make vanilla wolf persistance required when tame.

---
+++ src/main/java/doggytalents/mixin/TamableAnimalMixin.java
@@ -0,0 +1,27 @@
+package doggytalents.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import doggytalents.common.event.EventHandler;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.player.Player;
+
+@Mixin(TamableAnimal.class)
+public class TamableAnimalMixin {
+    
+    @Inject(at = @At("TAIL"),  method = "tame(Lnet/minecraft/world/entity/player/Player;)V", cancellable = false)
+    protected void dtn__tame(Player player, CallbackInfo info) {
+        var self = (TamableAnimal)(Object)this;
+        if (self.getType() != EntityType.WOLF)
+            return;
+        if (!(self instanceof Wolf wolf))
+            return;
+        EventHandler.onWolfTame(wolf);
+    }
+
+}
