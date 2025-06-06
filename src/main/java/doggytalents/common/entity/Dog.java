43d4e9d67f6cb0f50d5f8080c3c2e500f71d32d5
DashieDev <lamtinh.asian@gmail.com>
2025-06-06 23:19:34 +0700

    if dog is in lava allow the dog to treat lava/damage_fire as high malus path pos because they need to swim thru it to get out anyways.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4809,6 +4809,10 @@ public class Dog extends AbstractDog {
         case TRAPDOOR:
             return -1;
         }
+        if (this.isInLava()) {
+            if (type == PathType.LAVA || type == PathType.DAMAGE_FIRE)
+                return 40;
+        }
         return super.getPathfindingMalus(type);
     }
 
