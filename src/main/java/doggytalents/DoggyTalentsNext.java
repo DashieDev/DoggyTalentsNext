8767d10733dab448d0499139ecd65ed38593a6a0
DashieDev <lamtinh.asian@gmail.com>
2025-06-10 00:14:56 +0700

    Remove redundant chunk helper.

--- src/main/java/doggytalents/DoggyTalentsNext.java
+++ src/main/java/doggytalents/DoggyTalentsNext.java
@@ -14,7 +14,6 @@ import doggytalents.client.entity.render.world.CanineTrackerLocateRenderer;
 import doggytalents.client.event.ClientEventHandler;
 import doggytalents.client.screen.widget.DoggySpin.DoggySpinModel;
 import doggytalents.common.block.tileentity.RiceMillBlockEntity;
-import doggytalents.common.chunk.GarbageChunkCollector;
 //import doggytalents.common.addon.AddonManager;
 import doggytalents.common.command.DoggyCommands;
 import doggytalents.common.config.ConfigHandler;
@@ -141,7 +140,6 @@ public class DoggyTalentsNext {
             DoggyAdvancementTriggers.registerAll();
             DoggyBrewingRecipes.registerAll();
             ConfigHandler.initTalentConfig();
-            GarbageChunkCollector.init();
             RiceMillBlockEntity.initGrindMap();
             DTNItemCategory.init();
             DTNCompostables.init();
