bcf319c18068f231215376837e8dd830a7be51e9
DashieDev <lamtinh.asian@gmail.com>
2025-06-13 17:34:33 +0700

    make the load chunk before move behaviour configurable

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2501,6 +2501,8 @@ public class Dog extends AbstractDog {
         boolean changed_chunk = current_pos.x != x || current_pos.z != z;
         if (!changed_chunk)
             return;
+        if (!ConfigHandler.SERVER.DOG_LOAD_CHUNK_BEFORE_MOVE.get())
+            return;
         if (!this.isAddedToWorld())
             return;
         if (this.level().isClientSide)
