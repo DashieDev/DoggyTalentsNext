9a643cb7a22838940bba499859782c3d1bf9427b
DashieDev <lamtinh.asian@gmail.com>
2025-06-14 00:45:25 +0700

    1.20 fix

--- src/api/java/doggytalents/api/inferface/DogArmorItemHandler.java
+++ src/api/java/doggytalents/api/inferface/DogArmorItemHandler.java
@@ -1,6 +1,6 @@
 package doggytalents.api.inferface;
 
-import net.neoforged.neoforge.items.ItemStackHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public abstract class DogArmorItemHandler extends ItemStackHandler {
 
