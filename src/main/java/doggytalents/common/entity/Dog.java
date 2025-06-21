0f0b52b5b8a6ba6b1a7f457f62b1510e67fe34aa
DashieDev <lamtinh.asian@gmail.com>
2025-06-22 02:09:19 +0700

    Dedicated Dog tracking listener.
    Since there is some thing off going on with neo/forge's isAddedToWorld updating.....

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -246,6 +246,8 @@ public class Dog extends AbstractDog {
     public final StatsTracker statsTracker = new StatsTracker();
     public final DogDataSyncManager dogSyncedDataManager
         = new DogDataSyncManager(this);
+    public final DogTrackingTracker dogTrackingTracker =
+        new DogTrackingTracker();
     public final DogOwnerDistanceManager dogOwnerDistanceManager 
         = new DogOwnerDistanceManager(this);
     public final DogAvoidGoingInFrontOfOwnerManager avoidGoInFrontOfOwnerManager
@@ -2415,6 +2417,12 @@ public class Dog extends AbstractDog {
             DogDuplicationDetection.afterDogJoinLevel(this, data);
         }
         super.onAddedToWorld();
+    }
+
+    public void onDogStartTracking() {
+    }
+
+    public void onDogStopTracking() {
     }
 
     @Override
---
+++ src/main/java/doggytalents/common/entity/DogTrackingTracker.java
@@ -0,0 +1,15 @@
+package doggytalents.common.entity;
+
+public class DogTrackingTracker {
+    
+    private boolean isTracking = false;
+
+    public boolean isTracking() {
+        return isTracking;
+    }
+
+    public void setTracking(boolean val) {
+        this.isTracking = val;
+    }
+
+}
