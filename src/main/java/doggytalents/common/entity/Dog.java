6f70f65242accc933b42a8f5b9778d3830aae4a0
DashieDev <lamtinh.asian@gmail.com>
2025-06-21 16:16:29 +0700

    adjust how we mark locationUpdateUponRemove status for Dog
    we only log if dog is tamed.
    null check the mark just to be srue.
    We also mark when the dog is removed trusted and unloaded to respawn storage or untamed remove.
    Each mark type have their own log msg.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2383,7 +2383,8 @@ public class Dog extends AbstractDog {
         return false;
     }
 
-    public boolean locationUpdatedUponRemove = false;
+    public OnlineDogLocationManager.RemoveState locationUpdatedUponRemove 
+        = OnlineDogLocationManager.RemoveState.NONE;
     @Override
     public void onRemovedFromWorld() {
         if (this.level() instanceof ServerLevel serverLevel && this.isAlive()) {
@@ -2392,7 +2393,7 @@ public class Dog extends AbstractDog {
             var data = DogLocationStorage.get(serverLevel).getData(this);
             
             if (data != null) data.update(this);
-            locationUpdatedUponRemove = true;
+            locationUpdatedUponRemove = OnlineDogLocationManager.RemoveState.UPDATED;
         }
         super.onRemovedFromWorld();
     }
@@ -2419,11 +2420,19 @@ public class Dog extends AbstractDog {
                 && removalReason == RemovalReason.DISCARDED;
             if (!remove_trusted && removalReason.shouldDestroy()) {     
                 DogLocationStorage.get(this.level()).remove(this);
-                if (this.getOwnerUUID() != null)
+                if (this.getOwnerUUID() != null) {
                     DogRespawnStorage.get(this.level()).putData(this);
+                    this.locationUpdatedUponRemove = 
+                        OnlineDogLocationManager.RemoveState.UNLOADED_TO_RESPAWN;
+                } else {
+                    this.locationUpdatedUponRemove =
+                        OnlineDogLocationManager.RemoveState.REMOVED;
+                }
             }
             if (remove_trusted) {
                 DogLocationStorage.get(this.level()).remove(this);
+                this.locationUpdatedUponRemove =
+                    OnlineDogLocationManager.RemoveState.REMOVE_TRUSTED;
             }
         }
         
