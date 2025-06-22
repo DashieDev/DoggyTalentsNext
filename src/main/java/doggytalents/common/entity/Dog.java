695d6f9381af2924b5274390cf32e00c688b0fea
DashieDev <lamtinh.asian@gmail.com>
2025-06-22 23:15:27 +0700

    Integrating our Tracking listener
    to invalidate locationUpdatedUponRemove when the dog is tracked again.
    when checking if dog should load chunk before move
    Also we only sync the dog location data to storage as long as the dog is tracked.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -2420,6 +2420,7 @@ public class Dog extends AbstractDog {
     }
 
     public void onDogStartTracking() {
+        locationUpdatedUponRemove = OnlineDogLocationManager.RemoveState.NONE;
     }
 
     public void onDogStopTracking() {
@@ -2511,7 +2512,7 @@ public class Dog extends AbstractDog {
             return;
         if (!ConfigHandler.SERVER.DOG_LOAD_CHUNK_BEFORE_MOVE.get())
             return;
-        if (!this.isAddedToWorld())
+        if (!this.dogTrackingTracker.isTracking())
             return;
         if (this.level().isClientSide)
             return;
--- src/main/java/doggytalents/common/storage/OnlineDogLocationManager.java
+++ src/main/java/doggytalents/common/storage/OnlineDogLocationManager.java
@@ -75,6 +75,8 @@ public class OnlineDogLocationManager {
             var dog = entry.getValue();
             if (dog.isRemoved())
                 continue;
+            if (!dog.dogTrackingTracker.isTracking())
+                continue;
             syncDataToStorage(storage, dog);
         }
     }
