51f667b7fc58545f3773095880b4152420d8d060
DashieDev <lamtinh.asian@gmail.com>
2025-06-25 13:18:24 +0700

    Improve nav when dog about to approach "Danger Node"
    A "Danger Node" in this context is defined as a path node with a danger
    type and either it is the endNode of the path or its y is different
    than the prevNode's y (or the dog's y block if it is the
    current next node).
    When there is a Danger Node no greater than 2 nodes away from the dog current next node in the dog current path, the dog will begin
    sneaking, the closer the dog is, the slower the dog sneak.
    Also other dog will not push that dog and vice versa.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4712,6 +4712,12 @@ public class Dog extends AbstractDog {
         return this.defaultNavigation == this.getNavigation();
     }
 
+    public Optional<DogPathNavigation> getDefaultNavigationIfActive() {
+        if (this.isDefaultNavigation())
+            return Optional.of(this.defaultNavigation);
+        return Optional.empty();
+    }
+
     @Override
     protected DogPathNavigation createNavigation(Level p_21480_) {
         var dogPathNav = new DogPathNavigation(this, p_21480_);
