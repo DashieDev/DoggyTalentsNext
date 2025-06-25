e9a1e785266662feee64e1105eb141a8af35a541
DashieDev <lamtinh.asian@gmail.com>
2025-06-25 14:45:38 +0700

    Changes regarding dog being pushed by fluid when nav-ing
    Dog bypass liquid push if nav-ing
    Dog dont float if nav-ing and its next node y is not greater than the dog's y block.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4548,6 +4548,12 @@ public class Dog extends AbstractDog {
             return false;
         if (this.isDogForcePushAvoid())
             return false;
+        if (
+            this.getDefaultNavigationIfActive()
+            .map(DogPathNavigation::shouldDogBlockFluidPush)
+            .orElse(false)
+        )
+            return false;
         for (var alter : this.alterations) {
             var result = alter.canResistPushFromFluidType();
 
