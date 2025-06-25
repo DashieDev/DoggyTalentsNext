4ce1624b246ccd0c87bc294ec65ec72dd25a9d7c
DashieDev <lamtinh.asian@gmail.com>
2025-06-26 02:13:27 +0700

    Some action will now stop if dog needs to follow owner and is too far away.

--- src/main/java/doggytalents/common/talent/WaterHolderTalent.java
+++ src/main/java/doggytalents/common/talent/WaterHolderTalent.java
@@ -399,6 +399,16 @@ public class WaterHolderTalent extends TalentInstance {
         }
 
         @Override
+        public boolean shouldStopAndFollowOwner() {
+            return true;
+        }
+
+        @Override
+        public float getDistanceForFollowOwner() {
+            return 20;
+        }
+
+        @Override
         public void onStart() {
             //this.dog.getLookControl().setLookAt(target, 10.0F, this.dog.getMaxHeadXRot());
             ticksUntilPathRecalc = 0;
