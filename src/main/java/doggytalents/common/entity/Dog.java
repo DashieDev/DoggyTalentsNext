8b799d6351f1b94226bfbc524eb67d010e877e99
DashieDev <lamtinh.asian@gmail.com>
2025-06-26 17:23:11 +0700

    block random strolling goals for a few ticks after dog follow owner.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -305,7 +305,6 @@ public class Dog extends AbstractDog {
 
     private int healingTick;
     //private int wanderRestTime = 0;
-    private int wanderCooldown = 0;
     private int drunkTickLeft = 0;
     private int silentTickLeft = 0;
 
@@ -329,6 +328,7 @@ public class Dog extends AbstractDog {
     protected boolean isDogSwimming;
 
     public int lastOrderedToSitTick;
+    public int randomStrollCooldown;
     private int tickChopinTail;
     private boolean dogAnimHurtImpules = false;
     private int idleAnimHurtCooldown = 0;
@@ -946,6 +946,10 @@ public class Dog extends AbstractDog {
 
         if (this.tickChopinTail > 0) {
             --this.tickChopinTail;
+        }
+
+        if (this.randomStrollCooldown > 0) {
+            --this.randomStrollCooldown;
         }
 
         if (this.switchNavCooldown > 0) {
