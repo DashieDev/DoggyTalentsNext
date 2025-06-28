243343bc74fd2e54b476692e429228ecf26dd7f5
DashieDev <lamtinh.asian@gmail.com>
2025-06-29 01:57:50 +0700

    Revert dhange that allow dog to go to lava when in lava.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -4760,10 +4760,6 @@ public class Dog extends AbstractDog {
         case TRAPDOOR:
             return -1;
         }
-        if (this.isInLava()) {
-            if (type == PathType.LAVA || type == PathType.DAMAGE_FIRE)
-                return 40;
-        }
         return super.getPathfindingMalus(type);
     }
 
