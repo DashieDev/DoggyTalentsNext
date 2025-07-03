6f587bab04daeece5f4284d5427d943cca7ffb3c
DashieDev <lamtinh.asian@gmail.com>
2025-07-04 01:00:30 +0700

    1.21.7 port: fix renderLabels render 100% transparent text
    1.21.7 no longer ignore alpha component in text color.....

--- src/main/java/doggytalents/client/screen/DoggyToolsScreen.java
+++ src/main/java/doggytalents/client/screen/DoggyToolsScreen.java
@@ -31,7 +31,7 @@ public class DoggyToolsScreen extends AbstractContainerScreen<DoggyToolsMenu> {
 
     @Override
     protected void renderLabels(GuiGraphics p_281635_, int p_282681_, int p_283686_) {
-        p_281635_.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 4210752, false);
+        p_281635_.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, -12566464, false);
     }
 
     @Override
