e2ee8dba51e2e01cddbf8d924ce2f356ab23525a
DashieDev <lamtinh.asian@gmail.com>
2025-07-31 02:02:07 +0700

    DogModel render procedure refactor
    Remove code duplication on the main render logic that involves pivoting root, and transfrom baby head.
    ensure uniform rendering acrross all dog model and dog synced accessory model.

--- src/main/java/doggytalents/client/entity/model/dog/GlowingEyeDogModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/GlowingEyeDogModel.java
@@ -2,13 +2,13 @@ package doggytalents.client.entity.model.dog;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
 
 import doggytalents.common.entity.Dog;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 public class GlowingEyeDogModel extends DogModel {
@@ -37,70 +37,35 @@ public class GlowingEyeDogModel extends DogModel {
         super.setupAnim(dog, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
         this.glowingEyes.copyFrom(this.head);
         this.realGlowingEyes.copyFrom(this.realHead);
-        this.glowingEyes.visible = this.head.visible;
-        this.realGlowingEyes.visible = this.realHead.visible;
     }
-    
+
     @Override
-    public void renderToBuffer(PoseStack p_102034_, VertexConsumer p_102035_, int p_102036_, int p_102037_, int color_overlay) {
-        var pivot = DEFAULT_ROOT_PIVOT;
-        var custom_pivot = getCustomRootPivotPoint();
-        if (custom_pivot != null) {
-            pivot = custom_pivot;
-        }
-        p_102034_.pushPose();
-        p_102034_.translate((double)(root.x / 16.0F), (double)(root.y / 16.0F), (double)(root.z / 16.0F));
-        p_102034_.translate((double)(pivot.x / 16.0F), (double)(pivot.y / 16.0F), (double)(pivot.z / 16.0F));
-        if (root.zRot != 0.0F) {
-            p_102034_.mulPose(Axis.ZP.rotation(root.zRot));
-        }
+    public void renderToBuffer(PoseStack stack, VertexConsumer vertex_consumer, int light, int overlay,
+            int color_overlay) {
+        this.glowingEyes.visible = false;
+        this.realGlowingEyes.visible = false;
+        super.renderToBuffer(stack, vertex_consumer, light, overlay, color_overlay);
+    }
 
-        if (root.yRot != 0.0F) {
-            p_102034_.mulPose(Axis.YP.rotation(root.yRot));
-        }
+    @Override
+    protected Optional<AddtionalHeadRenderer> getDogModelAdditionalHeadRenderer() {
+        return Optional.of(this::renderGlowingEyes);
+    }
 
-        if (root.xRot != 0.0F) {
-            p_102034_.mulPose(Axis.XP.rotation(root.xRot));
-        }
-        float xRot0 = root.xRot, yRot0 = root.yRot, zRot0 = root.zRot;
-        float x0 = root.x, y0 = root.y, z0 = root.z;
-        root.xRot = 0; root.yRot = 0; root.zRot = 0;
-        root.x = 0; root.y = 0; root.z = 0;
-        p_102034_.pushPose();
-        p_102034_.translate((double)(-pivot.x / 16.0F), (double)(-pivot.y / 16.0F), (double)(-pivot.z / 16.0F));
-        
-        if (this.young && this.scaleBabyDog()) {
-
-            boolean headVisible0 = this.head.visible;
-            
-            this.head.visible = false;
-            p_102034_.pushPose();
-            //float f1 = 1.0F / 2f;
-            //p_102034_.scale(f1, f1, f1);
-            //p_102034_.translate(0.0D, (double)(24 / 16.0F), 0.0D);
-            this.root.render(p_102034_, p_102035_, p_102036_, p_102037_, color_overlay);
-            p_102034_.popPose();
-            
-            this.head.visible = headVisible0;
-            p_102034_.pushPose();
-            //p_102034_.translate(0.0D, (double)(5f / 16.0F), (double)(2f / 16.0F));
-            p_102034_.scale(2, 2, 2);
-            p_102034_.translate(0, -0.5, 0.15);
-            this.head.render(p_102034_, p_102035_, p_102036_, p_102037_, color_overlay);
-            p_102034_.popPose();            
-        } else {
-            boolean glowingEyes_visible0 = false;
-            glowingEyes_visible0 = this.glowingEyes.visible;
-            this.glowingEyes.render(p_102034_, p_102035_, 15728880, p_102037_, color_overlay);
+    private void renderGlowingEyes(PoseStack stack, Optional<DogModel.DogRenderPartContext> part_ctx_optional) {
+        if (!part_ctx_optional.isPresent())
+            return;
+        var part_ctx = part_ctx_optional.get();
+        if (this.head.visible) {
+            this.glowingEyes.visible = true;
+            part_ctx.renderGlowingPart(stack, this.glowingEyes);
             this.glowingEyes.visible = false;
-            this.root.render(p_102034_, p_102035_, p_102036_, p_102037_, color_overlay);
-            this.glowingEyes.visible = glowingEyes_visible0;
         }
-
-        p_102034_.popPose();
-        p_102034_.popPose();
-        root.xRot = xRot0; root.yRot = yRot0; root.zRot = zRot0;
-        root.x = x0; root.y = y0; root.z = z0;
+        if (this.realHead.visible) {
+            this.realGlowingEyes.visible = true;
+            part_ctx.renderGlowingPart(stack, this.realGlowingEyes);
+            this.realGlowingEyes.visible = false;
+        }
     }
 
 }
