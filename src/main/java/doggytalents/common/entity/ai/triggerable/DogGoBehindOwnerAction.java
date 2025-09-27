fe2a650684542804c0ee40ea4a2f719371f08d26
DashieDev <lamtinh.asian@gmail.com>
2025-09-28 02:41:03 +0700

    Fix DogGoBehindAction and use pathtype check for some open pos check
    We ultilize the pathtype check directly
    DogGoBehindAction can now lead dog to pos that contain valid walkable pos that contains a solid block and a non air, non collision block on top of it like a torch on a solid ground.
     DogGoBehindAction now properly move up and down when search pos, using the pathtype check

--- src/main/java/doggytalents/common/entity/ai/triggerable/DogGoBehindOwnerAction.java
+++ src/main/java/doggytalents/common/entity/ai/triggerable/DogGoBehindOwnerAction.java
@@ -68,27 +68,21 @@ public class DogGoBehindOwnerAction extends TriggerableAction {
         
         var vec_back = new Vec3(dx1, 0, dz1);
         double pX = owner_pos0.x, pY = owner_pos0.y , pZ = owner_pos0.z;
-        BlockPos bpos = null;
-        BlockState state = null;
-        BlockState state_below = null;
         for (int i = 0; i < 3; ++i) {
             pX += vec_back.x; pZ += vec_back.z;
-            bpos = BlockPos.containing(pX, pY, pZ);
-            state = dog.level().getBlockState(bpos);
-            if (!state.isAir()) {
+            var bpos = BlockPos.containing(pX, pY, pZ);
+            if (DogUtil.isWalkNodeEvaluatorBlockedPos(dog, bpos)) {
                 ++pY;
                 continue;
             }
-            state_below = dog.level().getBlockState(bpos.below());
-            if (state_below.isAir()) {
+            if (DogUtil.isWalkNodeEvaluatorOpenPos(dog, bpos.below())) {
                 --pY;
                 continue;
             }
         }
         
-        bpos = BlockPos.containing(pX, pY, pZ);
-        state = dog.level().getBlockState(bpos);
-        this.targetPos = ( state != null && state.isAir() ) ? bpos : null;
+        var bpos = BlockPos.containing(pX, pY, pZ);
+        this.targetPos = DogUtil.isWalkNodeEvaluatorOpenPos(dog, bpos) ? bpos : null;
     }
     
 }
