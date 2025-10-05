fa0b371b430c252718440814fd947f02180de1da
DashieDev <lamtinh.asian@gmail.com>
2025-10-06 03:38:07 +0700

    remove potentially dangerous client code reference in common

--- src/main/java/doggytalents/common/entity/BoostingFoodHandler.java
+++ src/main/java/doggytalents/common/entity/BoostingFoodHandler.java
@@ -5,7 +5,6 @@ import javax.annotation.Nullable;
 import doggytalents.api.inferface.AbstractDog;
 import doggytalents.api.inferface.IDogFoodHandler;
 import doggytalents.common.network.packet.ParticlePackets;
-import net.minecraft.client.gui.components.DebugScreenOverlay;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionResult;
