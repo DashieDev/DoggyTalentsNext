beaee2460ca8fc9d009e5156c3c76d21662146ec
DashieDev <lamtinh.asian@gmail.com>
2025-10-06 02:23:53 +0700

    Fix dog improperly dismounting entity into area too small.

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -151,6 +151,7 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.DismountHelper;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -4132,9 +4133,18 @@ public class Dog extends AbstractDog {
         var newZ = this.getZ() + dz1;
         var newPos = new Vec3(newX, this.getY() + 0.5, newZ);
         var b0 = BlockPos.containing(newPos);
+        newPos = Vec3.atBottomCenterOf(b0);
         var type = WalkNodeEvaluator.getPathTypeStatic(this, b0.mutable());
         if (type == PathType.WALKABLE) {
-            return newPos;
+            for (var pose : passenger.getDismountPoses()) {
+                var dismountBb = 
+                    passenger.getLocalBoundsForPose(pose)
+                    .move(newPos);
+                if (!DismountHelper.canDismountTo(this.level(), passenger, dismountBb)) 
+                    continue;
+                passenger.setPose(pose); // TODO Should we keep this ugly side effect?
+                return newPos;
+            }
         }
         return super.getDismountLocationForPassenger(passenger);
     }
