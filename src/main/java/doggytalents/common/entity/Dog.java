cc9778ac95571bd7610ce7c3ac4589e767ae0666
DashieDev <lamtinh.asian@gmail.com>
2025-10-11 03:43:41 +0700

    Probably fix dog still push owner by default,
    Separate active push and being push case
    Dogs now, by default, won't push owner. The owner can still push it tho.
    refactor customPush behaviour, intergating the ability to not push source into the Dog class itself.
    Owner won't push dog when force_push_avoid

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -5074,7 +5074,7 @@ public class Dog extends AbstractDog {
     protected void doPush(Entity pushTarget) {
         if (this.pettingManager.checkPush(pushTarget))
             return;
-        if (this.dogPushAvoidManager.shouldBlockPush(pushTarget))
+        if (this.dogPushAvoidManager.shouldBlockActivePush(pushTarget))
             return;
         if (DogSleepOnManager.shouldBlockPush(this))
             return;
@@ -5085,7 +5085,7 @@ public class Dog extends AbstractDog {
             return;        
         }
         if (this.isVehicle() && !this.hasControllingPassenger())
-            pushDogAsVehicle(pushTarget);
+            this.customDogPush(pushTarget, true);
         else
             super.doPush(pushTarget);
     }
@@ -5097,14 +5097,17 @@ public class Dog extends AbstractDog {
             return;
         if (this.pettingManager.checkPush(source))
             return;
-        if (this.isDefeated() && this.incapacitatedMananger.shouldApplyCustomPushBehaviour(source)) {
-            this.incapacitatedMananger.customBeingPushed(source, this::calcDogPushVec);
-            return;
-        }
         if (DogSleepOnManager.shouldBlockPush(this))
             return;
-        if (this.isVehicle() && !this.hasControllingPassenger())
-            pushDogAsVehicle(source);
+        var result = this.dogPushAvoidManager.onBeingPushed(source);
+        if (result == DogPushAvoidManager.OnBeingPushAction.BLOCK_ALL)
+            return;
+        if (result == DogPushAvoidManager.OnBeingPushAction.BLOCK_SOURCE
+            || (this.isDefeated() 
+                && this.incapacitatedMananger.shouldApplyCustomPushBehaviour(source)))
+            this.customDogPush(source, false);
+        else if (this.isVehicle() && !this.hasControllingPassenger())
+            this.customDogPush(source, true);
         else {
             if (isDogCurious())
                 setDogCurious(false);
@@ -5112,7 +5115,7 @@ public class Dog extends AbstractDog {
         }
     }
 
-    private void pushDogAsVehicle(Entity source) {
+    private void customDogPush(Entity source, boolean pushSource) {
         var push_vec_optional = calcDogPushVec(source);
         if (!push_vec_optional.isPresent())
             return;
@@ -5122,7 +5125,7 @@ public class Dog extends AbstractDog {
             this.push(-push_vec.x(), 0.0D, -push_vec.z());
         }
 
-        if (source.isPushable()) {
+        if (pushSource && source.isPushable()) {
             source.push(push_vec.x(), 0.0D, push_vec.z());
         }
     }
@@ -5156,7 +5159,7 @@ public class Dog extends AbstractDog {
 
     @Override
     public boolean canCollideWith(Entity otherEntity) {
-        if (this.dogPushAvoidManager.shouldBlockPush(otherEntity)) {
+        if (this.dogPushAvoidManager.shouldBlockActivePush(otherEntity)) {
             return false;
         }
         if (DogSleepOnManager.shouldBlockPush(this))
