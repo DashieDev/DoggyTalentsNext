63bf24ed749439f7e97c6dc4a75af74b60ebee13
DashieDev <lamtinh.asian@gmail.com>
2025-11-09 18:26:22 +0700

    1.21.9 port: Vanilla New Rendering System Port IX: BlockEntityRenderers Port

--- src/main/java/doggytalents/client/block/render/RiceMillRenderer.java
+++ src/main/java/doggytalents/client/block/render/RiceMillRenderer.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 
 import doggytalents.client.ClientSetup;
+import doggytalents.client.backward_imitate.LegacyBlockEntityRendererUtil_1_21_9.BlockEntityRenderer_1_21_9;
+import doggytalents.client.backward_imitate.LegacyBlockEntityRendererUtil_1_21_9.RenderContext_1_21_9;
 import doggytalents.client.block.model.RiceMillModel;
 import doggytalents.common.block.RiceMillBlock;
 import doggytalents.common.block.tileentity.RiceMillBlockEntity;
@@ -15,7 +17,7 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.phys.Vec3;
 
-public class RiceMillRenderer implements BlockEntityRenderer<RiceMillBlockEntity> {
+public class RiceMillRenderer extends BlockEntityRenderer_1_21_9<RiceMillBlockEntity> {
 
     private RiceMillModel model;
 
@@ -24,9 +26,9 @@ public class RiceMillRenderer implements BlockEntityRenderer<RiceMillBlockEntity
     }
 
     @Override
-    public void render(RiceMillBlockEntity mill, float pTicks, PoseStack stack, 
-            MultiBufferSource buffer,
-            int light, int overlay, Vec3 vec) {
+    public void submit(RiceMillBlockEntity mill, float pTicks, PoseStack stack, 
+            RenderContext_1_21_9<RiceMillBlockEntity> context_1_21_9,
+            int light, int overlay) {
         stack.pushPose();
         // float scaleFactor = 1f;
         // stack.scale(scaleFactor, -scaleFactor, -scaleFactor);
@@ -40,9 +42,11 @@ public class RiceMillRenderer implements BlockEntityRenderer<RiceMillBlockEntity
         stack.scale(2f, 2f, 2f);
         stack.translate(-0.25f, -1.501F, -0.25f);
         //stack.translate(0.47F, 0, -0f);
+        context_1_21_9.renderState().defferedSetup_1_21_9 = () -> { //1.21.9+
         this.model.setUpMillAnim(mill, pTicks);
-        var consumer = buffer.getBuffer(RenderType.entityCutoutNoCull(Resources.RICE_MILL_MODEL));
-        this.model.renderToBuffer(stack, consumer, light, OverlayTexture.NO_OVERLAY, 0xffffffff);
+        }; //1.21.9+
+        var renderType_1_21_9 = (RenderType.entityCutoutNoCull(Resources.RICE_MILL_MODEL));
+        this.submitModel(context_1_21_9, this.model, renderType_1_21_9, stack, light, OverlayTexture.NO_OVERLAY, 0xffffffff);
         stack.popPose();
     }
 
