1a1887e8dd7fed528af23609510612a073ce96d3
DashieDev <lamtinh.asian@gmail.com>
2025-11-18 14:05:32 +0700

    save 5

--- src/main/java/doggytalents/forge_imitate/event/util/ModEventBus.java
+++ src/main/java/doggytalents/forge_imitate/event/util/ModEventBus.java
@@ -1,12 +1,18 @@
 package doggytalents.forge_imitate.event.util;
 
+import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import com.google.common.collect.Maps;
 
 import doggytalents.forge_imitate.event.Event;
 
 public class ModEventBus {
     
-    private final EventBus eventBus = new EventBus(); 
+    private final EventBus eventBus = new EventBus();
+    private final Map<Class<? extends Event>, Supplier<? extends Event>> eventCreators =
+        Maps.newHashMap();
 
     private ModEventBus() {
 
@@ -16,16 +22,21 @@ public class ModEventBus {
         return new ModEventBus();
     }
 
-    public <T extends Event> void addListener(Class<T> eventType, Consumer<T> listener) {
-        this.eventBus.addListener(eventType, listener);
+    public <T extends Event> void addListener( 
+        Consumer<T> listener, ModBusEventInfo<T> eventInfo) {
+        this.eventBus.addListener(eventInfo.eventType(), listener);
+        eventCreators.putIfAbsent(eventInfo.eventType(), eventInfo.eventCreator());
     }
 
-    public void finishRegister() {
+    public void finishRegisterAndPostAll() {
         this.eventBus.finishRegister();
+
+        for (var entry : this.eventCreators.entrySet()) {
+            var e = entry.getValue().get();
+            this.eventBus.post(e);
+        }
     }
 
-    public ModEventBus post(Event e) {
-        this.eventBus.post(e);
-        return this;
-    }
+    public static record ModBusEventInfo<T extends Event>(
+        Class<T> eventType, Supplier<T> eventCreator) {}
 }
