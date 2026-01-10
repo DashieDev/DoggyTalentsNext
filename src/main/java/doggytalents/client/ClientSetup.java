413e944b1f7b5c91f203dadf59cd699a6e0898fa
DashieDev <lamtinh.asian@gmail.com>
2026-01-11 02:39:42 +0700

    DTN dedicated Animation Format: Loader

--- src/main/java/doggytalents/client/ClientSetup.java
+++ src/main/java/doggytalents/client/ClientSetup.java
@@ -17,6 +17,7 @@ import doggytalents.client.entity.model.DogRescueModel;
 import doggytalents.client.entity.model.FisherDogModel;
 import doggytalents.client.entity.model.SyncedRenderFunctionWithHeadModel;
 import doggytalents.client.entity.model.TorchDogModel;
+import doggytalents.client.entity.model.animation.DTNAnimationLoader;
 import doggytalents.client.entity.model.animation.DogAnimationRegistry;
 import doggytalents.client.entity.model.dog.DogModel;
 import doggytalents.client.entity.model.dog.NullDogModel;
@@ -442,5 +443,6 @@ public class ClientSetup {
     public static void addClientReloadListeners(final RegisterClientReloadListenersEvent event) {
         event.registerReloadListener(DogTextureManager.INSTANCE);
         event.registerReloadListener(DogRandomNameRegistry.getInstance());
+        event.registerReloadListener(DTNAnimationLoader.INSTANCE);
     }
 }
