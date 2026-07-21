fb7f5841241aaad289dd7d6fda48f046d3be77fc
DashieDev <lamtinh.asian@gmail.com>
2026-07-22 00:16:57 +0700

    invalidate all parsed model when resource reload

--- src/main/java/doggytalents/client/entity/model/DogModelRegistry.java
+++ src/main/java/doggytalents/client/entity/model/DogModelRegistry.java
@@ -36,6 +36,12 @@ public class DogModelRegistry {
         register(new ResourceLocation("doggytalents", name), getter);
     }
 
+    public static void invalidateAllParsed() {
+        MODEL_MAP.entrySet().removeIf(entry -> {
+            return entry.getValue() instanceof ResolvedDogModelHolder; 
+        });
+    }
+
     public static boolean registerParsed(ResourceLocation id, ParsedModelResult result,
         DTNModelCodec.DogModelProps props) {
 
