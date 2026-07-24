5d72fb6d9220ecb576fc1446590e3b4d0c07db32
DashieDev <lamtinh.asian@gmail.com>
2026-07-24 16:48:09 +0700

    props field in DTN DogModel format is now optional

--- src/main/java/doggytalents/client/entity/model/util/DTNModelCodec.java
+++ src/main/java/doggytalents/client/entity/model/util/DTNModelCodec.java
@@ -93,7 +93,7 @@ public class DTNModelCodec {
     );
 
     public static final Codec<Pair<ParsedModelResult, DogModelProps>> DOG_MODEL_CODEC = 
-        pairCodec(CODEC, DogModelProps.CODEC.fieldOf("props").codec());
+        pairCodec(CODEC, DogModelProps.CODEC.optionalFieldOf("props", DogModelProps.DEFAULT).codec());
 
     private static Codec<ParsedPart> parsedPartCodec() {
         return CodecUtil_1_20_under.recursive("DTNParsedPart", self -> {
