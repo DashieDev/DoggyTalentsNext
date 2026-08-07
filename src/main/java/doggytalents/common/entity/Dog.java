0d0526d6cc1ce8c16101d71bf9271fcda87a8038
DashieDev <lamtinh.asian@gmail.com>
2026-08-08 01:39:56 +0700

    tick animation manager even when not alive

--- src/main/java/doggytalents/common/entity/Dog.java
+++ src/main/java/doggytalents/common/entity/Dog.java
@@ -657,8 +657,10 @@ public class Dog extends AbstractDog {
             }
         }
         
+        this.animationManager.tick();
+
         if (this.isAlive()) {
-            this.animationManager.tick();
+            
             if (!this.level().isClientSide)
                 this.tickAnimAction();
         }
