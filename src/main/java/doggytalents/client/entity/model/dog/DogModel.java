e38c1d1fc932ff68ca7ffc2d0a06fae057f836a4
DashieDev <lamtinh.asian@gmail.com>
2026-08-12 17:23:16 +0700

    Refactor: Rename contract: field name of Captured Procedural State and Keyframe State

--- src/main/java/doggytalents/client/entity/model/dog/DogModel.java
+++ src/main/java/doggytalents/client/entity/model/dog/DogModel.java
@@ -184,7 +184,7 @@ public class DogModel extends EntityModel<Dog> {
 
         final var captured_procedural = 
             anim_manager.getBlendState(pticks).hasProceduralCapture() ? 
-                dog.animationManager.capturedStateForAnim
+                dog.animationManager.capturedProcedural
                 : DogCapturedProceduralState.NONE;
         
         var pose = dog.getDogPose();
@@ -321,11 +321,11 @@ public class DogModel extends EntityModel<Dog> {
         
         //TODO Move this into setupProceduralPose.
         if (blend_state.hasProceduralCapture() && !anim.freeHeadXRot()) {
-            this.head.xRot = dog.animationManager.capturedStateForAnim.headXRot() * Mth.DEG_TO_RAD; 
+            this.head.xRot = dog.animationManager.capturedProcedural.headXRot() * Mth.DEG_TO_RAD; 
         }
 
         if (blend_state == BlendState.ANIM_TO_ANIM) {
-            final var captured_keyframe = dog.animationManager.capturedStateForAnimPose;
+            final var captured_keyframe = dog.animationManager.capturedKeyframeAnim;
 
             if (!captured_keyframe.isNone()) {
                 setupKeyframeAnimationPose(dog, captured_keyframe.anim(), 
@@ -336,7 +336,7 @@ public class DogModel extends EntityModel<Dog> {
         pose_A.store(this);
 
         if (blend_state == BlendState.BLEND_OUT) {
-            final var captured_keyframe = dog.animationManager.capturedStateForAnimPose;
+            final var captured_keyframe = dog.animationManager.capturedKeyframeAnim;
 
             if (!captured_keyframe.isNone()) {
                 setupKeyframeAnimationPose(dog, captured_keyframe.anim(), 
