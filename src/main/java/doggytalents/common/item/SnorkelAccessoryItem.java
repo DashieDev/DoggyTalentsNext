27c4a28c5571e1f524cb3e45a2a0a36b62df9a7b
DashieDev <lamtinh.asian@gmail.com>
2026-08-19 19:11:53 +0700

    disable some trivial ref for now.

--- src/main/java/doggytalents/common/item/SnorkelAccessoryItem.java
+++ src/main/java/doggytalents/common/item/SnorkelAccessoryItem.java
@@ -18,12 +18,4 @@ public class SnorkelAccessoryItem extends AccessoryItem{
     public SnorkelAccessoryItem(Supplier<? extends Accessory> type, Properties properties) {
         super(type, properties);
     }
-    @Override
-    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> components,
-            TooltipFlag flags) {
-        var desc_id = this.getDescriptionId(stack) + ".description";
-        components.add(Component.translatable(desc_id).withStyle(
-            Style.EMPTY.withItalic(true)
-        ));
-    }
 }
