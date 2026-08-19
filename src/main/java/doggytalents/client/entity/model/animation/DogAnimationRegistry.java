2e01cd84c4c984277c5cc15a1b8ce0b8de33bf80
DashieDev <lamtinh.asian@gmail.com>
2026-08-20 02:04:32 +0700

    Add SLOW_TROT and GALLOP to DogAnimation registry for now and query from there
    So that I could freeze it in debug

--- src/main/java/doggytalents/client/entity/model/animation/DogAnimationRegistry.java
+++ src/main/java/doggytalents/client/entity/model/animation/DogAnimationRegistry.java
@@ -12,6 +12,8 @@ public class DogAnimationRegistry {
 
     private static Map<DogAnimation, AnimationDefinition> DEFINITION_MAP
          = Maps.newHashMap();
+    private static AnimationDefinition SLOW_TROT_ANIM = null;
+    private static AnimationDefinition GALLOP_ANIM = null;
     
     public static void register(DogAnimation animation, AnimationDefinition sequence) {
         DEFINITION_MAP.putIfAbsent(animation, sequence);
@@ -19,10 +21,20 @@ public class DogAnimationRegistry {
 
     public static void update(Map<DogAnimation, AnimationDefinition> newMap) {
         DEFINITION_MAP = new HashMap<>(newMap);
+        SLOW_TROT_ANIM = newMap.get(DogAnimation.SLOW_TROT);
+        GALLOP_ANIM = newMap.get(DogAnimation.GALLOP);
     }
 
     public static AnimationDefinition getSequence(DogAnimation animation) {
         return DEFINITION_MAP.get(animation);
+    }
+
+    public static AnimationDefinition getSlowTrot() {
+        return SLOW_TROT_ANIM;
+    } 
+
+    public static AnimationDefinition getGallop() {
+        return GALLOP_ANIM;
     }
 
     public static void init() {
